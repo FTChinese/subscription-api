54fab152713b98175956cfe4082bcee48c34bd3a
weiguo.ni <neefrankie@gmail.com>
2018-10-20 13:12:12 +0800

    add is prod field

--- util/config.go
+++ util/config.go
@@ -2,8 +2,8 @@ package util
 
 // WxConfig contains wxpay configuration.
 type WxConfig struct {
-	AppID     string
-	MchID     string
-	APIKey    string
-	IsSandbox bool
+	AppID  string
+	MchID  string
+	APIKey string
+	IsProd bool
 }
