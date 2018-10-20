3a9b8248b9df36ba136125e8b78ed29d5e2deca3
weiguo.ni <neefrankie@gmail.com>
2018-10-20 18:33:07 +0800

    custom xml decoder

---
+++ util/decode_xml.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+
+	"github.com/objcoding/wxpay"
+)
+
+// Decode parses wxpay's weird response XML data.
+// See https://github.com/objcoding/wxpay/issues/10
+func Decode(r io.Reader) wxpay.Params {
+	var (
+		d      *xml.Decoder
+		start  *xml.StartElement
+		params wxpay.Params
+	)
+	d = xml.NewDecoder(r)
+	params = make(wxpay.Params)
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			break
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			start = &t
+		case xml.CharData:
+			if t = bytes.TrimSpace(t); len(t) > 0 {
+				params.SetString(start.Name.Local, string(t))
+			}
+		}
+	}
+	return params
+}
