1d606f23554544b186248abd9a0bca17ca7795b2
weiguo.ni <neefrankie@gmail.com>
2018-10-28 21:11:25 +0800

    ali app pay verification response

--- controller/helper.go
+++ controller/helper.go
@@ -1,18 +1,14 @@
 package controller
 
 import (
-	"crypto"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"io"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
-	"github.com/smartwalle/alipay/encoding"
 	"github.com/tomasen/realip"
 )
 
@@ -104,61 +100,10 @@ func getClientInfo(req *http.Request) client {
 	return c
 }
 
-type appPayResponse struct {
-	Code        string `json:"code"`
-	Message     string `json:"msg"`
-	AppID       string `json:"app_id"`
-	FtcOrderID  string `json:"out_trade_no"`
-	AliOrderID  string `json:"trade_no"`
-	TotalAmount string `json:"total_amount"`
-	DateTime    string `json:"timestamp"`
-}
-
-type aliAppPayResult struct {
-	Response appPayResponse `json:"alipay_trade_app_pay_response"`
-	Sign     string         `json:"sign"`
-	SignType string         `json:"sign_type"`
-}
-
 // Parse parses input data to struct
 func parseJSON(data io.ReadCloser, v interface{}) error {
 	dec := json.NewDecoder(data)
 	defer data.Close()
 
 	return dec.Decode(v)
-}
-
-// This is used to parse Alibaba's stupid trick of using JSON as plain string.
-// Copied from github.com/smartwalle/alipay/alipay.go
-func extractAppPayResp(rawData string, nodeName string) string {
-	var nodeIndex = strings.LastIndex(rawData, nodeName)
-
-	var dataStartIndex = nodeIndex + len(nodeName) + 2
-	var signIndex = strings.LastIndex(rawData, "\""+keySignNodeName+"\"")
-	var dataEndIndex = signIndex - 1
-
-	var indexLen = dataEndIndex - dataStartIndex
-	if indexLen < 0 {
-		return ""
-	}
-	return rawData[dataStartIndex:dataEndIndex]
-}
-
-func verifyAliResp(data []byte, sign, singType string, key []byte) (ok bool, err error) {
-	signBytes, err := base64.StdEncoding.DecodeString(sign)
-	if err != nil {
-		return false, err
-	}
-
-	if singType == signTypeRSA {
-		err = encoding.VerifyPKCS1v15(data, signBytes, key, crypto.SHA1)
-	} else {
-		err = encoding.VerifyPKCS1v15(data, signBytes, key, crypto.SHA256)
-	}
-
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
 }
