e4e410ee04a300f3a1dfd153f2ea58271f9d8427
weiguo.ni <neefrankie@gmail.com>
2018-11-13 23:30:38 +0800

    test template

--- util/postoffice_test.go
+++ util/postoffice_test.go
@@ -1,10 +1,44 @@
 package util
 
-import "testing"
+import (
+	"html/template"
+	"os"
+	"testing"
+)
 
-var letter = `
+const letter = `
+Dear {{.Name}},
+{{if .Attended}}
+It was a pleasure to see you at the wedding.
+{{- else}}
+It is a shame you couldn't make it to the wedding.
+{{- end}}
+{{with .Gift -}}
+Thank you for the lovely {{.}}
+{{end}}
+Best wishes,
+Josie
 `
 
-func TestTemplate(t *testing.T) {
+type Recipient struct {
+	Name, Gift string
+	Attended   bool
+}
 
+var recipients = []Recipient{
+	{"Aunt Mildred", "bone china tea set", true},
+	{"Uncle John", "moleskin pants", false},
+	{"Cousin Rodney", "", false},
+}
+
+func TestTemplate(t *testing.T) {
+	tpl := template.Must(template.New("letter").Parse(letter))
+
+	for _, r := range recipients {
+		err := tpl.Execute(os.Stdout, r)
+
+		if err != nil {
+			t.Log("executing template:", err)
+		}
+	}
 }
