5eeeb9322a38f5334b09d02a31914cee92c3c21e
weiguo.ni <neefrankie@gmail.com>
2018-11-15 06:53:35 +0800

    add subscripiton order duration

--- model/subscription.go
+++ model/subscription.go
@@ -16,10 +16,10 @@ type Subscription struct {
 	TotalAmount   float64
 	PaymentMethod PaymentMethod
 	Currency      string
-	CreatedAt     string // Only for retrieval
-	ConfirmedAt   string
+	CreatedAt     string // When the order is created.
+	ConfirmedAt   string // When the payment is confirmed.
 	IsRenewal     bool   // If this order is used to renew membership
-	EffectiveDate string // Membership start date for this order
+	StartDate     string // Membership start date for this order
 	EndDate       string // Membership end date for this order
 	UserID        string
 }
@@ -49,37 +49,40 @@ func (s Subscription) DeduceExpireTime(t time.Time) time.Time {
 }
 
 // RenewExpireDate extends member's expiration date depending on subscription
-func (s Subscription) RenewExpireDate(previous string) string {
-	expire, err := util.ParseSQLDate(previous)
-	if err != nil {
-		return previous
-	}
+// func (s Subscription) RenewExpireDate(previous string) string {
+// 	expire, err := util.ParseSQLDate(previous)
+// 	if err != nil {
+// 		return previous
+// 	}
 
-	switch s.BillingCycle {
-	case Yearly:
-		expire = expire.AddDate(1, 0, 1)
+// 	switch s.BillingCycle {
+// 	case Yearly:
+// 		expire = expire.AddDate(1, 0, 1)
 
-	case Monthly:
-		expire = expire.AddDate(0, 1, 1)
-	}
+// 	case Monthly:
+// 		expire = expire.AddDate(0, 1, 1)
+// 	}
 
-	return util.SQLDateUTC.FromTime(expire)
-}
+// 	return util.SQLDateUTC.FromTime(expire)
+// }
 
-// SaveSubscription saves a new subscription order
+// SaveSubscription saves a new subscription order.
+// At this moment, you should already know if this subscription is
+// a renewal of a new one, based on current Membership's expire_date.
 func (env Env) SaveSubscription(s Subscription, c util.RequestClient) error {
 	query := `
 	INSERT INTO premium.ftc_trade
 	SET trade_no = ?,
 		trade_price = ?,
 		trade_amount = ?,
+		user_id = ?,
 		tier_to_buy = ?,
 		billing_cycle = ?,
 		payment_method = ?,
-		user_id = ?,
+		is_renewal = ?,
+		created_utc = UTC_TIMESTAMP(),
 		client_type = ?,
 		client_version = ?,
-		created_utc = UTC_TIMESTAMP(),
 		user_ip_bin = INET6_ATON(?),
 		user_agent = NULLIF(?, '')`
 
@@ -87,10 +90,11 @@ func (env Env) SaveSubscription(s Subscription, c util.RequestClient) error {
 		s.OrderID,
 		s.Price,
 		s.TotalAmount,
+		s.UserID,
 		string(s.TierToBuy),
 		string(s.BillingCycle),
 		string(s.PaymentMethod),
-		s.UserID,
+		s.IsRenewal,
 		c.ClientType,
 		c.Version,
 		c.UserIP,
@@ -105,7 +109,7 @@ func (env Env) SaveSubscription(s Subscription, c util.RequestClient) error {
 	return nil
 }
 
-// FindSubscription tries to find an order
+// FindSubscription tries to find an order to verify the authenticity of a subscription order.
 func (env Env) FindSubscription(orderID string) (Subscription, error) {
 	query := `
 	SELECT trade_no AS orderId,
@@ -115,6 +119,7 @@ func (env Env) FindSubscription(orderID string) (Subscription, error) {
 		IFNULL(tier_to_buy, '') AS tierToBuy,
 		IFNULL(billing_cycle, '') AS billingCycle,
 		IFNULL(payment_method, '') AS paymentMethod,
+		is_renewal AS isRenewal,
 		created_utc AS createdAt,
 		IFNULL(confirmed_utc, '') AS confirmedAt
 	FROM premium.ftc_trade
@@ -130,6 +135,7 @@ func (env Env) FindSubscription(orderID string) (Subscription, error) {
 		&s.TierToBuy,
 		&s.BillingCycle,
 		&s.PaymentMethod,
+		&s.IsRenewal,
 		&s.CreatedAt,
 		&s.ConfirmedAt,
 	)
@@ -142,16 +148,41 @@ func (env Env) FindSubscription(orderID string) (Subscription, error) {
 	return s, nil
 }
 
-// ConfirmSubscription marks an order as completed and create a member.
+// ConfirmSubscription marks an order as completed and create a member or renew membership.
 // Confirm order and create/renew a new member should be an all-or-nothing operation.
 // Or update membership duration.
 // NOTE: The passed in Subscription must be one retrieved from database. Otherwise you should never call this method.
 func (env Env) ConfirmSubscription(s Subscription, confirmTime time.Time) error {
-	// Subscription confirmation time.
-	confirmedAt := util.SQLDatetimeUTC.FromTime(confirmTime)
+	// SQL DATETIME string for confirmation time.
+	confirmedUTC := util.SQLDatetimeUTC.FromTime(confirmTime)
+
+	// By default we assume this order will take effect from its
+	// confirmation time.
+	startTime := confirmTime
+
+	// If this order is used for renewal, copy current memership's
+	// expire_date directly to s.StartDate.
+	if s.IsRenewal {
+		member, err := env.FindMember(s.UserID)
+
+		// If membership is found. Use its Expire date as startTime
+		if err == nil {
+			expireTime, err := util.ParseSQLDate(member.Expire)
+			// If Expire date could be properly parsed.
+			if err != nil {
+				startTime = expireTime
+			}
+		}
+	}
+
+	expireTime := s.DeduceExpireTime(startTime)
+
+	// SQL DATE string for start_date and end_date
+	startDate := util.SQLDateUTC.FromTime(startTime)
+	endDate := util.SQLDateUTC.FromTime(expireTime)
 
 	// Build membership based on subscription.
-	m := env.buildMembership(s, confirmTime)
+	// m := env.buildMembership(s, confirmTime)
 
 	tx, err := env.DB.Begin()
 	if err != nil {
@@ -160,12 +191,16 @@ func (env Env) ConfirmSubscription(s Subscription, confirmTime time.Time) error 
 	}
 	stmtUpdate := `
 	UPDATE premium.ftc_trade
-	SET confirmed_utc = ?
+	SET confirmed_utc = ?,
+		start_date = ?,
+		end_date = ?
 	WHERE trade_no = ?
 	LIMIT 1`
 
 	_, updateErr := tx.Exec(stmtUpdate,
-		confirmedAt,
+		confirmedUTC,
+		startDate,
+		endDate,
 		s.OrderID,
 	)
 
@@ -186,13 +221,13 @@ func (env Env) ConfirmSubscription(s Subscription, confirmTime time.Time) error 
 		expire_date = ?`
 
 	_, createErr := tx.Exec(stmtCreate,
-		m.UserID,
-		string(m.Tier),
-		string(m.Cycle),
-		m.Expire,
-		string(m.Tier),
-		string(m.Cycle),
-		m.Expire,
+		s.UserID,
+		string(s.TierToBuy),
+		string(s.BillingCycle),
+		endDate,
+		string(s.TierToBuy),
+		string(s.BillingCycle),
+		endDate,
 	)
 
 	if createErr != nil {
