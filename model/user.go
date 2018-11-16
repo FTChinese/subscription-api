ede0c9c22b1398902a0adb1537e484f0247148c0
weiguo.ni <neefrankie@gmail.com>
2018-11-16 08:31:48 +0800

    email

--- model/user.go
+++ model/user.go
@@ -1,7 +1,56 @@
 package model
 
+import (
+	"strings"
+
+	validate "github.com/asaskevich/govalidator"
+	"gitlab.com/ftchinese/subscription-api/util"
+)
+
 // User contains the minimal information to identify a user.
 type User struct {
 	Name  string
 	Email string
+}
+
+func (u User) NormalizeName() string {
+	if validate.IsEmail(u.Name) {
+		return strings.Split(u.Name, "@")[0]
+	}
+
+	return u.Name
+}
+
+// FindUser retrieves user's name and email to be used in an email.
+func (env Env) FindUser(id string) (User, error) {
+	query := `
+	SELECT user_name,
+		email
+	FROM cmstmp01.userinfo
+	WHERE user_id = ?`
+
+	var u User
+	err := env.DB.QueryRow(query, id).Scan(
+		&u.Name,
+		&u.Email,
+	)
+
+	if err != nil {
+		logger.WithField("location", "FindUser").Error(err)
+
+		return u, err
+	}
+
+	return u, nil
+}
+
+func ComposeEmail(s Subscription, u User) util.Parcel {
+	return util.Parcel{
+		FromAddress: "no-reply@ftchinese.com",
+		FromName:    "FT中文网会员订阅",
+		ToAddress:   u.Email,
+		ToName:      u.Name,
+		Subject:     "会员订阅",
+		Body:        "",
+	}
 }
