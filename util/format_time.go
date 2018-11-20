bf3582d103aa310ed2a0a01171216ca262342c48
weiguo.ni <neefrankie@gmail.com>
2018-11-20 22:48:53 +0800

    convert iso8601 to sql datetime

--- util/format_time.go
+++ util/format_time.go
@@ -6,13 +6,14 @@ import "time"
 // with timezone set in UTC.
 // Example 2006-01-02T15:04:05Z
 const (
-	secondsOfMinute = 60
-	secondsOfHour   = 60 * secondsOfMinute
-	iso8601Date     = "2006-01-02"
-	iso9075         = "2006-01-02 15:04:05" // Layout for SQL DATETIME
-	iso9075Date     = "2006-01-02"          // Layout for SQL DATE
-	layoutWxTime    = "20060102150405"
-	stmtUTC8Now     = "DATE_ADD(UTC_TIMESTAMP(), INTERVAL 8 HOUR)"
+	secondsOfMinute   = 60
+	secondsOfHour     = 60 * secondsOfMinute
+	layoutISO8601Date = "2006-01-02"
+	layoutISO9075     = "2006-01-02 15:04:05" // Layout for SQL DATETIME
+	layoutISO9075Date = "2006-01-02"          // Layout for SQL DATE
+	layoutWxTime      = "20060102150405"
+	layoutShanghai    = "2006年01月02日 15:04:05 亚洲/上海时间"
+	stmtUTC8Now       = "DATE_ADD(UTC_TIMESTAMP(), INTERVAL 8 HOUR)"
 )
 
 var (
@@ -21,17 +22,19 @@ var (
 	// ISO8601UTC turns time into ISO 8601 string in UTC.
 	ISO8601UTC = timeForamtter{time.RFC3339, time.UTC}
 	// SQLDatetimeUTC turns time into SQL's DATETIME string in UTC.
-	SQLDatetimeUTC = timeForamtter{iso9075, time.UTC}
+	SQLDatetimeUTC = timeForamtter{layoutISO9075, time.UTC}
 	// SQLDateUTC turns time into SQL's DATE string in UTC.
-	SQLDateUTC = timeForamtter{iso9075Date, time.UTC}
+	SQLDateUTC = timeForamtter{layoutISO9075Date, time.UTC}
 	// SQLDateUTC8 turns time into SQL's DATE string set in UTC+8.
-	SQLDateUTC8 = timeForamtter{iso9075Date, TZShanghai}
+	SQLDateUTC8 = timeForamtter{layoutISO9075Date, TZShanghai}
+	// FormatShanghai turns time into Chinese text set in Asia/Shanghai
+	FormatShanghai = timeForamtter{layoutShanghai, TZShanghai}
 )
 
 // timeFormatter converts a time.Time instance to the specified layout in specified location
 type timeForamtter struct {
 	layout string         // output layout
-	loc    *time.Location // destination timezone
+	loc    *time.Location // target timezone
 }
 
 func (f timeForamtter) ToLocation(loc *time.Location) timeForamtter {
@@ -44,6 +47,19 @@ func (f timeForamtter) FromUnix(sec int64) string {
 	return time.Unix(sec, 0).In(f.loc).Format(f.layout)
 }
 
+// FromISO8601 parses a ISO8601 time string and returns the
+// specified format, or returns the original string if parsing
+// failed.
+func (f timeForamtter) FromISO8601(value string) string {
+	t, err := time.Parse(time.RFC3339, value)
+
+	if err != nil {
+		return value
+	}
+
+	return t.In(f.loc).Format(f.layout)
+}
+
 // FromDatetime formats SQL DATETIME.
 // Parameter `loc` is input string's location since SQL DATETIME do not have time zone information.
 // If loc is nil, defaults to UTC.
@@ -52,7 +68,7 @@ func (f timeForamtter) FromDatetime(value string, loc *time.Location) string {
 		loc = time.UTC
 	}
 
-	t, err := time.ParseInLocation(iso9075, value, loc)
+	t, err := time.ParseInLocation(layoutISO9075, value, loc)
 
 	if err != nil {
 		return value
@@ -77,12 +93,12 @@ func (f timeForamtter) FromWx(value string) string {
 
 // ParseSQLDate parse string layout `2006-01-02`
 func ParseSQLDate(value string) (time.Time, error) {
-	return time.Parse(iso9075Date, value)
+	return time.Parse(layoutISO9075Date, value)
 }
 
 // ParseSQLDatetime parse SQL DATETIME string in UTC.
 func ParseSQLDatetime(value string) time.Time {
-	t, err := time.ParseInLocation(iso9075, value, time.UTC)
+	t, err := time.ParseInLocation(layoutISO9075, value, time.UTC)
 
 	if err != nil {
 		return time.Now()
@@ -100,5 +116,5 @@ func ParseWxTime(value string) (time.Time, error) {
 // ParseAliTime parses alipay time string.
 // Not clear what timezone it uses. Assming Shanghai time.
 func ParseAliTime(value string) (time.Time, error) {
-	return time.ParseInLocation(iso9075, value, TZShanghai)
+	return time.ParseInLocation(layoutISO9075, value, TZShanghai)
 }
