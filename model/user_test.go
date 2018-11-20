5f964ffb1759b4a6d297990c974ceff679e2799b
weiguo.ni <neefrankie@gmail.com>
2018-11-20 22:49:58 +0800

    subscripiton notification letter

---
+++ model/user_test.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"os"
+	"testing"
+	"text/template"
+)
+
+func TestFindUser(t *testing.T) {
+	u, err := devEnv.FindUser(mockUser.ID)
+
+	if err != nil {
+		t.Error(nil)
+	}
+
+	t.Log(u)
+}
+
+func TestTemplate(t *testing.T) {
+	tmpl, err := template.New("test").Parse(letter)
+
+	if err != nil {
+		t.Error(err)
+
+		return
+	}
+
+	data := struct {
+		User User
+		Subs Subscription
+	}{
+		mockUser,
+		mockSubs,
+	}
+
+	err = tmpl.Execute(os.Stdout, data)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestComposeEmail(t *testing.T) {
+	p, err := ComposeEmail(mockUser, mockSubs)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(p)
+}
