ff119e353444fb5aa48af297ce33b75cab2c32b8
weiguo.ni <neefrankie@gmail.com>
2018-11-30 18:13:01 +0800

    paywall

---
+++ controller/paywall.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"net/http"
+
+	"gitlab.com/ftchinese/subscription-api/model"
+	"gitlab.com/ftchinese/subscription-api/util"
+)
+
+// PaywallRouter handles pricing plans.
+type PaywallRouter struct {
+	model model.Env
+}
+
+// NewPaywallRouter creates a new instance of pricing router.
+func NewPaywallRouter(m model.Env) PaywallRouter {
+	return PaywallRouter{model: m}
+}
+
+// GetPromo gets the current effective promotion schedule.
+func (pr PaywallRouter) GetPromo(w http.ResponseWriter, req *http.Request) {
+	sch, found := pr.model.PromoFromCache()
+
+	if !found {
+		util.Render(w, util.NewNotFound())
+
+		return
+	}
+
+	util.Render(w, util.NewResponse().NoCache().SetBody(sch))
+}
+
+// DefaultPlans shows what our subscription plans are.
+func DefaultPlans(w http.ResponseWriter, req *http.Request) {
+
+	util.Render(
+		w,
+		util.NewResponse().
+			NoCache().
+			SetBody(model.DefaultPlans))
+}
