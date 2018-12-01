81271290547de8ef2143d5f40ecd8fc0a34f7070
weiguo.ni <neefrankie@gmail.com>
2018-12-01 12:48:55 +0800

    rm deprecated test

--- model/plan_test.go
+++ model/plan_test.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"testing"
-
-	cache "github.com/patrickmn/go-cache"
 )
 
 func TestPlan(t *testing.T) {
@@ -11,39 +9,4 @@ func TestPlan(t *testing.T) {
 
 	t.Log(plan.GetPriceString())
 	t.Log(plan.GetPriceCent())
-}
-
-func TestDiscountPlan(t *testing.T) {
-	plan, _ := devEnv.FindPlan(TierStandard, Yearly)
-
-	t.Log(plan)
-}
-
-func TestRetrieveSchedule(t *testing.T) {
-	sch, err := devEnv.RetrieveSchedule()
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log(sch)
-}
-
-func TestCache(t *testing.T) {
-	c := cache.New(cache.DefaultExpiration, 0)
-
-	c.Set("discountSchedule", DiscountSchedule, cache.NoExpiration)
-
-	sch, found := c.Get("discountSchedule")
-
-	if !found {
-		t.Log("Not Found")
-		return
-	}
-
-	schedule, ok := sch.(Schedule)
-
-	if ok {
-		t.Log(schedule)
-	}
 }
