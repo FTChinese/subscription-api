b7d1a65c78d8e938dbb32a0da07dce6e774bc6a0
weiguo.ni <neefrankie@gmail.com>
2018-12-04 15:09:20 +0800

    add ErrIncompatible type

--- util/errors.go
+++ util/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	// ErrRenewalForbidden indicates a user is not allowed to renew membership
 	ErrRenewalForbidden = errors.New("membership renewal forbidden")
+	// ErrIncompatible is returned by Scan interface
+	ErrIncompatible = errors.New("incompatible type to scan")
 )
