09e85b32f20d5f3d6646b08994636cd9a80de4dd
weiguo.ni <neefrankie@gmail.com>
2018-12-04 15:10:03 +0800

    add json interface to ISODateTime

--- util/datetime.go
+++ util/datetime.go
@@ -2,42 +2,77 @@ package util
 
 import (
 	"database/sql/driver"
-	"errors"
+	"encoding/json"
+	"time"
 )
 
-// Datetime is used to convert between ISO8601 and SQL DATETIME format.
-type Datetime string
+// ISODateTime is used to convert between ISO8601 format and SQL DATETIME format.
+// The conversion is assumed to happen in UTC, e.g. the zero time zone.
+// If your datetime is saved in another time zone,
+// convert it manually.
+type ISODateTime string
 
-// Scan converts SQL Datetime to ISO8601 format
-func (d *Datetime) Scan(src interface{}) error {
-	var source string
-	switch src.(type) {
-	case string:
-		source = src.(string)
-	default:
-		return errors.New("imcompatible type for datetime")
+// UnmarshalJSON implements the Unmarshaler interface to parse JSON into ISODateTime type.
+func (d *ISODateTime) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
 	}
 
-	if source == "" {
-		*d = ""
-		return nil
-	}
+	*d = ISODateTime(s)
 
-	dt := ISO8601UTC.FromDatetime(source, nil)
-
-	*d = Datetime(dt)
 	return nil
 }
 
+// MarshalJSON implements Marshaler interface to stringify a value.
+func (d ISODateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(string(d))
+}
+
+// Scan converts SQL Datetime to ISO8601 format.
+// If input is a SQL null, target will have a zero value, which is an empty string.
+func (d *ISODateTime) Scan(src interface{}) error {
+	// Handling SQL null value.
+	if src == nil {
+		*d = ISODateTime("")
+		return nil
+	}
+
+	switch s := src.(type) {
+	case []byte:
+		dt, err := ToISO8601UTC.FromDatetime(string(s), nil)
+
+		if err != nil {
+			return err
+		}
+
+		*d = ISODateTime(dt)
+		return nil
+
+	default:
+		return ErrIncompatible
+	}
+}
+
 // Value converts ISO8601 to SQL Datetime
-func (d Datetime) Value() (driver.Value, error) {
+func (d ISODateTime) Value() (driver.Value, error) {
 	s := string(d)
 
 	if s == "" {
-		return driver.Value(""), nil
+		return nil, nil
 	}
 
-	dt := SQLDatetimeUTC.FromISO8601(s)
+	dt, err := ToSQLDatetimeUTC.FromISO8601(s)
 
-	return driver.Value(dt), nil
+	// If input string cannot be parsed as ISO8601 format.
+	if err != nil {
+		return nil, err
+	}
+
+	return dt, nil
+}
+
+// ToTime parses an instance.
+func (d ISODateTime) ToTime() (time.Time, error) {
+	return ParseISO8601(string(d))
 }
