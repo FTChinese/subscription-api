dcfeab0d04aab42a33524d278dd5c8f49ede0c7d
weiguo.ni <neefrankie@gmail.com>
2018-12-11 14:13:40 +0800

    use enum for ClientType

--- util/request_client.go
+++ util/request_client.go
@@ -3,13 +3,15 @@ package util
 import (
 	"net/http"
 
+	"gitlab.com/ftchinese/subscription-api/enum"
+
 	"github.com/tomasen/realip"
 )
 
 // RequestClient contains essential data of a user who are placing an order
 // It is exported since both controller and model need it.
 type RequestClient struct {
-	ClientType string
+	ClientType enum.ClientPlatform
 	Version    string
 	UserIP     string
 	UserAgent  string
@@ -19,16 +21,13 @@ type RequestClient struct {
 func NewRequestClient(req *http.Request) RequestClient {
 	c := RequestClient{}
 
-	c.ClientType = req.Header.Get("X-Client-Type")
-	if c.ClientType == "" {
-		c.ClientType = "unknown"
-	}
+	c.ClientType = enum.NewPlatform(req.Header.Get("X-Client-Type"))
 
 	c.Version = req.Header.Get("X-Client-Version")
 
 	// Web app must forward user ip and user agent
 	// For other client like Android and iOS, request comes from user's device, not our web app.
-	if c.ClientType == "web" {
+	if c.ClientType == enum.PlatformWeb {
 		c.UserIP = req.Header.Get("X-User-Ip")
 		c.UserAgent = req.Header.Get("X-User-Agent")
 	} else {
