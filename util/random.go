1bc5461858e29be6722ede76d5432cfe196ef3d2
weiguo.ni <neefrankie@gmail.com>
2018-12-14 18:09:15 +0800

    randomBytes

--- util/random.go
+++ util/random.go
@@ -5,14 +5,25 @@ import (
 	"fmt"
 )
 
-// RandomHex generates a random hexadecimal number of 2*len chars
-func RandomHex(len int) (string, error) {
+func randomBytes(len int) ([]byte, error) {
 	b := make([]byte, len)
 
 	_, err := rand.Read(b)
 
 	if err != nil {
-		return "", nil
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// RandomHex generates a random hexadecimal number of 2*len chars
+func RandomHex(len int) (string, error) {
+
+	b, err := randomBytes(len)
+
+	if err != nil {
+		return "", err
 	}
 
 	return fmt.Sprintf("%x", b), nil
