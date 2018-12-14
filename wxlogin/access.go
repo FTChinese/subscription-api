143091b9b7bf8624dbf1c5dabcdd4e36345baa39
weiguo.ni <neefrankie@gmail.com>
2018-12-14 18:09:33 +0800

    example data

--- wxlogin/access.go
+++ wxlogin/access.go
@@ -7,12 +7,19 @@ import (
 
 // OAuthAccess contains data returned by exchange access token with oauth code.
 type OAuthAccess struct {
-	AccessToken  string      `json:"access_token"`
-	ExpiresIn    int64       `json:"expires_in"`
-	RefreshToken string      `json:"refresh_token"`
-	OpenID       string      `json:"openid"`
-	Scope        string      `json:"scope"`
-	UnionID      null.String `json:"unionid"`
+	RespStatus
+	// Example: 16_Ix0E3WfWs9u5Rh9f-lB7_LgsQJ4zm1eodolFJpSzoQibTAuhIlp682vDmkZSaYIjD9gekOa1zQl-6c6S_CrN_cN9vx9mybwXNVgFbwPMMwM
+	AccessToken string `json:"access_token"`
+	// Example: 7200
+	ExpiresIn int64 `json:"expires_in"`
+	// Exmaple: 16_IlmA9eLGjJw7gBKBT48wff1V1hAYAdpmIqUAypspepm6DsQ6kkcLeZmP932s9PcKp1WM5P_1YwUNQqF-29B_0CqGTqMpWkaaiNSYp26MmB4
+	RefreshToken string `json:"refresh_token"`
+	// Example: ob7fA0h69OO0sTLyQQpYc55iF_P0
+	OpenID string `json:"openid"`
+	// Example: snsapi_userinfo
+	Scope string `json:"scope"`
+	// Example: String:ogfvwjk6bFqv2yQpOrac0J3PqA0o Valid:true
+	UnionID null.String `json:"unionid"`
 }
 
 // SaveAccess saves the access token related data after acquired from wechat api.
