c219f6f46f35587131d37793bd916e198b2094de
weiguo.ni <neefrankie@gmail.com>
2018-12-18 18:09:20 +0800

    refactor

--- wxlogin/account_test.go
+++ wxlogin/account_test.go
@@ -2,8 +2,20 @@ package wxlogin
 
 import "testing"
 
-func TestLoadAccount(t *testing.T) {
-	acnt, err := devEnv.LoadAccountByWx(mockUserInfo.UnionID)
+func TestLoadAccountByWx(t *testing.T) {
+	acnt, err := devEnv.FindAccountByWx(mockUserInfo.UnionID)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("Account: %+v\n", acnt)
+	t.Logf("Wx account: %+v\n", acnt.Wechat)
+}
+
+func TestLoadAccountByFTC(t *testing.T) {
+	acnt, err := devEnv.FindAccountByFTC(mockUserID)
 
 	if err != nil {
 		t.Error(err)
@@ -41,3 +53,19 @@ func TestCheckWxAccount(t *testing.T) {
 
 	t.Logf("Wx account status: %+v\n", s)
 }
+
+// func TestSimpleQuery(t *testing.T) {
+// 	fChan := make(chan FTCAccount)
+// 	wChan := make(chan Wechat)
+// 	mChan := make(chan Membership)
+
+// 	go devEnv.FindAccountByFTC(mockUserID, fChan)
+// 	go devEnv.FindWxAccount(mockUserInfo.UnionID, wChan)
+// 	go devEnv.FindMemberByFTC(mockUserID, mChan)
+
+// 	acnt, wx, member := <-fChan, <-wChan, <-mChan
+
+// 	t.Logf("%+v\n", acnt)
+// 	t.Logf("%+v\n", wx)
+// 	t.Logf("%+v\n", member)
+// }
