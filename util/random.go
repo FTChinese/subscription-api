ecfdb044acef5df23eac49bf4884803c16a26765
weiguo.ni <neefrankie@gmail.com>
2018-12-20 18:20:07 +0800

    base64 random string

--- util/random.go
+++ util/random.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 )
 
@@ -27,4 +28,26 @@ func RandomHex(len int) (string, error) {
 	}
 
 	return fmt.Sprintf("%x", b), nil
+}
+
+// RandomBase64 returns a base64 url encoded, unpadded string of random bytes.
+// len is the length of bytes.
+// It should be the multiple of 3 to get a string without padding:
+// len * 8 / 6.
+// 3 bytes -- 4 chars
+// 6 bytes -- 8 chars
+// 9 bytes -- 12 chars
+// Wechat OAuth code has 32 characters, which is 24 bytes long;
+// Wechat Access Token has 110 chars, which is 82.5 bytes?
+// Wechat Refresh Token has 110 chars.
+// OpenID has 28 chars, which is 21 bytes
+// UnionID has 28 chars.
+func RandomBase64(len int) (string, error) {
+	b, err := randomBytes(len)
+
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(b), nil
 }
