137c5f7d18ca58338a2de3cf25836049f5f7d693
weiguo.ni <neefrankie@gmail.com>
2019-01-01 23:25:36 +0800

    use simple string instead of null string

--- util/client_app.go
+++ util/client_app.go
@@ -3,8 +3,6 @@ package util
 import (
 	"net/http"
 
-	"github.com/guregu/null"
-	log "github.com/sirupsen/logrus"
 	"gitlab.com/ftchinese/subscription-api/enum"
 
 	"github.com/tomasen/realip"
@@ -13,9 +11,9 @@ import (
 // ClientApp records the header information of a request.
 type ClientApp struct {
 	ClientType enum.ClientPlatform
-	Version    null.String
-	UserIP     null.String
-	UserAgent  null.String
+	Version    string
+	UserIP     string
+	UserAgent  string
 }
 
 // NewClientApp collects information from a request.
@@ -24,31 +22,17 @@ func NewClientApp(req *http.Request) ClientApp {
 
 	c.ClientType = enum.NewPlatform(req.Header.Get("X-Client-Type"))
 
-	v := req.Header.Get("X-Client-Version")
-	if v != "" {
-		c.Version = null.StringFrom(v)
-	}
+	c.Version = req.Header.Get("X-Client-Version")
 
 	// Web app must forward user ip and user agent
 	// For other client like Android and iOS, request comes from user's device, not our web app.
-	var ip, ua string
 	if c.ClientType == enum.PlatformWeb {
-		ip = req.Header.Get("X-User-Ip")
-		ua = req.Header.Get("X-User-Agent")
+		c.UserIP = req.Header.Get("X-User-Ip")
+		c.UserAgent = req.Header.Get("X-User-Agent")
 	} else {
-		ip = realip.FromRequest(req)
-		ua = req.UserAgent()
+		c.UserIP = realip.FromRequest(req)
+		c.UserAgent = req.UserAgent()
 	}
-
-	if ip != "" {
-		c.UserIP = null.StringFrom(ip)
-	}
-
-	if ua != "" {
-		c.UserAgent = null.StringFrom(ua)
-	}
-
-	log.WithField("location", "NewClientInfo").Infof("Request IP: %s", ip)
 
 	return c
 }
