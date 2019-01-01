315cd7790a6ab156c4fb45818d93db86812980fb
weiguo.ni <neefrankie@gmail.com>
2019-01-01 13:36:15 +0800

    add a custom Time type with both JSON and SQL support

---
+++ util/time.go
@@ -0,0 +1,104 @@
+package util
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// Time is used to save and output ISO8601 date time.
+type Time struct {
+	time.Time
+}
+
+// MarshalJSON converts a Time struct to ISO8601 string.
+func (t Time) MarshalJSON() ([]byte, error) {
+	if t.IsZero() {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(ToISO8601UTC.FromTime(t.Time))
+}
+
+// UnmarshalJSON converts ISO8601 data time into a Time struct.
+// Empty string and null will be turned into time.Time zero value.
+func (t *Time) UnmarshalJSON(data []byte) (err error) {
+
+	fmt.Printf("Unmarshal data: %s\n", data)
+
+	var v interface{}
+
+	if err = json.Unmarshal(data, &v); err != nil {
+		return
+	}
+
+	switch x := v.(type) {
+	case string:
+		if strings.TrimSpace(x) == "" {
+			t.Time = time.Time{}
+			return
+		}
+
+		t.Time, err = time.Parse(time.RFC3339, x)
+
+		return
+	case nil:
+		t.Time = time.Time{}
+		return
+	}
+
+	return fmt.Errorf("json: canot unmarshal %v into Go value of type Time", reflect.TypeOf(v).Name())
+}
+
+// Scan implements the Scanner interface.
+// SQL NULL will be turned into time zero value.
+func (t *Time) Scan(value interface{}) (err error) {
+	if value == nil {
+		t.Time = time.Time{}
+		return nil
+	}
+
+	switch v := value.(type) {
+	case time.Time:
+		t.Time = v
+		return
+	case []byte:
+		t.Time, err = ParseDateTime(string(v), time.UTC)
+		return
+	case string:
+		t.Time, err = ParseDateTime(v, time.UTC)
+		return
+	}
+
+	return fmt.Errorf("Can't convert %T to time.Time", value)
+}
+
+// Value implements the driver Valuer interface.
+// Zero value is turned into SQL NULL.
+func (t Time) Value() (driver.Value, error) {
+	if t.IsZero() {
+		return nil, nil
+	}
+
+	return ToSQLDatetimeUTC.FromTime(t.Time), nil
+}
+
+// TimeNow creates current time.
+func TimeNow() Time {
+	return Time{
+		time.Now(),
+	}
+}
+
+// TimeZero creates the zero value of Time.
+func TimeZero() Time {
+	return Time{time.Time{}}
+}
+
+// TimeFrom creates a new Time wrapping time.Time.
+func TimeFrom(t time.Time) Time {
+	return Time{t}
+}
