520caf4d9f2e33c5ccc226d88e1b794fbc50c9bb
weiguo.ni <neefrankie@gmail.com>
2019-01-04 14:57:33 +0800

    add ComposeParcel method

--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/guregu/null"
+	"gitlab.com/ftchinese/subscription-api/postoffice"
 	"gitlab.com/ftchinese/subscription-api/util"
 )
 
@@ -19,6 +20,41 @@ type User struct {
 // NormalizeName returns user name, or the name part of email if name does not exist.
 func (u User) NormalizeName() string {
 	return strings.Split(u.Name, "@")[0]
+}
+
+// ComposeParcel create a parcel for email after subscription is confirmed.
+func (u User) ComposeParcel(s Subscription) (postoffice.Parcel, error) {
+	tmpl, err := template.New("order").Parse(letter)
+
+	if err != nil {
+		logger.WithField("trace", "ComposeParcel").Error(err)
+		return postoffice.Parcel{}, err
+	}
+
+	data := struct {
+		User User
+		Subs Subscription
+	}{
+		u,
+		s,
+	}
+
+	var body strings.Builder
+	err = tmpl.Execute(&body, data)
+
+	if err != nil {
+		logger.WithField("trace", "ComposeParcel").Error(err)
+		return postoffice.Parcel{}, err
+	}
+
+	return postoffice.Parcel{
+		FromAddress: "no-reply@ftchinese.com",
+		FromName:    "FT中文网会员订阅",
+		ToAddress:   u.Email,
+		ToName:      u.NormalizeName(),
+		Subject:     "会员订阅",
+		Body:        body.String(),
+	}, nil
 }
 
 // FindUser retrieves user's name and email to be used in an email.
