e6eeab87c84978602c4259d221fa909fd912586f
weiguo.ni <neefrankie@gmail.com>
2019-01-04 18:55:28 +0800

    test membership

--- wxlogin/membership_test.go
+++ wxlogin/membership_test.go
@@ -1,1 +1,30 @@
 package wxlogin
+
+import (
+	"testing"
+)
+
+func TestMemberEqual(t *testing.T) {
+	mFTC := newFTCMember()
+	mWx := newWxMember()
+
+	t.Logf("Are memberships equal: %t\n", mFTC.IsEqualTo(mWx))
+}
+
+func TestMemberIsCoupled(t *testing.T) {
+	m := newBoundMember()
+
+	t.Logf("Is membership coupled: %t\n", m.IsCoupled())
+}
+
+func TestMemberIsFTC(t *testing.T) {
+	m := newFTCMember()
+
+	t.Logf("Is membership created with FTC account: %t\n", m.IsFromFTC())
+}
+
+func TestMemberIsWx(t *testing.T) {
+	m := newWxMember()
+
+	t.Logf("Is membership created with Wechat account: %t\n", m.IsFromWx())
+}
