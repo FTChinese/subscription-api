e8173f57aa05bc3fb02dadd5f75ee46c03b6c57c
weiguo.ni <neefrankie@gmail.com>
2019-01-05 18:27:12 +0800

    rm my personal wechat data

--- wxlogin/env_test.go
+++ wxlogin/env_test.go
@@ -30,12 +30,13 @@ var devEnv = newDevEnv()
 
 const (
 	myUUID         = "e1a1f5c0-0e23-11e8-aa75-977ba2bcc6ae"
-	mockEmail      = "weiguo.ni@ftchinese.com"
 	myUnionID      = "ogfvwjk6bFqv2yQpOrac0J3PqA0o"
 	myOpenID       = "ob7fA0h69OO0sTLyQQpYc55iF_P0"
 	myWxCode       = "001hPJNE0xvMjl25EaLE0k4PNE0hPJNl"
 	myAccessToken  = "16_Ix0E3WfWs9u5Rh9f-lB7_LgsQJ4zm1eodolFJpSzoQibTAuhIlp682vDmkZSaYIjD9gekOa1zQl-6c6S_CrN_cN9vx9mybwXNVgFbwPMMwM"
 	myRefreshToken = "16_IlmA9eLGjJw7gBKBT48wff1V1hAYAdpmIqUAypspepm6DsQ6kkcLeZmP932s9PcKp1WM5P_1YwUNQqF-29B_0CqGTqMpWkaaiNSYp26MmB4"
+	myNickname     = "Victor"
+	myAvatar       = "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLB34sBwSiaL3GJmejqDUqJw4CZ8Qs0ztibsRu6wzMpg7jg5icxWKwxF73ssZUmXmee1MvSvaZ6iaqs1A/132"
 )
 
 var mockClient = util.ClientApp{
@@ -167,27 +168,6 @@ func saveOneLogin(a OAuthAccess, u UserInfo) error {
 	}
 
 	return nil
-}
-
-var nwgAccess = OAuthAccess{
-	AccessToken:  myAccessToken,
-	ExpiresIn:    7200,
-	RefreshToken: myRefreshToken,
-	OpenID:       myOpenID,
-	Scope:        "snsapi_userinfo",
-	UnionID:      null.StringFrom(myUnionID),
-}
-
-var nwgUserInfo = UserInfo{
-	UnionID:    myUnionID,
-	OpenID:     myOpenID,
-	NickName:   "Victor",
-	AvatarURL:  "http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLB34sBwSiaL3GJmejqDUqJw4CZ8Qs0ztibsRu6wzMpg7jg5icxWKwxF73ssZUmXmee1MvSvaZ6iaqs1A/132",
-	Sex:        0,
-	Country:    "",
-	Province:   "",
-	City:       "",
-	Privileges: []string{},
 }
 
 func TestDecodeToken(t *testing.T) {
