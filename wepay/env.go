39280ec43a7066fabc926fa320aa2cbe22d7fe59
weiguo.ni <neefrankie@gmail.com>
2019-01-11 17:19:19 +0800

    add logger

--- wepay/env.go
+++ wepay/env.go
@@ -1,8 +1,14 @@
 package wepay
 
-import "database/sql"
+import (
+	"database/sql"
+
+	log "github.com/sirupsen/logrus"
+)
 
 // Env wraps DB.
 type Env struct {
 	DB *sql.DB
 }
+
+var logger = log.WithField("package", "subscription-api.wepay")
