bc55a2fde8a98755a76dbd6d764192da154211ce
weiguo.ni <neefrankie@gmail.com>
2019-01-11 17:19:46 +0800

    change CreatedAt to string

--- wepay/unified_order.go
+++ wepay/unified_order.go
@@ -5,6 +5,19 @@ import (
 	"github.com/objcoding/wxpay"
 )
 
+// PrepayOrder is the response send to client
+type PrepayOrder struct {
+	FtcOrderID string  `json:"ftcOrderId"`
+	Price      float64 `json:"price"`
+	AppID      string  `json:"appid"`
+	PartnerID  string  `json:"partnerid"`
+	PrepayID   string  `json:"prepayid"`
+	Package    string  `json:"package"`
+	Nonce      string  `json:"noncestr"`
+	Timestamp  string  `json:"timestamp"`
+	Signature  string  `json:"sign"`
+}
+
 // PrePay contains the response data from Wechat unified order.
 type PrePay struct {
 	StatusCode    string
