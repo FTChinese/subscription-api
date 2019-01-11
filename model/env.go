d787a5649987642ede211c89ef86eaf1dfd5c444
weiguo.ni <neefrankie@gmail.com>
2019-01-11 17:16:09 +0800

    a complete rewrite using SQL row locking

--- model/env.go
+++ model/env.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"gitlab.com/ftchinese/subscription-api/enum"
@@ -17,6 +18,47 @@ var logger = log.WithField("package", "subscription-api.model")
 const (
 	keySchedule = "discountSchedule"
 	keyPromo    = "promotionSchedule"
+
+	stmtSubs = `
+	SELECT user_id AS userId,
+		trade_no AS orderId,
+		login_method AS loginMethod,
+		tier_to_buy AS tierToBuy,
+		billing_cycle AS billingCycle,
+		trade_price AS price,
+		trade_amount AS totalAmount,
+		payment_method AS paymentMethod,
+		created_utc AS createdAt,
+		confirmed_utc AS confirmedAt
+	FROM premium.ftc_trade
+	WHERE trade_no = ?
+	LIMIT 1`
+
+	stmtUpdateSubs = `
+	UPDATE premium.ftc_trade
+	SET is_renewal = ?,
+		confirmed_utc = ?,
+		start_date = ?,
+		end_date = ?
+	WHERE trade_no = ?
+	LIMIT 1`
+
+	stmtCreateMember = `
+	INSERT INTO premium.ftc_vip
+	SET vip_id = ?,
+		vip_id_alias = ?,
+		member_tier = ?,
+		billing_cycle = ?,
+		expire_date = ?
+	ON DUPLICATE KEY UPDATE
+		member_tier = ?,
+		billing_cycle = ?,
+		expire_date = ?`
+)
+
+var (
+	stmtSubsLock = fmt.Sprintf(`%s
+	FOR UPDATE`, stmtSubs)
 )
 
 // Env wraps database connection
