e1a5182ba8bd849de5d089c8631adbf6713b77af
weiguo.ni <neefrankie@gmail.com>
2019-01-11 17:19:24 +0800

    test

---
+++ wepay/env_test.go
@@ -0,0 +1,15 @@
+package wepay
+
+import "database/sql"
+
+func newDevEnv() Env {
+	db, err := sql.Open("mysql", "sampadm:secret@unix(/tmp/mysql.sock)/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Env{DB: db}
+}
+
+var devEnv = newDevEnv()
