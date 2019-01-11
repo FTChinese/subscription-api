c06ac5201324c253ca83efe7d88435738a805541
weiguo.ni <neefrankie@gmail.com>
2019-01-11 18:46:43 +0800

    test

--- wepay/decode_xml_test.go
+++ wepay/decode_xml_test.go
@@ -3,41 +3,42 @@ package wepay
 import (
 	"strings"
 	"testing"
-
-	"github.com/objcoding/wxpay"
-	"gitlab.com/ftchinese/subscription-api/util"
 )
 
-const data = `<xml>
-	<appid><![CDATA[wx2421b1c4370ec43b]]></appid>
-	<attach><![CDATA[支付测试]]></attach>
-	<bank_type><![CDATA[CFT]]></bank_type>
-	<fee_type><![CDATA[CNY]]></fee_type>
-	<is_subscribe><![CDATA[Y]]></is_subscribe>
-	<mch_id><![CDATA[10000100]]></mch_id>
-	<nonce_str><![CDATA[5d2b6c2a8db53831f7eda20af46e531c]]></nonce_str>
-	<openid><![CDATA[oUpF8uMEb4qRXf22hE3X68TekukE]]></openid>
-	<out_trade_no><![CDATA[1409811653]]></out_trade_no>
-	<result_code><![CDATA[SUCCESS]]></result_code>
-	<return_code><![CDATA[SUCCESS]]></return_code>
-	<sign><![CDATA[B552ED6B279343CB493C5DD0D78AB241]]></sign>
-	<sub_mch_id><![CDATA[10000100]]></sub_mch_id>
-	<time_end><![CDATA[20140903131540]]></time_end>
-	<total_fee>1</total_fee>
-	<coupon_fee><![CDATA[10]]></coupon_fee>
-	<trade_type><![CDATA[JSAPI]]></trade_type>
-	<transaction_id><![CDATA[1004400740201409030005092168]]></transaction_id>
-</xml>`
+func TestDecodeXML(t *testing.T) {
+	resp := createNotificationResponse()
 
-// wxpay.XmlToMap cannot handle whitespaces.
-func TestParseWxXML(t *testing.T) {
-	params := wxpay.XmlToMap(data)
+	p := DecodeXML(strings.NewReader(resp))
 
-	t.Logf("+%v\n", params)
+	t.Logf("Wechat notification: %+v\n", p)
 }
 
-func TestParseXML(t *testing.T) {
-	params := util.DecodeXML(strings.NewReader(data))
+func TestParseResponse(t *testing.T) {
+	resp := createNotificationResponse()
 
-	t.Logf("%+v\n", params)
+	p, err := ParseResponse(mockClient, strings.NewReader(resp))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("Wechat notification: %+v\n", p)
+}
+
+func TestValidateResponse(t *testing.T) {
+	resp := createNotificationResponse()
+
+	p, err := ParseResponse(mockClient, strings.NewReader(resp))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	reason := ValidateResponse(p)
+
+	if reason != nil {
+		t.Logf("Valiation failed: %+v\n", reason)
+	}
 }
