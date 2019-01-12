1ed678e760b17f185d2713e6db00503bfecd2206
weiguo.ni <neefrankie@gmail.com>
2019-01-12 09:49:04 +0800

    add DB field

--- paywall/env.go
+++ paywall/env.go
@@ -1,6 +1,8 @@
 package paywall
 
 import (
+	"database/sql"
+
 	cache "github.com/patrickmn/go-cache"
 	log "github.com/sirupsen/logrus"
 )
@@ -9,5 +11,6 @@ var logger = log.WithField("package", "paywall")
 
 // Env wraps database connection
 type Env struct {
+	DB    *sql.DB
 	Cache *cache.Cache
 }
