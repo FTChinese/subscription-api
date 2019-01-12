edc7db2f108cbe200c817d79a548c72a75767631
weiguo.ni <neefrankie@gmail.com>
2019-01-12 18:32:45 +0800

    design the mocking object

--- wechat/client_test.go
+++ wechat/client_test.go
@@ -20,25 +20,60 @@ var mockClient = NewClient(appID, mchID, apiKey)
 
 var mockPlan = paywall.GetDefaultPricing()["standard_year"]
 
-func generateWxID() string {
-	id, _ := util.RandomBase64(21)
-	return id
+// Mock a user for a specific subscription session.
+type mocker struct {
+	orderID string
+	openID  string
+	ip      string
 }
 
-func (c Client) generatNoti() string {
-	p := make(wxpay.Params)
+func newMocker() mocker {
+	id, _ := util.RandomBase64(21)
 
+	return mocker{
+		orderID: mockPlan.OrderID(),
+		openID:  id,
+		ip:      fake.IPv4(),
+	}
+}
+
+func (m mocker) unifiedOrder() wxpay.Params {
+	return GenerateUnifiedOrder(mockPlan, m.ip, m.orderID)
+}
+
+func fillResp(p wxpay.Params) wxpay.Params {
 	nonce, _ := util.RandomHex(16)
 
 	p.SetString("return_code", "SUCCESS")
 	p.SetString("return_msg", "OK")
-	p.SetString("appid", c.appID)
-	p.SetString("mch_id", c.mchID)
+	p.SetString("appid", appID)
+	p.SetString("mch_id", mchID)
 	p.SetString("nonce_str", nonce)
 	p.SetString("result_code", "SUCCESS")
-	p.SetString("openid", generateWxID())
-	p.SetString("is_subscribe", "N")
 	p.SetString("trade_type", "APP")
+
+	return p
+}
+
+func (m mocker) prepayResp() string {
+	p := make(wxpay.Params)
+
+	p = fillResp(p)
+	p.SetString("prepay_id", fake.CharactersN(26))
+
+	s := mockClient.Sign(p)
+
+	p.SetString("sign", s)
+
+	return wxpay.MapToXml(p)
+}
+
+func (m mocker) notiResp() string {
+	p := make(wxpay.Params)
+
+	p = fillResp(p)
+	p.SetString("openid", m.openID)
+	p.SetString("is_subscribe", "N")
 	p.SetString("bank_type", "CMC")
 	p.SetString("total_fee", "25800")
 	p.SetString("cash_fee", "25800")
@@ -46,17 +81,28 @@ func (c Client) generatNoti() string {
 	p.SetString("out_trade_no", mockPlan.OrderID())
 	p.SetString("time_end", time.Now().Format("20060102150405"))
 
-	s := c.Client.Sign(p)
+	s := mockClient.Sign(p)
 
 	p.SetString("sign", s)
 
 	return wxpay.MapToXml(p)
 }
 
-func TestSignature(t *testing.T) {
-	order := GenerateUnifiedOrder(mockPlan, fake.IPv4(), mockPlan.OrderID())
+func TestUnifiedOrder(t *testing.T) {
+	m := newMocker()
 
-	h := mockClient.Sign(order)
+	uo := m.unifiedOrder()
+
+	t.Logf("Created unified order: %+v\n", uo)
+}
+func TestSignature(t *testing.T) {
+	m := newMocker()
+
+	uo := m.unifiedOrder()
+
+	t.Logf("Created unified order: %+v\n", uo)
+
+	h := mockClient.Sign(uo)
 
 	t.Logf("Hash: %s\n", h)
 }
@@ -67,6 +113,8 @@ func TestIsValidSign(t *testing.T) {
 	h := mockClient.Sign(order)
 
 	order.SetString(wxpay.Sign, h)
+
+	t.Logf("Unified order: %+v\n", order)
 
 	ok := mockClient.ValidSign(order)
 
