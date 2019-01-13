afab02a1c6749fddf97fa9c17a4e972c7e99bfb1
weiguo.ni <neefrankie@gmail.com>
2019-01-13 18:20:41 +0800

    ValidateResponse

--- wechat/client.go
+++ wechat/client.go
@@ -36,45 +36,62 @@ func NewClient(appID, mchID, apiKey string) Client {
 	}
 }
 
-// ParseResponse parses and validate wechat response.
-func (c Client) ParseResponse(r io.Reader) (wxpay.Params, error) {
-	var returnCode string
-	params := DecodeXML(r)
+// ValidateResponse checks if wechat response is generated for our app.
+// NOTE: this sdk treat return_code == FAIL as valid.
+// Possible return_msg:
+// appid不存在;
+// 商户号mch_id与appid不匹配;
+// invalid spbill_create_ip;
+// spbill_create_ip参数长度有误; (Wx does not accept IPv6 like 9b5b:2ef9:6c9f:cf5:130e:984d:8958:75f9 :-<)
+func (c Client) ValidateResponse(params wxpay.Params) *view.Reason {
+	if params.GetString("return_code") == wxpay.Fail {
+		statusMsg := params.GetString("return_msg")
 
-	if params.ContainsKey("return_code") {
-		returnCode = params.GetString("return_code")
-	} else {
-		return nil, errors.New("no return_code in XML")
-	}
-
-	switch returnCode {
-	case wxpay.Fail:
-		return nil, errors.New("wx notification failed")
-
-	case wxpay.Success:
-		if c.ValidSign(params) {
-			return params, nil
+		reason := &view.Reason{
+			Field: "status",
+			Code:  "fail",
 		}
-		return nil, errors.New("invalid sign value in XML")
+		reason.SetMessage(statusMsg)
 
-	default:
-		return nil, errors.New("return_code value is invalid in XML")
+		return reason
 	}
-}
 
-// VerifyIdentity checks if wechat response is generated for our app.
-func (c Client) VerifyIdentity(params wxpay.Params) bool {
+	if params.GetString("result_code") == wxpay.Fail {
+		errCode := params.GetString("err_code")
+		errDesc := params.GetString("err_code_des")
+
+		reason := &view.Reason{
+			Field: "result",
+			Code:  errCode,
+		}
+		reason.SetMessage(errDesc)
+
+		return reason
+	}
+
 	if !params.ContainsKey("appid") || (params.GetString("appid") != c.appID) {
-		logger.WithField("trace", "VerifyIdentity").Error("Missing or wrong appid")
-		return false
+
+		reason := &view.Reason{
+			Field: "app_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing or wrong app id")
+
+		return reason
 	}
 
 	if !params.ContainsKey("mch_id") || (params.GetString("mch_id") != c.mchID) {
-		logger.WithField("trace", "VerifyIdentity").Error("Missing or wrong mch_id")
-		return false
+
+		reason := &view.Reason{
+			Field: "mch_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing or wrong merchant id")
+
+		return reason
 	}
 
-	return true
+	return nil
 }
 
 // BuildPrepayOrder for client.
@@ -106,63 +123,28 @@ func (c Client) BuildPrepayOrder(orderID string, price float64, prepayID string)
 	}
 }
 
-// ValidateResponse verifies if wechat response is valid.
-//
-// Example response:
-// return_code:SUCCESS|FAIL
-// return_msg:OK
-//
-// Present only if return_code == SUCCESS
-// appid:wx......
-// mch_id:........
-// nonce_str:8p8ZlUBkLsFPxC6g
-// sign:DB68F0D9F193D499DF9A2EDBFFEAF312
-// result_code:SUCCESS|FAIL
-// err_code
-// err_code_des
-//
-// Present only if returnd_code == SUCCESS and result_code == SUCCCESS
-// trade_type:APP
-// prepay_id:wx20125006086590be8d9519f40090763508
-// NOTE: this sdk treat return_code == FAIL as valid.
-// Possible return_msg:
-// appid不存在;
-// 商户号mch_id与appid不匹配;
-// invalid spbill_create_ip;
-// spbill_create_ip参数长度有误; (Wx does not accept IPv6 like 9b5b:2ef9:6c9f:cf5:130e:984d:8958:75f9 :-<)
-func ValidateResponse(resp wxpay.Params) *view.Reason {
-	if resp.GetString("return_code") == wxpay.Fail {
-		returnMsg := resp.GetString("return_msg")
-		logger.
-			WithField("trace", "ValidateResponse").
-			Errorf("return_code is FAIL. return_msg: %s", returnMsg)
+// ParseResponse parses and validate wechat response.
+func (c Client) ParseResponse(r io.Reader) (wxpay.Params, error) {
+	var returnCode string
+	params := DecodeXML(r)
 
-		reason := &view.Reason{
-			Field: "return_code",
-			Code:  "fail",
-		}
-		reason.SetMessage(returnMsg)
-
-		return reason
+	if params.ContainsKey("return_code") {
+		returnCode = params.GetString("return_code")
+	} else {
+		return nil, errors.New("no return_code in XML")
 	}
 
-	if resp.GetString("result_code") == wxpay.Fail {
-		errCode := resp.GetString("err_code")
-		errCodeDes := resp.GetString("err_code_des")
+	switch returnCode {
+	case wxpay.Fail:
+		return params, nil
 
-		logger.WithField("trace", "ValidateResponse").
-			WithField("err_code", errCode).
-			WithField("err_code_des", errCodeDes).
-			Error("Wx unified order result failed")
-
-		reason := &view.Reason{
-			Field: "result_code",
-			Code:  errCode,
+	case wxpay.Success:
+		if c.ValidSign(params) {
+			return params, nil
 		}
-		reason.SetMessage(errCodeDes)
+		return nil, errors.New("invalid sign value in XML")
 
-		return reason
+	default:
+		return nil, errors.New("return_code value is invalid in XML")
 	}
-
-	return nil
 }
