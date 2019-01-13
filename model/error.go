fa4051b6762b005145073e953a0c2a37794288cb
weiguo.ni <neefrankie@gmail.com>
2019-01-13 18:18:42 +0800

    fix typo

--- model/error.go
+++ model/error.go
@@ -4,7 +4,7 @@ import "errors"
 
 // Errors for controller to identify actions to take.
 var (
-	ErrOrderNotFound    = errors.New("Subscripiton order is not found")
+	ErrOrderNotFound    = errors.New("Subscription order is not found")
 	ErrAlreadyConfirmed = errors.New("Subscription order is already confirmed")
 	ErrPriceMismatch    = errors.New("Price does not match")
 )
