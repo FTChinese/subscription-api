6d25f573e29c7ba8dae8cfa48e7284966377027a
weiguo.ni <neefrankie@gmail.com>
2019-01-14 12:25:06 +0800

    split db operation from wxlogin pkg to model pkg

--- wxlogin/userinfo.go
+++ wxlogin/userinfo.go
@@ -1,8 +1,6 @@
 package wxlogin
 
 import (
-	"strings"
-
 	"github.com/guregu/null"
 )
 
@@ -22,7 +20,8 @@ type UserInfo struct {
 	RespStatus
 }
 
-func (u UserInfo) gender() null.String {
+// GetGender creates a nullable string from Wechat's `sex` field so  that it could saved as enum in database.
+func (u UserInfo) GetGender() null.String {
 	var g null.String
 	switch u.Sex {
 	case 1:
@@ -32,90 +31,4 @@ func (u UserInfo) gender() null.String {
 	}
 
 	return g
-}
-
-// SaveUserInfo from wechat API.
-// Since a user can authorize multiple times, use ON DUPLICATE to handle such situations.
-func (env Env) SaveUserInfo(u UserInfo) error {
-	query := `
-	INSERT INTO user_db.wechat_userinfo
-	SET union_id = ?,
-		nickname = ?,
-		avatar_url = ?,
-		gender = ?,
-		country = ?,
-		province = ?,
-		city = ?,
-		privilege = NULLIF(?, '')
-	ON DUPLICATE KEY UPDATE
-		nickname = ?,
-		avatar_url = ?,
-		gender = ?,
-		country = ?,
-		province = ?,
-		city = ?,
-		privilege = NULLIF(?, '')`
-
-	prvlg := strings.Join(u.Privileges, ",")
-
-	_, err := env.DB.Exec(query,
-		u.UnionID,
-		u.NickName,
-		u.AvatarURL,
-		u.gender(),
-		u.Country,
-		u.Province,
-		u.City,
-		prvlg,
-		u.NickName,
-		u.AvatarURL,
-		u.gender(),
-		u.Country,
-		u.Province,
-		u.City,
-		prvlg,
-	)
-
-	if err != nil {
-		logger.WithField("trace", "SaveUserInfo").Error(err)
-		return err
-	}
-
-	return nil
-}
-
-// UpdateUserInfo update data of one union id.
-// This will update multiple rows if user logged in with wechat
-// on different platforms.
-func (env Env) UpdateUserInfo(u UserInfo) error {
-	query := `
-	UPDATE user_db.wechat_userinfo
-	SET nickname = ?,
-		gender = ?,
-		country = ?,
-		province = ?,
-		city = ?,
-		avatar_url = ?,
-		privilege = NULLIF(?, '')
-	WHERE union_id = ?`
-
-	prvl := strings.Join(u.Privileges, ",")
-
-	_, err := env.DB.Exec(query,
-		u.NickName,
-		u.gender(),
-		u.Country,
-		u.Province,
-		u.City,
-		u.AvatarURL,
-		prvl,
-		u.UnionID,
-	)
-
-	if err != nil {
-		logger.WithField("trace", "UpdateUserInfo").Error(err)
-		return err
-	}
-
-	return nil
 }
