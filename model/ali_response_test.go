f0b534d5f7a5c2febffd3493e774f763a646d95b
weiguo.ni <neefrankie@gmail.com>
2019-01-22 15:33:43 +0800

    test

--- model/ali_response_test.go
+++ model/ali_response_test.go
@@ -1,12 +1,45 @@
 package model
 
-import "testing"
+import (
+	"database/sql"
+	"testing"
 
-func TestSaveAliNoti(t *testing.T) {
-	n := aliNoti()
+	cache "github.com/patrickmn/go-cache"
+	"github.com/smartwalle/alipay"
+)
 
-	err := devEnv.SaveAliNotification(n)
-	if err != nil {
-		t.Error(err)
+func TestEnv_SaveAliNotification(t *testing.T) {
+	type fields struct {
+		sandbox bool
+		db      *sql.DB
+		cache   *cache.Cache
+	}
+	type args struct {
+		n alipay.TradeNotification
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Save Ali Notification",
+			fields:  fields{db: db},
+			args:    args{n: aliNoti()},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				sandbox: tt.fields.sandbox,
+				db:      tt.fields.db,
+				cache:   tt.fields.cache,
+			}
+			if err := env.SaveAliNotification(tt.args.n); (err != nil) != tt.wantErr {
+				t.Errorf("Env.SaveAliNotification() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
