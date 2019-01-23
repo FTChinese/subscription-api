9f3401e221f49c840647fd971ac57480087d6f00
weiguo.ni <neefrankie@gmail.com>
2019-01-23 18:55:36 +0800

    test

--- util/db_test.go
+++ util/db_test.go
@@ -1,1 +1,31 @@
 package util
+
+import (
+	"github.com/spf13/viper"
+	"testing"
+)
+
+func TestViporConfig(t *testing.T) {
+	viper.SetConfigName("api")
+	viper.AddConfigPath("$HOME/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var conn Conn
+	err = viper.UnmarshalKey("mysql.dev", &conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("%+v", conn)
+
+	err = viper.UnmarshalKey("mysql.master", &conn)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("%+v", conn)
+}
