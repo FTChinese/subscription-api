a22f84dab74aa25769e8aa41a0636db9ca55e139
weiguo.ni <neefrankie@gmail.com>
2019-01-23 18:55:16 +0800

    ali App

--- ali/app.go
+++ ali/app.go
@@ -1,1 +1,18 @@
 package ali
+
+import "errors"
+
+// App contains the data of an Alipay app.
+type App struct {
+	ID         string `mapstructure:"app_id"`
+	PublicKey  string `mapstructure:"public_key"`
+	PrivateKey string `mapstructure:"private_key"`
+}
+
+func (a App) Ensure() error {
+	if a.ID == "" || a.PublicKey == "" || a.PrivateKey == "" {
+		return errors.New("ali.App has empty fields")
+	}
+
+	return nil
+}
