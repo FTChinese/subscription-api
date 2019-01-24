60cb7380dca2d1708a68cf07b800772172a039bb
weiguo.ni <neefrankie@gmail.com>
2019-01-24 15:20:52 +0800

    fix mysql driver parameter string

--- util/db.go
+++ util/db.go
@@ -24,8 +24,13 @@ func NewDB(c Conn) (*sql.DB, error) {
 		Net:    "tcp",
 		Addr:   fmt.Sprintf("%s:%d", c.Host, c.Port),
 		// Always use UTC time.
+		// Pay attention to how string values are specified.
+		// The string value provided to MySQL must be quoted in single quote for this driver to work,
+		// which means the single quote itself must be included in the string value.
+		// The resulting string value passed to MySQL should look like: `%27<you string value>%27`
+		// See ASCII Encoding Reference https://www.w3schools.com/tags/ref_urlencode.asp
 		Params: map[string]string{
-			"time_zone": "+00:00",
+			"time_zone": `'+00:00'`,
 		},
 		AllowNativePasswords: true,
 	}
