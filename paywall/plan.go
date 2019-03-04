8450376ea58cc1c785e1d45da5f15a5e3f515c3f
weiguo.ni <neefrankie@gmail.com>
2019-03-04 09:40:06 +0800

    rm unused methods

--- paywall/plan.go
+++ paywall/plan.go
@@ -1,8 +1,6 @@
 package paywall
 
 import (
-	"strconv"
-
 	"github.com/FTChinese/go-rest/enum"
 )
 
@@ -16,25 +14,3 @@ type Plan struct {
 	NetPrice    float64    `json:"netPrice"`
 	Description string     `json:"description"`
 }
-
-// WxPrice converts price to Wechat pay format.
-func (p Plan) WxPrice() int64 {
-	return int64(p.ListPrice * 100)
-}
-
-// AliPrice converts price to Alipay format.
-func (p Plan) AliPrice() string {
-	return strconv.FormatFloat(p.ListPrice, 'f', 2, 32)
-}
-
-// OrderID generates an FT order id based
-// on the plan's id, a random number between 100 to 999,
-// and unix timestamp.
-// func (p Plan) OrderID() string {
-// 	rand.Seed(time.Now().UnixNano())
-
-// 	// Generate a random number between [100, 999)
-// 	rn := 100 + rand.Intn(999-100+1)
-
-// 	return fmt.Sprintf("FT%03d%d%d", p.ID, rn, time.Now().Unix())
-// }
