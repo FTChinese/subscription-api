10eb7548a6fb4aca6f808ef8de5485d0137ef2d1
neefrankie <neefrankie@gmail.com>
2019-04-02 16:31:24 +0800

    refactor: rename WxResp to Resp

--- wechat/resp.go
+++ wechat/resp.go
@@ -1,12 +1,14 @@
 package wechat
 
 import (
+	"errors"
+	"github.com/FTChinese/go-rest/view"
 	"github.com/guregu/null"
 	"github.com/objcoding/wxpay"
 )
 
 // WxResp contains the shared fields all wechat pay endpoints contains.
-type WxResp struct {
+type Resp struct {
 	// return_code: SUCCESS/FAIL
 	StatusCode string
 	// return_msg
@@ -27,7 +29,8 @@ type WxResp struct {
 	ErrorMessage null.String
 }
 
-func (r *WxResp) Populate(p wxpay.Params) {
+func (r *Resp) Populate(p wxpay.Params) {
+
 	r.StatusCode = p.GetString("return_code")
 	r.StatusMessage = p.GetString("return_msg")
 
@@ -59,3 +62,93 @@ func (r *WxResp) Populate(p wxpay.Params) {
 		r.ErrorMessage = null.StringFrom(v)
 	}
 }
+
+// IsStatusValid checks if wechat reponse contains `return_code`.
+// It does not check if `return_code` is SUCCESS of FAIL,
+// following `wxpay` package's convention.
+func (r Resp) IsStatusValid() error {
+
+	switch r.StatusCode {
+	case "":
+		return errors.New("no return_code in XML")
+
+	case wxpay.Fail, wxpay.Success:
+		return nil
+
+	default:
+		return errors.New("return_code value is invalid in XML")
+	}
+}
+
+// Validate wechat if wechat response is valid.
+// This validation does not include notification since the
+// `wxpay` package does not provide such capability.
+// The `wxpay` pakcage already performs an initial check
+// on `return_code` and `sign`.
+// But its check is incomplete since `return_code == FAIL`
+// is regarded as ok.
+// You have to check if return_code == SUCCESS, appid, mch_id, result_code are valid.
+func (r Resp) Validate(app PayApp) *view.Reason {
+	if r.StatusCode == wxpay.Fail {
+		reason := &view.Reason{
+			Field: "status",
+			Code:  "fail",
+		}
+		reason.SetMessage(r.StatusMessage)
+
+		return reason
+	}
+
+	if r.ResultCode.String == wxpay.Fail {
+		reason := &view.Reason{
+			Field: "result",
+			Code: r.ErrorCode.String,
+		}
+		reason.SetMessage(r.ErrorMessage.String)
+
+		return reason
+	}
+
+	if r.AppID.IsZero() {
+		reason := &view.Reason{
+			Field: "app_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing app id")
+
+		return reason
+	}
+
+	if r.MID.IsZero() {
+		reason := &view.Reason{
+			Field: "mch_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing merchant id")
+
+		return reason
+	}
+
+	if r.AppID.String != app.AppID {
+		reason := &view.Reason{
+			Field: "app_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing or wrong app id")
+
+		return reason
+	}
+
+	if r.MID.String != app.MchID {
+		reason := &view.Reason{
+			Field: "mch_id",
+			Code:  view.CodeInvalid,
+		}
+		reason.SetMessage("Missing or wrong merchant id")
+
+		return reason
+	}
+
+	return nil
+}
+
