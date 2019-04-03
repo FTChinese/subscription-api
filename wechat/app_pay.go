11e21306a3b6c7e6ececddd3116c3c96c767a9fb
neefrankie <neefrankie@gmail.com>
2019-04-03 16:14:18 +0800

    refactor: create LegacyAppPay for backward compatibility

--- wechat/app_pay.go
+++ wechat/app_pay.go
@@ -5,7 +5,12 @@ import (
 )
 
 // AppPay is used by native app.
-type AppPay struct {
+// Android <= 2.0.4 uses this structure.
+// Actually we do not need to strictly conform to wechat's
+// field naming convention, which is messy and arbitrary.
+// `package` is a keyword in Java and Kotlin.
+// Deprecate
+type LegacyAppPay struct {
 	FtcOrderID string  `json:"ftcOrderId"`
 	Price      float64 `json:"price"`
 	ListPrice  float64 `json:"listPrice"`
@@ -19,7 +24,34 @@ type AppPay struct {
 	Signature  string  `json:"sign"`
 }
 
-func (p AppPay) Param() wxpay.Params {
+func (p LegacyAppPay) Param() wxpay.Params {
+	param := make(wxpay.Params)
+	param["appid"] = p.AppID
+	param["partnerid"] = p.PartnerID
+	param["prepayid"] = p.PrepayID
+	param["package"] = p.Package
+	param["noncestr"] = p.Nonce
+	param["timestamp"] = p.Timestamp
+
+	return param
+}
+
+func (p LegacyAppPay) WithHash(sign string) LegacyAppPay {
+	p.Signature = sign
+	return p
+}
+
+type AppPay struct {
+	Pay
+	PartnerID string `json:"partnerId"`
+	PrepayID  string `json:"prepayId"`
+	Timestamp string `json:"timestamp"`
+	Nonce     string `json:"nonce"`
+	Package   string `json:"pkg"`
+	Signature string `json:"signature"`
+}
+
+func (p AppPay) Params() wxpay.Params {
 	param := make(wxpay.Params)
 	param["appid"] = p.AppID
 	param["partnerid"] = p.PartnerID
