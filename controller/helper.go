1bd143b4a145d3f9c49d90b1cb466ab211929580
weiguo.ni <neefrankie@gmail.com>
2019-04-12 12:18:01 +0800

    refactor: callback url for next-user

--- controller/helper.go
+++ controller/helper.go
@@ -7,44 +7,13 @@ import (
 	"net/http"
 )
 
+const (
+	alipayCallback  = "http://next.ftchinese.com/user/subscription/alipay/callback?"
+	wxOAuthCallback = "http://next.ftchinese.com/user/login/callback?"
+)
+
 var logger = log.WithField("project", "subscription-api").
 	WithField("package", "controller")
-
-// Param represents a pair of query parameter from URL.
-//type Param struct {
-//	key   string
-//	value string
-//}
-
-//// ToBool converts a query parameter to boolean value.
-//func (p Param) ToBool() (bool, error) {
-//	return strconv.ParseBool(p.value)
-//}
-//
-//// ToString converts a query parameter to string value.
-//// Returns error for an empty value.
-//func (p Param) ToString() (string, error) {
-//	if p.value == "" {
-//		return "", fmt.Errorf("%s have empty value", p.key)
-//	}
-//
-//	return p.value, nil
-//}
-//
-//// ToInt converts the value of a query parameter to int64
-//func (p Param) ToInt() (int64, error) {
-//	if p.value == "" {
-//		return 0, fmt.Errorf("%s have empty value", p.key)
-//	}
-//
-//	num, err := strconv.ParseInt(p.value, 10, 0)
-//
-//	if err != nil {
-//		return 0, err
-//	}
-//
-//	return num, nil
-//}
 
 // GetURLParam gets a url parameter.
 func GetURLParam(req *http.Request, key string) gorest.Param {
