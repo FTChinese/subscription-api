f9beff6a4bd7a693b62d198c2afc926376a10c18
weiguo.ni <neefrankie@gmail.com>
2019-05-08 12:25:52 +0800

    feat: get user id or union id

--- controller/helper.go
+++ controller/helper.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/FTChinese/go-rest"
 	"github.com/go-chi/chi"
+	"github.com/guregu/null"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -20,4 +21,16 @@ func GetURLParam(req *http.Request, key string) gorest.Param {
 	v := chi.URLParam(req, key)
 
 	return gorest.NewParam(key, v)
+}
+
+// GetUserOrUnionID is a convenient function to extract
+// user's ftc id or wechat union id from request header.
+func GetUserOrUnionID(h http.Header) (null.String, null.String) {
+	uID := h.Get(userIDKey)
+	wID := h.Get(unionIDKey)
+
+	ftcID := null.NewString(uID, uID != "")
+	unionID := null.NewString(wID, wID != "")
+
+	return ftcID, unionID
 }
