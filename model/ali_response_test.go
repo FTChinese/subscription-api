2586c4ef34d30b64053942cdb2eae78b832ba378
weiguo.ni <neefrankie@gmail.com>
2019-05-19 10:55:49 +0800

    refactor: test

--- model/ali_response_test.go
+++ model/ali_response_test.go
@@ -1,42 +1,34 @@
 package model
 
 import (
-	"database/sql"
+	"gitlab.com/ftchinese/subscription-api/test"
 	"testing"
 
-	cache "github.com/patrickmn/go-cache"
 	"github.com/smartwalle/alipay"
 )
 
 func TestEnv_SaveAliNotification(t *testing.T) {
-	type fields struct {
-		sandbox bool
-		db      *sql.DB
-		cache   *cache.Cache
+	env := Env{
+		db: test.DB,
 	}
+
 	type args struct {
 		n alipay.TradeNotification
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name:    "Save Ali Notification",
-			fields:  fields{db: db},
-			args:    args{n: aliNoti()},
+			args:    args{n: test.AliNoti()},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				sandbox: tt.fields.sandbox,
-				db:      tt.fields.db,
-				cache:   tt.fields.cache,
-			}
+
 			if err := env.SaveAliNotification(tt.args.n); (err != nil) != tt.wantErr {
 				t.Errorf("Env.SaveAliNotification() error = %v, wantErr %v", err, tt.wantErr)
 			}
