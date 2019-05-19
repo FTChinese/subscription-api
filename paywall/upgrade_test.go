607a5ef0ea9db164a17dfeb63e0f27df7bde1806
weiguo.ni <neefrankie@gmail.com>
2019-05-19 10:52:14 +0800

    test

---
+++ paywall/upgrade_test.go
@@ -0,0 +1,16 @@
+package paywall
+
+var orders = []Proration{
+	{
+		OrderID: "FTA9964A889D756CA4",
+		Balance: 141.36,
+	},
+	{
+		OrderID: "FTA3B605C8884841C0",
+		Balance: 258.0,
+	},
+	{
+		OrderID: "FTBFFCD9011CC93C9A",
+		Balance: 258.0,
+	},
+}
