f07dd6ebbab97c94ec65cbe6094f822f283d5085
weiguo.ni <neefrankie@gmail.com>
2019-05-19 10:51:23 +0800

    refactor: include letter for upgrade

--- paywall/letter.go
+++ paywall/letter.go
@@ -4,10 +4,12 @@ package paywall
 const confirmationLetter = `
 FT中文网用户 {{.User.NormalizeName}},
 
-您好！{{if .Subs.IsRenewal -}}
-感谢您续订FT中文网会员服务。
-{{- else -}}
+您好！{{if .Subs.IsNewMember -}}
 感谢您订阅FT中文网会员服务。
+{{- else if .Subs.IsRenewal -}}
+感谢您续订FT中文网会员服务。
+{{- else if .Subs.IsUpgrade -}}
+感谢您升级位高级会员。
 {{- end}}
 
 您本次订单的详细信息如下：
@@ -15,9 +17,17 @@ FT中文网用户 {{.User.NormalizeName}},
 订单号: {{.Subs.OrderID}}
 会员类型: {{.Subs.TierToBuy.ToCN}}/{{.Subs.BillingCycle.ToCN}}
 支付金额: ¥{{.Subs.NetPrice}}
+{{if .Subs.IsValidPay -}}
 支付方式: {{.Subs.PaymentMethod.ToCN}}
+{{- end}}
 订单日期: {{.Subs.CreatedAt.StringCN}}
 本次订单购买的会员期限: {{.Subs.StartDate}} 至 {{.Subs.EndDate}}
+
+{{if .Subs.IsUpgrade -}}
+此前订单未使用部分的余额已折换进本次订单：
+订单号: {{.Subs.ProratedOrders}}
+余额共计: ¥{{.Subs.ProratedAmount}}
+{{- end}}
 
 如有疑问，请联系客服：subscriber.service@ftchinese.com。
 
@@ -29,9 +39,9 @@ FT中文网
 
 Dear FTC user {{.User.NormalizeName}},
 {{if .Subs.IsRenewal -}}
-You have renewed your subcription to FTC membership.
+You have renewed your subscription to FTC membership.
 {{- else}}
-You have subscriped to FTC membership.
+You have subscribed to FTC membership.
 {{- end}} Thanks.
 
 Here is your order details:
@@ -44,6 +54,12 @@ Created At: {{.Subs.CreatedAt.StringEN}}
 Duration: {{.Subs.StartDate}} to {{.Subs.EndDate}}
 
 To get help with subscription and purchases, please contact subscriber.service@ftchinese.com.
+
+{{if .Subs.IsUpgrade -}}
+The unused portion of all your previous orders have been prorated and deducted from this payment：
+Those orders are: {{.Subs.ProratedOrders}}
+Total balance: ¥{{.Subs.ProratedAmount}}
+{{- end}}
 
 Again, we appreciate your support.
 
