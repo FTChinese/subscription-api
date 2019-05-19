4579af9ce5c2ff4401a2564e493b1ad4ebfa1392
weiguo.ni <neefrankie@gmail.com>
2019-05-19 10:56:21 +0800

    feat: add RetrieveMember method

--- model/membership.go
+++ model/membership.go
@@ -5,42 +5,39 @@ import (
 	"gitlab.com/ftchinese/subscription-api/paywall"
 )
 
-// findMember retrieves a user's membership based on subscription information.
-func (env Env) findMember(subs paywall.Subscription) (paywall.Membership, error) {
-	query := env.stmtSelectMember()
+// RetrieveMember retrieves a user's membership info by ftc id
+// or wechat union id.
+func (env Env) RetrieveMember(u paywall.User) (paywall.Membership, error) {
 
 	var m paywall.Membership
-	var vipType int64
 
+	// In the ftc_vip table, vip_id might be ftc uuid or wechat
+	// id, and vip_id_alias is always wechat id.
+	// In future, the table will be refactor with two extra
+	// columns ftc_user_id dedicated to ftc uuid and wx_union_id
+	// dedicated for wechat union id. The vip_id column will be
+	// use only as a unique constraint on these two columns.
 	err := env.db.QueryRow(
-		query,
-		subs.CompoundID,
-		subs.UnionID,
+		env.query.SelectMember(),
+		u.CompoundID,
+		u.UnionID,
 	).Scan(
 		&m.CompoundID,
 		&m.UnionID,
-		&vipType,
 		&m.Tier,
 		&m.Cycle,
-		&m.Timestamp,
-		&m.ExpireDate,
-	)
+		&m.ExpireDate)
 
 	if err != nil {
-		logger.WithField("trace", "findMember").Error(err)
+		logger.WithField("trace", "RetrieveMember").Error(err)
 
 		return m, err
 	}
 
-	if m.Tier == enum.InvalidTier {
-		m.Tier = normalizeMemberTier(vipType)
-	}
-
+	// TODO: this is not accurate.
 	if m.Cycle == enum.InvalidCycle {
 		m.Cycle = enum.CycleYear
 	}
-
-	m.NormalizeDate()
 
 	return m, nil
 }
