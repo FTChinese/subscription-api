2e1f561337c900e877d31889e8db2cfe1d60e91e
weiguo.ni <neefrankie@gmail.com>
2019-05-19 10:50:04 +0800

    refactor: add Charge type; use a single NewSubs to replace NewWxSubs and NewAliSubs

--- paywall/subscription.go
+++ paywall/subscription.go
@@ -12,6 +12,7 @@ import (
 	"github.com/guregu/null"
 )
 
+// GenerateOrderID creates an order id.
 func GenerateOrderID() (string, error) {
 
 	id, err := gorest.RandomHex(8)
@@ -22,154 +23,264 @@ func GenerateOrderID() (string, error) {
 	return "FT" + strings.ToUpper(id), nil
 }
 
+type Charge struct {
+	ListPrice   float64
+	NetPrice    float64
+	IsConfirmed bool
+}
+
+// AliNetPrice converts Charged price to ailpay format
+func (c Charge) AliNetPrice() string {
+	return strconv.FormatFloat(c.NetPrice, 'f', 2, 32)
+}
+
+// WxNetPrice converts Charged price to int64 in cent for comparison with wx notification.
+func (c Charge) WxNetPrice() int64 {
+	return int64(c.NetPrice * 100)
+}
+
 // Subscription contains the details of a user's action to place an order.
 // This is the centrum of the whole subscription process.
+// An order could represents 12 status of user:
+// A user is allowed to to at max 2 ids - ftc or wechat, or both. This is 3 possible choices.;
+// A user could choose between 2 payment methods;
+// An order could create, renew or upgrade a member.
+// And tier + cycle have 3 combination.
+// All those combination add up to 3 * 2 * 3 * 3 = 54
 type Subscription struct {
-	OrderID       string // At first I mean to unexport it, but it is a scan target so must be exported.
-	CompoundID    string // Use FTCUserID if it is valid, then use UnionID if it is valid, then throw error. This column is acting only as non-null constraint.
-	FTCUserID     null.String
-	UnionID       null.String
-	TierToBuy     enum.Tier
-	BillingCycle  enum.Cycle // Calculate expiration date
-	ListPrice     float64
-	NetPrice      float64
+	// Fields common to all.
+	OrderID string
+	User
+	Charge
+	TierToBuy    enum.Tier
+	BillingCycle enum.Cycle
+	CycleCount   int64 // Default to 1. Change it for upgrade
+	ExtraDays    int64 // Default to 1. Change it for upgraded.
+
+	// The category of this order.
+	Kind SubsKind
+
+	// Fields only applicable to upgrade.
+	ProrationSource []string   // for upgrade
+	ProrationAmount null.Float // for upgrade
+
+	// Payment method
 	PaymentMethod enum.PayMethod
-	WxAppID       null.String
-	CreatedAt     chrono.Time // When the order is created.
-	ConfirmedAt   chrono.Time // When the payment is confirmed.
-	IsRenewal     bool        // If this order is used to renew membership. Determined the moment notification is received. Mostly used for data analysis and email.
-	StartDate     chrono.Date // Membership start date for this order. If might be ConfirmedAt or user's existing membership's expire date.
-	EndDate       chrono.Date // Membership end date for this order. Depends on start date.
+	WxAppID       null.String // Wechat specific
+
+	// Fields populated only after payment finished.
+	ConfirmedAt chrono.Time // When the payment is confirmed.
+	StartDate   chrono.Date // Membership start date for this order. If might be ConfirmedAt or user's existing membership's expire date.
+	EndDate     chrono.Date // Membership end date for this order. Depends on start date.
 }
 
-// NewWxpaySubs creates a new Subscription with payment method set to Wechat.
-// Note wechat login and wechat pay we talked here are two totally non-related things.
-func NewWxpaySubs(ftcID null.String, unionID null.String, p Plan) (Subscription, error) {
+// NewSubs creates a new subscription with shared fields
+// populated. PaymentMethod, Kind, ProrationSource,
+// ProrationAmount are left to the controller layer.
+func NewSubs(u User, p Plan) (Subscription, error) {
 	s := Subscription{
-		FTCUserID:     ftcID,
-		UnionID:       unionID,
-		TierToBuy:     p.Tier,
-		BillingCycle:  p.Cycle,
-		ListPrice:     p.ListPrice,
-		NetPrice:      p.NetPrice,
-		PaymentMethod: enum.PayMethodWx,
+		TierToBuy:    p.Tier,
+		BillingCycle: p.Cycle,
+		CycleCount:   1,
+		ExtraDays:    1,
 	}
 
-	compoundID, err := s.pickCompoundID()
+	s.CompoundID = u.CompoundID
+	s.FtcID = u.FtcID
+	s.UnionID = u.UnionID
+	s.ListPrice = p.ListPrice
+	s.NetPrice = p.NetPrice
+
+	id, err := GenerateOrderID()
+
 	if err != nil {
 		return s, err
 	}
 
-	s.CompoundID = compoundID
-
-	if err := s.generateOrderID(); err != nil {
-		return s, err
-	}
+	s.OrderID = id
 
 	return s, nil
 }
 
-// NewAlipaySubs creates a new Subscription with payment method set to Alipay.
-func NewAlipaySubs(ftcID null.String, unionID null.String, p Plan) (Subscription, error) {
-	s := Subscription{
-		FTCUserID:     ftcID,
-		UnionID:       unionID,
-		TierToBuy:     p.Tier,
-		BillingCycle:  p.Cycle,
-		ListPrice:     p.ListPrice,
-		NetPrice:      p.NetPrice,
-		PaymentMethod: enum.PayMethodAli,
-	}
+// WithWxpay sets payment method to wechat
+func (s Subscription) WithWxpay(appID string) Subscription {
+	s.PaymentMethod = enum.PayMethodWx
+	s.WxAppID = null.StringFrom(appID)
 
-	compoundID, err := s.pickCompoundID()
-	if err != nil {
-		return s, err
-	}
-
-	s.CompoundID = compoundID
-
-	err = s.generateOrderID()
-	if err != nil {
-		return s, err
-	}
-
-	return s, nil
+	return s
 }
 
-// Use ftc user id for the value of column user_id if it exists;
-// otherwise fallback to wechat union id.
-func (s Subscription) pickCompoundID() (string, error) {
-	if s.FTCUserID.Valid {
-		return s.FTCUserID.String, nil
-	} else if s.UnionID.Valid {
-		return s.UnionID.String, nil
-	} else {
-		return "", errors.New("ftc user id and union id should not both be null")
-	}
+// WithAlipay sets payment method to alipay
+func (s Subscription) WithAlipay() Subscription {
+	s.PaymentMethod = enum.PayMethodAli
+
+	return s
 }
 
-// GenerateOrderID creates an id for this order. The order id is created only created upon the initial call of this method. Multiple calls won't change the this order's id.
-func (s *Subscription) generateOrderID() error {
+// WithUpgrade returns a new Subscription based on an
+// upgrade plan.
+func (s Subscription) WithUpgrade(up UpgradePlan) Subscription {
 
-	id, err := gorest.RandomHex(8)
-	if err != nil {
-		return err
+	s.NetPrice = up.Payable
+	s.CycleCount = up.CycleCount
+	s.ExtraDays = up.ExtraDays
+	s.Kind = SubsKindUpgrade
+
+	s.ProrationSource = up.OrderIDs
+	s.ProrationAmount = null.NewFloat(up.Balance, up.Balance > 0.01)
+
+	return s
+}
+
+// IsNewMember checks whether this order is used to create a
+// new member
+func (s Subscription) IsNewMember() bool {
+	return s.Kind == SubsKindCreate
+}
+
+// IsRenewal checks whether this order is used to renew
+// membership.
+func (s Subscription) IsRenewal() bool {
+	return s.Kind == SubsKindRenew
+}
+
+// IsUpgrade checks whether this order is used to upgrade
+// member tier.
+func (s Subscription) IsUpgrade() bool {
+	return s.Kind == SubsKindUpgrade
+}
+
+// IsValidPay checks whether user actually used any payment
+// method.
+func (s Subscription) IsValidPay() bool {
+	return s.PaymentMethod != enum.InvalidPay
+}
+
+// ProratedOrders is used to render templates.
+func (s Subscription) ProratedOrders() null.String {
+	if s.ProrationSource == nil {
+		return null.String{}
 	}
 
-	s.OrderID = "FT" + strings.ToUpper(id)
+	return null.StringFrom(strings.Join(s.ProrationSource, ", "))
+}
+
+// SetDuration updates the StartDate and EndDate fields.
+func (s *Subscription) SetDuration(start time.Time) error {
+
+	var endTime time.Time
+
+	switch s.BillingCycle {
+	case enum.CycleYear:
+		endTime = start.AddDate(int(s.CycleCount), 0, int(s.ExtraDays))
+
+	case enum.CycleMonth:
+		endTime = start.AddDate(0, int(s.CycleCount), int(s.ExtraDays))
+
+	default:
+		return errors.New("invalid billing cycle")
+	}
+
+	s.StartDate = chrono.DateFrom(start)
+	s.EndDate = chrono.DateFrom(endTime)
 
 	return nil
 }
 
-// AliNetPrice converts Charged price to ailpay format
-func (s Subscription) AliNetPrice() string {
-	return strconv.FormatFloat(s.NetPrice, 'f', 2, 32)
+func (s Subscription) IsKindConsistent(m Membership) bool {
+	switch s.Kind {
+	case SubsKindCreate:
+		return m.Tier == enum.InvalidTier
+
+	case SubsKindRenew:
+		return m.Tier == s.TierToBuy
+
+	// For upgrade, is m.Tier == s.TierToBuy, it indicates
+	// this is a duplicate order for upgrade.
+	case SubsKindUpgrade:
+		return m.Tier != s.TierToBuy
+
+	default:
+		return false
+	}
 }
 
-// WxNetPrice converts Charged price to int64 in cent for comparison with wx notification.
-func (s Subscription) WxNetPrice() int64 {
-	return int64(s.NetPrice * 100)
-}
+// Validate ensures the order to confirm must match
+// the state of membership prior to creation/upgrading.
+// If subs.Kind == SubsKindCreate, member.Tier == InvalidTier;
+// If subs.Kind == SubsKindRenew, member.Tier == subs.Tier;
+// If subs.Kind == SubsKindUpgrade, member.Tier != subs.Tier && member.Tier != TierInvalid
+func (s Subscription) Validate(m Membership) error {
+	switch s.Kind {
 
-// IsWxChargeMatched tests if the order's charge matches the one from wechat response.
-func (s Subscription) IsWxChargeMatched(cent int64) bool {
-	return s.WxNetPrice() == cent
-}
+	case SubsKindUpgrade:
+		if s.TierToBuy != enum.TierPremium {
+			return ErrTierMismatched
+		}
+		if m.Tier == enum.InvalidTier {
+			return ErrNoUpgradingTarget
+		}
+		// For upgrading, order's tier must be different
+		// from member's tier; otherwise this might be
+		// a duplicate upgrading request.
+		if m.Tier == s.TierToBuy {
+			return ErrDuplicateUpgrading
+		}
 
-// IsConfirmed checks if the order is confirmed.
-func (s Subscription) IsConfirmed() bool {
-	return !s.ConfirmedAt.IsZero()
-}
-
-// ConfirmWithDuration populate a subscription's ConfirmedAt, StartDate, EndDate and IsRenewal based on a user's current membership duration.
-// It picks whichever comes last from Duration.ExpireDate
-// or confirmedAt.
-func (s Subscription) ConfirmWithDuration(dur Duration, confirmedAt time.Time) (Subscription, error) {
-	s.ConfirmedAt = chrono.TimeFrom(confirmedAt)
-
-	dur.NormalizeDate()
-
-	s.IsRenewal = dur.ExpireDate.After(confirmedAt)
-
-	var startTime time.Time
-	// If a membership's ExpireDate is after confirmedAt,
-	// use expireDate as new subscription's startTime;
-	// otherwise use the confirmedAt as startTime.
-	// Since the zero value of ExpireDate is always before
-	// confirmedAt, the `else` branch always wins.
-	if s.IsRenewal {
-		startTime = dur.ExpireDate.Time
-	} else {
-		startTime = confirmedAt
+	default:
+		return nil
 	}
 
-	expireTime, err := s.BillingCycle.TimeAfterACycle(startTime)
+	return nil
+}
+
+// WithMember updates an order with existing membership.
+// Zero membership is a valid value.
+func (s Subscription) ConfirmWithMember(m Membership, confirmedAt time.Time) (Subscription, error) {
+	s.ConfirmedAt = chrono.TimeFrom(confirmedAt)
+
+	var startTime time.Time
+
+	// If membership is expired, always use the confirmation
+	// time as start time.
+	if m.IsExpired() {
+		startTime = confirmedAt
+	} else {
+		// If membership is not expired, this order might be
+		// used to either renew or upgrade.
+		// For renewal, we use current membership's
+		// expiration date;
+		// For upgrade, we use confirmation time.
+		if s.Kind == SubsKindUpgrade {
+			startTime = confirmedAt
+		} else {
+			startTime = m.ExpireDate.Time
+		}
+	}
+
+	err := s.SetDuration(startTime)
+
 	if err != nil {
 		return s, err
 	}
 
-	s.StartDate = chrono.DateFrom(startTime)
-	s.EndDate = chrono.DateFrom(expireTime)
-
+	s.IsConfirmed = true
 	return s, nil
+}
+
+// BuildMembership creates a Membership based on a
+// confirmed order.
+func (s Subscription) BuildMembership() (Membership, error) {
+	if !s.IsConfirmed {
+		return Membership{}, errors.New("only confirmed order could be used to build membership")
+	}
+
+	return Membership{
+		CompoundID: s.CompoundID,
+		FTCUserID:  s.FtcID,
+		UnionID:    s.UnionID,
+		Tier:       s.TierToBuy,
+		Cycle:      s.BillingCycle,
+		ExpireDate: s.EndDate,
+	}, nil
 }
