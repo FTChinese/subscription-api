51c601bcbf35d083344077ae498991eca37f7d53
weiguo.ni <neefrankie@gmail.com>
2019-05-21 10:48:35 +0800

    test

--- model/subscription_test.go
+++ model/subscription_test.go
@@ -1,13 +1,13 @@
 package model
 
 import (
-	"github.com/FTChinese/go-rest/enum"
-	"gitlab.com/ftchinese/subscription-api/test"
-	"gitlab.com/ftchinese/subscription-api/util"
 	"testing"
 	"time"
 
+	"github.com/FTChinese/go-rest/enum"
 	"gitlab.com/ftchinese/subscription-api/paywall"
+	"gitlab.com/ftchinese/subscription-api/query"
+	"gitlab.com/ftchinese/subscription-api/test"
 )
 
 func TestEnv_SaveSubscription(t *testing.T) {
@@ -15,7 +15,8 @@ func TestEnv_SaveSubscription(t *testing.T) {
 	p := test.NewProfile()
 
 	env := Env{
-		db: test.DB,
+		db:    test.DB,
+		query: query.NewBuilder(false),
 	}
 
 	type args struct {
@@ -130,6 +131,15 @@ func TestEnv_SaveSubscription(t *testing.T) {
 				k:      paywall.SubsKindRenew,
 			},
 		},
+
+		{
+			name: "Upgrade order",
+			args: args{
+				idType: test.IDFtc,
+				pm:     enum.PayMethodWx,
+				k:      paywall.SubsKindUpgrade,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -144,49 +154,14 @@ func TestEnv_SaveSubscription(t *testing.T) {
 	}
 }
 
-func TestEnv_SaveUpgradeSubscription(t *testing.T) {
-	env := Env{
-		db: test.DB,
-	}
-
-	subs := test.MyProfile.UpgradeSubs()
-
-	type args struct {
-		s paywall.Subscription
-		c util.ClientApp
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "Save upgrade subscription",
-			args: args{
-				s: subs,
-				c: test.RandomClientApp(),
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := env.SaveSubscription(tt.args.s, tt.args.c)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Env.SaveSubscription() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
-}
-
 func TestEnv_FindSubsCharge(t *testing.T) {
 
 	env := Env{
-		db: test.DB,
+		db:    test.DB,
+		query: query.NewBuilder(false),
 	}
 
-	subs := test.MyProfile.RandomSubs()
+	subs := test.MyProfile.SubsRandom()
 
 	err := env.SaveSubscription(subs, test.RandomClientApp())
 	if err != nil {
@@ -223,8 +198,19 @@ func TestEnv_FindSubsCharge(t *testing.T) {
 
 func TestEnv_ConfirmPayment(t *testing.T) {
 	env := Env{
-		db: test.DB,
+		db:    test.DB,
+		query: query.NewBuilder(false),
 	}
+
+	p := test.NewProfile()
+
+	t.Logf("User: %+v", p.User(test.IDBound))
+
+	subsCreate := p.SubsCreate()
+	subsRenew := p.SubsRenew()
+	subsUpgrade := p.SubsUpgrade()
+
+	subsUpgrade.ProrationSource = []string{subsCreate.OrderID, subsRenew.OrderID}
 
 	type args struct {
 		subs        paywall.Subscription
@@ -238,7 +224,7 @@ func TestEnv_ConfirmPayment(t *testing.T) {
 		{
 			name: "New or renew",
 			args: args{
-				subs:        test.MyProfile.RandomSubs(),
+				subs:        subsCreate,
 				confirmedAt: time.Now(),
 			},
 			wantErr: false,
@@ -246,14 +232,14 @@ func TestEnv_ConfirmPayment(t *testing.T) {
 		{
 			name: "Renew",
 			args: args{
-				subs:        test.MyProfile.RandomSubs(),
+				subs:        subsRenew,
 				confirmedAt: time.Now(),
 			},
 		},
 		{
 			name: "Upgrade",
 			args: args{
-				subs:        test.MyProfile.UpgradeSubs(),
+				subs:        subsUpgrade,
 				confirmedAt: time.Now(),
 			},
 		},
@@ -273,6 +259,122 @@ func TestEnv_ConfirmPayment(t *testing.T) {
 			}
 
 			t.Logf("%+v\n", got)
+		})
+	}
+}
+
+// Use this test to generate orders to test upgrading in postman.
+func TestEnv_FindProration(t *testing.T) {
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	// You need to create at least one confirmed standard order.
+	p := test.NewProfile()
+	subsCreate := p.SubsCreate()
+	subsRenew := p.SubsRenew()
+
+	for _, subs := range []paywall.Subscription{subsCreate, subsRenew} {
+		err := env.SaveSubscription(subs, test.RandomClientApp())
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = env.ConfirmPayment(subs.OrderID, time.Now())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	type args struct {
+		u paywall.User
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Find Proration",
+			args: args{
+				u: p.User(test.IDBound),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.FindProration(tt.args.u)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Env.FindProration() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("Env.FindProration() = %v, want %v", got, tt.want)
+			//}
+			t.Logf("User: %+v", tt.args.u)
+			t.Logf("Proration orders: %+v", got)
+		})
+	}
+}
+
+func TestEnv_BuildUpgradePlan(t *testing.T) {
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	// You need to create at least one confirmed standard order.
+	p := test.NewProfile()
+	subsCreate := p.SubsCreate()
+	subsRenew := p.SubsRenew()
+
+	for _, subs := range []paywall.Subscription{subsCreate, subsRenew} {
+		err := env.SaveSubscription(subs, test.RandomClientApp())
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = env.ConfirmPayment(subs.OrderID, time.Now())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	type args struct {
+		u paywall.User
+		p paywall.Plan
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Build Upgrade Plan",
+			args: args{
+				u: p.User(test.IDBound),
+				p: paywall.GetDefaultPricing()["premium_year"],
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.BuildUpgradePlan(tt.args.u, tt.args.p)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Env.BuildUpgradePlan() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("Env.BuildUpgradePlan() = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("User: %+v", tt.args.u)
+			t.Logf("Upgrade plan: %+v", got)
 		})
 	}
 }
