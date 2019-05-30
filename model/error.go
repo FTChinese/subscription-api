d27e61fd425cfe7be21216d72a51cf5d6657a63f
weiguo.ni <neefrankie@gmail.com>
2019-05-30 14:44:53 +0800

    feat: add ErrAlreadyUpgraded

--- model/error.go
+++ model/error.go
@@ -7,4 +7,5 @@ var (
 	ErrAlreadyConfirmed = errors.New("subscription order is already confirmed")
 	ErrAllowRetry       = errors.New("resend notification")
 	ErrDenyRetry        = errors.New("no longer resend notification")
+	ErrAlreadyUpgraded  = errors.New("cannot upgrade: already a premium member")
 )
