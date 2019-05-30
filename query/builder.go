eaff7560f1776fe9b1f1a9c9172c6e3b4f53d926
weiguo.ni <neefrankie@gmail.com>
2019-05-30 14:40:47 +0800

    refactor: user transaction for order creation

--- query/builder.go
+++ query/builder.go
@@ -1,22 +1,5 @@
 package query
 
-const (
-	selectMember = `
-	SELECT vip_id AS userId,
-		vip_id_alias AS unionId,
-		CASE vip_type
-			WHEN 10 THEN 'standard'
-			WHEN 100 THEN 'premium'
-			ELSE member_tier
-		END AS memberTier,
-		billing_cycle AS billingCycle,
-		IF(expire_time, DATE(FROM_UNIXTIME(expire_time)), expire_date) AS expireDate
-	FROM %s.ftc_vip
-	WHERE vip_id = ? 
-		OR vip_id_alias = ?
-	LIMIT 1`
-)
-
 type Builder struct {
 	sandbox bool
 }
