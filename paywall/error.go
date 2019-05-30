28694fcb1d3de6120254cd45bcb0224ee338947b
weiguo.ni <neefrankie@gmail.com>
2019-05-30 14:45:20 +0800

    feat: add ErrDowngrade and ErrBeyondRenewal

--- paywall/error.go
+++ paywall/error.go
@@ -6,4 +6,6 @@ var (
 	ErrTierMismatched     = errors.New("the tier of upgrading order is not used for upgrading")
 	ErrDuplicateUpgrading = errors.New("duplicate upgrading")
 	ErrNoUpgradingTarget  = errors.New("membership to upgrade not found")
+	ErrDowngrade          = errors.New("membership downgrading is not allowed")
+	ErrBeyondRenewal      = errors.New("exceed maximum allowed membership duration")
 )
