b4208a3086e1be7b89dfbbeaf4f747d406686008
weiguo.ni <neefrankie@gmail.com>
2019-05-31 10:12:13 +0800

    refactor: verify notification

---
+++ ali/verify.go
@@ -0,0 +1,28 @@
+package ali
+
+import "github.com/smartwalle/alipay"
+
+const (
+	tradeFinished = "TRADE_FINISHED"
+	tradeSuccess  = "TRADE_SUCCESS"
+	tradePending  = "WAIT_BUYER_PAY"
+	tradeClosed   = "TRADE_CLOSED"
+)
+
+func IsPaySuccess(n *alipay.TradeNotification) bool {
+	switch n.TradeStatus {
+	case tradeSuccess, tradeFinished:
+		return true
+	default:
+		return false
+	}
+}
+
+func ShouldRetry(n *alipay.TradeNotification) bool {
+	switch n.TradeStatus {
+	case tradePending:
+		return true
+	default:
+		return false
+	}
+}
