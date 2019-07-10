e31ae45958383d884a3451552cf78d037bed8b96
neefrankie <neefrankie@gmail.com>
2019-07-10 23:46:20 +0800

    feat: data types for upgrading

--- billing/plan.go
+++ billing/plan.go
@@ -9,15 +9,15 @@ import (
 // The list price is the price that buyers pay for your product or service without any discounts.
 // The net price of a product or service is the actual price that customers pay for the product or service.
 type Plan struct {
-	ID          string      `json:"id"`
-	Tier        enum.Tier   `json:"tier"` // This is product.
-	Cycle       enum.Cycle  `json:"cycle"`
-	CycleCount  int64       `json:"intervalCount"`
-	TrialDays   int64       `json:"trialDays"`
-	Amount      float64     `json:"amount"`
-	Currency    string      `json:"currency"`
-	Description string      `json:"description"`
-	Active      bool        `json:"active"`
-	ProductID   string      `json:"productId"`
-	CreatedAt   chrono.Time `json:"createdAt"`
+	ID         string      `json:"id"`
+	Active     bool        `json:"active"`
+	Amount     float64     `json:"amount"`
+	Caption    string      `json:"caption"`
+	Cycle      enum.Cycle  `json:"cycle"`
+	CycleCount int64       `json:"intervalCount"`
+	Currency   string      `json:"currency"`
+	ProductID  string      `json:"productId"`
+	Tier       enum.Tier   `json:"tier"` // This is product.
+	TrialDays  int64       `json:"trialDays"`
+	CreatedAt  chrono.Time `json:"createdAt"`
 }
