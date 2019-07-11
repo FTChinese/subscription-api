792e7083a81fcaedf07ec885496edc5e3dde7553
neefrankie <neefrankie@gmail.com>
2019-07-11 23:48:09 +0800

    update test

--- model/order_tx_test.go
+++ model/order_tx_test.go
@@ -100,7 +100,7 @@ func TestOrderTx_SaveOrder(t *testing.T) {
 	}
 }
 
-func TestOrderTx_FindUnusedOrders(t *testing.T) {
+func TestOrderTx_FindBalanceSource(t *testing.T) {
 	u := test.NewProfile().User(test.IDFtc)
 
 	env := Env{db: test.DB, query: query.NewBuilder(false)}
@@ -147,7 +147,7 @@ func TestOrderTx_FindUnusedOrders(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Find Unused Order",
+			name:    "Find Balance Source",
 			args:    args{u: u},
 			wantErr: false,
 		},
@@ -167,99 +167,5 @@ func TestOrderTx_FindUnusedOrders(t *testing.T) {
 
 	if err := otx.commit(); err != nil {
 		panic(err)
-	}
-}
-
-func TestOrderTx_BuildUpgradeOrder(t *testing.T) {
-	// Prerequisites.
-	u := test.NewProfile().User(test.IDFtc)
-
-	env := Env{
-		db:    test.DB,
-		query: query.NewBuilder(false),
-	}
-
-	subsCreate := test.SubsCreate(u)
-	subsRenew := test.SubsRenew(u)
-
-	// Create orders.
-	for _, subs := range []paywall.Subscription{subsCreate, subsRenew} {
-		otx, err := env.BeginOrderTx()
-		if err != nil {
-			panic(err)
-		}
-
-		err = otx.SaveOrder(subs, test.RandomClientApp())
-		if err != nil {
-			panic(err)
-		}
-		if err := otx.commit(); err != nil {
-			panic(err)
-		}
-	}
-
-	// Confirm the orders.
-	for _, subs := range []paywall.Subscription{subsCreate, subsRenew} {
-		_, err := env.ConfirmPayment(subs.OrderID, time.Now())
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	// Test starts.
-	type fields struct {
-		env   Env
-		query query.Builder
-	}
-	type args struct {
-		user paywall.UserID
-		plan paywall.Plan
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "Build Upgrade Order",
-			fields: fields{
-				env:   env,
-				query: query.NewBuilder(false),
-			},
-			args: args{
-				user: u,
-				plan: test.YearlyPremium,
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			otx, err := tt.fields.env.BeginOrderTx()
-			if err != nil {
-				panic(err)
-			}
-
-			got, err := otx.BuildUpgradeOrder(tt.args.user, tt.args.plan)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("OrderTx.BuildUpgradeOrder() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			if err := otx.SaveOrder(got, test.RandomClientApp()); err != nil {
-				t.Errorf("OrderTx.SaveOrder() err = %v", err)
-			}
-
-			if err := otx.SaveUpgradeSource(got.OrderID, got.UpgradeSource); err != nil {
-				panic(err)
-			}
-
-			if err := otx.commit(); err != nil {
-				panic(err)
-			}
-
-			t.Logf("Upgrade Order: %+v", got)
-		})
 	}
 }
