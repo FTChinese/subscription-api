3d8bedb3753a4035b5efe4f6616937fc623a7ed5
weiguo.ni <neefrankie@gmail.com>
2019-07-11 17:55:29 +0800

    refactor: add upgrade table

--- query/order.go
+++ query/order.go
@@ -2,38 +2,6 @@ package query
 
 import "fmt"
 
-// ProratedOrders finds all orders whose end date is later
-// than today, and calculate how much left for each order
-// up to current point.
-func (b Builder) UnusedOrders() string {
-	return fmt.Sprintf(`
-	SELECT s.trade_no AS orderId,
-		s.trade_amount AS netPrice,
-		CASE s.trade_subs
-			WHEN 0 THEN s.start_date
-			WHEN 10 THEN IF(
-				s.billing_cycle = 'month', 
-				DATE(DATE_ADD(FROM_UNIXTIME(s.trade_end), INTERVAL -1 MONTH)), 
-				DATE(DATE_ADD(FROM_UNIXTIME(s.trade_end), INTERVAL -1 YEAR))
-			)
-			WHEN 100 THEN DATE(DATE_ADD(FROM_UNIXTIME(s.trade_end), INTERVAL -1 YEAR))
-		 END AS startDate,
-		IF(s.end_date IS NOT NULL, s.end_date, DATE(FROM_UNIXTIME(s.trade_end))) AS endDate
-	FROM %s.ftc_trade AS s
-		LEFT JOIN %s.upgrade_premium AS t
-		ON s.trade_no = t.source_order_id
-	WHERE s.user_id IN (?, ?)
-		AND (s.tier_to_buy = 'standard' OR s.trade_subs = 10)
-		AND (
-			s.end_date > UTC_DATE() OR
-			s.trade_end > UNIX_TIMESTAMP()
-		)
-		AND (s.confirmed_utc IS NOT NULL OR s.trade_end != 0)
-		AND t.confirmed_utc IS NULL
- 	ORDER BY start_date ASC
-	FOR UPDATE`, b.MemberDB(), b.MemberDB())
-}
-
 func (b Builder) BalanceSource() string {
 	return fmt.Sprintf(`
 	SELECT s.trade_no AS orderId,
@@ -63,29 +31,36 @@ func (b Builder) BalanceSource() string {
 	FOR UPDATE`, b.MemberDB(), b.MemberDB())
 }
 
-func (b Builder) InsertUpgradeSource() string {
+func (b Builder) SetUpgradeIDOnSource() string {
 	return fmt.Sprintf(`
-	INSERT INTO %s.upgrade_premium (
-		source_order_id,
-		target_order_id,
-		created_utc)
-	SELECT trade_no, 
-		?, 
-		UTC_TIMESTAMP()
-	FROM %s.ftc_trade
-	WHERE FIND_IN_SET(trade_no, ?) > 0`,
-		b.MemberDB(),
+	UPDATE %s.ftc_trade
+	SET last_upgrade_id = ?
+	WHERE FIND_IN_SET(trade_no, ?) > 0`, b.MemberDB())
+}
+
+func (b Builder) InsertUpgrade() string {
+	return fmt.Sprintf(`
+	INSERT INTO %s.upgrade
+	SET id = ?,
+		order_id = ?,
+		balance = ?,
+		source_id = ?,
+		created_utc = UTC_TIMESTAMP(),
+		member_id = ?,
+		cycle = ?,
+		expired_date = ?,
+		ftc_id = ?,
+		wx_union_id = ?,
+		product_tier = ?`,
 		b.MemberDB(),
 	)
 }
 
-func (b Builder) SelectUpgradeSource() string {
+func (b Builder) ConfirmUpgrade() string {
 	return fmt.Sprintf(`
-	SELECT source_order_id
-	FROM %s.upgrade_premium
-	WHERE target_order_id = ?
-		AND confirmed_utc IS NULL
-	FOR UPDATE`, b.MemberDB())
+	UPDATE %s.upgrade
+	SET confirmed_utc = UTC_TIMESTAMP()
+	WHERE id = ?`, b.MemberDB())
 }
 
 // Statement to insert a subscription order.
@@ -103,7 +78,7 @@ func (b Builder) InsertSubs() string {
 		cycle_count = ?,
 		extra_days = ?,
 		category = ?,
-		upgrade_balance = ?,
+		upgrade_id = ?
 		payment_method = ?,
 		wx_app_id = ?,
 		created_utc = UTC_TIMESTAMP(),
@@ -148,7 +123,8 @@ func (b Builder) SelectSubsLock() string {
 		billing_cycle AS Cycle,
 		cycle_count AS cycleCount,
 		extra_days AS extraDays,
-		category AS cateogry,
+		category AS usage,
+		upgrade_id AS upgradeId,
 		payment_method AS paymentMethod,
 		confirmed_utc AS confirmedAt,
 		confirmed_utc IS NOT NULL AS isConfirmed
@@ -158,20 +134,14 @@ func (b Builder) SelectSubsLock() string {
 	FOR UPDATE`, b.MemberDB())
 }
 
-func (b Builder) InvalidUpgrade() string {
+func (b Builder) UpgradeFailure() string {
 	return fmt.Sprintf(`
 	UPDATE %s.ftc_trade
-	SET upgrade_failed = ?,
+	SET result = ?,
+		failure_reason = ?
 		confirmed_utc = UTC_TIMESTAMP()
 	WHERE trade_no = ?
 	LIMIT 1`, b.MemberDB())
-}
-
-func (b Builder) ConfirmUpgradeSource() string {
-	return fmt.Sprintf(`
-	UPDATE %s.upgrade_premium
-		SET confirmed_utc = UTC_TIMESTAMP()
-	WHERE target_order_id = ?`, b.MemberDB())
 }
 
 // Statement to update a subscription order after received notification from payment provider.
@@ -179,18 +149,9 @@ func (b Builder) ConfirmSubs() string {
 	return fmt.Sprintf(`
 	UPDATE %s.ftc_trade
 	SET confirmed_utc = ?,
+		result = 'success',
 		start_date = ?,
 		end_date = ?
 	WHERE trade_no = ?
 	LIMIT 1`, b.MemberDB())
-}
-
-// Deprecate
-func (b Builder) Prorated() string {
-	return fmt.Sprintf(`
-	UPDATE %s.ftc_trade
-	SET upgrade_target = ?
-	WHERE user_id IN (?, ?)
-		AND FIND_IN_SET(trade_no, ?) > 0
-		AND upgrade_target IS NULL`, b.MemberDB())
 }
