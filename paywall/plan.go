6abe7c5daf4eb6281bcaacb6f323d3451596d820
neefrankie <neefrankie@gmail.com>
2019-07-14 22:56:54 +0800

    refactor: add BuildUpgradePlan

--- paywall/plan.go
+++ paywall/plan.go
@@ -1,6 +1,9 @@
 package paywall
 
-import "github.com/FTChinese/go-rest/enum"
+import (
+	"fmt"
+	"github.com/FTChinese/go-rest/enum"
+)
 
 // Plan is a pricing plan.
 // The list price is the price that buyers pay for your product or service without any discounts.
@@ -16,10 +19,46 @@ type Plan struct {
 	ExtraDays   int64      `json:"extraDays"`
 }
 
+func (p Plan) BuildUpgradePlan(balance float64) Plan {
+	if p.ListPrice >= balance {
+		p.NetPrice = p.ListPrice - balance
+	} else {
+		p.CycleCount, p.ExtraDays = convertBalance(balance, p.ListPrice)
+		p.NetPrice = 0
+	}
+
+	return p
+}
+
 func (p Plan) ProductID() string {
 	return p.Tier.String() + "_" + p.Cycle.String()
 }
 
 func (p Plan) StripePrice() int64 {
 	return int64(p.NetPrice * 100)
+}
+
+// FtcPlans defines a collection pricing plan.
+type FtcPlans map[string]Plan
+
+// FindPlan picks a pricing plan from a group a pre-defined plans.
+func (plans FtcPlans) FindPlan(tier, cycle string) (Plan, error) {
+	key := tier + "_" + cycle
+
+	p, ok := plans[key]
+
+	if !ok {
+		return p, fmt.Errorf("pricing plan for %s not found", key)
+	}
+
+	return p, nil
+}
+
+func (plans FtcPlans) GetPlanByID(id string) (Plan, error) {
+	p, ok := plans[id]
+	if !ok {
+		return p, fmt.Errorf("pricing plan for %s not found", id)
+	}
+
+	return p, nil
 }
