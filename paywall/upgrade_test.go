ded39e81e3ee36209b0ed0f9043ec1b7efd8e13d
neefrankie <neefrankie@gmail.com>
2019-07-14 22:58:23 +0800

    fix: helpers for test

--- paywall/upgrade_test.go
+++ paywall/upgrade_test.go
@@ -1,127 +1,146 @@
 package paywall
 
 import (
+	"math"
 	"testing"
 	"time"
 
 	"github.com/FTChinese/go-rest/chrono"
 )
 
-func TestUpgradePlan_CalculatePayable(t *testing.T) {
-
-	plan := GetDefaultPlans()["premium_year"]
-
-	type fields struct {
-		Plan       Plan
-		Balance    float64
-		CycleCount int64
-		ExtraDays  int64
-		Payable    float64
-		OrderIDs   []string
+func orderID() string {
+	id, err := GenerateOrderID()
+	if err != nil {
+		panic(err)
 	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   Upgrade
-	}{
-		{
-			name: "Exactly Cover One Cycle",
-			fields: fields{
-				Plan:    plan,
-				Balance: 1998,
-			},
-		},
-		{
-			name: "More Than One Cycle",
-			fields: fields{
-				Plan:    plan,
-				Balance: 2000,
-			},
-		},
-		{
-			name: "One Cycle Plus Many Days",
-			fields: fields{
-				Plan:    plan,
-				Balance: 3000,
-			},
-		},
-		{
-			name: "Two Cycle Plus Many Days",
-			fields: fields{
-				Plan:    plan,
-				Balance: 4000,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := Upgrade{
-				Plan:    tt.fields.Plan,
-				Balance: tt.fields.Balance,
-				Payable: tt.fields.Payable,
-			}
-			//if got := p.CalculatePayable(); !reflect.DeepEqual(got, tt.want) {
-			//	t.Errorf("Upgrade.CalculatePayable() = %v, want %v", got, tt.want)
-			//}
-
-			got := p.CalculatePayable()
-
-			t.Logf("Payable: %+v", got)
-		})
-	}
+	return id
 }
 
-//func TestUnusedOrder_Balance(t *testing.T) {
-//	order := BalanceSource{
-//		NetPrice: 258.0,
-//		StartDate: chrono.DateFrom(time.Now().Truncate(24*time.Hour)),
-//		EndDate: chrono.DateFrom(time.Now().AddDate(0, 1, 0).Truncate(24*time.Hour)),
-//	}
-//
-//	t.Logf("Balance: %f", order.Balance())
-//}
+func buildBalanceSources(count int) []BalanceSource {
+	var sources []BalanceSource
 
-func TestUnusedOrder_Balance(t *testing.T) {
-	today := time.Now().Truncate(24 * time.Hour)
+	now := time.Now()
+	for i := 0; i < count; i++ {
+		startTime := now.AddDate(i, 0, 0)
+		endTime := startTime.AddDate(i+1, 0, 1)
 
-	type fields struct {
-		ID        string
-		NetPrice  float64
-		StartDate chrono.Date
-		EndDate   chrono.Date
+		s := BalanceSource{
+			ID:        orderID(),
+			NetPrice:  258.00,
+			StartDate: chrono.DateFrom(startTime),
+			EndDate:   chrono.DateFrom(endTime),
+		}
+
+		sources = append(sources, s)
 	}
+
+	return sources
+}
+
+func TestBalanceSource_Balance(t *testing.T) {
+
 	tests := []struct {
 		name   string
-		fields fields
+		source BalanceSource
 	}{
 		{
-			name: "Unused order",
-			fields: fields{
-				NetPrice:  258.0,
-				StartDate: chrono.DateFrom(today),
-				EndDate:   chrono.DateFrom(today.AddDate(0, 1, 0)),
+			name: "Unused Balance Source",
+			source: BalanceSource{
+				ID:        orderID(),
+				NetPrice:  258.00,
+				StartDate: chrono.DateNow(),
+				EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 1)),
 			},
 		},
 		{
-			name: "Partly used order",
-			fields: fields{
-				NetPrice:  258.0,
-				StartDate: chrono.DateFrom(today.AddDate(0, -2, 0)),
-				EndDate:   chrono.DateFrom(today.AddDate(0, 10, 0)),
+			name: "Partly Used Balance Source",
+			source: BalanceSource{
+				ID:        orderID(),
+				NetPrice:  258.00,
+				StartDate: chrono.DateFrom(time.Now().AddDate(0, -5, 0)),
+				EndDate:   chrono.DateFrom(time.Now().AddDate(0, 7, 1)),
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			o := BalanceSource{
-				ID:        tt.fields.ID,
-				NetPrice:  tt.fields.NetPrice,
-				StartDate: tt.fields.StartDate,
-				EndDate:   tt.fields.EndDate,
-			}
-			got := o.Balance()
+
+			t.Logf("Duration %s to %s", tt.source.StartDate, tt.source.EndDate)
+			got := tt.source.Balance()
 
 			t.Logf("Remaining amount: %f", got)
 		})
 	}
+}
+
+func TestUpgrade_SetBalance(t *testing.T) {
+	plan, _ := GetDefaultPlans().GetPlanByID("premium_year")
+
+	type fields struct {
+		Upgrade Upgrade
+	}
+	type args struct {
+		sources []BalanceSource
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Not Enough Balance",
+			fields: fields{
+				Upgrade: NewUpgrade(plan),
+			},
+			args: args{
+				sources: buildBalanceSources(2),
+			},
+		},
+		{
+			name: "Enough to Cover",
+			fields: fields{
+				Upgrade: NewUpgrade(plan),
+			},
+			args: args{
+				sources: buildBalanceSources(8),
+			},
+		},
+		{
+			name: "More than 2 cycles",
+			fields: fields{
+				Upgrade: NewUpgrade(plan),
+			},
+			args: args{
+				sources: buildBalanceSources(16),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := tt.fields.Upgrade
+			got := p.
+				SetBalance(tt.args.sources).
+				CalculatePayable()
+
+			t.Logf("Upgrade: %+v", got)
+		})
+	}
+}
+
+func TestCalculateCycles(t *testing.T) {
+	balance := 2064.00
+	price := 1998.00
+
+	cycleCount := 0
+
+	for balance > price {
+		cycleCount = cycleCount + 1
+		balance = balance - price
+	}
+
+	t.Logf("Balance %f, cycle cout %d", balance, cycleCount)
+
+	days := math.Ceil(balance * 365 / price)
+
+	t.Logf("Days: %d", int64(days))
 }
