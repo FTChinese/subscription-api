8dfa1e91de7d0d9d17fa69139dc14493266d7906
neefrankie <neefrankie@gmail.com>
2019-07-15 00:35:51 +0800

    test

--- model/order_tx_test.go
+++ model/order_tx_test.go
@@ -12,6 +12,57 @@ import (
 	"gitlab.com/ftchinese/subscription-api/test"
 )
 
+func createUpgrade(userID paywall.UserID) paywall.Upgrade {
+	upgrade := test.GenUpgrade(userID)
+	orderID, _ := paywall.GenerateOrderID()
+	upgrade.Member = test.GenMember(userID, false)
+
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tx.SaveUpgrade(orderID, upgrade); err != nil {
+		panic(err)
+	}
+
+	if err := tx.commit(); err != nil {
+		panic(err)
+	}
+
+	return upgrade
+}
+
+func createMember(userID paywall.UserID) paywall.Membership {
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		panic(err)
+	}
+
+	m := test.GenMember(userID, false)
+
+	err = tx.CreateMember(m, null.String{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tx.commit(); err != nil {
+		panic(err)
+	}
+
+	return m
+}
+
 // Those tests only checks whether db operations are correct.
 // It does not guarantees logical correctness.
 func TestOrderTx_SaveOrder(t *testing.T) {
@@ -37,6 +88,163 @@ func TestOrderTx_SaveOrder(t *testing.T) {
 	}
 
 	t.Logf("Saved new order: %+s", subs.OrderID)
+}
+
+// Create a new order in db and returns it.
+func createOrder(order paywall.Subscription) paywall.Subscription {
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tx.SaveOrder(order, test.RandomClientApp()); err != nil {
+		panic(err)
+	}
+
+	if err := tx.commit(); err != nil {
+		panic(err)
+	}
+
+	return order
+}
+
+func TestOrderTx_RetrieveOrder(t *testing.T) {
+	// Prerequisites
+	userID := test.MyProfile.RandomUserID()
+
+	subs := createOrder(test.SubsCreate(userID))
+	t.Logf("Saved new order: %+s", subs.OrderID)
+
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	// Start testing.
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		t.Error(err)
+	}
+
+	order, err := tx.RetrieveOrder(subs.OrderID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Retrieved order: %+v", order)
+}
+
+func TestOrderTx_ConfirmOrder(t *testing.T) {
+	userID := test.NewProfile().RandomUserID()
+
+	order, _ := createOrder(test.SubsCreate(userID)).
+		Confirm(paywall.Membership{}, time.Now())
+	t.Logf("Created order: %s", order.OrderID)
+
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	type args struct {
+		order paywall.Subscription
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save confirmed order for creation",
+			args: args{
+				order: order,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx, err := env.BeginOrderTx()
+			if err != nil {
+				t.Error(err)
+			}
+
+			if err := tx.ConfirmOrder(tt.args.order); (err != nil) != tt.wantErr {
+				t.Errorf("OrderTx.ConfirmOrder() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if err := tx.commit(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
+
+func createConfirmedOrder(
+	unconfirmedOrder paywall.Subscription,
+	previousMember paywall.Membership,
+) paywall.Subscription {
+
+	confirmedOrder, _ := createOrder(unconfirmedOrder).
+		Confirm(previousMember, time.Now())
+
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tx.ConfirmOrder(confirmedOrder); err != nil {
+		panic(err)
+	}
+
+	if err := tx.commit(); err != nil {
+		panic(err)
+	}
+
+	return confirmedOrder
+}
+
+func TestOrderTx_CreateMember(t *testing.T) {
+	userID := test.NewProfile().RandomUserID()
+
+	previousMember := paywall.NewMember(userID)
+	order := createConfirmedOrder(test.SubsCreate(userID), previousMember)
+	t.Logf("A confirmed order: %+v", order)
+
+	env := Env{
+		db:    test.DB,
+		query: query.NewBuilder(false),
+	}
+
+	// User new order to build member
+	m, err := previousMember.FromAliOrWx(order)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Member: %+v", m)
+
+	tx, err := env.BeginOrderTx()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := tx.CreateMember(m, null.String{}); err != nil {
+		t.Error()
+	}
+
+	if err := tx.commit(); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestOrderTx_FindBalanceSources(t *testing.T) {
@@ -195,53 +403,6 @@ func TestOrderTx_SetUpgradeIDOnSource(t *testing.T) {
 	}
 }
 
-func TestOrderTx_ConfirmOrder(t *testing.T) {
-	userID := test.NewProfile().RandomUserID()
-
-	order := createOrder(userID)
-	t.Logf("Created order: %s", order.OrderID)
-
-	order, _ = order.Confirm(paywall.Membership{}, time.Now())
-
-	env := Env{
-		db:    test.DB,
-		query: query.NewBuilder(false),
-	}
-
-	type args struct {
-		order paywall.Subscription
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "Confirm an order",
-			args: args{
-				order: order,
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tx, err := env.BeginOrderTx()
-			if err != nil {
-				t.Error(err)
-			}
-
-			if err := tx.ConfirmOrder(tt.args.order); (err != nil) != tt.wantErr {
-				t.Errorf("OrderTx.ConfirmOrder() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if err := tx.commit(); err != nil {
-				t.Error(err)
-			}
-		})
-	}
-}
-
 func TestOrderTx_ConfirmUpgrade(t *testing.T) {
 	userID := test.NewProfile().RandomUserID()
 
@@ -282,40 +443,6 @@ func TestOrderTx_ConfirmUpgrade(t *testing.T) {
 				t.Error(err)
 			}
 		})
-	}
-}
-
-func TestOrderTx_CreateMember(t *testing.T) {
-	userID := test.NewProfile().RandomUserID()
-
-	t.Logf("User ID: %+v", userID)
-
-	env := Env{
-		db:    test.DB,
-		query: query.NewBuilder(false),
-	}
-
-	order := createOrder(userID)
-	m := paywall.Membership{}
-
-	// User current membership to confirm order.
-	order, _ = order.Confirm(m, time.Now())
-	t.Logf("A cofirmed new order: %+v", order)
-	// User new order to build member
-	m, _ = m.FromAliOrWx(order)
-	t.Logf("A new member: %+v", m)
-
-	tx, err := env.BeginOrderTx()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := tx.CreateMember(m, null.String{}); err != nil {
-		t.Error()
-	}
-
-	if err := tx.commit(); err != nil {
-		t.Error(err)
 	}
 }
 
