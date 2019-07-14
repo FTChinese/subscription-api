4c69aa1b848a80231aa4f1f9680de6aba3da1013
neefrankie <neefrankie@gmail.com>
2019-07-14 22:57:35 +0800

    refactor: omit CreatedAt and ConfirmedAt for json

--- paywall/upgrade_plan.go
+++ paywall/upgrade_plan.go
@@ -9,8 +9,8 @@ type UpgradePreview struct {
 	ID          string      `json:"id"`
 	Balance     float64     `json:"balance"` // Accumulated on all BalanceSource.Balance
 	SourceIDs   []string    `json:"sources"` // The order ids which still have portion of days unused.
-	CreatedAt   chrono.Time `json:"createdAt"`
-	ConfirmedAt chrono.Time `json:"confirmedAt"`
+	CreatedAt   chrono.Time `json:"-"`
+	ConfirmedAt chrono.Time `json:"-"`
 	Plan        Plan        `json:"plan"`
 	Member      Membership  `json:"-"`
 }
@@ -18,19 +18,19 @@ type UpgradePreview struct {
 func NewUpgradePreview(sources []BalanceSource) UpgradePreview {
 	id, _ := GenerateUpgradeID()
 
-	p := UpgradePreview{
+	up := UpgradePreview{
 		ID:        id,
 		CreatedAt: chrono.TimeNow(),
 	}
 
 	for _, v := range sources {
-		p.Balance = p.Balance + v.Balance()
-		p.SourceIDs = append(p.SourceIDs, v.ID)
+		up.Balance = up.Balance + v.Balance()
+		up.SourceIDs = append(up.SourceIDs, v.ID)
 	}
 
-	return p
+	return up
 }
 
-func (p UpgradePreview) SourceOrderIDs() string {
-	return strings.Join(p.SourceIDs, ",")
+func (up UpgradePreview) SourceOrderIDs() string {
+	return strings.Join(up.SourceIDs, ",")
 }
