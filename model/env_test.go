0bf3b390d3942811707cf546739f722ddfd1f6b0
weiguo.ni <neefrankie@gmail.com>
2019-07-15 12:07:59 +0800

    test

--- model/env_test.go
+++ model/env_test.go
@@ -1,1 +1,13 @@
 package model
+
+import (
+	"gitlab.com/ftchinese/subscription-api/test"
+	"testing"
+)
+
+// Create a customer to be used for postman testing.
+func TestAPI_NewUser(t *testing.T) {
+	user := test.NewProfile().FtcUser()
+
+	t.Logf("Customer %+v", user)
+}
