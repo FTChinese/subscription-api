78b6ac919953133dd43930d0141ac1c759ac36fd
weiguo.ni <neefrankie@gmail.com>
2019-07-15 12:08:22 +0800

    refactor: nested data structure

--- wechat/order.go
+++ wechat/order.go
@@ -18,23 +18,19 @@ func GenerateTimestamp() string {
 	return fmt.Sprintf("%d", time.Now().Unix())
 }
 
-// AppOrder creates an order used when your called wechat pay
-// inside your own app.
-type AppOrder struct {
-	paywall.Subscription
-	FtcOrderID string `json:"ftcOrderId"` // Deprecate
-	AppID      string `json:"appId"`
-	PartnerID  string `json:"partnerId"`
-	PrepayID   string `json:"prepayId"`
-	Timestamp  string `json:"timestamp"`
-	Nonce      string `json:"nonce"`
-	Package    string `json:"pkg"`
-	Signature  string `json:"signature"`
+type AppOrderParams struct {
+	AppID     string `json:"appId"`
+	PartnerID string `json:"partnerId"`
+	PrepayID  string `json:"prepayId"`
+	Timestamp string `json:"timestamp"`
+	Nonce     string `json:"nonce"`
+	Package   string `json:"pkg"`
+	Signature string `json:"signature"`
 }
 
-func (a *AppOrder) Params() wxpay.Params {
+func (a AppOrderParams) ToMap() wxpay.Params {
 	param := make(wxpay.Params)
-	param["appid"] = a.WxAppID.String
+	param["appid"] = a.AppID
 	param["partnerid"] = a.PartnerID
 	param["prepayid"] = a.PrepayID
 	param["package"] = a.Package
@@ -44,23 +40,54 @@ func (a *AppOrder) Params() wxpay.Params {
 	return param
 }
 
+// AppOrder creates an order used when your called wechat pay
+// inside your own app.
+type AppOrder struct {
+	paywall.Subscription
+	FtcOrderID     string         `json:"ftcOrderId"` // Deprecate
+	AppOrderParams                // Deprecate
+	Params         AppOrderParams `json:"params"`
+}
+
 // BuildAppOrder creates an order that can be used inside
 // a native app.
 func (c Client) BuildAppOrder(u UnifiedOrderResp, subs paywall.Subscription) AppOrder {
-	o := AppOrder{
-		Subscription: subs,
-		FtcOrderID:   subs.OrderID,
-		AppID:        subs.WxAppID.String,
-		PartnerID:    u.MID.String,
-		PrepayID:     u.PrepayID.String,
-		Timestamp:    GenerateTimestamp(),
-		Nonce:        GenerateNonce(),
-		Package:      "Sign=WXPay",
+	p := AppOrderParams{
+		AppID:     subs.WxAppID.String,
+		PartnerID: u.MID.String,
+		PrepayID:  u.PrepayID.String,
+		Timestamp: GenerateTimestamp(),
+		Nonce:     GenerateNonce(),
+		Package:   "Sign=WXPay",
 	}
+	p.Signature = c.Sign(p.ToMap())
 
-	o.Signature = c.Sign(o.Params())
-
+	o := AppOrder{
+		Subscription:   subs,
+		FtcOrderID:     subs.OrderID,
+		AppOrderParams: p,
+		Params:         p,
+	}
 	return o
+}
+
+type InWxBrowserParams struct {
+	Timestamp string `json:"timestamp"`
+	Nonce     string `json:"nonce"`
+	Package   string `json:"pkg"`
+	Signature string `json:"signature"`
+	SignType  string `json:"signType"`
+}
+
+func (w InWxBrowserParams) ToMap(appID string) wxpay.Params {
+	param := make(wxpay.Params)
+	param["appId"] = appID
+	param["timeStamp"] = w.Timestamp
+	param["nonceStr"] = w.Nonce
+	param["package"] = w.Package
+	param["signType"] = w.SignType
+
+	return param
 }
 
 // InAppBrowserOrder creates an order when user is trying to pay
@@ -71,36 +98,29 @@ func (c Client) BuildAppOrder(u UnifiedOrderResp, subs paywall.Subscription) App
 // for such insignificant differences.
 type InAppBrowserOrder struct {
 	paywall.Subscription
-	Timestamp string `json:"timestamp"`
-	Nonce     string `json:"nonce"`
-	Package   string `json:"pkg"`
-	Signature string `json:"signature"`
-	SignType  string `json:"signType"`
-}
-
-func (w *InAppBrowserOrder) Params() wxpay.Params {
-	param := make(wxpay.Params)
-	param["appId"] = w.WxAppID.String
-	param["timeStamp"] = w.Timestamp
-	param["nonceStr"] = w.Nonce
-	param["package"] = w.Package
-	param["signType"] = w.SignType
-
-	return param
+	InWxBrowserParams
+	Params InWxBrowserParams
 }
 
 // BuildInAppBrowserOrder creates an order for payment inside
 // wechat embedded browser.
 func (c Client) BuildInAppBrowserOrder(u UnifiedOrderResp, subs paywall.Subscription) InAppBrowserOrder {
+
+	p := InWxBrowserParams{
+		Timestamp: GenerateTimestamp(),
+		Nonce:     GenerateNonce(),
+		Package:   "prepay_id=" + u.PrepayID.String,
+		SignType:  "MD5",
+	}
+	p.Signature = c.Sign(p.ToMap(subs.WxAppID.String))
+
 	o := InAppBrowserOrder{
-		Subscription: subs,
-		Timestamp:    GenerateTimestamp(),
-		Nonce:        GenerateNonce(),
-		Package:      "prepay_id=" + u.PrepayID.String,
-		SignType:     "MD5",
+		Subscription:      subs,
+		InWxBrowserParams: p,
+		Params:            p,
 	}
 
-	o.Signature = c.Sign(o.Params())
+	//o.Signature = c.Sign(o.Params())
 
 	return o
 }
