78dc72e1ae560a5c96f4a4f5a029710ba9b6dd60
neefrankie <neefrankie@gmail.com>
2019-07-20 11:10:36 +0800

    feat: log stripe error to db

---
+++ query/stripe_resp.go
@@ -0,0 +1,15 @@
+package query
+
+const (
+	InsertStripeError = `
+	INSERT INTO premium.log_stripe_error
+	SET ftc_user_id = ?,
+		charge_id = ?,
+		error_code = ?,
+		http_status = ?,
+		error_message = ?,
+		parameter = ?,
+		request_id = ?,
+		error_type = ?,
+		create_utc = UTC_TIMESTAMP()`
+)
