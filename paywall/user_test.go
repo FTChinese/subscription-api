68b25cf335c5f360c521d248a61b7ffa8f34a6d4
neefrankie <neefrankie@gmail.com>
2019-07-21 23:53:30 +0800

    test

---
+++ paywall/user_test.go
@@ -0,0 +1,52 @@
+package paywall
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/guregu/null"
+	"github.com/icrowley/fake"
+	"github.com/stripe/stripe-go"
+	"testing"
+)
+
+func newFtcUser() FtcUser {
+	return FtcUser{
+		UserID:   uuid.New().String(),
+		UnionID:  null.String{},
+		StripeID: null.String{},
+		Email:    fake.EmailAddress(),
+		UserName: null.StringFrom(fake.UserName()),
+	}
+}
+
+func TestFtcUser_StripeSubParcel(t *testing.T) {
+	var s stripe.Subscription
+
+	if err := json.Unmarshal([]byte(subData), &s); err != nil {
+		t.Error(err)
+	}
+
+	ss := NewStripeSub(&s)
+
+	parcel, err := newFtcUser().StripeSubParcel(ss)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("%+v", parcel)
+}
+
+func TestFtcUser_StripeInvoiceParcel(t *testing.T) {
+	var i stripe.Invoice
+
+	if err := json.Unmarshal([]byte(invoiceData), &i); err != nil {
+		t.Error(err)
+	}
+
+	parcel, err := newFtcUser().StripeInvoiceParcel(EmailedInvoice{i})
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("%+v", parcel)
+}
