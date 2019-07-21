dcf8030d643c49ebe483fdc02b34bf81208ebb23
neefrankie <neefrankie@gmail.com>
2019-07-21 23:56:02 +0800

    feat: retrieve user by stripe customer id

--- model/ftc_user.go
+++ model/ftc_user.go
@@ -18,7 +18,26 @@ func (env Env) FindFtcUser(ftcId string) (paywall.FtcUser, error) {
 	)
 
 	if err != nil {
-		logger.WithField("trace", "EnvFindUser").Error(err)
+		logger.WithField("trace", "Env.FindFtcUser").Error(err)
+
+		return u, err
+	}
+
+	return u, nil
+}
+
+func (env Env) FindStripeCustomer(cusID string) (paywall.FtcUser, error) {
+	var u paywall.FtcUser
+	err := env.db.QueryRow(query.SelectStripeCustomer, cusID).Scan(
+		&u.UserID,
+		&u.UnionID,
+		&u.StripeID,
+		&u.UserName,
+		&u.Email,
+	)
+
+	if err != nil {
+		logger.WithField("trace", "Env.FindStripeCustomer").Error(err)
 
 		return u, err
 	}
