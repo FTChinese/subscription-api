eb8467a0d2fa0921e4d16c0f0dbdffc8e3fe8df4
neefrankie <neefrankie@gmail.com>
2019-07-21 23:54:49 +0800

    feat: Currency type

---
+++ paywall/currency.go
@@ -0,0 +1,21 @@
+package paywall
+
+type Currency string
+
+const (
+	CurrencyCNY Currency = "cny" // Chinese Renminbi Yuan
+	CurrencyEUR Currency = "eur" // Euro
+	CurrencyGBP Currency = "gbp" // British Pound
+	CurrencyHKD Currency = "hkd" // Hong Kong Dollar
+	CurrencyJPY Currency = "jpy" // Japanese Yen
+	CurrencyUSD Currency = "usd" // United States Dollar
+)
+
+var currencySymbolMap = map[Currency]string{
+	CurrencyCNY: "¥",
+	CurrencyEUR: "€",
+	CurrencyGBP: "£",
+	CurrencyHKD: "HK$",
+	CurrencyJPY: "¥",
+	CurrencyUSD: "US$",
+}
