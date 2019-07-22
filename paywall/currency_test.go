40f57d8934cf8c0460ab16da72a3b6df9e0b0025
weiguo.ni <neefrankie@gmail.com>
2019-07-22 17:30:54 +0800

    feat: get currency symbol

---
+++ paywall/currency_test.go
@@ -0,0 +1,24 @@
+package paywall
+
+import "testing"
+
+func TestCurrency_Symbol(t *testing.T) {
+	tests := []struct {
+		name string
+		c    Currency
+		want string
+	}{
+		{
+			name: "Get a currency symbol",
+			c:    CurrencyGBP,
+			want: "£",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.c.Symbol(); got != tt.want {
+				t.Errorf("Currency.Symbol() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
