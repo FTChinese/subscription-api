f74c11d54820b759ce49f420458074c5d861ac97
weiguo.ni <neefrankie@gmail.com>
2019-07-25 11:58:17 +0800

    test

--- test/model.go
+++ test/model.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"database/sql"
-	"github.com/guregu/null"
 	"gitlab.com/ftchinese/subscription-api/paywall"
 	"gitlab.com/ftchinese/subscription-api/query"
 	"time"
@@ -20,14 +19,14 @@ func NewModel(db *sql.DB) Model {
 	}
 }
 
-func (m Model) ClearUser(u paywall.AccountID) error {
+func (model Model) ClearUser(u paywall.AccountID) error {
 	query := `
 	DELETE FROM cmstmp01.userinfo
 	WHERE user_id = ?
 		OR wx_union_id = ?
 	LIMIT 1`
 
-	_, err := m.db.Exec(query, u.CompoundID, u.UnionID)
+	_, err := model.db.Exec(query, u.CompoundID, u.UnionID)
 
 	if err != nil {
 		return err
@@ -36,13 +35,13 @@ func (m Model) ClearUser(u paywall.AccountID) error {
 	return nil
 }
 
-func (m Model) ClearMember(u paywall.AccountID) error {
+func (model Model) ClearMember(u paywall.AccountID) error {
 	q := `
 	DELETE FROM premium.ftc_vip
 	WHERE vip_id = ?
 		OR vip_id_alias = ?`
 
-	_, err := m.db.Exec(q, u.CompoundID, u.UnionID)
+	_, err := model.db.Exec(q, u.CompoundID, u.UnionID)
 
 	if err != nil {
 		return err
@@ -51,12 +50,12 @@ func (m Model) ClearMember(u paywall.AccountID) error {
 	return nil
 }
 
-func (m Model) ClearOrder(u paywall.AccountID) error {
+func (model Model) ClearOrder(u paywall.AccountID) error {
 	q := `
 	DELETE FROM premium.ftc_trade
 	WHERE user_id IN (?, ?)`
 
-	_, err := m.db.Exec(q, u.FtcID, u.UnionID)
+	_, err := model.db.Exec(q, u.FtcID, u.UnionID)
 
 	if err != nil {
 		return err
@@ -65,12 +64,12 @@ func (m Model) ClearOrder(u paywall.AccountID) error {
 	return nil
 }
 
-func (m Model) ClearUserByEmail(email string) error {
+func (model Model) ClearUserByEmail(email string) error {
 	query := `
 	DELETE FROM cmstmp01.userinfo
 	WHERE email = ?`
 
-	_, err := m.db.Exec(query, email)
+	_, err := model.db.Exec(query, email)
 
 	if err != nil {
 		return err
@@ -79,41 +78,7 @@ func (m Model) ClearUserByEmail(email string) error {
 	return nil
 }
 
-func (m Model) CreateFtcUser(p Profile) error {
-	query := `
-	INSERT INTO cmstmp01.userinfo
-	SET user_id = ?,
-		wx_union_id = ?,
-		email = ?,
-		password = MD5(?),
-		user_name = ?,
-		client_type = ?,
-		client_version = ?,
-		user_ip = INET6_ATON(?),
-		user_agent = ?,
-		created_utc = UTC_TIMESTAMP()`
-
-	app := RandomClientApp()
-
-	_, err := m.db.Exec(query,
-		p.FtcID,
-		null.String{},
-		p.Email,
-		p.Password,
-		p.UserName,
-		app.ClientType,
-		app.Version,
-		app.UserIP,
-		app.UserAgent)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (m Model) CreateGiftCard() paywall.GiftCard {
+func (model Model) CreateGiftCard() paywall.GiftCard {
 	c := GiftCard()
 
 	q := `
@@ -140,4 +105,64 @@ func (m Model) CreateGiftCard() paywall.GiftCard {
 	}
 
 	return c
+}
+
+func (model Model) CreateSub(s paywall.Subscription) {
+
+	var stmt = model.query.InsertSubs() + `
+		confirmed_utc = ?,
+		start_date = ?,
+		end_date = ?`
+
+	c := RandomClientApp()
+
+	_, err := model.db.Exec(stmt,
+		s.ID,
+		s.CompoundID,
+		s.FtcID,
+		s.UnionID,
+		s.ListPrice,
+		s.NetPrice,
+		s.Tier,
+		s.Cycle,
+		s.CycleCount,
+		s.ExtraDays,
+		s.Usage,
+		s.PaymentMethod,
+		s.WxAppID,
+		s.CreatedAt,
+		c.ClientType,
+		c.Version,
+		c.UserIP,
+		c.UserAgent,
+		s.ConfirmedAt,
+		s.StartDate,
+		s.EndDate)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (model Model) CreateMember(m paywall.Membership) {
+	_, err := model.db.Exec(model.query.InsertMember(),
+		m.ID,
+		m.CompoundID,
+		m.UnionID,
+		m.TierCode(),
+		m.ExpireDate.Unix(),
+		m.FtcID,
+		m.UnionID,
+		m.Tier,
+		m.Cycle,
+		m.ExpireDate,
+		m.PaymentMethod,
+		m.StripeSubID,
+		m.StripePlanID,
+		m.AutoRenewal,
+		m.Status)
+
+	if err != nil {
+		panic(err)
+	}
 }
