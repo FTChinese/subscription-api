6aad1a2122814ad24ac26fc3673584c85788e77c
weiguo.ni <neefrankie@gmail.com>
2019-07-26 14:23:05 +0800

    refactor: generate upgrade

--- test/sub_store_test.go
+++ test/sub_store_test.go
@@ -18,15 +18,43 @@ func TestSubStore_AddOrder(t *testing.T) {
 
 	store.AddOrder(paywall.SubsKindCreate)
 
-	t.Logf("%+v", store)
+	t.Logf("Current orders %+v", store.Orders)
 }
 
-func TestSubStore_GetLastOrder(t *testing.T) {
+func TestSubStore_GetOrder(t *testing.T) {
 	store := NewSubStore(NewProfile().AccountID(AccountKindFtc))
 
-	store.AddOrder(paywall.SubsKindCreate)
-	store.AddOrder(paywall.SubsKindRenew)
+	o := store.AddOrder(paywall.SubsKindCreate)
 
-	t.Logf("Store: %+v", store)
-	t.Logf("Last order: %+v", store.GetLastOrder())
+	got, err := store.GetOrder(o.ID)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Get order %+v", got)
+}
+
+func TestSubStore_ConfirmOrder(t *testing.T) {
+	store := NewSubStore(NewProfile().FtcAccountID())
+
+	o := store.AddOrder(paywall.SubsKindCreate)
+
+	o, err := store.ConfirmOrder(o.ID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Confirmed order: %+v", o)
+}
+
+func TestSubStore_UpgradeOrder(t *testing.T) {
+	store := NewSubStore(NewProfile().FtcAccountID())
+
+	upgradeOrder, err := store.UpgradeOrder(5)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Upgrade order: %+v", upgradeOrder)
+	t.Logf("Upgrade v1: %+v", store.UpgradeV1)
+	t.Logf("Upgrade v2: %+v", store.UpgradeV2)
 }
