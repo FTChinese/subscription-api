241b8e7a6bfcd7ed5a86fd0320f14157142e721c
weiguo.ni <neefrankie@gmail.com>
2019-07-26 16:28:31 +0800

    refactor: remove CmsTmp and UserDB

--- query/builder.go
+++ query/builder.go
@@ -12,21 +12,6 @@ func (b Builder) MemberDB() string {
 	return "premium"
 }
 
-func (b Builder) CmsTmp() string {
-	if b.sandbox {
-		return "sandbox"
-	}
-
-	return "cmstmp01"
-}
-
-func (b Builder) UserDB() string {
-	if b.sandbox {
-		return "sandbox"
-	}
-
-	return "user_db"
-}
 func NewBuilder(sandbox bool) Builder {
 	return Builder{sandbox: sandbox}
 }
