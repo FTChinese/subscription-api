03c258dc53e5696b931dde72ea1e89e4e9835c65
weiguo.ni <neefrankie@gmail.com>
2019-07-26 16:06:12 +0800

    feat: create ConfirmationResult type

--- paywall/subscription.go
+++ paywall/subscription.go
@@ -232,3 +232,29 @@ func (s Subscription) Confirm(m Membership, confirmedAt time.Time) (Subscription
 
 	return s, nil
 }
+
+type ConfirmationResult struct {
+	OrderID   string
+	Succeeded bool
+	Failed    null.String
+	Retry     bool
+}
+
+func (r ConfirmationResult) Error() string {
+	return r.Failed.String
+}
+
+func NewConfirmationSucceeded(orderID string) *ConfirmationResult {
+	return &ConfirmationResult{
+		OrderID:   orderID,
+		Succeeded: true,
+	}
+}
+
+func NewConfirmationFailed(orderID string, reason error, retry bool) *ConfirmationResult {
+	return &ConfirmationResult{
+		OrderID: orderID,
+		Failed:  null.StringFrom(reason.Error()),
+		Retry:   retry,
+	}
+}
