c1d76c5158299baf5f558c0f344c643012eeb191
weiguo.ni <neefrankie@gmail.com>
2019-07-26 14:23:34 +0800

    test

--- test/model_test.go
+++ test/model_test.go
@@ -12,13 +12,3 @@ func TestUUID(t *testing.T) {
 func TestGenerateNewUser(t *testing.T) {
 	t.Logf("%+v", NewProfile())
 }
-
-func TestModel_CreateNewMember(t *testing.T) {
-	model := NewModel()
-	store := NewSubStore(NewProfile().AccountID(AccountKindFtc))
-
-	model.CreateNewMember(store)
-
-	t.Logf("Created a new order %s", store.GetLastOrder().ID)
-	t.Logf("Created a new member %+v", store.Member)
-}
