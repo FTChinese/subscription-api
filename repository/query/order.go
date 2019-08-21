572f827f6529679f2a68b98898b7586e3be72ff8
weiguo.ni <neefrankie@gmail.com>
2019-08-21 09:30:35 +0800

    refactor: for sqlx

--- repository/query/order.go
+++ repository/query/order.go
@@ -2,6 +2,89 @@ package query
 
 import "fmt"
 
+// Statement to insert a subscription order.
+func (b Builder) InsertSubs() string {
+	return fmt.Sprintf(`
+	INSERT INTO %s.ftc_trade
+	SET trade_no = :order_id,
+		user_id = :compound_id,
+		ftc_user_id = :ftc_id,
+		wx_union_id = :union_id,
+		trade_price = :price,
+		trade_amount = :amount,
+		tier_to_buy = :tier,
+		billing_cycle = :cycle,
+		cycle_count = :cycle_count,
+		extra_days = :extra_days,
+		category = :usage_type,
+		payment_method = :payment_method,
+		wx_app_id = wx_app_id,
+		created_utc = UTC_TIMESTAMP(),
+		client_type = :client_type,
+		client_version = :client_version,
+		user_ip_bin = INET6_ATON(:user_ip),
+		user_agent = :user_agent`, b.MemberDB())
+}
+
+// SelectSubsPrice retrieves an order's price when payment
+// provider send confirmation notice.
+func (b Builder) SelectSubsPrice() string {
+	return fmt.Sprintf(`
+	SELECT trade_price AS :price,
+		trade_amount AS :amount,
+		confirmed_utc IS NOT NULL AS :is_confirmed
+	FROM %s.ftc_trade
+	WHERE trade_no = ?
+	LIMIT 1`, b.MemberDB())
+}
+
+func (b Builder) FtcUserOrderBilling() string {
+	return fmt.Sprintf(`
+	SELECT trade_price AS listPrice,
+		trade_amount AS netPrice,
+		confirmed_utc IS NOT NULL AS isConfirmed
+	FROM %s.ftc_trade
+	WHERE trade_no = ?
+		AND ftc_user_id = ?
+	LIMIT 1`, b.MemberDB())
+}
+
+// SelectSubsLock select an order upon webhook received
+// notification.
+func (b Builder) SelectSubsLock() string {
+	return fmt.Sprintf(`
+	SELECT trade_no AS :order_id,
+		user_id AS :compound_id,
+		ftc_user_id AS :ftc_id,
+		wx_union_id AS :union_id,
+		trade_price AS :price,
+		trade_amount AS :amount,
+		tier_to_buy AS :tier,
+		billing_cycle AS :cycle,
+		cycle_count AS :cycle_count,
+		extra_days AS :extra_days,
+		category AS :usage_type,
+		payment_method AS :payment_method,
+		created_utc AS :created_at,
+		confirmed_utc AS :confirmed_at,
+		confirmed_utc IS NOT NULL AS :is_confirmed
+	FROM %s.ftc_trade
+	WHERE trade_no = ?
+	LIMIT 1
+	FOR UPDATE`, b.MemberDB())
+}
+
+// Statement to update a subscription order after received notification from payment provider.
+func (b Builder) ConfirmOrder() string {
+	return fmt.Sprintf(`
+	UPDATE %s.ftc_trade
+	SET confirmed_utc = :confirmed_at,
+		start_date = :start_date,
+		end_date = :end_date
+	WHERE trade_no = :id
+	LIMIT 1`, b.MemberDB())
+}
+
 func (b Builder) BalanceSource() string {
 	return fmt.Sprintf(`
 	SELECT s.trade_no AS orderId,
@@ -41,17 +124,17 @@ func (b Builder) SetLastUpgradeID() string {
 func (b Builder) InsertUpgrade() string {
 	return fmt.Sprintf(`
 	INSERT INTO %s.upgrade
-	SET id = ?,
-		order_id = ?,
-		balance = ?,
-		source_id = ?,
+	SET id = :up_id,
+		order_id = :order_id,
+		balance = :balance,
+		source_id = :source_id,
 		created_utc = UTC_TIMESTAMP(),
-		member_id = ?,
-		cycle = ?,
-		expire_date = ?,
-		ftc_id = ?,
-		wx_union_id = ?,
-		product_tier = ?`,
+		member_id = :member_id,
+		ftc_id = :ftc_id,
+		wx_union_id = :union_id,
+		product_tier = :tier
+		cycle = :cycle,
+		expire_date = :expire_date`,
 		b.MemberDB(),
 	)
 }
@@ -74,89 +157,6 @@ func (b Builder) ConfirmUpgrade() string {
 	UPDATE %s.upgrade
 	SET confirmed_utc = UTC_TIMESTAMP()
 	WHERE id = ?`, b.MemberDB())
-}
-
-// Statement to insert a subscription order.
-func (b Builder) InsertSubs() string {
-	return fmt.Sprintf(`
-	INSERT INTO %s.ftc_trade
-	SET trade_no = ?,
-		user_id = ?,
-		ftc_user_id = ?,
-		wx_union_id = ?,
-		trade_price = ?,
-		trade_amount = ?,
-		tier_to_buy = ?,
-		billing_cycle = ?,
-		cycle_count = ?,
-		extra_days = ?,
-		category = ?,
-		payment_method = ?,
-		wx_app_id = ?,
-		created_utc = UTC_TIMESTAMP(),
-		client_type = ?,
-		client_version = ?,
-		user_ip_bin = INET6_ATON(?),
-		user_agent = ?`, b.MemberDB())
-}
-
-// Statement to update a subscription order after received notification from payment provider.
-func (b Builder) ConfirmSubs() string {
-	return fmt.Sprintf(`
-	UPDATE %s.ftc_trade
-	SET confirmed_utc = ?,
-		start_date = ?,
-		end_date = ?
-	WHERE trade_no = ?
-	LIMIT 1`, b.MemberDB())
-}
-
-// SelectSubsPrice retrieves an order's price when payment
-// provider send confirmation notice.
-func (b Builder) SelectSubsPrice() string {
-	return fmt.Sprintf(`
-	SELECT trade_price AS listPrice,
-		trade_amount AS netPrice,
-		confirmed_utc IS NOT NULL AS isConfirmed
-	FROM %s.ftc_trade
-	WHERE trade_no = ?
-	LIMIT 1`, b.MemberDB())
-}
-
-func (b Builder) FtcUserOrderBilling() string {
-	return fmt.Sprintf(`
-	SELECT trade_price AS listPrice,
-		trade_amount AS netPrice,
-		confirmed_utc IS NOT NULL AS isConfirmed
-	FROM %s.ftc_trade
-	WHERE trade_no = ?
-		AND ftc_user_id = ?
-	LIMIT 1`, b.MemberDB())
-}
-
-// SelectSubsLock select an order upon webhook received
-// notification.
-func (b Builder) SelectSubsLock() string {
-	return fmt.Sprintf(`
-	SELECT trade_no AS orderId,
-		user_id AS userId,
-		ftc_user_id AS ftcUserId,
-		wx_union_id AS unionId,
-		trade_price AS listPrice,
-		trade_amount AS amount,
-		tier_to_buy AS tier,
-		billing_cycle AS Cycle,
-		cycle_count AS cycleCount,
-		extra_days AS extraDays,
-		category AS usageType,
-		payment_method AS paymentMethod,
-		created_utc AS createdAt,
-		confirmed_utc AS confirmedAt,
-		confirmed_utc IS NOT NULL AS confirmed
-	FROM %s.ftc_trade
-	WHERE trade_no = ?
-	LIMIT 1
-	FOR UPDATE`, b.MemberDB())
 }
 
 func (b Builder) ConfirmationResult() string {
