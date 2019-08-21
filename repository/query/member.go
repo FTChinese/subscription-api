e6e96a405041b84cf48f49d9522ef48cf625acc8
weiguo.ni <neefrankie@gmail.com>
2019-08-21 09:27:39 +0800

    refactor: modify for sqlx

--- repository/query/member.go
+++ repository/query/member.go
@@ -2,71 +2,92 @@ package query
 
 import "fmt"
 
-func (b Builder) SelectMemberLock() string {
+type MemberCol string
+
+const (
+	MemberColCompoundID MemberCol = "vip_id"
+	MemberColUnionID              = "vip_id_alias"
+)
+
+// SelectMemberLock builds statement to select a membership
+// in a transaction. The where clause might be `vip_id`
+// or `vip_id_alias` depending on user's current login method
+// and account linking status.
+// If user logged-in with ftc-only account, retrieve membership
+// by `vip_id` column;
+// If user logged-in with wechat-only account, retrieve it
+// by `vip_id_alias` column, which should be wechat's
+// union id;
+// If user account is linked, use `vip_id` as ftc id should
+// be enough.
+func (b Builder) SelectMemberLock(col MemberCol) string {
+	// 10 -> standard
+	// 100 -> premium
 	return fmt.Sprintf(`
-	SELECT id, 
-		vip_id AS compoundId,
-		NULLIF(vip_id, vip_id_alias) AS ftcId,
-		vip_id_alias AS unionId,
-		IF(member_tier, member_tier, CASE vip_type
-			WHEN 10 THEN 'standard'
-			WHEN 100 THEN 'premium'
-			ELSE member_tier
-		END) AS tier,
+	SELECT id AS member_id, 
+		vip_id AS compound_id,
+		NULLIF(vip_id, vip_id_alias) AS ftc_id,
+		vip_id_alias AS union_id,
+		vip_type,
+		expire_time,
+		member_tier AS tier,
 		billing_cycle AS cycle,
-		CASE
-			WHEN expire_date IS NOT NULL THEN expire_date
-			WHEN expire_time > 0 THEN DATE(FROM_UNIXTIME(expire_time))
-			ELSE NULL
-		END AS expireDate,
-		payment_method AS paymentMethod,
-		stripe_subscription_id AS stripeSubId,
-		auto_renewal AS autoRenewal,
-		sub_status AS subStatus
+		expire_date,
+		payment_method,
+		stripe_subscription_id AS stripe_sub_id,
+		auto_renewal,
+		sub_status
 	FROM %s.ftc_vip
-	WHERE vip_id = ? 
-		OR vip_id_alias = ?
+	WHERE %s = ?
 	LIMIT 1
-	FOR UPDATE`, b.MemberDB())
+	FOR UPDATE`, b.MemberDB(), string(col))
 }
 
 func (b Builder) InsertMember() string {
 	return fmt.Sprintf(`
 	INSERT INTO %s.ftc_vip
-	SET id = ?,
-		vip_id = ?,
-		vip_id_alias = ?,
-		vip_type = ?,
-		expire_time = ?,
-		ftc_user_id = ?,
-		wx_union_id = ?,
-		member_tier = ?,
-		billing_cycle = ?,
-		expire_date = ?,
-		payment_method = ?,
-		stripe_subscription_id = ?,
-		stripe_plan_id = ?,
-		auto_renewal = ?,
-		sub_status = ?`, b.MemberDB())
+	SET id = :member_id,
+		vip_id = :compound_id,
+		vip_id_alias = :union_id,
+		vip_type = :vip_type,
+		expire_time = :expire_time,
+		ftc_user_id = :ftc_id,
+		wx_union_id = :union_id,
+		member_tier = :tier,
+		billing_cycle = :cycle,
+		expire_date = :expire_date,
+		payment_method = :payment_method,
+		stripe_subscription_id = :stripe_sub_id,
+		stripe_plan_id = :stripe_plan_id,
+		auto_renewal = :auto_renewal,
+		sub_status = :sub_status`, b.MemberDB())
 }
 
 // UpdateMember update an existing member for stripe pay.
 // The only works fot FTC users. Wechat user are not allowed
 // to use stripe since those users are mostly located in China.
-func (b Builder) UpdateMember() string {
+func (b Builder) UpdateMember(whereCol MemberCol) string {
 	return fmt.Sprintf(`
 	UPDATE %s.ftc_vip
-	SET id = IFNULL(id, ?),
-		vip_type = ?,
-		expire_time = ?,
-		member_tier = ?,
-		billing_cycle = ?,
-		expire_date = ?,
-		payment_method = ?,
-		stripe_subscription_id = ?,
-		stripe_plan_id = ?,
-		auto_renewal = ?,
-		sub_status = ?
-	WHERE vip_id IN (?, ?)
-	LIMIT 1`, b.MemberDB())
+	SET id = :member_id,
+		vip_type = :vip_type,
+		expire_time = :expire_time,
+		member_tier = :tier,
+		billing_cycle = :cycle,
+		expire_date = :expire_date,
+		payment_method = :payment_method,
+		stripe_subscription_id = :stripe_sub_id,
+		stripe_plan_id = :stripe_plan_id,
+		auto_renewal = :auto_renewal,
+		sub_status = :sub_status
+	WHERE %s = :coumpound_id
+	LIMIT 1`, b.MemberDB(), string(whereCol))
+}
+
+func (b Builder) AddMemberID(whereCol MemberCol) string {
+	return fmt.Sprintf(`
+	UPDATE %s.ftc_vip
+	SET id = IF(id IS NULL, :id, id)
+	WHERE %s = :compound_id
+	LIMIT 1`, b.MemberDB(), string(whereCol))
 }
