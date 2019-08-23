c3751e8625f54903f9e0eee2dd62be4fbea02966
weiguo.ni <neefrankie@gmail.com>
2019-08-23 20:47:43 +0800

    refactor: save confirmation result

---
+++ repository/confirmation.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"gitlab.com/ftchinese/subscription-api/models/paywall"
+)
+
+func (env Env) SaveConfirmationResult(r *paywall.ConfirmationResult) error {
+	_, err := env.db.Exec(env.query.ConfirmationResult(),
+		r.OrderID,
+		r.Succeeded,
+		r.Failed)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
