d01217ace843bb336d7dbbe6beeb4d83723e9796
weiguo.ni <neefrankie@gmail.com>
2019-08-23 20:47:22 +0800

    refactor: save client app info into separate table

---
+++ repository/client.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"gitlab.com/ftchinese/subscription-api/models/util"
+)
+
+func (env Env) SaveOrderClient(orderID string, app util.ClientApp) error {
+	data := struct {
+		OrderID string `db:"order_id"`
+		util.ClientApp
+	}{
+		orderID,
+		app,
+	}
+
+	_, err := env.db.NamedExec(
+		env.query.InsertClientApp(),
+		data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
