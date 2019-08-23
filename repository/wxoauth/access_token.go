a9deb7b752282b7cc241913307627c162d0f26f2
weiguo.ni <neefrankie@gmail.com>
2019-08-23 20:29:10 +0800

    refactor: a separate package for wx login

---
+++ repository/wxoauth/access_token.go
@@ -0,0 +1,81 @@
+package wxoauth
+
+import (
+	"gitlab.com/ftchinese/subscription-api/models/util"
+	"gitlab.com/ftchinese/subscription-api/models/wxlogin"
+)
+
+// SaveWxAccess saves the access token related data after acquired from wechat api.
+func (env Env) SaveWxAccess(appID string, acc wxlogin.OAuthAccess, c util.ClientApp) error {
+
+	_, err := env.db.Exec(stmtInsertAccess,
+		acc.SessionID,
+		appID,
+		acc.AccessToken,
+		acc.ExpiresIn,
+		acc.RefreshToken,
+		acc.OpenID,
+		acc.Scope,
+		acc.UnionID,
+		c.ClientType,
+		c.Version,
+		c.UserIP,
+		c.UserAgent,
+		acc.CreatedAt,
+		acc.UpdatedAt,
+	)
+
+	if err != nil {
+		log.WithField("trace", "Env.SaveAccess").Error(err)
+		return err
+	}
+
+	return nil
+}
+
+// LoadWxAccess retrieves previously saved access token by open id.
+// Is it possible wechat generate different openID under the same app?
+// Or is it possible wechat generate same openID for different app?
+// What if iOS and Android used the same Wechat app? Will they use the same access token or different one?
+// Open ID is always the same the under the same Wechat app.
+func (env Env) LoadWxAccess(appID, sessionID string) (wxlogin.OAuthAccess, error) {
+
+	var acc wxlogin.OAuthAccess
+	err := env.db.QueryRow(stmtSelectAccess,
+		sessionID,
+		appID,
+	).Scan(
+		&acc.SessionID,
+		&acc.AccessToken,
+		&acc.ExpiresIn,
+		&acc.RefreshToken,
+		&acc.OpenID,
+		&acc.Scope,
+		&acc.UnionID,
+		&acc.CreatedAt,
+		&acc.UpdatedAt,
+	)
+
+	if err != nil {
+		log.WithField("trace", "Env.LoadAccess").Error(err)
+		return acc, err
+	}
+
+	return acc, nil
+}
+
+// UpdateWxAccess saves refreshed access token.
+func (env Env) UpdateWxAccess(sessionID, accessToken string) error {
+
+	_, err := env.db.Exec(stmtUpdateAccess,
+		accessToken,
+		sessionID,
+	)
+
+	if err != nil {
+		log.WithField("trace", "Env.UpdateAccess").Error(err)
+		return err
+	}
+
+	return nil
+}
