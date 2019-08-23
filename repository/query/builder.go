92688099305ebf041d6c96792a6894fc81558d53
weiguo.ni <neefrankie@gmail.com>
2019-08-23 20:43:40 +0800

    feat: extract the select part of membership

--- repository/query/builder.go
+++ repository/query/builder.go
@@ -15,3 +15,20 @@ func (b Builder) MemberDB() string {
 func NewBuilder(sandbox bool) Builder {
 	return Builder{sandbox: sandbox}
 }
+
+const (
+	stmtSelectMember = `
+	SELECT id AS member_id, 
+		vip_id AS compound_id,
+		NULLIF(vip_id, vip_id_alias) AS ftc_id,
+		vip_id_alias AS union_id,
+		vip_type,
+		expire_time,
+		member_tier AS tier,
+		billing_cycle AS cycle,
+		expire_date,
+		payment_method,
+		stripe_subscription_id AS stripe_sub_id,
+		auto_renewal,
+		sub_status`
+)
