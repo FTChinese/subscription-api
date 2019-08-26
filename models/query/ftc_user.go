eb10f9fd18f877a03cc61bc71403e64a82d3118a
weiguo.ni <neefrankie@gmail.com>
2019-08-26 12:07:55 +0800

    refactor: use sqlx to add stripe customer id

--- models/query/ftc_user.go
+++ models/query/ftc_user.go
@@ -16,8 +16,8 @@ const (
 
 	SaveStripeID = `
 	UPDATE cmstmp01.userinfo
-	SET stripe_customer_id = ?
-	WHERE user_id = ?
+	SET stripe_customer_id = :stripe_id
+	WHERE user_id = :ftc_id
 	LIMIT 1`
 
 	SelectStripeCustomer = `
