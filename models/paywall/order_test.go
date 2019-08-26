dcd3285be24898067d14ab256d745808810669d5
weiguo.ni <neefrankie@gmail.com>
2019-08-26 15:14:39 +0800

    refactor: test order amount in cent

---
+++ models/paywall/order_test.go
@@ -0,0 +1,19 @@
+package paywall
+
+import "testing"
+
+func TestOrder_AmountInCent(t *testing.T) {
+	order := Order{
+		Amount: 258.00,
+	}
+
+	payResult := PaymentResult{
+		Amount: 25800,
+	}
+
+	if order.AmountInCent() == payResult.Amount {
+		t.Logf("Equal")
+	} else {
+		t.Error("Not Equal")
+	}
+}
