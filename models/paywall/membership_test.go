aa1dfdc7da8a5043734f9dd094df2169d0bfef95
weiguo.ni <neefrankie@gmail.com>
2019-08-26 17:25:46 +0800

    test: various permissions

--- models/paywall/membership_test.go
+++ models/paywall/membership_test.go
@@ -13,13 +13,363 @@ func TestGenerateSnapshotID(t *testing.T) {
 	t.Log(GenerateSnapshotID())
 }
 
-func TestMembership_PermitStripeCreate(t *testing.T) {
+func TestMembership_IsExpired(t *testing.T) {
 	profile := NewProfile()
-	m := profile.Membership(reader.AccountKindFtc, enum.PayMethodStripe, false)
-	m.Status = SubStatusIncomplete
 
 	type fields struct {
-		member Membership
+		ID            null.String
+		AccountID     reader.AccountID
+		LegacyTier    null.Int
+		LegacyExpire  null.Int
+		Coordinate    Coordinate
+		ExpireDate    chrono.Date
+		PaymentMethod enum.PayMethod
+		StripeSubID   null.String
+		StripePlanID  null.String
+		AutoRenewal   bool
+		Status        SubStatus
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "Empty Member",
+			fields: fields{},
+			want:   true,
+		},
+		{
+			name: "Expired",
+			fields: fields{
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
+			},
+			want: true,
+		},
+		{
+			name: "Expired but auto renewal",
+			fields: fields{
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate:  chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
+				AutoRenewal: true,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Membership{
+				ID:            tt.fields.ID,
+				AccountID:     tt.fields.AccountID,
+				LegacyTier:    tt.fields.LegacyTier,
+				LegacyExpire:  tt.fields.LegacyExpire,
+				Coordinate:    tt.fields.Coordinate,
+				ExpireDate:    tt.fields.ExpireDate,
+				PaymentMethod: tt.fields.PaymentMethod,
+				StripeSubID:   tt.fields.StripeSubID,
+				StripePlanID:  tt.fields.StripePlanID,
+				AutoRenewal:   tt.fields.AutoRenewal,
+				Status:        tt.fields.Status,
+			}
+			if got := m.IsExpired(); got != tt.want {
+				t.Errorf("IsExpired() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMembership_inRenewalPeriod(t *testing.T) {
+	type fields struct {
+		ID            null.String
+		AccountID     reader.AccountID
+		LegacyTier    null.Int
+		LegacyExpire  null.Int
+		Coordinate    Coordinate
+		ExpireDate    chrono.Date
+		PaymentMethod enum.PayMethod
+		StripeSubID   null.String
+		StripePlanID  null.String
+		AutoRenewal   bool
+		Status        SubStatus
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "Expired Member",
+			fields: fields{},
+			want:   false,
+		},
+		{
+			name: "Exceed",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(3, 1, 0)),
+			},
+			want: false,
+		},
+		{
+			name: "Expired",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
+			},
+			want: false,
+		},
+		{
+			name: "Expires on Today",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now()),
+			},
+			want: true,
+		},
+		{
+			name: "Expires three years later",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(3, 0, 0)),
+			},
+			want: true,
+		},
+		{
+			name: "Renewal",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Membership{
+				ID:            tt.fields.ID,
+				AccountID:     tt.fields.AccountID,
+				LegacyTier:    tt.fields.LegacyTier,
+				LegacyExpire:  tt.fields.LegacyExpire,
+				Coordinate:    tt.fields.Coordinate,
+				ExpireDate:    tt.fields.ExpireDate,
+				PaymentMethod: tt.fields.PaymentMethod,
+				StripeSubID:   tt.fields.StripeSubID,
+				StripePlanID:  tt.fields.StripePlanID,
+				AutoRenewal:   tt.fields.AutoRenewal,
+				Status:        tt.fields.Status,
+			}
+			if got := m.inRenewalPeriod(); got != tt.want {
+				t.Errorf("inRenewalPeriod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMembership_PermitRenewal(t *testing.T) {
+	type fields struct {
+		ID            null.String
+		AccountID     reader.AccountID
+		LegacyTier    null.Int
+		LegacyExpire  null.Int
+		Coordinate    Coordinate
+		ExpireDate    chrono.Date
+		PaymentMethod enum.PayMethod
+		StripeSubID   null.String
+		StripePlanID  null.String
+		AutoRenewal   bool
+		Status        SubStatus
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name:   "Empty member",
+			fields: fields{},
+			want:   false,
+		},
+		{
+			name: "Auto renewal",
+			fields: fields{
+				AutoRenewal: true,
+			},
+			want: false,
+		},
+		{
+			name: "Not Ali or Wx",
+			fields: fields{
+				PaymentMethod: enum.PayMethodStripe,
+			},
+			want: false,
+		},
+		{
+			name: "Beyond renewal",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(3, 1, 0)),
+			},
+			want: false,
+		},
+		{
+			name: "Allow Renewal",
+			fields: fields{
+				PaymentMethod: enum.PayMethodAli,
+				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Membership{
+				ID:            tt.fields.ID,
+				AccountID:     tt.fields.AccountID,
+				LegacyTier:    tt.fields.LegacyTier,
+				LegacyExpire:  tt.fields.LegacyExpire,
+				Coordinate:    tt.fields.Coordinate,
+				ExpireDate:    tt.fields.ExpireDate,
+				PaymentMethod: tt.fields.PaymentMethod,
+				StripeSubID:   tt.fields.StripeSubID,
+				StripePlanID:  tt.fields.StripePlanID,
+				AutoRenewal:   tt.fields.AutoRenewal,
+				Status:        tt.fields.Status,
+			}
+			if got := m.PermitRenewal(); got != tt.want {
+				t.Errorf("PermitRenewal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMembership_SubsKind(t *testing.T) {
+
+	profile := NewProfile()
+
+	type fields struct {
+		ID            null.String
+		AccountID     reader.AccountID
+		LegacyTier    null.Int
+		LegacyExpire  null.Int
+		Coordinate    Coordinate
+		ExpireDate    chrono.Date
+		PaymentMethod enum.PayMethod
+		StripeSubID   null.String
+		StripePlanID  null.String
+		AutoRenewal   bool
+		Status        SubStatus
+	}
+	type args struct {
+		p Plan
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    SubsKind
+		wantErr bool
+	}{
+		{
+			name:   "Empty member",
+			fields: fields{},
+			args: args{
+				p: standardYearlyPlan,
+			},
+			want:    SubsKindCreate,
+			wantErr: false,
+		},
+		{
+			name: "Invalid Stripe Subscription",
+			fields: fields{
+				Status: SubStatusIncompleteExpired,
+			},
+			args:    args{p: standardYearlyPlan},
+			want:    SubsKindCreate,
+			wantErr: false,
+		},
+		{
+			name: "Expired subscription",
+			fields: fields{
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(1, -1, 0)),
+			},
+			args:    args{p: standardYearlyPlan},
+			want:    SubsKindCreate,
+			wantErr: false,
+		},
+		{
+			name: "Renewal",
+			fields: fields{
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+			},
+			args:    args{p: standardYearlyPlan},
+			want:    SubsKindRenew,
+			wantErr: false,
+		},
+		{
+			name: "Upgrading",
+			fields: fields{
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+			},
+			args:    args{p: premiumYearlyPlan},
+			want:    SubsKindUpgrade,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Membership{
+				ID:            tt.fields.ID,
+				AccountID:     tt.fields.AccountID,
+				LegacyTier:    tt.fields.LegacyTier,
+				LegacyExpire:  tt.fields.LegacyExpire,
+				Coordinate:    tt.fields.Coordinate,
+				ExpireDate:    tt.fields.ExpireDate,
+				PaymentMethod: tt.fields.PaymentMethod,
+				StripeSubID:   tt.fields.StripeSubID,
+				StripePlanID:  tt.fields.StripePlanID,
+				AutoRenewal:   tt.fields.AutoRenewal,
+				Status:        tt.fields.Status,
+			}
+			got, err := m.SubsKind(tt.args.p)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SubsKind() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SubsKind() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMembership_PermitStripeCreate(t *testing.T) {
+	profile := NewProfile()
+
+	type fields struct {
+		ID            null.String
+		AccountID     reader.AccountID
+		LegacyTier    null.Int
+		LegacyExpire  null.Int
+		Coordinate    Coordinate
+		ExpireDate    chrono.Date
+		PaymentMethod enum.PayMethod
+		StripeSubID   null.String
+		StripePlanID  null.String
+		AutoRenewal   bool
+		Status        SubStatus
 	}
 	tests := []struct {
 		name    string
@@ -27,150 +377,98 @@ func TestMembership_PermitStripeCreate(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Empty member",
-			fields: fields{
-				member: Membership{},
-			},
+			name:    "Empty Member",
+			fields:  fields{},
 			wantErr: false,
 		},
+
 		{
 			name: "Expired Alipay",
 			fields: fields{
-				member: profile.Membership(reader.AccountKindFtc, enum.PayMethodAli, true),
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
 			},
 			wantErr: false,
 		},
 		{
-			name: "Not expired wxpay",
+			name: "Not Expired Wx",
 			fields: fields{
-				member: profile.Membership(reader.AccountKindFtc, enum.PayMethodWx, false),
+				AccountID: profile.AccountID(reader.AccountKindFtc),
+				Coordinate: Coordinate{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+				ExpireDate: chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
 			},
 			wantErr: true,
 		},
 		{
-			name: "Active Stripe User",
+			name: "Valid Stripe",
 			fields: fields{
-				member: profile.Membership(reader.AccountKindFtc, enum.PayMethodStripe, false),
+				AccountID:     profile.AccountID(reader.AccountKindFtc),
+				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+				PaymentMethod: enum.PayMethodStripe,
+				AutoRenewal:   true,
+				Status:        SubStatusActive,
 			},
 			wantErr: true,
 		},
 		{
-			name: "Incomplete Stripe User",
+			name: "Stripe Incomplete Expired",
 			fields: fields{
-				member: m,
+				AccountID:     profile.AccountID(reader.AccountKindFtc),
+				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+				PaymentMethod: enum.PayMethodStripe,
+				AutoRenewal:   true,
+				Status:        SubStatusIncompleteExpired,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Expired stripe but auto renewal",
+			fields: fields{
+				AccountID:     profile.AccountID(reader.AccountKindFtc),
+				ExpireDate:    chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
+				PaymentMethod: enum.PayMethodStripe,
+				AutoRenewal:   true,
+				Status:        SubStatusActive,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Expired stripe auto renewal but inactive",
+			fields: fields{
+				AccountID:     profile.AccountID(reader.AccountKindFtc),
+				ExpireDate:    chrono.DateFrom(time.Now().AddDate(0, -1, 0)),
+				PaymentMethod: enum.PayMethodStripe,
+				AutoRenewal:   true,
+				Status:        SubStatusIncompleteExpired,
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := tt.fields.member
-
+			m := Membership{
+				ID:            tt.fields.ID,
+				AccountID:     tt.fields.AccountID,
+				LegacyTier:    tt.fields.LegacyTier,
+				LegacyExpire:  tt.fields.LegacyExpire,
+				Coordinate:    tt.fields.Coordinate,
+				ExpireDate:    tt.fields.ExpireDate,
+				PaymentMethod: tt.fields.PaymentMethod,
+				StripeSubID:   tt.fields.StripeSubID,
+				StripePlanID:  tt.fields.StripePlanID,
+				AutoRenewal:   tt.fields.AutoRenewal,
+				Status:        tt.fields.Status,
+			}
 			if err := m.PermitStripeCreate(); (err != nil) != tt.wantErr {
-				t.Errorf("Membership.PermitStripeCreate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("PermitStripeCreate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
-}
-
-func TestMembership_PermitStripeUpgrade(t *testing.T) {
-
-	profile := NewProfile()
-	incompleteM := profile.Membership(reader.AccountKindFtc, enum.PayMethodStripe, false)
-	incompleteM.Status = SubStatusIncomplete
-
-	p := StripeSubParams{
-		Coordinate: Coordinate{
-			Tier:  enum.TierPremium,
-			Cycle: enum.CycleYear,
-		},
-	}
-
-	type fields struct {
-		member Membership
-	}
-	type args struct {
-		p StripeSubParams
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
-	}{
-		{
-			name: "Empty member",
-			fields: fields{
-				member: Membership{},
-			},
-			args: args{
-				p: p,
-			},
-			want: false,
-		},
-		{
-			name: "Non stripe member",
-			fields: fields{
-				member: profile.Membership(reader.AccountKindFtc, enum.PayMethodAli, false),
-			},
-			args: args{
-				p: p,
-			},
-			want: false,
-		},
-		{
-			name: "Not active stripe member",
-			fields: fields{
-				member: incompleteM,
-			},
-			args: args{
-				p: p,
-			},
-			want: false,
-		},
-		{
-			name: "Stripe active standard member",
-			fields: fields{
-				member: profile.Membership(reader.AccountKindFtc, enum.PayMethodStripe, false),
-			},
-			args: args{
-				p: p,
-			},
-			want: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := tt.fields.member
-
-			if got := m.PermitStripeUpgrade(tt.args.p); got != tt.want {
-				t.Errorf("Membership.PermitStripeUpgrade() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestMembership_IsZero(t *testing.T) {
-	m := Membership{
-		ID: null.String{},
-		Coordinate: Coordinate{
-			Tier:  enum.TierStandard,
-			Cycle: enum.CycleYear,
-		},
-		ExpireDate:    chrono.DateFrom(time.Date(2020, time.August, 16, 0, 0, 0, 0, time.UTC)),
-		PaymentMethod: 0,
-		StripeSubID:   null.String{},
-		StripePlanID:  null.String{},
-		AutoRenewal:   false,
-		Status:        0,
-	}
-
-	t.Logf("Is zeror: %t", m.IsZero())
-	t.Logf("Is wx or ali: %t", m.IsAliOrWxPay())
-
-	subKind, err := m.SubsKind(standardYearlyPlan)
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("Sub kind: %+v", subKind)
 }
