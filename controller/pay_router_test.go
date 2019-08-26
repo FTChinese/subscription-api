bc0cfa7c2c8bbff85523d48de0be6ea05a52c760
weiguo.ni <neefrankie@gmail.com>
2019-08-26 14:11:44 +0800

    test

---
+++ controller/pay_router_test.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"gitlab.com/ftchinese/subscription-api/models/reader"
+	"gitlab.com/ftchinese/subscription-api/repository"
+	"gitlab.com/ftchinese/subscription-api/test"
+	"testing"
+)
+
+func TestPayRouter_sendConfirmationEmail(t *testing.T) {
+	router := PayRouter{
+		env: repository.New(
+			test.DB,
+			test.Cache,
+			repository.BuildConfig{},
+		),
+		postman: test.Postman,
+	}
+
+	testRepo := test.NewRepo()
+
+	store := test.NewSubStore(test.MyProfile, reader.AccountKindFtc)
+
+	order := store.MustConfirm(store.MustCreate(test.YearlyStandardLive).ID)
+	if err := router.sendConfirmationEmail(order); err != nil {
+		t.Error(err)
+	}
+
+	order = store.MustConfirm(store.MustCreate(test.YearlyStandardLive).ID)
+	if err := router.sendConfirmationEmail(order); err != nil {
+		t.Error(err)
+	}
+
+	order = store.MustConfirm(store.MustCreate(test.YearlyPremiumLive).ID)
+	testRepo.SaveUpgradePlan(store.UpgradePlan)
+	testRepo.SaveBalanceSources(store.UpgradePlan.Data)
+
+	if err := router.sendConfirmationEmail(order); err != nil {
+		t.Error(err)
+	}
+}
