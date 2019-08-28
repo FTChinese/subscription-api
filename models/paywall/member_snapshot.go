8c810caad27f424b2b75032bbbaf8075ca723b40
weiguo.ni <neefrankie@gmail.com>
2019-08-28 11:21:47 +0800

    refactor: relocate GenerateSnashotID

--- models/paywall/member_snapshot.go
+++ models/paywall/member_snapshot.go
@@ -1,6 +1,13 @@
 package paywall
 
-import "github.com/FTChinese/go-rest/chrono"
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"gitlab.com/ftchinese/subscription-api/models/rand"
+)
+
+func GenerateSnapshotID() string {
+	return "snp_" + rand.String(12)
+}
 
 // MemberSnapshot saves a membership's status prior to
 // placing an order.
