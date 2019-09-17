dbcfb5ccbd4d527f1c2f7bf043b5c7235df3195b
weiguo.ni <neefrankie@gmail.com>
2019-09-17 17:52:15 +0800

    test

---
+++ controller/confirm_order_test.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/guregu/null"
+	"gitlab.com/ftchinese/subscription-api/models/paywall"
+	"gitlab.com/ftchinese/subscription-api/models/reader"
+	"gitlab.com/ftchinese/subscription-api/repository"
+	"gitlab.com/ftchinese/subscription-api/test"
+	"testing"
+	"time"
+)
+
+func mockOrderAndConfirm(id reader.AccountID) paywall.Order {
+	router := PayRouter{
+		env: repository.New(test.DB, test.Cache, repository.BuildConfig{
+			Sandbox:    false,
+			Production: false,
+		}),
+		postman: test.Postman,
+	}
+
+	order, err := router.createOrder(
+		id,
+		test.YearlyStandardLive,
+		enum.PayMethodWx,
+		test.RandomClientApp(),
+		null.StringFrom(test.WxPayApp.AppID),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	paymentResult := paywall.PaymentResult{
+		Amount:      int64(test.YearlyStandardLive.NetPrice * 100),
+		OrderID:     order.ID,
+		ConfirmedAt: time.Now(),
+	}
+
+	confirmedOrder, resultErr := router.confirmPayment(paymentResult)
+	if resultErr != nil {
+		panic(err)
+	}
+
+	return confirmedOrder
+}
+
+func TestPayRouter_confirmPayment(t *testing.T) {
+	profile := test.NewProfile()
+	accountID := profile.AccountID(reader.AccountKindFtc)
+
+	orderToCreate := mockOrderAndConfirm(accountID)
+	t.Logf("Creation order: %+v", orderToCreate)
+
+	orderToRenew := mockOrderAndConfirm(accountID)
+	t.Logf("Renewal order: %+v", orderToRenew)
+}
