181079fdab156025f3ec5a4264b5740b63de4f50
weiguo.ni <neefrankie@gmail.com>
2019-11-29 18:01:39 +0800

    feat: retrieve and create membership for apple user

--- repository/iaprepo/membership.go
+++ repository/iaprepo/membership.go
@@ -1,10 +1,31 @@
 package iaprepo
 
 import (
+	"database/sql"
+	"fmt"
 	"gitlab.com/ftchinese/subscription-api/models/paywall"
 	"gitlab.com/ftchinese/subscription-api/models/reader"
 )
 
-func (env IAPEnv) RetrieveMember(id reader.MemberID) (paywall.Membership, error) {
+func (env IAPEnv) RetrieveMembership(id reader.MemberID) (paywall.Membership, error) {
+	var m paywall.Membership
 
+	err := env.db.Get(
+		&m,
+		fmt.Sprintf(selectMember, id.MemberColumn()),
+		id.CompoundID,
+	)
+
+	if err != nil && err != sql.ErrNoRows {
+		logger.WithField("trace", "IAPEnv.RetrieveMember").Error(err)
+		return m, err
+	}
+
+	m.Normalize()
+
+	return m, nil
+}
+
+func CreateMembership() error {
+	return nil
 }
