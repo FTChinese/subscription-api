50155019bda444f740da0763b9fc1b070e7b9f9f
neefrankie <neefrankie@gmail.com>
2019-11-30 22:44:29 +0800

    refactor: rename QuoRem func and move it to util pkg

---
+++ models/util/division.go
@@ -0,0 +1,13 @@
+package util
+
+// Division returns the integer quotient and remainder of x/y
+func Division(x, y float64) (int64, float64) {
+	var q int64
+
+	for x > y {
+		q = q + 1
+		x = x - y
+	}
+
+	return q, x
+}
