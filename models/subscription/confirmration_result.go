293ac9506d98d954b82df88c79ec1a6dcca51217
neefrankie <neefrankie@gmail.com>
2019-12-01 12:47:45 +0800

    refactor: moved from order.go

---
+++ models/subscription/confirmration_result.go
@@ -0,0 +1,31 @@
+package subscription
+
+import "github.com/guregu/null"
+
+// ConfirmationResult logs the result of confirmation.
+type ConfirmationResult struct {
+	OrderID   string
+	Succeeded bool
+	Failed    null.String
+	Retry     bool
+}
+
+func (r ConfirmationResult) Error() string {
+	return r.Failed.String
+}
+
+// NewConfirmationSucceeded createa a new instance of ConfirmationResult for success.
+func NewConfirmationSucceeded(orderID string) *ConfirmationResult {
+	return &ConfirmationResult{
+		OrderID:   orderID,
+		Succeeded: true,
+	}
+}
+
+func NewConfirmationFailed(orderID string, reason error, retry bool) *ConfirmationResult {
+	return &ConfirmationResult{
+		OrderID: orderID,
+		Failed:  null.StringFrom(reason.Error()),
+		Retry:   retry,
+	}
+}
