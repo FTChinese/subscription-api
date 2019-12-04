9e68223a266ffc529bf41aa853669d91ea1b62b2
weiguo.ni <neefrankie@gmail.com>
2019-12-04 17:43:13 +0800

    refactor: iap router

--- controller/iap_router.go
+++ controller/iap_router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/FTChinese/go-rest/view"
 	"gitlab.com/ftchinese/subscription-api/models/apple"
 	"gitlab.com/ftchinese/subscription-api/models/reader"
+	"gitlab.com/ftchinese/subscription-api/models/subscription"
 	"gitlab.com/ftchinese/subscription-api/repository/iaprepo"
 	"gitlab.com/ftchinese/subscription-api/repository/subrepo"
 	"net/http"
@@ -26,89 +27,7 @@ func NewIAPRouter(iapEnv iaprepo.IAPEnv, subEnv subrepo.SubEnv, p postoffice.Pos
 	}
 }
 
-type verificationResult struct {
-	resp            apple.VerificationResponseBody
-	err             error
-	invalidRequest  *view.Reason
-	invalidResponse bool
-}
-
-func (router IAPRouter) doVerification(req *http.Request) verificationResult {
-
-	log := logger.WithField("trace", "IAPRouter.doVerification")
-
-	var receiptReq apple.VerificationRequestBody
-	if err := gorest.ParseJSON(req.Body, &receiptReq); err != nil {
-		log.Error(err)
-
-		return verificationResult{
-			resp:            apple.VerificationResponseBody{},
-			err:             err,
-			invalidRequest:  nil,
-			invalidResponse: false,
-		}
-	}
-
-	// Validate input.
-	if r := receiptReq.Validate(); r != nil {
-		return verificationResult{
-			resp:            apple.VerificationResponseBody{},
-			err:             nil,
-			invalidRequest:  r,
-			invalidResponse: false,
-		}
-	}
-
-	// Verify
-	resp, err := router.iapEnv.VerifyReceipt(receiptReq)
-	if err != nil {
-		return verificationResult{
-			resp:            apple.VerificationResponseBody{},
-			err:             err,
-			invalidRequest:  nil,
-			invalidResponse: false,
-		}
-	}
-
-	log.Infof("Environment %s, is retryable %t, status %d", resp.Environment, resp.IsRetryable, resp.Status)
-
-	// If the response is not valid.
-	if !resp.Validate() {
-		log.Info("Verification response is not valid")
-
-		go func() {
-			_ = router.iapEnv.SaveVerificationFailure(
-				resp.FailureSchema(receiptReq.ReceiptData),
-			)
-		}()
-
-		return verificationResult{
-			resp:            apple.VerificationResponseBody{},
-			err:             nil,
-			invalidRequest:  nil,
-			invalidResponse: true,
-		}
-	}
-
-	return verificationResult{
-		resp:            resp,
-		err:             nil,
-		invalidRequest:  nil,
-		invalidResponse: false,
-	}
-}
-
-func (router IAPRouter) handleTransactions(ur apple.UnifiedReceipt, id reader.MemberID) apple.Subscription {
-
-	// Save verification session
-	transaction := ur.FindLatestTransaction()
-
-	// TODO: how to save verification session?
-	//go func() {
-	//	_ = router.iapEnv.SaveVerificationSession(
-	//		resp.SessionSchema(transaction),
-	//	)
-	//}()
+func (router IAPRouter) handleTransactions(ur apple.UnifiedReceipt, originalTransactionID string) {
 
 	// save latest receipt array
 	go func() {
@@ -129,68 +48,135 @@ func (router IAPRouter) handleTransactions(ur apple.UnifiedReceipt, id reader.Me
 	}()
 
 	// Save the receipt as a token for status polling.
-	receiptToken := ur.ReceiptToken(transaction.OriginalTransactionID)
+	receiptToken := ur.ReceiptToken(
+		originalTransactionID,
+	)
 	go func() {
 		_ = router.iapEnv.SaveReceiptToken(receiptToken)
 	}()
-
-	// Create a subscription.
-	// Here we do not have reader's ids
-	sub := ur.Subscription(id, transaction)
-	// TODO: CreateSubscription in main thread.
-	//go func() {
-	//	_ = router.iapEnv.CreateSubscription(sub)
-	//}()
-
-	return sub
 }
 
 // VerifyReceipt perform app store receipt verification
 // Input {"receipt-data": string}
 func (router IAPRouter) VerifyReceipt(w http.ResponseWriter, req *http.Request) {
-	result := router.doVerification(req)
+	log := logger.WithField("trace", "IAPRouter.VerifyReceipt")
 
-	switch {
-	case result.err != nil:
-		_ = view.Render(w, view.NewBadRequest(result.err.Error()))
+	// Parse input data.
+	var receiptReq apple.VerificationRequestBody
+	if err := gorest.ParseJSON(req.Body, &receiptReq); err != nil {
+		log.Error(err)
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
 		return
+	}
 
-	case result.invalidRequest != nil:
-		_ = view.Render(w, view.NewUnprocessable(result.invalidRequest))
+	// Validate input.
+	if r := receiptReq.Validate(); r != nil {
+		_ = view.Render(w, view.NewUnprocessable(r))
 		return
+	}
 
-	case result.invalidResponse:
+	// Verify
+	resp, err := router.iapEnv.VerifyReceipt(receiptReq)
+	if err != nil {
+		_ = view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	log.Infof("Environment %s, is retryable %t, status %d", resp.Environment, resp.IsRetryable, resp.Status)
+
+	// If the response is not valid.
+	if !resp.Validate() {
+		log.Info("Verification response is not valid")
+
+		go func() {
+			_ = router.iapEnv.SaveVerificationFailure(
+				resp.FailureSchema(receiptReq.ReceiptData),
+			)
+		}()
+
+		r := view.NewReason()
+		r.Field = "receipt-data"
+		r.Code = view.CodeInvalid
+		r.SetMessage("receipt data is not valid")
+		_ = view.Render(w, view.NewUnprocessable(r))
+		return
+	}
+
+	// Save verification session
+	transaction := resp.FindLatestTransaction()
+	go func() {
+		_ = router.iapEnv.SaveVerificationSession(
+			resp.SessionSchema(transaction),
+		)
+	}()
+
+	sub := resp.Subscription(transaction)
+	_ = router.iapEnv.CreateSubscription(sub)
+
+	// Check user's ids.
+	memberID, err := reader.NewMemberID(
+		req.Header.Get(ftcIDKey),
+		req.Header.Get(unionIDKey),
+	)
+
+	// If reader ids not found, it indicates this is not
+	// used to link membership.
+	if err != nil {
 		_ = view.Render(w, view.NewResponse().SetBody(map[string]bool{
-			"isValid": false,
+			"isValid": true,
 		}))
 		return
 	}
 
-	_ = router.handleTransactions(
-		result.resp.UnifiedReceipt,
-		reader.MemberID{},
-	)
+	// Start to link apple subscription to ftc membership.
+	err = router.iapEnv.Link(sub, memberID)
 
-	_ = view.Render(w, view.NewResponse().SetBody(map[string]bool{
-		"isValid": true,
-	}))
+	switch err {
+
+	case subscription.ErrValidFTCMember:
+		r := view.NewReason()
+		r.Field = "link"
+		r.Code = "target_valid"
+		r.SetMessage("Cannot linking iap subscription to your logged in account since your ftc account already has a valid membership.")
+		_ = view.Render(w, view.NewUnprocessable(r))
+		return
+
+	case nil:
+		_ = view.Render(w, view.NewNoContent())
+		return
+
+	default:
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
 }
 
 // WebHook receives app store server-to-server notification.
 func (router IAPRouter) WebHook(w http.ResponseWriter, req *http.Request) {
 	var wh apple.WebHook
 	if err := gorest.ParseJSON(req.Body, &wh); err != nil {
+		logger.WithField("trace", "IAPRouter.WebHook").Error(err)
+
 		_ = view.Render(w, view.NewBadRequest(""))
 		return
 	}
 
 	_ = router.iapEnv.SaveNotification(wh.Schema())
 
-	// Upsert each transaction
-	// Upsert each pending renewal
-	// Upsert verification token.
-	// Upsert apple subscription.
-	//sub := router.handleTransactions(wh.UnifiedReceipt, reader.MemberID{})
+	// Find the latest transaction and save transaction
+	// history.
+	transaction := wh.UnifiedReceipt.FindLatestTransaction()
+
+	router.handleTransactions(
+		wh.UnifiedReceipt,
+		transaction.OriginalTransactionID,
+	)
+
+	// Build apple's subscription and save it.
+	sub := wh.UnifiedReceipt.Subscription(transaction)
+	if err := router.iapEnv.CreateSubscription(sub); err != nil {
+		_ = view.Render(w, view.NewBadRequest(""))
+	}
 
 	// Retrieve apple subscription by original transaction id.
 	// together with membership.
@@ -199,4 +185,36 @@ func (router IAPRouter) WebHook(w http.ResponseWriter, req *http.Request) {
 	// continue to find the membership by linked id,
 	// and take a snapshot of membership, and then
 	// update it.
+
+	tx, err := router.iapEnv.BeginTx(sub.Environment)
+	if err != nil {
+		_ = view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	currMember, err := tx.RetrieveAppleMember(sub.OriginalTransactionID)
+	if err != nil {
+		_ = tx.Rollback()
+		_ = view.Render(w, view.NewBadRequest(""))
+		return
+	}
+
+	if currMember.IsZero() {
+		_ = tx.Commit()
+		_ = view.Render(w, view.NewResponse())
+		return
+	}
+
+	newMember := sub.NewMembership(currMember.MemberID)
+	newMember.ID = currMember.ID
+
+	if err := tx.UpdateMember(newMember); err != nil {
+		_ = tx.Rollback()
+		_ = view.Render(w, view.NewBadRequest(""))
+		return
+	}
+
+	_ = tx.Commit()
+
+	_ = view.Render(w, view.NewResponse())
 }
