75737234611639c3e079121dbcdd4de27bbb0c0e
weiguo.ni <neefrankie@gmail.com>
2019-12-05 17:28:55 +0800

    refactor: rename errors

--- models/subscription/errors.go
+++ models/subscription/errors.go
@@ -2,10 +2,8 @@ package subscription
 
 import "errors"
 
-var ErrValidFTCMember = errors.New("linking target has valid membership")
-
 var ErrLinkToMultipleFTC = errors.New("one apple subscription cannot be linked to multiple FTC account")
 
-var ErrLinkTargetAlreadyTaken = errors.New("target ftc account is already linked to another apple subscription")
+var ErrTargetLinkedToOtherIAP = errors.New("target ftc account is already linked to another apple subscription")
 
-var ErrLinkToExistingMember = errors.New("target ftc account already has a valid membership")
+var ErrHasValidNonIAPMember = errors.New("target ftc account already has a valid membership")
