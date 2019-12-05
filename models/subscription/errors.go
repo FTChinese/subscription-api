0d419c62647ff186ff86128a9d2b14b4b0a153ce
weiguo.ni <neefrankie@gmail.com>
2019-12-05 15:56:34 +0800

    feat: merge iap member to ftc member

--- models/subscription/errors.go
+++ models/subscription/errors.go
@@ -3,3 +3,9 @@ package subscription
 import "errors"
 
 var ErrValidFTCMember = errors.New("linking target has valid membership")
+
+var ErrLinkToMultipleFTC = errors.New("one apple subscription cannot be linked to multiple FTC account")
+
+var ErrLinkTargetAlreadyTaken = errors.New("target ftc account is already linked to another apple subscription")
+
+var ErrLinkToExistingMember = errors.New("target ftc account already has a valid membership")
