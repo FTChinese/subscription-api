7195d22b4b426231057c8e820e1c47f0520b6bcf
neefrankie <neefrankie@gmail.com>
2019-12-08 23:32:46 +0800

    refactor: remove all methods and functions to modify plans

--- models/plan/plan.go
+++ models/plan/plan.go
@@ -1,22 +1,16 @@
 package plan
 
 import (
-	"errors"
 	"fmt"
 	"github.com/FTChinese/go-rest/enum"
-	"github.com/guregu/null"
 	"github.com/stripe/stripe-go"
-	"gitlab.com/ftchinese/subscription-api/models/util"
-	"math"
 	"strings"
-	"time"
 )
 
 // BasePlan includes a product and a plan billing cycle to identify the plan to subscribe.
 type BasePlan struct {
-	Tier       enum.Tier  `json:"tier" db:"sub_tier"`
-	Cycle      enum.Cycle `json:"cycle" db:"sub_cycle"`
-	LegacyTier null.Int   `json:"-" db:"vip_type"`
+	Tier  enum.Tier  `json:"tier" db:"sub_tier"`
+	Cycle enum.Cycle `json:"cycle" db:"sub_cycle"`
 }
 
 func (p BasePlan) IsZero() bool {
@@ -33,16 +27,16 @@ func (p BasePlan) NamedKey() string {
 // The net price of a product or service is the actual price that customers pay for the product or service.
 type Plan struct {
 	BasePlan
-	ListPrice        float64 `json:"listPrice"`          // Deprecate
-	NetPrice         float64 `json:"netPrice"`           // Deprecate
-	Price            float64 `json:"price" db:"price"`   // Price of a plan, prior to discount.
-	Amount           float64 `json:"amount" db:"amount"` // Actually paid amount.
-	Duration                 // This should be removed.
-	Currency         string  `json:"currency" db:"currency"`
-	Title            string  `json:"description"`
-	stripeLivePlanID string  `json:"-"`
-	stripeTestPlanID string  `json:"-"`
-	AppleProductID   string  `json:"-"`
+	ListPrice float64 `json:"listPrice"`          // Deprecate
+	NetPrice  float64 `json:"netPrice"`           // Deprecate
+	Price     float64 `json:"price" db:"price"`   // Price of a plan, prior to discount.
+	Amount    float64 `json:"amount" db:"amount"` // Actually paid amount.
+	//Duration                 // This should be removed.
+	Currency         string `json:"currency" db:"currency"`
+	Title            string `json:"description"`
+	stripeLivePlanID string `json:"-"`
+	stripeTestPlanID string `json:"-"`
+	AppleProductID   string `json:"-"`
 }
 
 // GetTitle compose the message shown for wxpay or alipay.
@@ -62,11 +56,11 @@ func (p Plan) GetStripePlanID(live bool) string {
 }
 
 // withSandboxPrice returns the sandbox version of a plan.
-func (p Plan) withSandboxPrice() Plan {
-	p.NetPrice = 0.01
-	p.Amount = 0.01
-	return p
-}
+//func (p Plan) withSandboxPrice() Plan {
+//	p.NetPrice = 0.01
+//	p.Amount = 0.01
+//	return p
+//}
 
 func (p Plan) WithStripePrice(sp stripe.Plan) Plan {
 	p.ListPrice = float64(sp.Amount / 100)
@@ -79,59 +73,45 @@ func (p Plan) WithStripePrice(sp stripe.Plan) Plan {
 }
 
 // WithUpgrade creates an upgrading plan.
-func (p Plan) WithUpgrade(balance float64) Plan {
-
-	if balance < p.Price {
-		p.Price = p.Price - balance
-	} else {
-		p.Price = 0
-	}
-
-	p.Amount = p.Price
-
-	dur := p.CalculateConversion(balance)
-
-	p.CycleCount = dur.CycleCount
-	p.ExtraDays = dur.ExtraDays
-	p.Title = "FT中文网 - 升级高端会员"
-
-	return p
-}
+//func (p Plan) WithUpgrade(balance float64) Plan {
+//
+//	if balance < p.Price {
+//		p.Price = p.Price - balance
+//	} else {
+//		p.Price = 0
+//	}
+//
+//	p.Amount = p.Price
+//
+//	dur := p.CalculateConversion(balance)
+//
+//	//p.CycleCount = dur.CycleCount
+//	//p.ExtraDays = dur.ExtraDays
+//	p.Title = "FT中文网 - 升级高端会员"
+//
+//	return p
+//}
 
 // ConvertBalance checks to see how many cycles and extra
 // days a user's balance could be exchanged.
-func (p Plan) CalculateConversion(balance float64) Duration {
-
-	if balance <= p.Amount {
-		return Duration{
-			CycleCount: 1,
-			ExtraDays:  1,
-		}
-	}
-
-	cycles, r := util.Division(balance, p.NetPrice)
-
-	days := math.Ceil(r * 365 / p.NetPrice)
-
-	return Duration{
-		CycleCount: cycles,
-		ExtraDays:  int64(days),
-	}
-}
-
-func (p Plan) GetPeriodEnd(start time.Time) (time.Time, error) {
-
-	switch p.Cycle {
-	case enum.CycleYear:
-		return start.AddDate(int(p.CycleCount), 0, int(p.ExtraDays)), nil
-
-	case enum.CycleMonth:
-		return start.AddDate(0, int(p.CycleCount), int(p.ExtraDays)), nil
-
-	default:
-		return time.Time{}, errors.New("invalid plan cycle")
-	}
-}
+//func (p Plan) CalculateConversion(balance float64) Duration {
+//
+//	if balance <= p.Amount {
+//		return Duration{
+//			CycleCount: 1,
+//			ExtraDays:  1,
+//		}
+//	}
+//
+//	cycles, r := util.Division(balance, p.NetPrice)
+//
+//	days := math.Ceil(r * 365 / p.NetPrice)
+//
+//	return Duration{
+//		CycleCount: cycles,
+//		ExtraDays:  int64(days),
+//	}
+//}
 
 // Desc is used for displaying to user.
 // The price show here is not the final price user paid.
