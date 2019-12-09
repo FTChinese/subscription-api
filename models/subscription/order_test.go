3c057709b9de8790a2fd5ec2c3e1d6082bb91705
weiguo.ni <neefrankie@gmail.com>
2019-12-09 17:57:00 +0800

    test

--- models/subscription/order_test.go
+++ models/subscription/order_test.go
@@ -1,19 +1,1 @@
 package subscription
-
-import "testing"
-
-func TestOrder_AmountInCent(t *testing.T) {
-	order := Order{
-		Amount: 258.00,
-	}
-
-	payResult := PaymentResult{
-		Amount: 25800,
-	}
-
-	if order.AmountInCent() == payResult.Amount {
-		t.Logf("Equal")
-	} else {
-		t.Error("Not Equal")
-	}
-}
