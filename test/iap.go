eeb84003b766a9413e678bd4393b1cf4b3b4ac0e
weiguo.ni <neefrankie@gmail.com>
2019-12-10 17:04:28 +0800

    test

---
+++ test/iap.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"encoding/json"
+	"github.com/Pallinder/go-randomdata"
+	"gitlab.com/ftchinese/subscription-api/models/apple"
+)
+
+func GetReceiptToken() string {
+	return iapReceipt
+}
+
+func GetVerificationResponse() apple.VerificationResponseBody {
+	var r apple.VerificationResponseBody
+	if err := json.Unmarshal([]byte(iapVerificationResponse), &r); err != nil {
+		panic(err)
+	}
+
+	return r
+}
+
+func GetIAPTransaction() apple.Transaction {
+	resp := GetVerificationResponse()
+
+	l := len(resp.LatestTransactions)
+
+	return resp.LatestTransactions[randomdata.Number(l)]
+}
+
+func GetPendingRenewal() apple.PendingRenewal {
+	resp := GetVerificationResponse()
+
+	l := len(resp.PendingRenewalInfo)
+
+	return resp.PendingRenewalInfo[randomdata.Number(l)]
+}
