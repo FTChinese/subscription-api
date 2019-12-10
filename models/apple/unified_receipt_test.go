e13420426093ee59a59e587f261df5e810e31922
weiguo.ni <neefrankie@gmail.com>
2019-12-10 18:05:31 +0800

    test

---
+++ models/apple/unified_receipt_test.go
@@ -0,0 +1,48 @@
+package apple
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestUnifiedReceipt_sortLatestReceiptDesc(t *testing.T) {
+	ur := UnifiedReceipt{
+		Environment: EnvSandbox,
+		LatestToken: "",
+		LatestTransactions: []Transaction{
+			{
+				ExpiresDateMs: "1532421737000",
+			},
+			{
+				ExpiresDateMs: "1532428954000",
+			},
+		},
+		Status:            0,
+		latestTransaction: Transaction{},
+	}
+
+	ur.Parse()
+
+	isSorted := sort.SliceIsSorted(ur.LatestTransactions, func(i, j int) bool {
+		return ur.LatestTransactions[i].ExpiresDateMs > ur.LatestTransactions[j].ExpiresDateMs
+	})
+
+	if !isSorted {
+		t.Error("Not sorted")
+	}
+
+	if ur.latestTransaction.ExpiresDateMs != "1532428954000" {
+		t.Error("latest not found")
+	}
+}
+
+func TestUnifiedReceipt_findPendingRenewal(t *testing.T) {
+	resp := getReceiptResponse()
+	resp.Parse()
+
+	pr := resp.findPendingRenewal()
+
+	if pr.OriginalTransactionID != "1000000595951896" {
+		t.Error("not found")
+	}
+}
