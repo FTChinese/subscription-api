70d4d28a410f46dd723a496273d845a6a5f59244
weiguo.ni <neefrankie@gmail.com>
2019-12-11 16:53:53 +0800

    feat: created ConfirmationResult to wrap all confirmation data

--- models/subscription/confirmration_result.go
+++ models/subscription/confirmration_result.go
@@ -7,34 +7,40 @@ type ConfirmError struct {
 	Retry bool
 }
 
-func (c ConfirmError) Error() string {
-	return c.Err.Error()
-}
-
-// ConfirmationResult logs the result of confirmation.
-type ConfirmationResult struct {
-	OrderID   string      `db:"order_id"`
-	Succeeded bool        `db:"succeeded"`
-	Failed    null.String `db:"failed"`
-	Retry     bool
-}
-
-func (r ConfirmationResult) Error() string {
-	return r.Failed.String
-}
-
-func NewConfirmationResult(orderID string, err error) ConfirmationResult {
-	r := ConfirmationResult{
+func (c ConfirmError) Schema(orderID string) ConfirmErrSchema {
+	s := ConfirmErrSchema{
 		OrderID:   orderID,
 		Succeeded: false,
 		Failed:    null.String{},
 	}
 
-	if err != nil {
-		r.Failed = null.StringFrom(err.Error())
+	if c.Err != nil {
+		s.Failed = null.StringFrom(c.Err.Error())
 	} else {
-		r.Succeeded = true
+		s.Succeeded = true
 	}
 
-	return r
+	return s
+}
+
+func (c ConfirmError) Error() string {
+	return c.Err.Error()
+}
+
+// ConfirmErrSchema logs the result of confirmation.
+type ConfirmErrSchema struct {
+	OrderID   string      `db:"order_id"`
+	Succeeded bool        `db:"succeeded"`
+	Failed    null.String `db:"failed"`
+}
+
+// FreeUpgrade contains the data after creating/upgrading/renewing a membership.
+// All data here is in a final state.
+// Those data can be directly save into database.
+// The generated of those fields has a intertwined dependency
+// on each other, so they are return in one batch.
+type ConfirmationResult struct {
+	Order      Order          // The confirmed order.
+	Membership Membership     // The updated membership.
+	Snapshot   MemberSnapshot // // Snapshot of previous membership
 }
