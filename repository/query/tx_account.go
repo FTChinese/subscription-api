77121637fb79c62ffbf5690179f4d157df6791cf
neefrankie <neefrankie@gmail.com>
2019-12-14 23:13:08 +0800

    refactor: move OrderTx and AccountTx into separate pkg since they are imported by multiple pkgs

---
+++ repository/query/tx_account.go
@@ -0,0 +1,39 @@
+package query
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"gitlab.com/ftchinese/subscription-api/models/reader"
+)
+
+var logger = logrus.WithField("package", "query")
+
+type AccountTx struct {
+	*sqlx.Tx
+}
+
+func NewAccountTx(tx *sqlx.Tx) AccountTx {
+	return AccountTx{tx}
+}
+
+// RetrieveAccount loads an ftc account by uuid
+func (tx AccountTx) RetrieveAccount(ftcID string) (reader.Account, error) {
+	var account reader.Account
+	err := tx.Get(&account, BuildSelectReader(false, true), ftcID)
+	if err != nil {
+		logger.WithField("trace", "AccountTx.RetrieveAccount").Error(err)
+		return reader.Account{}, err
+	}
+
+	return account, nil
+}
+
+func (tx AccountTx) SavedStripeID(account reader.Account) error {
+	_, err := tx.NamedExec(SaveStripeID, account)
+	if err != nil {
+		logger.WithField("trace", "AccountTx.SaveStripeID").Error(err)
+		return err
+	}
+
+	return nil
+}
