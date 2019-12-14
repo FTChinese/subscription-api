3893aaa578bcdaf6641cf009df062b65129304d3
neefrankie <neefrankie@gmail.com>
2019-12-14 23:17:16 +0800

    refactor:

--- repository/subrepo/env.go
+++ repository/subrepo/env.go
@@ -5,6 +5,7 @@ import (
 	"github.com/patrickmn/go-cache"
 	"github.com/sirupsen/logrus"
 	"gitlab.com/ftchinese/subscription-api/models/util"
+	"gitlab.com/ftchinese/subscription-api/repository/query"
 )
 
 // SubEnv wraps database connection
@@ -16,25 +17,22 @@ type SubEnv struct {
 
 // NewSubEnv creates a new instance of SubEnv.
 // `sandbox` is used to determine which table to write subscription data.
-func NewSubEnv(db *sqlx.DB, c *cache.Cache, b util.BuildConfig) SubEnv {
+func NewSubEnv(db *sqlx.DB, c *cache.Cache, config util.BuildConfig) SubEnv {
 	return SubEnv{
-		BuildConfig: b,
+		BuildConfig: config,
 		db:          db,
 		cache:       c,
 	}
 }
 
-func (env SubEnv) BeginOrderTx() (OrderTx, error) {
+func (env SubEnv) BeginOrderTx() (query.OrderTx, error) {
 	tx, err := env.db.Beginx()
 
 	if err != nil {
-		return OrderTx{}, err
+		return query.OrderTx{}, err
 	}
 
-	return OrderTx{
-		tx:      tx,
-		sandbox: env.UseSandboxDB(),
-	}, nil
+	return query.NewOrderTx(tx, env.UseSandboxDB()), nil
 }
 
 var logger = logrus.
