a4734b2500f8db6adb15d3e0ec90965495242b79
neefrankie <neefrankie@gmail.com>
2019-12-15 11:29:46 +0800

    refactor

---
+++ repository/subrepo/upgrade_preview_test.go
@@ -0,0 +1,51 @@
+package subrepo
+
+import (
+	"gitlab.com/ftchinese/subscription-api/models/reader"
+	"gitlab.com/ftchinese/subscription-api/test"
+	"testing"
+)
+
+func TestSubEnv_PreviewUpgrade(t *testing.T) {
+	profile := test.NewProfile()
+
+	store := test.NewSubStore(profile)
+	orders := store.MustRenewN(3)
+
+	// To have upgrading balance, a user must have an existing standard membership,
+	// and some valid orders.
+	repo := test.NewRepo()
+	repo.MustSaveMembership(store.MustGetMembership())
+	repo.MustSaveRenewalOrders(orders)
+
+	env := SubEnv{db: test.DB}
+
+	type args struct {
+		userID reader.MemberID
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Preview upgrade wallet",
+			args: args{
+				userID: profile.AccountID(),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.PreviewUpgrade(tt.args.userID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PreviewUpgrade() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Payment intent: %+v", got)
+		})
+	}
+}
