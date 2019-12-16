610f11af97717fa15addff325771f5080f2d668d
weiguo.ni <neefrankie@gmail.com>
2019-12-16 13:41:51 +0800

    fix: json tag

--- models/subscription/duration.go
+++ models/subscription/duration.go
@@ -2,6 +2,6 @@ package subscription
 
 // Duration is the number of billing cycles and extra days of a subscription plan provides.
 type Duration struct {
-	CycleCount int64 `json:"cycle_count" db:"cycle_count"`
-	ExtraDays  int64 `json:"extra_days" db:"extra_days"`
+	CycleCount int64 `json:"cycleCount" db:"cycle_count"`
+	ExtraDays  int64 `json:"extraDays" db:"extra_days"`
 }
