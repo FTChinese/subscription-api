46a54530d3c05d99932327e6cf8fefdf0704ef22
neefrankie <neefrankie@gmail.com>
2020-08-16 22:55:29 +0800

    feat: check discount duratoin

--- pkg/product/discount.go
+++ pkg/product/discount.go
@@ -1,6 +1,9 @@
 package product
 
-import "github.com/guregu/null"
+import (
+	"github.com/guregu/null"
+	"time"
+)
 
 type Discount struct {
 	// The id fields started with Disc to avoid conflict when used in ExpandedPlanSchema.
@@ -8,4 +11,18 @@ type Discount struct {
 	PriceOff null.Float  `json:"priceOff" db:"price_off"`
 	Percent  null.Int    `json:"percent" db:"percent"`
 	Period
+}
+
+func (d Discount) IsValid() bool {
+	if d.PriceOff.IsZero() || d.PriceOff.Float64 <= 0 {
+		return false
+	}
+
+	now := time.Now()
+
+	if now.Before(d.StartUTC.Time) || now.After(d.EndUTC.Time) {
+		return false
+	}
+
+	return true
 }
