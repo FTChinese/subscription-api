338d00a17ed4a71fa6076f001fafcb02a7c31f28
neefrankie <neefrankie@gmail.com>
2020-08-17 09:54:25 +0800

    feat: retrieve a plan by edition

--- repository/products/plans_test.go
+++ repository/products/plans_test.go
@@ -1,9 +1,11 @@
 package products
 
 import (
+	"github.com/FTChinese/go-rest/enum"
 	"github.com/jmoiron/sqlx"
 	"github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/ftchinese/subscription-api/pkg/product"
 	"gitlab.com/ftchinese/subscription-api/test"
 	"testing"
 )
@@ -46,7 +48,7 @@ func TestEnv_loadPlans(t *testing.T) {
 	}
 }
 
-func TestEnv_retrievePlan(t *testing.T) {
+func TestEnv_PlanByID(t *testing.T) {
 	type fields struct {
 		db    *sqlx.DB
 		cache *cache.Cache
@@ -76,9 +78,55 @@ func TestEnv_retrievePlan(t *testing.T) {
 				db:    tt.fields.db,
 				cache: tt.fields.cache,
 			}
-			got, err := env.retrievePlan(tt.args.id)
+			got, err := env.PlanByID(tt.args.id)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("retrievePlan() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("retrievePlanByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("%+v", got)
+		})
+	}
+}
+
+func TestEnv_PlanByEdition(t *testing.T) {
+	type fields struct {
+		db    *sqlx.DB
+		cache *cache.Cache
+	}
+	type args struct {
+		e product.Edition
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Load a plan by tier and cycle",
+			fields: fields{
+				db:    test.DB,
+				cache: test.Cache,
+			},
+			args: args{
+				e: product.Edition{
+					Tier:  enum.TierStandard,
+					Cycle: enum.CycleYear,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:    tt.fields.db,
+				cache: tt.fields.cache,
+			}
+			got, err := env.PlanByEdition(tt.args.e)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PlanByEdition() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
