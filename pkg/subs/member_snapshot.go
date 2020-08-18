75d2457e2a6ea466f64f8a46660ac0db7c36e8b7
weiguo.ni <neefrankie@gmail.com>
2020-08-18 18:23:33 +0800

    refactor: GetSnapshotReason

--- pkg/subs/member_snapshot.go
+++ pkg/subs/member_snapshot.go
@@ -3,13 +3,8 @@ package subs
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/go-rest/rand"
 	"github.com/guregu/null"
 )
-
-func GenerateSnapshotID() string {
-	return "snp_" + rand.String(12)
-}
 
 // MemberSnapshot saves a membership's status prior to
 // placing an order.
@@ -19,4 +14,19 @@ type MemberSnapshot struct {
 	CreatedUTC chrono.Time         `db:"created_utc"`
 	OrderID    null.String         `db:"order_id"` // Only exists when user is performing renewal or upgrading.
 	Membership
+}
+
+// GetSnapshotReason deduces why a membership is snapshot
+// when an order is confirmed and membership updated.
+func GetSnapshotReason(k enum.OrderKind) enum.SnapshotReason {
+	switch k {
+	case enum.OrderKindRenew:
+		return enum.SnapshotReasonRenew
+
+	case enum.OrderKindUpgrade:
+		return enum.SnapshotReasonUpgrade
+
+	default:
+		return enum.SnapshotReasonNull
+	}
 }
