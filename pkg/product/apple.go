daaaeb491977d6ac9f8f6b83beebaa6cbdc160a8
neefrankie <neefrankie@gmail.com>
2020-08-20 22:54:17 +0800

    feat: search and find stripe and apple product

--- pkg/product/apple.go
+++ pkg/product/apple.go
@@ -1,1 +1,70 @@
 package product
+
+import "fmt"
+
+type AppleProduct struct {
+	Edition
+	ID string
+}
+
+type appleStore struct {
+	products     []AppleProduct
+	indexEdition map[string]int
+	indexID      map[string]int
+}
+
+func newAppleStore() appleStore {
+	s := appleStore{
+		products: []AppleProduct{
+			{
+				Edition: NewStdMonthEdition(),
+				ID:      "com.ft.ftchinese.mobile.subscription.member.monthly",
+			},
+			{
+				Edition: NewStdYearEdition(),
+				ID:      "com.ft.ftchinese.mobile.subscription.member",
+			},
+			{
+				Edition: NewPremiumEdition(),
+				ID:      "com.ft.ftchinese.mobile.subscription.vip",
+			},
+		},
+		indexEdition: make(map[string]int),
+		indexID:      make(map[string]int),
+	}
+
+	for i, v := range s.products {
+		s.indexEdition[v.NamedKey()] = i
+		s.indexEdition[v.ID] = i
+	}
+
+	return s
+}
+
+func (s appleStore) findByEdition(e Edition) (AppleProduct, error) {
+	i, ok := s.indexEdition[e.NamedKey()]
+	if !ok {
+		return AppleProduct{}, fmt.Errorf("apple product for %s is not found", e.NamedKey())
+	}
+
+	return s.products[i], nil
+}
+
+func (s appleStore) findByID(id string) (AppleProduct, error) {
+	i, ok := s.indexID[id]
+	if !ok {
+		return AppleProduct{}, fmt.Errorf("apple prodct with id %s not found", id)
+	}
+
+	return s.products[i], nil
+}
+
+var appleProducts = newAppleStore()
+
+func GetAppleProductByEdition(e Edition) (AppleProduct, error) {
+	return appleProducts.findByEdition(e)
+}
+
+func GetAppleProductByID(id string) (AppleProduct, error) {
+	return appleProducts.findByID(id)
+}
--- pkg/product/stripe.go
+++ pkg/product/stripe.go
@@ -1,63 +1,105 @@
 package product
 
-import "github.com/FTChinese/go-rest/enum"
+import (
+	"fmt"
+)
 
-type NonFtcPlan struct {
+type StripePlan struct {
 	Edition
-	stripeLivePlanID string
-	stripeTestPlanID string
-	AppleProductID   string
+	PlanID string
+	Live   bool
 }
 
-func (s NonFtcPlan) GetID(live bool) string {
+func stripeEditionKey(e Edition, live bool) string {
+	var suffix string
 	if live {
-		return s.stripeLivePlanID
+		suffix = "live"
+	} else {
+		suffix = "test"
 	}
 
-	return s.stripeTestPlanID
+	return e.NamedKey() + "_" + suffix
 }
 
-var stdMonth = NonFtcPlan{
-	Edition: Edition{
-		Tier:  enum.TierStandard,
-		Cycle: enum.CycleMonth,
-	},
-	stripeLivePlanID: "plan_FXZYLOEbcvj5Tx",
-	stripeTestPlanID: "plan_FOdgPTznDwHU4i",
-	AppleProductID:   "com.ft.ftchinese.mobile.subscription.member.monthly",
+type stripeStore struct {
+	plans        []StripePlan
+	indexEdition map[string]int
+	indexID      map[string]int
 }
 
-var stdYear = NonFtcPlan{
-	Edition: Edition{
-		Tier:  enum.TierStandard,
-		Cycle: enum.CycleYear,
-	},
-	stripeLivePlanID: "plan_FXZZUEDpToPlZK",
-	stripeTestPlanID: "plan_FOdfeaqzczp6Ag",
-	AppleProductID:   "com.ft.ftchinese.mobile.subscription.member",
+func newStripeStoreSchema() *stripeStore {
+	return &stripeStore{
+		plans:        make([]StripePlan, 0),
+		indexEdition: make(map[string]int),
+		indexID:      make(map[string]int),
+	}
 }
 
-var prmYear = NonFtcPlan{
-	Edition:          NewPremiumEdition(),
-	stripeLivePlanID: "plan_FXZbv1cDTsUKOg",
-	stripeTestPlanID: "plan_FOde0uAr0V4WmT",
-	AppleProductID:   "com.ft.ftchinese.mobile.subscription.vip",
+func (s *stripeStore) Add(p StripePlan) *stripeStore {
+	s.plans = append(s.plans, p)
+
+	pos := len(s.plans) - 1
+	s.indexEdition[stripeEditionKey(p.Edition, p.Live)] = pos
+	s.indexID[p.PlanID] = pos
+
+	return s
 }
 
-type NonFtcPlanStore struct {
-	plans               []NonFtcPlan
-	indexStripeLivePlan map[string]int
-	indexStripeTestPlan map[string]int
-	indexAppleProduct   map[string]int
+func (s *stripeStore) findByEdition(e Edition, live bool) (StripePlan, error) {
+	key := stripeEditionKey(e, live)
+	i, ok := s.indexEdition[key]
+	if !ok {
+		return StripePlan{}, fmt.Errorf("stripe plan for %s is not found", key)
+	}
+
+	return s.plans[i], nil
 }
 
-var NonFtcPlans = NonFtcPlanStore{
-	plans: []NonFtcPlan{
-		stdMonth,
-		stdYear,
-		prmYear,
-	},
-	indexStripeLivePlan: map[string]int{},
-	indexStripeTestPlan: map[string]int{},
-	indexAppleProduct:   nil,
+func (s *stripeStore) findByID(planID string) (StripePlan, error) {
+	i, ok := s.indexID[planID]
+	if !ok {
+		return StripePlan{}, fmt.Errorf("stripe plan with id %s is not found", planID)
+	}
+
+	return s.plans[i], nil
+}
+
+var stripePlans = newStripeStoreSchema().
+	Add(StripePlan{
+		Edition: NewStdMonthEdition(),
+		PlanID:  "plan_FXZYLOEbcvj5Tx",
+		Live:    true,
+	}).
+	Add(StripePlan{
+		Edition: NewStdMonthEdition(),
+		PlanID:  "plan_FOdgPTznDwHU4i",
+		Live:    false,
+	}).
+	Add(StripePlan{
+		Edition: NewStdYearEdition(),
+		PlanID:  "plan_FXZZUEDpToPlZK",
+		Live:    true,
+	}).
+	Add(StripePlan{
+		Edition: NewStdYearEdition(),
+		PlanID:  "plan_FOdfeaqzczp6Ag",
+		Live:    false,
+	}).
+	Add(StripePlan{
+		Edition: NewPremiumEdition(),
+		PlanID:  "plan_FXZbv1cDTsUKOg",
+		Live:    true,
+	}).
+	Add(StripePlan{
+		Edition: NewPremiumEdition(),
+		PlanID:  "plan_FOde0uAr0V4WmT",
+		Live:    false,
+	})
+
+func GetStripePlanByEdition(edition Edition, live bool) (StripePlan, error) {
+	return stripePlans.findByEdition(edition, live)
+}
+
+func GetStripePlanByID(id string) (StripePlan, error) {
+	return stripePlans.findByID(id)
 }
