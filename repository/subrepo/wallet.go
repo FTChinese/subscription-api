fc0764e321e619b93915bbc54417cfc6d9858d02
weiguo.ni <neefrankie@gmail.com>
2020-08-20 17:19:52 +0800

    feat: ListProratedOrders

---
+++ repository/subrepo/wallet.go
@@ -0,0 +1,19 @@
+package subrepo
+
+import "github.com/FTChinese/subscription-api/pkg/subs"
+
+func (env Env) ListProratedOrders(upOrderID string) ([]subs.ProratedOrder, error) {
+	var po = make([]subs.ProratedOrder, 0)
+
+	err := env.db.Select(
+		&po,
+		subs.StmtListProratedOrders(env.GetSubsDB()),
+		upOrderID)
+
+	if err != nil {
+		logger.WithField("trace", "SubEnv.RetrieveProratedOrders").Error(err)
+		return nil, err
+	}
+
+	return po, nil
+}
