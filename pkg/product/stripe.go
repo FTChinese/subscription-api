7bce5a05eede30801d36a9a125564eb7d7d59cbb
weiguo.ni <neefrankie@gmail.com>
2020-08-20 17:44:40 +0800

    feat: store for stripe and apple id

--- pkg/product/stripe.go
+++ pkg/product/stripe.go
@@ -1,15 +1,63 @@
 package product
 
-type StripePlan struct {
+import "github.com/FTChinese/go-rest/enum"
+
+type NonFtcPlan struct {
 	Edition
-	livePlanID string
-	testPlanID string
+	stripeLivePlanID string
+	stripeTestPlanID string
+	AppleProductID   string
 }
 
-func (s StripePlan) GetID(live bool) string {
+func (s NonFtcPlan) GetID(live bool) string {
 	if live {
-		return s.livePlanID
+		return s.stripeLivePlanID
 	}
 
-	return s.testPlanID
+	return s.stripeTestPlanID
+}
+
+var stdMonth = NonFtcPlan{
+	Edition: Edition{
+		Tier:  enum.TierStandard,
+		Cycle: enum.CycleMonth,
+	},
+	stripeLivePlanID: "plan_FXZYLOEbcvj5Tx",
+	stripeTestPlanID: "plan_FOdgPTznDwHU4i",
+	AppleProductID:   "com.ft.ftchinese.mobile.subscription.member.monthly",
+}
+
+var stdYear = NonFtcPlan{
+	Edition: Edition{
+		Tier:  enum.TierStandard,
+		Cycle: enum.CycleYear,
+	},
+	stripeLivePlanID: "plan_FXZZUEDpToPlZK",
+	stripeTestPlanID: "plan_FOdfeaqzczp6Ag",
+	AppleProductID:   "com.ft.ftchinese.mobile.subscription.member",
+}
+
+var prmYear = NonFtcPlan{
+	Edition:          NewPremiumEdition(),
+	stripeLivePlanID: "plan_FXZbv1cDTsUKOg",
+	stripeTestPlanID: "plan_FOde0uAr0V4WmT",
+	AppleProductID:   "com.ft.ftchinese.mobile.subscription.vip",
+}
+
+type NonFtcPlanStore struct {
+	plans               []NonFtcPlan
+	indexStripeLivePlan map[string]int
+	indexStripeTestPlan map[string]int
+	indexAppleProduct   map[string]int
+}
+
+var NonFtcPlans = NonFtcPlanStore{
+	plans: []NonFtcPlan{
+		stdMonth,
+		stdYear,
+		prmYear,
+	},
+	indexStripeLivePlan: map[string]int{},
+	indexStripeTestPlan: map[string]int{},
+	indexAppleProduct:   nil,
 }
