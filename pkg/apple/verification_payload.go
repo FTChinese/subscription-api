37e0dba27c6a0db060669c1ffd3310ef779fa091
neefrankie <neefrankie@gmail.com>
2020-08-22 16:07:49 +0800

    refactor:rename

---
+++ pkg/apple/verification_payload.go
@@ -0,0 +1,31 @@
+package apple
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/validator"
+	"strings"
+)
+
+// VerificationPayload contains the the JSON contents
+// you submit with the request to the App Store when verifying receipt.
+// The ReceiptData is encrypted and the Password is used to decrypt it.
+// Per the doc https://developer.apple.com/documentation/storekit/in-app_purchase/validating_receipts_with_the_app_store:
+// On your server, create a JSON object with the
+// * `receipt-data`,
+// * `password` (if the receipt contains an auto-renewable subscription), and
+// * `exclude-old-transactions` keys.
+// Submit this JSON object as the payload of an HTTP POST request.
+// Use the test environment URL https://sandbox.itunes.apple.com/verifyReceipt.
+// Use the production URL https://buy.itunes.apple.com/verifyReceipt.
+// See https://developer.apple.com/documentation/appstorereceipts/requestbody
+type VerificationPayload struct {
+	ReceiptData            string `json:"receipt-data"`             // Required. The Base64 encoded receipt data.
+	Password               string `json:"password"`                 // Required. Your app's shared secret (a hexadecimal string).
+	ExcludeOldTransactions bool   `json:"exclude-old-transactions"` // Set this value to true for the response to include only the latest renewal transaction for any subscriptions. Applicable only to auto-renewable subscriptions.
+}
+
+// Validate checks if ReceiptData is set.
+func (v *VerificationPayload) Validate() *render.ValidationError {
+	v.ReceiptData = strings.TrimSpace(v.ReceiptData)
+	return validator.New("receipt-data").Required().Validate(v.ReceiptData)
+}
