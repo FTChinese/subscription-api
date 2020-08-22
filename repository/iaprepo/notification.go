2dc009e2fbb07dbef7bc19bab3bf39ebff322158
neefrankie <neefrankie@gmail.com>
2020-08-22 21:15:28 +0800

    refactor: relocate webhook stmt

--- repository/iaprepo/notification.go
+++ repository/iaprepo/notification.go
@@ -3,7 +3,7 @@ package iaprepo
 import "github.com/FTChinese/subscription-api/pkg/apple"
 
 func (env Env) SaveNotification(w apple.WebHookSchema) error {
-	_, err := env.db.NamedExec(insertWebHook, w)
+	_, err := env.db.NamedExec(apple.StmtLoggingWebhook, w)
 
 	if err != nil {
 		logger.WithField("trace", "Env.SaveNotification").Error(err)
