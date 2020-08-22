3d9eab32f86a2919c7395ad064668ae4544b1902
neefrankie <neefrankie@gmail.com>
2020-08-22 15:52:45 +0800

    refactor: rename Env

---
+++ repository/iaprepo/env.go
@@ -0,0 +1,40 @@
+package iaprepo
+
+import (
+	"github.com/FTChinese/subscription-api/pkg/config"
+	"github.com/FTChinese/subscription-api/repository/txrepo"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+)
+
+var logger = logrus.
+	WithField("project", "subscription-api").
+	WithField("package", "iap_repo")
+
+type Env struct {
+	cfg config.BuildConfig
+	db  *sqlx.DB
+}
+
+func NewEnv(db *sqlx.DB, c config.BuildConfig) Env {
+	return Env{
+		cfg: c,
+		db:  db,
+	}
+}
+
+// BeginTx starts a transaction.
+// NOTE: here the sandbox is different from the environment
+// field send by apple. It only determines whether the
+// sandbox db should be used and is determined by
+// the CLI argument `-sandbox`.
+// All messages from apple is save in production DBs.
+func (env Env) BeginTx() (txrepo.MemberTx, error) {
+	tx, err := env.db.Beginx()
+
+	if err != nil {
+		return txrepo.MemberTx{}, err
+	}
+
+	return txrepo.NewMemberTx(tx, env.cfg), nil
+}
