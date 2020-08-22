4306dadbd9f0b8cbf3361a22ef7a8e7fe70f8653
neefrankie <neefrankie@gmail.com>
2020-08-22 20:15:19 +0800

    doc

--- pkg/apple/subscription.go
+++ pkg/apple/subscription.go
@@ -9,6 +9,9 @@ import (
 	"github.com/guregu/null"
 )
 
+// Subscription contains a user's subscription data.
+// It it built from app store's verification response.
+// The original transaction id is used to uniquely identify a user.
 type Subscription struct {
 	Environment           Environment `json:"environment" db:"environment"`
 	OriginalTransactionID string      `json:"originalTransactionId" db:"original_transaction_id"`
