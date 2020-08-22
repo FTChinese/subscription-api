045aaddc1039fc6ac62bdc48af71de2f4bf4fb01
neefrankie <neefrankie@gmail.com>
2020-08-22 20:17:46 +0800

    refactor: unexport getProductByID

--- pkg/apple/unified_receipt.go
+++ pkg/apple/unified_receipt.go
@@ -141,7 +141,7 @@ func (u *UnifiedReceipt) Subscription() (Subscription, error) {
 		autoRenew = false
 	}
 
-	prod, err := GetProductByID(u.latestTransaction.ProductID)
+	prod, err := getProductByID(u.latestTransaction.ProductID)
 
 	if err != nil {
 		return Subscription{}, err
