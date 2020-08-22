331bca4c4c8301ad7d03c6cb0f6ef04ee0a8d15a
neefrankie <neefrankie@gmail.com>
2020-08-22 15:59:27 +0800

    refactor: rename Env

--- repository/iaprepo/notification.go
+++ repository/iaprepo/notification.go
@@ -2,11 +2,11 @@ package iaprepo
 
 import "github.com/FTChinese/subscription-api/pkg/apple"
 
-func (env IAPEnv) SaveNotification(w apple.WebHookSchema) error {
+func (env Env) SaveNotification(w apple.WebHookSchema) error {
 	_, err := env.db.NamedExec(insertWebHook, w)
 
 	if err != nil {
-		logger.WithField("trace", "IAPEnv.SaveNotification").Error(err)
+		logger.WithField("trace", "Env.SaveNotification").Error(err)
 
 		return err
 	}
