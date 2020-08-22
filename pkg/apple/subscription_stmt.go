ef5a34f50b8e3b939a4f23cb1f063dad645ff7e6
neefrankie <neefrankie@gmail.com>
2020-08-22 16:05:53 +0800

    feat: sql stmt

---
+++ pkg/apple/subscription_stmt.go
@@ -0,0 +1,37 @@
+package apple
+
+const colsSubsBase = `
+last_transaction_id = :last_transaction_id,
+product_id = :product_id,
+purchase_date_utc = :purchase_date_utc,
+expires_date_utc = :expires_date_utc,
+tier = :tier,
+cycle = :cycle,
+auto_renewal = :auto_renewal,
+updated_utc = UTC_TIMESTAMP()
+`
+
+const StmtUpsertSubs = `
+INSERT INTO premium.apple_subscription
+SET environment = :environment,
+	original_transaction_id = :original_transaction_id,
+` + colsSubsBase + `,
+	created_utc = UTC_TIMESTAMP()
+ON DUPLICATE KEY UPDATE
+` + colsSubsBase
+
+const StmtLoadSubs = `
+SELECT environment,
+	original_transaction_id,
+	last_transaction_id,
+	product_id,
+	purchase_date_utc,
+	expires_date_utc,
+	tier,
+	cycle,
+	auto_renewal,
+	created_utc,
+	updated_utc
+FROM premium.apple_subscription
+WHERE original_transaction_id = ?
+LIMIT 1`
