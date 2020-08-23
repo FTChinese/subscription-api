37fa2b1645ba9b25ff5b1ed8eb4d26e371b34b16
neefrankie <neefrankie@gmail.com>
2020-08-23 22:18:04 +0800

    fix: test

--- test/persona_test.go
+++ test/persona_test.go
@@ -1,10 +1,27 @@
 package test
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewProfile(t *testing.T) {
 	t.Log(NewPersona().Email)
 	t.Log(NewPersona().Email)
+}
+
+func TestPersona_IAPSubs(t *testing.T) {
+	p := NewPersona()
+
+	sub := p.IAPSubs()
+
+	m := sub.NewMembership(p.AccountID())
+
+	m = m.Normalize()
+
+	assert.NotZero(t, m.LegacyExpire)
+	assert.NotZero(t, m.LegacyTier)
+
+	t.Log(m.LegacyTier)
+	t.Log(m.LegacyExpire)
 }
