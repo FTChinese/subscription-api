e033614a6b058b724ead1a4cdb71e2fc7258e73d
neefrankie <neefrankie@gmail.com>
2020-08-23 18:05:48 +0800

    refactor:

--- pkg/reader/errors.go
+++ pkg/reader/errors.go
@@ -2,13 +2,6 @@ package reader
 
 import "errors"
 
-// Errors when deducing a SubsKind from current membership and the new plan to subscribe.
-var (
-	ErrRenewalForbidden   = errors.New("current membership expiration date exceeds max renewal period")
-	ErrDowngradeForbidden = errors.New("downgrading membership is not allowed")
-	ErrUnknownSubsKind    = errors.New("unknown subscription usage")
-)
-
 // For subscription via stripe
 var (
 	ErrNonStripeValidSub = errors.New("you already have a valid subscription purchased via non-stripe method")
