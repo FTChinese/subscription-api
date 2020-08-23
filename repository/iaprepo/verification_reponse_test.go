527fca9c9218936e3872bf5ab9e415ad2ae6c68f
neefrankie <neefrankie@gmail.com>
2020-08-23 12:04:41 +0800

    test

--- repository/iaprepo/verification_reponse_test.go
+++ repository/iaprepo/verification_reponse_test.go
@@ -48,3 +48,81 @@ func TestEnv_SaveVerifiedReceipt(t *testing.T) {
 		})
 	}
 }
+
+func TestEnv_SaveTransaction(t *testing.T) {
+	type fields struct {
+		cfg config.BuildConfig
+		db  *sqlx.DB
+	}
+	type args struct {
+		r apple.TransactionSchema
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save a transaction",
+			fields: fields{
+				cfg: cfg,
+				db:  test.DB,
+			},
+			args: args{
+				r: test.MustIAPTransaction().Schema(apple.EnvSandbox),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				cfg: tt.fields.cfg,
+				db:  tt.fields.db,
+			}
+			if err := env.SaveTransaction(tt.args.r); (err != nil) != tt.wantErr {
+				t.Errorf("SaveTransaction() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestEnv_SavePendingRenewal(t *testing.T) {
+	type fields struct {
+		cfg config.BuildConfig
+		db  *sqlx.DB
+	}
+	type args struct {
+		p apple.PendingRenewalSchema
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save a pending renewal",
+			fields: fields{
+				cfg: cfg,
+				db:  test.DB,
+			},
+			args: args{
+				p: test.MustPendingRenewal().Schema(apple.EnvSandbox),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				cfg: tt.fields.cfg,
+				db:  tt.fields.db,
+			}
+			if err := env.SavePendingRenewal(tt.args.p); (err != nil) != tt.wantErr {
+				t.Errorf("SavePendingRenewal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
