471c3717b07af8bedd304a84b1502a5f53141ea4
neefrankie <neefrankie@gmail.com>
2020-08-23 18:19:38 +0800

    refactor: backup snapshot in reader.Env

---
+++ repository/readerrepo/snapshot.go
@@ -0,0 +1,16 @@
+package readerrepo
+
+import "github.com/FTChinese/subscription-api/pkg/reader"
+
+// BackUpMember saves a member's snapshot at a specific moment.
+func (env Env) BackUpMember(snapshot reader.MemberSnapshot) error {
+	_, err := env.db.NamedExec(
+		reader.StmtSnapshotMember(env.GetSubsDB()),
+		snapshot)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
