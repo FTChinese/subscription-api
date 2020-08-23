132207f66cb88a443943c13e6acf4908cb5073ac
neefrankie <neefrankie@gmail.com>
2020-08-23 22:48:31 +0800

    test

---
+++ pkg/stripe/plan_test.go
@@ -0,0 +1,54 @@
+package stripe
+
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/pkg/config"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stripe/stripe-go"
+	"log"
+	"testing"
+)
+
+func mustConfigViper() {
+	viper.SetConfigName("api")
+	viper.AddConfigPath("$HOME/config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Test_newPlanStore(t *testing.T) {
+	store := newPlanStore()
+
+	assert.Len(t, store.plans, 6)
+	assert.Len(t, store.indexEdition, 6)
+	assert.Len(t, store.indexID, 6)
+}
+
+func Test_planStore_findByEdition(t *testing.T) {
+	p, err := stripePlans.findByEdition("standard_year", false)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, p.Tier, enum.TierStandard)
+}
+
+func TestFetchPlan(t *testing.T) {
+	mustConfigViper()
+
+	cfg := config.NewBuildConfig(false, false)
+	stripe.Key = cfg.MustStripeAPIKey()
+
+	p, err := FetchPlan("standard_year", cfg.Live())
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.NotNil(t, p)
+
+	t.Logf("%+v", p)
+}
