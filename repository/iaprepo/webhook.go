6a9280303d89c24d374f699578f6c1cf73d397dd
neefrankie <neefrankie@gmail.com>
2020-08-23 12:02:37 +0800

    refactor: move notification to webhook

--- repository/iaprepo/webhook.go
+++ repository/iaprepo/webhook.go
@@ -1,6 +1,8 @@
 package iaprepo
 
-import "github.com/FTChinese/subscription-api/pkg/apple"
+import (
+	"github.com/FTChinese/subscription-api/pkg/apple"
+)
 
 func (env Env) SaveNotification(w apple.WebHookSchema) error {
 	_, err := env.db.NamedExec(apple.StmtLoggingWebhook, w)
