50f237b13c6f134611e01aba03f872792f8a0c6b
neefrankie <neefrankie@gmail.com>
2020-08-23 20:18:00 +0800

    test test helper

---
+++ test/repo_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestRepo_SaveAccount(t *testing.T) {
+	p := NewPersona()
+
+	err := NewRepo().SaveAccount(p.Account())
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRepo_SaveMembership(t *testing.T) {
+	p := NewPersona()
+
+	err := NewRepo().SaveMembership(p.Membership())
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRepo_SaveOrder(t *testing.T) {
+	p := NewPersona()
+
+	err := NewRepo().SaveOrder(p.CreateOrder())
+
+	if err != nil {
+		t.Error(err)
+	}
+}
