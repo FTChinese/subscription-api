25e2bdaf72c82687ef57e539cbdcfa57405540ec
weiguo.ni <neefrankie@gmail.com>
2020-08-24 13:34:21 +0800

    refactor: do not return sandbox price

--- pkg/product/charge.go
+++ pkg/product/charge.go
@@ -12,21 +12,13 @@ type Charge struct {
 }
 
 // AliPrice converts Charged price to ailpay format
-func (c Charge) AliPrice(live bool) string {
-	if live {
-		return strconv.FormatFloat(c.Amount, 'f', 2, 32)
-	}
-
-	return strconv.FormatFloat(0.01, 'f', 2, 32)
+func (c Charge) AliPrice() string {
+	return strconv.FormatFloat(c.Amount, 'f', 2, 32)
 }
 
 // AmountInCent converts Charged price to int64 in cent for comparison with wx notification.
-func (c Charge) AmountInCent(live bool) int64 {
-	if live {
-		return int64(c.Amount * 100)
-	}
-
-	return 1
+func (c Charge) AmountInCent() int64 {
+	return int64(c.Amount * 100)
 }
 
 // ReadableAmount turns the charged amount to human readable format.
