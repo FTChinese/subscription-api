c1b23d7de13f6f1515f2e0c3e43683975f82ecf9
neefrankie <neefrankie@gmail.com>
2020-09-05 14:37:51 +0800

    refactor: removed logger

--- repository/readerrepo/env.go
+++ repository/readerrepo/env.go
@@ -3,12 +3,7 @@ package readerrepo
 import (
 	"github.com/FTChinese/subscription-api/pkg/config"
 	"github.com/jmoiron/sqlx"
-	"github.com/sirupsen/logrus"
 )
-
-var logger = logrus.
-	WithField("project", "subscription-api").
-	WithField("package", "reader_repo")
 
 type Env struct {
 	config.BuildConfig
