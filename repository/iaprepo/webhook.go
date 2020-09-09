a3afb62d0012322e47f25c92b6373298ba5f288d
weiguo.ni <neefrankie@gmail.com>
2020-09-09 16:17:43 +0800

    refactor: removed logrus

--- repository/iaprepo/webhook.go
+++ repository/iaprepo/webhook.go
@@ -8,8 +8,6 @@ func (env Env) SaveNotification(w apple.WebHookSchema) error {
 	_, err := env.db.NamedExec(apple.StmtLoggingWebhook, w)
 
 	if err != nil {
-		logger.WithField("trace", "Env.SaveNotification").Error(err)
-
 		return err
 	}
 
