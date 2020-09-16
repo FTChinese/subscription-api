4b57f9e158478e3daa7c38952677c120dccdaffa
weiguo.ni <neefrankie@gmail.com>
2020-09-16 11:10:42 +0800

    feat: add IsOrderNotFound

--- pkg/wechat/resp.go
+++ pkg/wechat/resp.go
@@ -8,12 +8,19 @@ import (
 	"github.com/objcoding/wxpay"
 )
 
+const (
+	ResultCodeOrderNotFound = "ORDERNOTEXIST"
+	ResultCodeSystemError   = "SYSTEMERROR"
+)
+
 // WxResp contains the shared fields all wechat pay endpoints contains.
 type Resp struct {
-	// return_code: SUCCESS/FAIL
+	// return_code: SUCCESS/FAIL.
+	// 此字段是通信标识，非交易标识，交易是否成功需要查看trade_state来判断
 	StatusCode string `db:"status_code"`
-	// return_msg
+	// return_msg. 返回信息，如非空，为错误原因
 	StatusMessage string `db:"status_message"`
+	// 以下字段在return_code为SUCCESS的时候有返回
 	// appid
 	AppID null.String `db:"app_id"`
 	// mch_id
@@ -23,10 +30,14 @@ type Resp struct {
 	// sign
 	Signature null.String `db:"signature"`
 	// result_code: SUCCESS/FAIL
+	// 业务结果
 	ResultCode null.String `db:"result_code"`
-	// err_code
+	// err_code 错误代码
+	// ORDERNOTEXIST
+	// SYSTEMERROR
 	ErrorCode null.String `db:"error_code"`
 	// err_code_des
+	// 错误代码描述
 	ErrorMessage null.String `db:"error_message"`
 }
 
@@ -98,7 +109,8 @@ func (r Resp) IsStatusValid() error {
 	}
 }
 
-// Validate wechat if wechat response is valid.
+// Validate wechat if wechat response itself is valid.
+// It does not indicate whether the payment is success or not.
 // This validation does not include notification since the
 // `wxpay` package does not provide such capability.
 // The `wxpay` pakcage already performs an initial check
@@ -107,21 +119,24 @@ func (r Resp) IsStatusValid() error {
 // is regarded as ok.
 // You have to check if return_code == SUCCESS, appid, mch_id, result_code are valid.
 func (r Resp) Validate(app PayApp) *render.ValidationError {
+	// If `return_code` is FAIL
+	// 此字段是通信标识，非交易标识，交易是否成功需要查看trade_state来判断
 	if r.StatusCode == wxpay.Fail {
 		reason := &render.ValidationError{
-			Message: r.StatusMessage,
-			Field:   "status",
-			Code:    "fail",
+			Message: r.StatusCode + ":" + r.StatusMessage,
+			Field:   "return_code",
+			Code:    render.CodeInvalid,
 		}
 
 		return reason
 	}
 
+	// If `result_code` is FAIL
 	if r.ResultCode.String == wxpay.Fail {
 		reason := &render.ValidationError{
-			Message: r.ErrorMessage.String,
-			Field:   "result",
-			Code:    render.InvalidCode(r.ErrorCode.String),
+			Message: r.ErrorCode.String + ":" + r.ErrorMessage.String,
+			Field:   "result_code",
+			Code:    render.CodeInvalid,
 		}
 
 		return reason
@@ -168,4 +183,8 @@ func (r Resp) Validate(app PayApp) *render.ValidationError {
 	}
 
 	return nil
+}
+
+func (r Resp) IsOrderNotFound() bool {
+	return r.ResultCode.String == ResultCodeOrderNotFound
 }
