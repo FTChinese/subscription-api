7b9a9751b9fa17d6ec9a753f97aaa59b79178b49
weiguo.ni <neefrankie@gmail.com>
2020-09-16 11:07:40 +0800

    refactor: add trade status message

--- pkg/ali/verify.go
+++ pkg/ali/verify.go
@@ -4,23 +4,38 @@ import (
 	"github.com/smartwalle/alipay"
 )
 
-type TradeStatus string
+const (
+	SubCodeSysErr       = "ACQ.SYSTEM_ERROR"
+	SubCodeInvalidParam = "ACQ.INVALID_PARAMETER"
+	SubCodeNotExists    = "ACQ.TRADE_NOT_EXIST"
+)
 
 const (
-	TradeStatusFinished TradeStatus = "TRADE_FINISHED"
-	TradeStatusSuccess  TradeStatus = "TRADE_SUCCESS"
-	TradeStatusPending  TradeStatus = "WAIT_BUYER_PAY"
-	TradeStatusClosed   TradeStatus = "TRADE_CLOSED"
-
-	tradeFinished = "TRADE_FINISHED"
-	tradeSuccess  = "TRADE_SUCCESS"
-	tradePending  = "WAIT_BUYER_PAY"
-	tradeClosed   = "TRADE_CLOSED"
+	TradeStatusFinished = "TRADE_FINISHED"
+	TradeStatusSuccess  = "TRADE_SUCCESS"
+	TradeStatusPending  = "WAIT_BUYER_PAY"
+	TradeStatusClosed   = "TRADE_CLOSED"
 )
+
+var TradeStatusMsg = map[string]string{
+	TradeStatusPending:  "交易创建，等待付款",
+	TradeStatusClosed:   "交易关闭",
+	TradeStatusSuccess:  "交易支付成功",
+	TradeStatusFinished: "交易结束",
+}
 
 func IsPaySuccess(n *alipay.TradeNotification) bool {
 	switch n.TradeStatus {
-	case tradeSuccess, tradeFinished:
+	case TradeStatusSuccess, TradeStatusFinished:
+		return true
+	default:
+		return false
+	}
+}
+
+func IsStatusSuccess(s string) bool {
+	switch s {
+	case TradeStatusSuccess, TradeStatusFinished:
 		return true
 	default:
 		return false
@@ -29,7 +44,7 @@ func IsPaySuccess(n *alipay.TradeNotification) bool {
 
 func ShouldRetry(n *alipay.TradeNotification) bool {
 	switch n.TradeStatus {
-	case tradePending:
+	case TradeStatusPending:
 		return true
 	default:
 		return false
