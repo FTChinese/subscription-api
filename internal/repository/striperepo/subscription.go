3217120b202176a1d78292628d9185a26990721e
weiguo.ni <neefrankie@gmail.com>
2020-10-16 17:13:33 +0800

    refactor: GetSubscription signature

--- internal/repository/striperepo/subscription.go
+++ internal/repository/striperepo/subscription.go
@@ -11,6 +11,13 @@ import (
 )
 
 // CreateSubscription creates a new subscription.
+// If performs multiple steps:
+// 1. Find existing membership if exist;
+// 2. Check what kind of subscription it is, and whether we allow it to continue;
+// 3. Create subscription at Stripe API.
+// 4. Build FTC membership from Stripe subscription.
+// It returns stripe's subscription as is.
+//
 // error could be:
 // util.ErrNonStripeValidSub
 // util.ErrActiveStripeSub
@@ -31,6 +38,7 @@ func (env Env) CreateSubscription(input ftcStripe.SubsInput) (*stripe.Subscripti
 		FtcID:      null.StringFrom(input.FtcID),
 		UnionID:    null.String{},
 	}.MustNormalize())
+	sugar.Infof("A stripe membership: %+v", mmb)
 
 	if err != nil {
 		sugar.Error(err)
@@ -52,7 +60,6 @@ func (env Env) CreateSubscription(input ftcStripe.SubsInput) (*stripe.Subscripti
 	}
 
 	sugar.Info("Creating stripe subscription")
-
 	// Contact Stripe API.
 	ss, err := input.CreateSubs()
 
@@ -66,9 +73,7 @@ func (env Env) CreateSubscription(input ftcStripe.SubsInput) (*stripe.Subscripti
 		return nil, err
 	}
 
-	sugar.Infof("A stripe membership: %+v", mmb)
-
-	// What if the user exists but is invalid?
+	// Create membership from Stripe subscription.
 	if mmb.IsZero() {
 		newMmb := input.NewMembership(ss)
 		sugar.Infof("A new stripe membership: %+v", newMmb)
@@ -105,7 +110,7 @@ func (env Env) SaveSubsError(e ftcStripe.APIError) error {
 }
 
 // GetSubscription refresh stripe subscription data if stale.
-func (env Env) GetSubscription(id reader.MemberID) (*stripe.Subscription, error) {
+func (env Env) GetSubscription(ftcID string) (*stripe.Subscription, error) {
 	defer env.logger.Sync()
 	sugar := env.logger.Sugar()
 
@@ -115,7 +120,11 @@ func (env Env) GetSubscription(id reader.MemberID) (*stripe.Subscription, error)
 		return nil, err
 	}
 
-	mmb, err := tx.RetrieveMember(id)
+	mmb, err := tx.RetrieveMember(reader.MemberID{
+		CompoundID: "",
+		FtcID:      null.StringFrom(ftcID),
+		UnionID:    null.String{},
+	}.MustNormalize())
 	if err != nil {
 		sugar.Error(err)
 		_ = tx.Rollback()
@@ -123,7 +132,7 @@ func (env Env) GetSubscription(id reader.MemberID) (*stripe.Subscription, error)
 	}
 
 	if mmb.IsZero() {
-		sugar.Infof("Membership not found for %v", id)
+		sugar.Infof("Membership not found for %v", ftcID)
 		_ = tx.Rollback()
 		return nil, sql.ErrNoRows
 	}
