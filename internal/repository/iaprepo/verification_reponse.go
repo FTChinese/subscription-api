497815acf15d7a2cc52d0f7779d544b38fa1ff0c
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:25:31 +0800

    refactor: save iap response

--- internal/repository/iaprepo/verification_reponse.go
+++ internal/repository/iaprepo/verification_reponse.go
@@ -17,7 +17,7 @@ import (
 // And all should record those data:
 // Transaction array
 // PendingRenewal array
-// ReceiptToken
+// ReceiptSchema
 // Subscription
 
 // SaveDecodedReceipt saved the decoded apple.ClientReceipt from response body.
@@ -48,20 +48,6 @@ func (env Env) SaveTransaction(r apple.TransactionSchema) error {
 // Duplicate entries are ignores.
 func (env Env) SavePendingRenewal(p apple.PendingRenewalSchema) error {
 	_, err := env.db.NamedExec(apple.StmtSavePendingRenewal, p)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// SaveReceiptTokenDB saves the base-64 encoded receipt data
-// for one original transaction id.
-// It is not in use since we changed to disk files.
-// TODO: try to save it in HBase.
-func (env Env) SaveReceiptTokenDB(r apple.ReceiptToken) error {
-	_, err := env.db.NamedExec(apple.StmtSaveReceiptToken, r)
 
 	if err != nil {
 		return err
@@ -103,26 +89,8 @@ func (env Env) SaveResponsePayload(ur apple.UnifiedReceipt) {
 		}
 	}()
 
-	rt := ur.ReceiptToken()
-
-	// Save the LatestReceipt field to a file.
-	// Initially it was designed to save in SQL.
-	sugar.Info("Saving receipt to disk")
+	// Save receipt file.
 	go func() {
-		_ = SaveReceiptTokenFile(rt)
-	}()
-
-	// Save in redis
-	sugar.Infof("Saving receipt to redis")
-	go func() {
-		err := env.rdb.Set(
-			ctx,
-			rt.ReceiptKeyName(),
-			rt.LatestReceipt,
-			0,
-		).Err()
-		if err != nil {
-			sugar.Errorf("error saving receipt to redis: %s", err)
-		}
+		env.SaveReceipt(apple.NewReceiptSchema(ur))
 	}()
 }
