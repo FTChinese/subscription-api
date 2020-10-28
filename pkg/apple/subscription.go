0813693691440716ba7be6d83fbefbcc09b30f0a
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:39:02 +0800

    feat: add NewSubscription

--- pkg/apple/subscription.go
+++ pkg/apple/subscription.go
@@ -7,6 +7,7 @@ import (
 	"github.com/FTChinese/subscription-api/pkg/product"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/guregu/null"
+	"time"
 )
 
 type SubsFilter struct {
@@ -29,6 +30,78 @@ type Subscription struct {
 	AutoRenewal bool        `json:"autoRenewal" db:"auto_renewal"`
 	CreatedUTC  chrono.Time `json:"createdUtc" db:"created_utc"`
 	UpdatedUTC  chrono.Time `json:"updatedUtc" db:"updated_utc"`
+	FtcUserID   null.String `json:"ftcUserId" db:"ftc_user_id"` // This field is only manipulated upon link/unlink. We won't touch it upon insert/create.
+}
+
+// NewSubscription builds a subscription for a user based on
+// the receipt information available.
+// Returns Subscription or error if the corresponding product is not found for this transaction.
+// When we build a new Subscription from apple verification response,
+// we do no know user's ftc id,  so leave it empty.
+// And do not touch the ftc_user_id field when you inserting/updating a Subscription.
+func NewSubscription(u UnifiedReceipt) (Subscription, error) {
+	pendingRenewal := u.findPendingRenewal()
+
+	autoRenew := pendingRenewal.IsAutoRenew()
+	if u.latestTransaction.IsCancelled() {
+		autoRenew = false
+	}
+
+	prod, err := appleProducts.findByID(u.latestTransaction.ProductID)
+
+	if err != nil {
+		return Subscription{}, err
+	}
+
+	return Subscription{
+		BaseSchema: BaseSchema{
+			Environment:           u.Environment,
+			OriginalTransactionID: u.latestTransaction.OriginalTransactionID,
+		},
+		LastTransactionID: u.latestTransaction.TransactionID,
+		ProductID:         u.latestTransaction.ProductID,
+		PurchaseDateUTC: chrono.TimeFrom(
+			time.Unix(u.latestTransaction.PurchaseDateUnix(), 0),
+		),
+		ExpiresDateUTC: chrono.TimeFrom(
+			time.Unix(u.latestTransaction.ExpiresUnix(), 0),
+		),
+		Edition:     prod.Edition,
+		AutoRenewal: autoRenew,
+		CreatedUTC:  chrono.TimeNow(),
+		UpdatedUTC:  chrono.TimeNow(),
+	}, nil
+}
+
+// PermitLink checks whether a Subscription is allowed to link
+// to an ftc  account. It is allowed only when the FtcUserID
+// field is still empty, or is the same as the target id.
+func (s Subscription) PermitLink(ftcID string) bool {
+	if s.FtcUserID.IsZero() {
+		return true
+	}
+
+	if s.FtcUserID.String == ftcID {
+		return true
+	}
+
+	return false
+}
+
+func (s Subscription) ShouldUpdate(m reader.Membership) bool {
+	if !m.IsIAP() {
+		return false
+	}
+
+	if s.ExpiresDateUTC.Before(m.ExpireDate.Time) {
+		return false
+	}
+
+	if s.ExpiresDateUTC.Equal(m.ExpireDate.Time) {
+		return false
+	}
+
+	return true
 }
 
 // Membership build ftc's membership based on subscription
