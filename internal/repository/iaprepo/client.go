528d49b142c6d08589990144091ebec5e8235483
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:28:38 +0800

    feat: add logger

--- internal/repository/iaprepo/client.go
+++ internal/repository/iaprepo/client.go
@@ -1,10 +1,11 @@
 package iaprepo
 
 import (
+	"encoding/json"
 	"github.com/FTChinese/subscription-api/pkg/apple"
 	"github.com/FTChinese/subscription-api/pkg/config"
 	"github.com/FTChinese/subscription-api/pkg/fetch"
-	"io/ioutil"
+	"go.uber.org/zap"
 	"log"
 )
 
@@ -13,14 +14,16 @@ type Client struct {
 	sandboxUrl string
 	prodUrl    string
 	password   string
+	logger     *zap.Logger
 }
 
-func NewClient(sandbox bool) Client {
+func NewClient(sandbox bool, logger *zap.Logger) Client {
 	return Client{
 		isSandbox:  sandbox,
 		sandboxUrl: "https://sandbox.itunes.apple.com/verifyReceipt",
 		prodUrl:    "https://buy.itunes.apple.com/verifyReceipt",
 		password:   config.MustIAPSecret(),
+		logger:     logger,
 	}
 }
 
@@ -34,21 +37,34 @@ func (c Client) pickUrl() string {
 	return c.prodUrl
 }
 
-func (c Client) Verify(receipt string) ([]byte, error) {
+func (c Client) Verify(receipt string) (apple.VerificationResp, error) {
+	defer c.logger.Sync()
+	sugar := c.logger.Sugar()
+
 	payload := apple.VerificationPayload{
 		ReceiptData:            receipt,
 		Password:               c.password,
 		ExcludeOldTransactions: false,
 	}
 
-	resp, errs := fetch.New().
+	resp, b, errs := fetch.New().
 		Post(c.pickUrl()).
 		SendJSON(payload).
-		End()
+		EndRaw()
+
+	sugar.Infof("App store response status code %d", resp.StatusCode)
+	sugar.Infof("App store verification raw content %s", resp.StatusCode, b)
 
 	if errs != nil {
-		return nil, errs[0]
+		return apple.VerificationResp{}, errs[0]
 	}
 
-	return ioutil.ReadAll(resp.Body)
+	var vrfResult apple.VerificationResp
+	if err := json.Unmarshal(b, &vrfResult); err != nil {
+		return apple.VerificationResp{}, err
+	}
+
+	sugar.Infof("Environment %s, is retryable %t, status %d", vrfResult.Environment, vrfResult.IsRetryable, vrfResult.Status)
+
+	return vrfResult, nil
 }
