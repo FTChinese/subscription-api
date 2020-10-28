a532f3b8c8a59829f3dee87fe1a4a5511916b566
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:34:17 +0800

    refactor: removed order builder

--- pkg/subs/order_builder_test.go
+++ pkg/subs/order_builder_test.go
@@ -3,15 +3,9 @@ package subs
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/pkg/product"
-	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/FTChinese/subscription-api/pkg/wechat"
-	"github.com/brianvoe/gofakeit/v5"
-	"github.com/google/uuid"
 	"github.com/guregu/null"
-	"github.com/stretchr/testify/assert"
-	"testing"
 	"time"
 )
 
@@ -46,99 +40,4 @@ func mustGetWxClient() wechat.Client {
 	}
 
 	return client
-}
-
-func mustWxPayBuilder() *OrderBuilder {
-	faker.SeedGoFake()
-
-	return NewOrderBuilder(reader.MemberID{
-		CompoundID: "",
-		FtcID:      null.StringFrom(uuid.New().String()),
-		UnionID:    null.String{},
-	}.MustNormalize()).
-		SetPlan(planStdYear).
-		SetPayMethod(enum.PayMethodWx).
-		SetTest(false).
-		SetWxAppID(mustGetWxClient().GetApp().AppID).
-		SetWxParams(wechat.UnifiedOrder{
-			IP:        gofakeit.IPv4Address(),
-			TradeType: wechat.TradeTypeApp,
-		})
-}
-
-func TestOrderBuilder_getWebHookURL(t *testing.T) {
-	builder := NewOrderBuilder(reader.MemberID{
-		CompoundID: "",
-		FtcID:      null.StringFrom(uuid.New().String()),
-		UnionID:    null.String{},
-	}.MustNormalize()).
-		SetPayMethod(enum.PayMethodWx).
-		SetTest(false)
-
-	wh := builder.getWebHookURL()
-
-	assert.Equal(t, wh, "http://www.ftacademy.cn/api/v1/webhook/wxpay")
-}
-
-func TestOrderBuilder_Build(t *testing.T) {
-	builder := mustWxPayBuilder()
-
-	err := builder.DeduceSubsKind(reader.Membership{})
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = builder.Build()
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestOrderBuilder_GetOrder(t *testing.T) {
-	builder := mustWxPayBuilder()
-
-	err := builder.DeduceSubsKind(reader.Membership{})
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = builder.Build()
-	if err != nil {
-		t.Error(err)
-	}
-
-	o, err := builder.GetOrder()
-	if err != nil {
-		t.Error(err)
-	}
-
-	assert.NotZero(t, o.ID)
-	assert.NotZero(t, o.Price)
-	assert.NotZero(t, o.Amount)
-}
-
-func TestOrderBuilder_AliAppPayParams(t *testing.T) {
-
-	builder := mustWxPayBuilder()
-
-	err := builder.DeduceSubsKind(reader.Membership{})
-	if err != nil {
-		t.Error(err)
-	}
-
-	err = builder.Build()
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	order, err := builder.GetOrder()
-
-	t.Logf("%+v", order)
-
-	aliPayIntent := builder.AliAppPayParams()
-
-	assert.NotEqual(t, aliPayIntent.TotalAmount, 0.01)
-
-	t.Logf("%+v", aliPayIntent)
 }
