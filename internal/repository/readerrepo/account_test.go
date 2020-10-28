8b07deba0679a98ecd8218a30cd5ac1c464b830f
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:29:33 +0800

    refactor: rename

--- internal/repository/readerrepo/account_test.go
+++ internal/repository/readerrepo/account_test.go
@@ -28,7 +28,7 @@ func TestReaderEnv_FindAccountByFtcID(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			got, err := env.FtcAccountByFtcID(tt.args.id)
+			got, err := env.AccountByFtcID(tt.args.id)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("FindAccountByFtcID() error = %v, wantErr %v", err, tt.wantErr)
 				return
