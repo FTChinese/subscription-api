37689731d96f3f098c365975cb5532e7766ed005
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:41:27 +0800

    refactor

--- pkg/product/plan_test.go
+++ pkg/product/plan_test.go
@@ -4,9 +4,6 @@ import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/guregu/null"
-	"github.com/stretchr/testify/assert"
-	"reflect"
-	"testing"
 	"time"
 )
 
@@ -59,81 +56,4 @@ var planPrm = ExpandedPlan{
 		Description: null.String{},
 	},
 	Discount: Discount{},
-}
-
-func TestExpandedPlan_Amount(t *testing.T) {
-	type fields struct {
-		Plan ExpandedPlan
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   float64
-	}{
-		{
-			name: "Standard yearly plan",
-			fields: fields{
-				Plan: planStdYear,
-			},
-			want: 128,
-		},
-		{
-			name:   "Standard monthly plan",
-			fields: fields{Plan: planStdMonth},
-			want:   28,
-		},
-		{
-			name:   "Premium plan",
-			fields: fields{Plan: planPrm},
-			want:   1998,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			e := tt.fields.Plan
-
-			got := e.Amount()
-
-			assert.Equal(t, got, tt.want)
-		})
-	}
-}
-
-func TestExpandedPlan_Payable(t *testing.T) {
-
-	tests := []struct {
-		name   string
-		fields ExpandedPlan
-		want   Charge
-	}{
-		{
-			name:   "With discount",
-			fields: planStdYear,
-			want: Charge{
-				Amount:     128,
-				DiscountID: null.StringFrom("dsc_F7gEwjaF3OsR"),
-				Currency:   "cny",
-			},
-		},
-		{
-			name:   "No discount",
-			fields: planStdMonth,
-			want: Charge{
-				Amount:     28,
-				DiscountID: null.String{},
-				Currency:   "cny",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			e := ExpandedPlan{
-				Plan:     tt.fields.Plan,
-				Discount: tt.fields.Discount,
-			}
-			if got := e.Payable(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Payable() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
