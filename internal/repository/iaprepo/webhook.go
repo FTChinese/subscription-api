d36484f8acd067a9da46f05b5c9831743e8b780b
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:23:51 +0800

    refactor: rename

--- internal/repository/iaprepo/webhook.go
+++ internal/repository/iaprepo/webhook.go
@@ -4,7 +4,7 @@ import (
 	"github.com/FTChinese/subscription-api/pkg/apple"
 )
 
-func (env Env) SaveNotification(w apple.WebHookSchema) error {
+func (env Env) SaveWebhook(w apple.WebHookSchema) error {
 	_, err := env.db.NamedExec(apple.StmtLoggingWebhook, w)
 
 	if err != nil {
