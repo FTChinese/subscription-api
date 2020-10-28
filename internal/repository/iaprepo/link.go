0f45cab5e12d1f292c260e1b7b36af2ef8aa9d0b
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:33:31 +0800

    refactor: iap link process

--- internal/repository/iaprepo/link.go
+++ internal/repository/iaprepo/link.go
@@ -2,9 +2,6 @@ package iaprepo
 
 import (
 	"database/sql"
-	"errors"
-
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/FTChinese/subscription-api/pkg/apple"
 	"github.com/FTChinese/subscription-api/pkg/reader"
@@ -30,87 +27,98 @@ import (
 // This is a suspicious operation that should always be denied.
 // The returned error could be *render.ValidationError
 // if link if forbidden.
-func (env Env) Link(account reader.FtcAccount, iapSubs apple.Subscription) (apple.SubsResult, error) {
+func (env Env) Link(input apple.LinkInput, account reader.FtcAccount) (apple.LinkResult, error) {
 	defer env.logger.Sync()
 	sugar := env.logger.Sugar()
 
 	tx, err := env.BeginTx()
 	if err != nil {
 		sugar.Error(err)
-		return apple.SubsResult{}, err
+		return apple.LinkResult{}, err
 	}
 
-	// Try to retrieve membership by apple original transaction id.
-	iapMember, err := tx.RetrieveAppleMember(iapSubs.OriginalTransactionID)
+	sub, err := tx.RetrieveAppleSubs(input.OriginalTxID)
 	if err != nil {
 		sugar.Error(err)
 		_ = tx.Rollback()
-		return apple.SubsResult{}, err
+		return apple.LinkResult{}, err
+	}
+
+	// Possible cheating.
+	if !sub.PermitLink(input.FtcID) {
+		_ = tx.Rollback()
+		return apple.LinkResult{}, apple.ErrIAPAlreadyLinked
+	}
+
+	// Try to retrieve membership by apple original transaction id.
+	iapMember, err := tx.RetrieveAppleMember(input.OriginalTxID)
+	if err != nil {
+		sugar.Error(err)
+		_ = tx.Rollback()
+		return apple.LinkResult{}, err
 	}
 	// Try to retrieve membership by ftc id.
 	ftcMember, err := tx.RetrieveMember(account.MemberID())
 	if err != nil {
 		sugar.Error(err)
 		_ = tx.Rollback()
-		return apple.SubsResult{}, err
+		return apple.LinkResult{}, err
 	}
 
-	// Merge two memberships.
-	// If iap membership is already linked, the merged
-	// membership won't be changed and we only need to
-	// update it based on apple transaction.
-	err = ftcMember.ValidateMergeIAP(iapMember, iapSubs.ExpiresDateUTC)
+	builder := apple.LinkBuilder{
+		Account:    account,
+		CurrentFtc: ftcMember,
+		CurrentIAP: iapMember,
+		IAPSubs:    sub,
+	}
+
+	result, err := builder.Build()
 	if err != nil {
-		sugar.Error(err)
-
-		if errors.Is(err, reader.ErrIAPFtcLinked) {
-			_ = tx.Rollback()
-			return apple.SubsResult{
-				InitialLink: false,
-				Member:      iapMember,
-				Snapshot:    reader.MemberSnapshot{},
-			}, nil
-		}
-
-		_ = tx.Rollback()
-		return apple.SubsResult{}, err
+		return apple.LinkResult{}, err
 	}
-
-	// If reached here, possible cases of FTC and IAP:
-	// FTC	    |  IAP
-	// ----------------
-	// zero	    |  zero  | No backup
-	// Equal    |  Equal | Stop
-	// Expired  |  zero  | Backup FTC and Update
-	// -----------------
-	// From this table we can see we only need to backup the FTC side if it exists.
-	var newMmb = iapSubs.NewMembership(account.MemberID())
-
-	if !ftcMember.IsZero() {
+	// If membership is take a snapshot, we must delete it.
+	if !result.Snapshot.IsZero() {
 		err := tx.DeleteMember(ftcMember.MemberID)
 		if err != nil {
 			sugar.Error(err)
 			_ = tx.Rollback()
-			return apple.SubsResult{}, err
+			return apple.LinkResult{}, err
+		}
+	}
+	// Save membership only when it is touched.
+	if result.Touched {
+		err := tx.CreateMember(result.Member)
+		if err != nil {
+			sugar.Error(err)
+			_ = tx.Rollback()
+			return apple.LinkResult{}, err
 		}
 	}
 
-	err = tx.CreateMember(newMmb)
-	if err != nil {
-		sugar.Error(err)
-		_ = tx.Rollback()
-		return apple.SubsResult{}, err
+	// Set ftc_user_id field if it is empty.
+	if sub.FtcUserID.IsZero() {
+		err := tx.LinkAppleSubs(input)
+		if err != nil {
+			sugar.Error(err)
+			_ = tx.Rollback()
+			return apple.LinkResult{}, err
+		}
 	}
 
 	if err := tx.Commit(); err != nil {
-		return apple.SubsResult{}, err
+		return apple.LinkResult{}, err
 	}
 
-	return apple.SubsResult{
-		InitialLink: iapMember.IsZero(), // As long as iap side is zero, this is initial link.
-		Member:      newMmb,
-		Snapshot:    ftcMember.Snapshot(enum.SnapshotReasonAppleLink),
-	}, nil
+	return result, nil
+}
+
+func (env Env) ArchiveLinkCheating(link apple.LinkInput) error {
+	_, err := env.db.NamedExec(apple.StmtArchiveLinkCheat, link)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Unlink deletes a membership created from IAP.
@@ -118,22 +126,28 @@ func (env Env) Link(account reader.FtcAccount, iapSubs apple.Subscription) (appl
 // unlinking IAP must delete the membership since IAP's owner should be unknown
 // after link severed.
 func (env Env) Unlink(input apple.LinkInput) (reader.MemberSnapshot, error) {
+	defer env.logger.Sync()
+	sugar := env.logger.Sugar()
+
 	tx, err := env.BeginTx()
 
 	if err != nil {
 		return reader.MemberSnapshot{}, err
 	}
 
+	// Find current membership by original transaction id.
 	m, err := tx.RetrieveAppleMember(input.OriginalTxID)
 	if err != nil {
 		_ = tx.Rollback()
 		return reader.MemberSnapshot{}, err
 	}
+	// If membership is not found, stop.
 	if m.IsZero() {
 		_ = tx.Rollback()
 		return reader.MemberSnapshot{}, sql.ErrNoRows
 	}
 
+	// If the found membership's ftc user id does not match the requested ftc user id, stop.
 	if m.FtcID.String != input.FtcID {
 		_ = tx.Rollback()
 		return reader.MemberSnapshot{}, &render.ValidationError{
@@ -143,14 +157,41 @@ func (env Env) Unlink(input apple.LinkInput) (reader.MemberSnapshot, error) {
 		}
 	}
 
+	// Delete this membership.
 	if err := tx.DeleteMember(m.MemberID); err != nil {
 		_ = tx.Rollback()
 		return reader.MemberSnapshot{}, err
+	}
+
+	// Try to remove ftc_user_id from apple_subscription.
+	// Ignore errors.
+	sub, err := tx.RetrieveAppleSubs(input.OriginalTxID)
+	if err != nil {
+		sugar.Error(err)
+	}
+
+	// Remove the ftc user id in apple_subscription.
+	// Ignore errors.
+	if sub.FtcUserID.String == input.FtcID {
+		err := tx.UnlinkAppleSubs(input)
+		if err != nil {
+			sugar.Error(err)
+		}
 	}
 
 	if err := tx.Commit(); err != nil {
 		return reader.MemberSnapshot{}, err
 	}
 
-	return m.Snapshot(enum.SnapshotReasonAppleUnlink), nil
+	// Return the snapshot of the membership for archiving.
+	return m.Snapshot(reader.ArchiverAppleUnlink), nil
+}
+
+func (env Env) ArchiveUnlink(link apple.LinkInput) error {
+	_, err := env.db.NamedExec(apple.StmtArchiveUnlink, link)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
