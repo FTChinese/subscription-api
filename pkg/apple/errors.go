d45feee400993a9dafbdf711f47c3b98c8d49411
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:31:14 +0800

    feat: errors

--- pkg/apple/errors.go
+++ pkg/apple/errors.go
@@ -3,5 +3,10 @@ package apple
 import "errors"
 
 var (
-	ErrUnlinkMismatchedFTC = errors.New("IAP subscription is not linked to the provided ftc id")
+	// This error is a possible cheating.
+	ErrIAPAlreadyLinked = errors.New("iap subscription is already taken by another ftc account")
+	// This error might be caused by one ftc account linking to multiple IAP.
+	ErrFtcAlreadyLinked = errors.New("ftc account is already linked to an iap subscription")
+	// Ftc account has a non-expired membership that comes from other payment method, thus we cannot override it.
+	ErrFtcMemberValid = errors.New("ftc account has non-expired, non-iap membership")
 )
