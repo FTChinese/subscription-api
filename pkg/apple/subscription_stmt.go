cc6fb92e7b545e0c025936ffaa23864fe8523c7c
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:39:23 +0800

    refactor: stmt for link

--- pkg/apple/subscription_stmt.go
+++ pkg/apple/subscription_stmt.go
@@ -1,6 +1,6 @@
 package apple
 
-const colsInsertSubs = `
+const colsUpsertSubs = `
 environment = :environment,
 last_transaction_id = :last_transaction_id,
 product_id = :product_id,
@@ -15,10 +15,10 @@ updated_utc = UTC_TIMESTAMP()
 const StmtUpsertSubs = `
 INSERT INTO premium.apple_subscription
 SET original_transaction_id = :original_transaction_id,
-` + colsInsertSubs + `,
+` + colsUpsertSubs + `,
 	created_utc = UTC_TIMESTAMP()
-ON DUPLICATE KEY UPDATE
-` + colsInsertSubs
+ON DUPLICATE KEY  UPDATE
+` + colsUpsertSubs
 
 const colsSubs = `
 SELECT environment,
@@ -31,7 +31,8 @@ SELECT environment,
 	cycle,
 	auto_renewal,
 	created_utc,
-	updated_utc
+	updated_utc,
+	ftc_user_id
 FROM premium.apple_subscription
 `
 
@@ -39,10 +40,38 @@ const StmtLoadSubs = colsSubs + `
 WHERE original_transaction_id = ?
 LIMIT 1`
 
+const StmtLockSubs = StmtLoadSubs + `
+FOR UPDATE`
+
 const StmtListSubs = colsSubs + `
-ORDER BY updated_utc DESC
+WHERE ftc_user_id = ?
+ORDER BY expires_date_utc DESC
 LIMIT ? OFFSET ?`
 
 const StmtCountSubs = `
 SELECT COUNT(*) AS row_count
 FROM premium.apple_subscription`
+
+const StmtLinkSubs = `
+UPDATE premium.apple_subscription
+SET ftc_user_id = :ftc_user_id
+WHERE original_transaction_id = :original_transaction_id
+LIMIT 1`
+
+const StmtUnlinkSubs = `
+UPDATE premium.apple_subscription
+SET ftc_user_id = NULL
+WHERE original_transaction_id = :original_transaction_id
+LIMIT 1`
+
+const StmtArchiveLinkCheat = `
+INSERT INTO premium.apple_cheat
+SET original_transaction_id = :original_transaction_id
+	ftc_user_id = :ftc_user_id,
+	created_utc = UTC_TIMESTAMP()`
+
+const StmtArchiveUnlink = `
+INSERT INTO premium.apple_unlink_archive
+SET original_transaction_id = :original_transaction_id
+	ftc_user_id = :ftc_user_id,
+	created_utc = UTC_TIMESTAMP()`
