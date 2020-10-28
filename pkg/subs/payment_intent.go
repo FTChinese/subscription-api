ddcebdc54895ea37a25958e97a53da5b01281e5d
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:35:17 +0800

    refactor: rename

--- pkg/subs/payment_intent.go
+++ pkg/subs/payment_intent.go
@@ -1,13 +1,11 @@
 package subs
 
 import (
-	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/subscription-api/pkg/product"
 	"github.com/FTChinese/subscription-api/pkg/wechat"
 )
 
-// WxpayNativeAppOrder creates an order used by native apps.
-type WxpayNativeAppOrder struct {
+// WxpayNativeAppIntent creates an order used by native apps.
+type WxpayNativeAppIntent struct {
 	Order
 	wechat.AppOrderParams
 	Params wechat.AppOrderParams `json:"params"`
@@ -19,46 +17,34 @@ type WxpayNativeAppOrder struct {
 // perform actions inside wechat app.
 // It's a shame wechat cannot even use the same data structure
 // for such insignificant differences.
-type WxpayEmbedBrowserOrder struct {
+type WxpayEmbedBrowserIntent struct {
 	Order
 	Params wechat.InWxBrowserParams `json:"params"`
 }
 
-// WxpayBrowserOrder creates order for payment via wechat
+// WxpayBrowserIntent creates order for payment via wechat
 // made in browsers.
 // For desktop browser, wechat send back a custom url
 // for the client to generate a QR image;
 // For mobile browser, wechat sends back a canonical url
 // that can be redirected to.
 // and MobileOrder into a single data structure.
-type WxpayBrowserOrder struct {
+type WxpayBrowserIntent struct {
 	Order
 	// TODO: rename json tag codeUrl to qrCode
 	QRCode  string `json:"qrCodeUrl,omitempty"`         // Used by desktop browser. It is a custom url like wexin://wxpay/bizpayurl
 	MWebURL string `json:"mobileRedirectUrl,omitempty"` // This is a standard url that can be redirected to.
 }
 
-// AlipayBrowserOrder represents an order creates for alipay inside
+// AlipayBrowserIntent represents an order creates for alipay inside
 // browsers
-type AlipayBrowserOrder struct {
+type AlipayBrowserIntent struct {
 	Order
 	RedirectURL string `json:"redirectUrl"`
 }
 
 // AliPayNative is an order created inside a native app.
-type AlipayNativeAppOrder struct {
+type AlipayNativeIntent struct {
 	Order
-	//FtcOrderID string `json:"ftcOrderId"` // Deprecate
 	Param string `json:"param"`
-}
-
-// PaymentIntent contains the data describing user's intent to pay.
-// The data are constructed prior to payment.
-type PaymentIntent struct {
-	product.Charge   // How much user should pay.
-	product.Duration // How long the membership this payment purchased.
-
-	SubsKind enum.OrderKind     `json:"subscriptionKind"`
-	Wallet   Wallet             `json:"wallet"`
-	Plan     product.IntentPlan `json:"plan"` // The plan to subscribe.
 }
