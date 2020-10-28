987af669f1a2bd3edbbe66617be6691036761a57
weiguo.ni <neefrankie@gmail.com>
2020-10-28 17:30:59 +0800

    refactor: doc

--- pkg/product/duration.go
+++ pkg/product/duration.go
@@ -1,6 +1,6 @@
 package product
 
-// Duration is the number of billing cycles and trial period a subscription plan provides.
+// Duration is the number of billing cycles and trial days a subscription purchased.
 type Duration struct {
 	CycleCount int64 `json:"cycleCount" db:"cycle_count"`
 	ExtraDays  int64 `json:"extraDays" db:"extra_days"`
