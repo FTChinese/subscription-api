bf2d8a0f8935153ee9eb856752b8eb52f8206525
neefrankie <neefrankie@gmail.com>
2020-11-01 11:56:58 +0800

    refactor: cleanup

--- pkg/subs/errors.go
+++ pkg/subs/errors.go
@@ -3,10 +3,6 @@ package subs
 import "errors"
 
 var (
-	// In case the plan is not set in OrderBuilder
-	ErrInvalidPlan = errors.New("no pricing plan is provided")
-
 	// Reasons to deny upgrade.
-	ErrUpgradeInvalid            = errors.New("upgrade could only be performed on an existing non-premium membership purchased via alipay or wxpay")
-	ErrBalanceCannotCoverUpgrade = errors.New("current balance is not enough to cover upgrading cost")
+	ErrUpgradeInvalid = errors.New("upgrade could only be performed on an existing non-premium membership purchased via alipay or wxpay")
 )
