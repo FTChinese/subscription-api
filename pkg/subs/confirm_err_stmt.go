8ed4ad7def8186e11ff1ab78528869207e5fc4fe
neefrankie <neefrankie@gmail.com>
2020-11-01 11:50:04 +0800

    refactor: deleted

--- pkg/subs/confirm_err_stmt.go
+++
@@ -1,8 +0,0 @@
-package subs
-
-const StmtSaveConfirmResult = `
-INSERT INTO premium.confirmation_result
-SET order_id = :order_id,
-	succeeded = :succeeded,
-	failed = :failed,
-	created_utc = UTC_TIMESTAMP()`
