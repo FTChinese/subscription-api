cd812e53e07af95e5bf9019a934ab747846cd9aa
neefrankie <neefrankie@gmail.com>
2020-11-02 00:25:30 +0800

    fix: sql

--- pkg/reader/account_stmt.go
+++ pkg/reader/account_stmt.go
@@ -19,12 +19,12 @@ const StmtAccountByWx = `
 SELECT
 	IFNULL(u.user_id, '')	AS ftc_id,
 	w.union_id				AS union_id,
-	u.stripe.customer_id	AS stripe_id,
+	u.stripe_customer_id	AS stripe_id,
 	IFNULL(u.email, '')		AS email,
 	u.user_name				AS user_name,
 	IFNULL(u.is_vip, FALSE) AS is_vip
 FROM user_db.wechat_userinfo AS w
-LEFT JOIN cmstmp01.userinfo AS u
+	LEFT JOIN cmstmp01.userinfo AS u
 	ON w.union_id = u.wx_union_id
 WHERE w.union_id = ?
 LIMIT 1`
