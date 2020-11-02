47e51548514f719995dca41cf2017c1a08625e1e
weiguo.ni <neefrankie@gmail.com>
2020-11-02 10:38:35 +0800

    refactor: missed wx app id in order

--- internal/controller/helper.go
+++ internal/controller/helper.go
@@ -15,7 +15,6 @@ import (
 
 const (
 	wxOAuthCallback = "http://users.ftchinese.com/login/wechat/callback?"
-	wxAppNativeApp  = "***REMOVED***" // Used by native app to pay and log in.
 )
 
 // getURLParam gets a url parameter.
