edec3b02180977ffab1021d4d60d4030855c191d
weiguo.ni <neefrankie@gmail.com>
2020-11-02 10:27:40 +0800

    refactor: regroup

--- test/iap.go
+++ test/iap.go
@@ -4,9 +4,11 @@ package test
 
 import (
 	"encoding/json"
+	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/rand"
 	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/pkg/apple"
+	"time"
 )
 
 func MustVerificationResponse() *apple.VerificationResp {
@@ -34,4 +36,32 @@ func MustPendingRenewal() apple.PendingRenewal {
 	l := len(resp.PendingRenewalInfo)
 
 	return resp.PendingRenewalInfo[rand.IntRange(0, l)]
+}
+
+func (p *Persona) IAPSubs() apple.Subscription {
+	s := apple.Subscription{
+		BaseSchema: apple.BaseSchema{
+			Environment:           apple.EnvSandbox,
+			OriginalTransactionID: p.AppleSubID,
+		},
+		LastTransactionID: faker.GenAppleSubID(),
+		ProductID:         "",
+		PurchaseDateUTC:   chrono.TimeNow(),
+		ExpiresDateUTC:    chrono.TimeFrom(time.Now().AddDate(1, 0, 0)),
+		Edition:           p.plan.Edition,
+		AutoRenewal:       true,
+	}
+
+	if p.expired {
+		s.ExpiresDateUTC = chrono.TimeFrom(time.Now().AddDate(-1, 0, 0))
+	}
+
+	return s
+}
+
+func (p *Persona) IAPLinkInput() apple.LinkInput {
+	return apple.LinkInput{
+		FtcID:        p.FtcID,
+		OriginalTxID: p.AppleSubID,
+	}
 }
