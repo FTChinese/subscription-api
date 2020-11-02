4bfb50726f0c1a1290773146d26c4f835d85a9d7
weiguo.ni <neefrankie@gmail.com>
2020-11-02 10:39:19 +0800

    test

--- internal/repository/subrepo/order_test.go
+++ internal/repository/subrepo/order_test.go
@@ -1,6 +1,7 @@
 package subrepo
 
 import (
+	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/pkg/subs"
 	"github.com/FTChinese/subscription-api/test"
 	"github.com/jmoiron/sqlx"
@@ -62,6 +63,46 @@ func TestEnv_CreateOrder(t *testing.T) {
 	}
 }
 
+func TestEnv_SaveProratedOrders(t *testing.T) {
+	type fields struct {
+		db     *sqlx.DB
+		cache  *cache.Cache
+		logger *zap.Logger
+	}
+	type args struct {
+		pos []subs.ProratedOrder
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save prorated orders",
+			fields: fields{
+				db: test.DB,
+			},
+			args: args{
+				pos: test.GenProratedOrders(subs.MustGenerateOrderID()),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				cache:  tt.fields.cache,
+				logger: tt.fields.logger,
+			}
+			if err := env.SaveProratedOrders(tt.args.pos); (err != nil) != tt.wantErr {
+				t.Errorf("SaveProratedOrders() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
 func TestEnv_ProratedOrdersUsed(t *testing.T) {
 	upOrderID := subs.MustGenerateOrderID()
 
@@ -105,6 +146,99 @@ func TestEnv_ProratedOrdersUsed(t *testing.T) {
 			if err := env.ProratedOrdersUsed(tt.args.upOrderID); (err != nil) != tt.wantErr {
 				t.Errorf("ProratedOrdersUsed() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestEnv_LogOrderMeta(t *testing.T) {
+	type fields struct {
+		db     *sqlx.DB
+		cache  *cache.Cache
+		logger *zap.Logger
+	}
+	type args struct {
+		m subs.OrderMeta
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Log order meta data",
+			fields: fields{
+				db: test.DB,
+			},
+			args: args{
+				m: subs.OrderMeta{
+					OrderID: subs.MustGenerateOrderID(),
+					Client:  faker.RandomClientApp(),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				cache:  tt.fields.cache,
+				logger: tt.fields.logger,
+			}
+			if err := env.LogOrderMeta(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("LogOrderMeta() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestEnv_RetrieveOrder(t *testing.T) {
+	p := test.NewPersona()
+	order := p.CreateOrder()
+
+	repo := test.NewRepo()
+	repo.MustSaveOrder(order)
+
+	type fields struct {
+		db     *sqlx.DB
+		cache  *cache.Cache
+		logger *zap.Logger
+	}
+	type args struct {
+		orderID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Retrieve order",
+			fields: fields{
+				db: test.DB,
+			},
+			args: args{
+				orderID: order.ID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				cache:  tt.fields.cache,
+				logger: tt.fields.logger,
+			}
+			got, err := env.RetrieveOrder(tt.args.orderID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RetrieveOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Order %+v", got)
 		})
 	}
 }
