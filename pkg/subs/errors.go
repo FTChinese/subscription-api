de3ede3ad3207532a812514f8934edbf119bdca4
weiguo.ni <neefrankie@gmail.com>
2020-11-02 11:37:19 +0800

    refacor: rename

--- pkg/subs/errors.go
+++ pkg/subs/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	// Reasons to deny upgrade.
-	ErrUpgradeInvalid = errors.New("upgrade could only be performed on an existing non-premium membership purchased via alipay or wxpay")
+	ErrNotUpgradeIntent = errors.New("upgrade could only be performed on an existing non-premium membership purchased via alipay or wxpay")
 )
