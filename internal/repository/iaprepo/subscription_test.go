3a9b93e5736c62633b0d0531ff3f8923fa17bccc
weiguo.ni <neefrankie@gmail.com>
2020-11-03 11:04:27 +0800

    test

--- internal/repository/iaprepo/subscription_test.go
+++ internal/repository/iaprepo/subscription_test.go
@@ -2,12 +2,65 @@ package iaprepo
 
 import (
 	gorest "github.com/FTChinese/go-rest"
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/apple"
 	"github.com/FTChinese/subscription-api/test"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zaptest"
 	"testing"
+	"time"
 )
+
+func TestEnv_SaveSubs(t *testing.T) {
+	p := test.NewPersona().SetPayMethod(enum.PayMethodApple)
+
+	env := NewEnv(test.DB, test.Redis, zaptest.NewLogger(t))
+
+	p2 := test.NewPersona().
+		SetPayMethod(enum.PayMethodApple)
+
+	m := p2.Membership()
+	m.ExpireDate = chrono.DateFrom(time.Now().AddDate(0, -6, 0))
+	test.NewRepo().MustSaveMembership(m)
+
+	type args struct {
+		s apple.Subscription
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save unlinked iap subscription",
+			args: args{
+				s: p.IAPSubs(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Save linked iap subscription",
+			args: args{
+				s: p2.IAPSubs(),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := env.SaveSubs(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SaveSubs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("%v", got)
+		})
+	}
+}
 
 func TestEnv_LoadSubs(t *testing.T) {
 	p := test.NewPersona()
@@ -54,7 +107,7 @@ func TestEnv_LoadSubs(t *testing.T) {
 
 func TestEnv_countSubs(t *testing.T) {
 	p := test.NewPersona()
-	test.NewRepo().MustSaveIAPSubs(p.IAPSubs())
+	test.NewRepo().MustSaveIAPSubs(p.IAPSubsLinked())
 
 	type fields struct {
 		db *sqlx.DB
@@ -96,7 +149,7 @@ func TestEnv_countSubs(t *testing.T) {
 
 func TestEnv_listSubs(t *testing.T) {
 	p := test.NewPersona()
-	test.NewRepo().MustSaveIAPSubs(p.IAPSubs())
+	test.NewRepo().MustSaveIAPSubs(p.IAPSubsLinked())
 
 	type fields struct {
 		db *sqlx.DB
@@ -147,44 +200,35 @@ func TestEnv_listSubs(t *testing.T) {
 
 func TestEnv_ListSubs(t *testing.T) {
 	p := test.NewPersona()
-	test.NewRepo().MustSaveIAPSubs(p.IAPSubs())
+	test.NewRepo().MustSaveIAPSubs(p.IAPSubsLinked())
 
-	type fields struct {
-		db *sqlx.DB
-	}
+	env := NewEnv(test.DB, test.Redis, zaptest.NewLogger(t))
+
+	t.Logf("Create IAP %s", p.AppleSubID)
+
 	type args struct {
 		ftcID string
 		p     gorest.Pagination
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name: "Count and list subs",
-			fields: fields{
-				db: test.DB,
-			},
 			args: args{
-				p: gorest.NewPagination(1, 20),
+				ftcID: p.FtcID,
+				p:     gorest.NewPagination(1, 20),
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				db: tt.fields.db,
-			}
 			got, err := env.ListSubs(tt.args.ftcID, tt.args.p)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ListSubs() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if tt.wantErr {
-				assert.Error(t, err)
 				return
 			}
 
