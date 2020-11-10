a296b5c50bdae5a4d6209a0ca9c5e01e7f71faf6
weiguo.ni <neefrankie@gmail.com>
2020-11-10 16:47:42 +0800

    cleanup

--- test/init.go
+++ test/init.go
@@ -3,12 +3,10 @@
 package test
 
 import (
-	"github.com/FTChinese/go-rest/postoffice"
 	"github.com/FTChinese/subscription-api/pkg/ali"
 	"github.com/FTChinese/subscription-api/pkg/config"
 	"github.com/FTChinese/subscription-api/pkg/db"
 	"github.com/FTChinese/subscription-api/pkg/wechat"
-	"github.com/FTChinese/subscription-api/pkg/wxlogin"
 	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
 	"github.com/patrickmn/go-cache"
@@ -19,14 +17,12 @@ const (
 )
 
 var (
-	CFG        = config.NewBuildConfig(false, false)
-	DB         *sqlx.DB
-	Redis      *redis.Client
-	Postman    postoffice.PostOffice
-	Cache      *cache.Cache
-	WxOAuthApp wxlogin.OAuthApp
-	WxPayApp   wechat.PayApp
-	AliApp     ali.App
+	CFG      = config.NewBuildConfig(false, false)
+	DB       *sqlx.DB
+	Redis    *redis.Client
+	Cache    *cache.Cache
+	WxPayApp wechat.PayApp
+	AliApp   ali.App
 )
 
 func init() {
@@ -34,9 +30,7 @@ func init() {
 
 	DB = db.MustNewMySQL(CFG.MustGetDBConn(""))
 	Redis = db.NewRedis(config.MustRedisAddress().Pick(false))
-	Postman = postoffice.New(config.MustGetHanqiConn())
 	Cache = cache.New(cache.DefaultExpiration, 0)
-	WxOAuthApp = wxlogin.MustNewOAuthApp("wxapp.native_app")
 	WxPayApp = wechat.MustNewPayApp("wxapp.native_app")
 	AliApp = ali.MustInitApp()
 }
