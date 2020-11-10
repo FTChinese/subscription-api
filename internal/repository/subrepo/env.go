45a5a1f257650f27ede20a6bb0ed8f64fd38e260
weiguo.ni <neefrankie@gmail.com>
2020-11-10 16:54:58 +0800

    refactor: removed cache field

--- internal/repository/subrepo/env.go
+++ internal/repository/subrepo/env.go
@@ -3,23 +3,20 @@ package subrepo
 import (
 	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
 	"github.com/jmoiron/sqlx"
-	"github.com/patrickmn/go-cache"
 	"go.uber.org/zap"
 )
 
 // Env wraps database connection
 type Env struct {
 	db     *sqlx.DB
-	cache  *cache.Cache
 	logger *zap.Logger
 }
 
 // NewEnv creates a new instance of Env.
 // `sandbox` is used to determine which table to write subscription data.
-func NewEnv(db *sqlx.DB, c *cache.Cache, logger *zap.Logger) Env {
+func NewEnv(db *sqlx.DB, logger *zap.Logger) Env {
 	return Env{
 		db:     db,
-		cache:  c,
 		logger: logger,
 	}
 }
