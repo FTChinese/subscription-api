eb71805fbbc30610a69284383fdf0aa89aeed919
weiguo.ni <neefrankie@gmail.com>
2020-11-10 17:04:52 +0800

    refactor: embed ftcpay.FtcPay

--- internal/controller/subs_router.go
+++ internal/controller/subs_router.go
@@ -2,18 +2,13 @@ package controller
 
 import (
 	"errors"
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/go-rest/postoffice"
 	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/internal/ftcpay"
 	"github.com/FTChinese/subscription-api/internal/repository/products"
-	"github.com/FTChinese/subscription-api/internal/repository/readerrepo"
-	"github.com/FTChinese/subscription-api/internal/repository/subrepo"
-	"github.com/FTChinese/subscription-api/pkg/ali"
 	"github.com/FTChinese/subscription-api/pkg/client"
 	"github.com/FTChinese/subscription-api/pkg/config"
-	"github.com/FTChinese/subscription-api/pkg/letter"
 	"github.com/FTChinese/subscription-api/pkg/subs"
-	"github.com/FTChinese/subscription-api/pkg/wechat"
 	"github.com/jmoiron/sqlx"
 	"github.com/patrickmn/go-cache"
 	"go.uber.org/zap"
@@ -22,32 +17,17 @@ import (
 
 // SubsRouter is the base type used to handle shared payment operations.
 type SubsRouter struct {
-	subRepo    subrepo.Env
-	readerRepo readerrepo.Env
-	prodRepo   products.Env
-	postman    postoffice.PostOffice
-	config     config.BuildConfig
-	logger     *zap.Logger
-
-	aliPayClient subrepo.AliPayClient
-
-	wxPayClients subrepo.WxPayClientStore
+	ftcpay.FtcPay
+	prodRepo products.Env
+	config   config.BuildConfig
 }
 
 func NewSubsRouter(db *sqlx.DB, c *cache.Cache, cfg config.BuildConfig, p postoffice.PostOffice, logger *zap.Logger) SubsRouter {
 
-	aliApp := ali.MustInitApp()
-	wxApps := wechat.MustGetPayApps()
-
 	return SubsRouter{
-		subRepo:      subrepo.NewEnv(db, c, logger),
-		readerRepo:   readerrepo.NewEnv(db),
-		prodRepo:     products.NewEnv(db, c),
-		postman:      p,
-		config:       cfg,
-		logger:       logger,
-		aliPayClient: subrepo.NewAliPayClient(aliApp, logger),
-		wxPayClients: subrepo.NewWxClientStore(wxApps, logger),
+		FtcPay:   ftcpay.New(db, p, logger),
+		prodRepo: products.NewEnv(db, c),
+		config:   cfg,
 	}
 }
 
@@ -64,11 +44,11 @@ func (router SubsRouter) handleOrderErr(w http.ResponseWriter, err error) {
 }
 
 func (router SubsRouter) postOrderCreation(order subs.Order, client client.Client) error {
-	defer router.logger.Sync()
-	sugar := router.logger.Sugar()
+	defer router.Logger.Sync()
+	sugar := router.Logger.Sugar()
 
 	go func() {
-		err := router.subRepo.LogOrderMeta(subs.OrderMeta{
+		err := router.SubsRepo.LogOrderMeta(subs.OrderMeta{
 			OrderID: order.ID,
 			Client:  client,
 		})
@@ -80,143 +60,15 @@ func (router SubsRouter) postOrderCreation(order subs.Order, client client.Clien
 	return nil
 }
 
-//https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_2&index=4
-func (router SubsRouter) verifyWxPayment(order subs.Order) (subs.PaymentResult, error) {
-	defer router.logger.Sync()
-	sugar := router.logger.Sugar()
-
-	payClient, err := router.wxPayClients.ClientByAppID(order.WxAppID.String)
-	if err != nil {
-		sugar.Error(err)
-		return subs.PaymentResult{}, err
-	}
-
-	wxOrder, err := payClient.QueryOrder(order)
-	// If there are any errors when querying order.
-	if err != nil {
-		sugar.Error(err)
-		return subs.PaymentResult{}, err
-	}
-
-	// Save raw response.
-	go func() {
-		if err := router.subRepo.SaveWxQueryResp(wxOrder); err != nil {
-			sugar.Error(err)
-		}
-	}()
-
-	// Validate if response is correct. This does not verify the payment is successful.
-	// We have to send the payment status as is to client.
-	// field: return_code, code: invalid
-	// field: result_code, code: invalid
-	// field: app_id, code: invalid
-	// field: mch_id, code: invalid
-	err = wxOrder.Validate(payClient.GetApp())
-	if err != nil {
-		sugar.Error(err)
-		return subs.PaymentResult{}, err
-	}
-
-	return subs.NewWxPayResult(wxOrder), nil
-}
-
 func (router SubsRouter) processWebhookResult(result subs.PaymentResult) (subs.ConfirmationResult, *subs.ConfirmError) {
-	defer router.logger.Sync()
-	sugar := router.logger.Sugar()
+	defer router.Logger.Sync()
+	sugar := router.Logger.Sugar()
 
-	order, err := router.subRepo.LoadFullOrder(result.OrderID)
+	order, err := router.SubsRepo.LoadFullOrder(result.OrderID)
 	if err != nil {
 		sugar.Error(err)
 		return subs.ConfirmationResult{}, result.ConfirmError(err, true)
 	}
 
-	return router.confirmOrder(result, order)
-}
-
-func (router SubsRouter) confirmOrder(result subs.PaymentResult, order subs.Order) (subs.ConfirmationResult, *subs.ConfirmError) {
-	defer router.logger.Sync()
-	sugar := router.logger.Sugar()
-
-	confirmed, cfmErr := router.subRepo.ConfirmOrder(result, order)
-	if cfmErr != nil {
-		go func() {
-			err := router.subRepo.SaveConfirmationErr(cfmErr)
-			if err != nil {
-				sugar.Error(err)
-			}
-		}()
-		return confirmed, cfmErr
-	}
-
-	go func() {
-		if !confirmed.Snapshot.IsZero() {
-			err := router.readerRepo.ArchiveMember(confirmed.Snapshot)
-			if err != nil {
-				sugar.Error(err)
-			}
-		}
-
-		// Flag upgrade balance as consumed.
-		if confirmed.Order.Kind == enum.OrderKindUpgrade {
-			err := router.subRepo.ProratedOrdersUsed(confirmed.Order.ID)
-			if err != nil {
-				sugar.Error(err)
-			}
-		}
-
-		if err := router.sendConfirmationEmail(confirmed.Order); err != nil {
-			sugar.Error(err)
-		}
-	}()
-
-	return confirmed, nil
-}
-
-// SendConfirmationLetter sends a confirmation email if user logged in with FTC account.
-func (router SubsRouter) sendConfirmationEmail(order subs.Order) error {
-	defer router.logger.Sync()
-	sugar := router.logger.Sugar()
-
-	// If the FtcID field is null, it indicates this user
-	// does not have an FTC account linked. You cannot find out
-	// its email address.
-	if !order.FtcID.Valid {
-		return nil
-	}
-	// Find this user's personal data
-	account, err := router.readerRepo.AccountByFtcID(order.FtcID.String)
-
-	if err != nil {
-		return err
-	}
-
-	var parcel postoffice.Parcel
-	switch order.Kind {
-	case enum.OrderKindCreate:
-		parcel, err = letter.NewSubParcel(account, order)
-
-	case enum.OrderKindRenew:
-		parcel, err = letter.NewRenewalParcel(account, order)
-
-	case enum.OrderKindUpgrade:
-		pos, err := router.subRepo.ListProratedOrders(order.ID)
-		if err != nil {
-			return err
-		}
-		parcel, err = letter.NewUpgradeParcel(account, order, pos)
-	}
-
-	if err != nil {
-		sugar.Error(err)
-		return err
-	}
-
-	sugar.Info("Send subscription confirmation letter")
-
-	err = router.postman.Deliver(parcel)
-	if err != nil {
-		sugar.Error(err)
-		return err
-	}
-	return nil
+	return router.ConfirmOrder(result, order)
 }
