e8edd09bbdbcc65c6f3f1205071d4712a1fc3da1
weiguo.ni <neefrankie@gmail.com>
2020-11-11 17:50:23 +0800

    refactor: removed ManualConfirm

--- internal/controller/subs_order.go
+++ internal/controller/subs_order.go
@@ -6,79 +6,6 @@ import (
 	"net/http"
 )
 
-func (router SubsRouter) ManualConfirm(w http.ResponseWriter, req *http.Request) {
-	defer router.Logger.Sync()
-	sugar := router.Logger.Sugar()
-
-	// Get ftc order id from URL
-	orderID, err := getURLParam(req, "id").ToString()
-	if err != nil {
-		sugar.Error()
-		_ = render.New(w).BadRequest(err.Error())
-		return
-	}
-
-	order, err := router.SubsRepo.LoadFullOrder(orderID)
-	if err != nil {
-		sugar.Error(err)
-		_ = render.New(w).DBError(err)
-		return
-	}
-
-	if !order.IsAliWxPay() {
-		_ = render.New(w).BadRequest("order not paid via ali or wx")
-		return
-	}
-
-	// Return error or data?
-	if order.IsConfirmed() {
-		m, err := router.ReaderRepo.RetrieveMember(order.MemberID)
-		if err != nil {
-			_ = render.New(w).DBError(err)
-			return
-		}
-
-		_ = render.New(w).OK(subs.ConfirmationResult{
-			Order:      order,
-			Membership: m,
-			Payment:    subs.PaymentResult{},
-		})
-		return
-	}
-
-	payResult, err := router.VerifyOrder(order)
-	sugar.Infof("Payment result: %+v", payResult)
-
-	if err != nil {
-		sugar.Error(err)
-		_ = render.New(w).InternalServerError(err.Error())
-		return
-	}
-
-	if !payResult.IsOrderPaid() {
-		m, err := router.ReaderRepo.RetrieveMember(order.MemberID)
-		if err != nil {
-			_ = render.New(w).DBError(err)
-			return
-		}
-		_ = render.New(w).OK(subs.ConfirmationResult{
-			Order:      order,
-			Membership: m,
-			Payment:    payResult,
-		})
-
-		return
-	}
-
-	confirmed, cfmErr := router.ConfirmOrder(payResult, order)
-	if cfmErr != nil {
-		_ = render.New(w).DBError(cfmErr)
-		return
-	}
-
-	_ = render.New(w).OK(confirmed)
-}
-
 // VerifyPayment checks against payment provider's api to get
 // the payment result of an order.
 // GET /wxpay/query/{orderId}
@@ -120,20 +47,76 @@ func (router SubsRouter) VerifyPayment(w http.ResponseWriter, req *http.Request)
 		return
 	}
 
-	if !payResult.IsOrderPaid() {
-		sugar.Info("Not paid")
-		_ = render.New(w).OK(payResult)
+	if !payResult.IsOrderPaid() || order.IsConfirmed() {
+		sugar.Info("Order is either not paid or already confirmed")
+
+		m, err := router.ReaderRepo.RetrieveMember(order.MemberID)
+		if err != nil {
+			_ = render.New(w).DBError(err)
+			return
+		}
+
+		_ = render.New(w).OK(subs.VerificationResult{
+			PaymentResult: payResult,
+			ConfirmationResult: subs.ConfirmationResult{
+				Order:      order,
+				Membership: m,
+				Payment:    payResult,
+			},
+		})
+
 		return
 	}
 
-	if order.IsConfirmed() {
-		sugar.Info("Already confirmed")
-		_ = render.New(w).OK(payResult)
+	cfmResult, cfmErr := router.ConfirmOrder(payResult, order)
+	if cfmErr != nil {
+		_ = render.New(w).DBError(err)
 		return
 	}
 
-	_, _ = router.ConfirmOrder(payResult, order)
+	_ = render.New(w).OK(subs.VerificationResult{
+		PaymentResult:      payResult,
+		ConfirmationResult: cfmResult,
+	})
+}
+
+// PaymentResult gets payment result from providers
+// This does not modify data on our side.
+func (router SubsRouter) PaymentResult(w http.ResponseWriter, req *http.Request) {
+	defer router.Logger.Sync()
+	sugar := router.Logger.Sugar()
+
+	// Get ftc order id from URL
+	orderID, err := getURLParam(req, "id").ToString()
+	if err != nil {
+		sugar.Error(err)
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	sugar = sugar.With("orderId", orderID)
+
+	order, err := router.SubsRepo.LoadFullOrder(orderID)
+	if err != nil {
+		sugar.Error(err)
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	if !order.IsAliWxPay() {
+		sugar.Info("Not ali or wx pay")
+		_ = render.New(w).BadRequest("Order not paid via alipay or wxpay")
+		return
+	}
+
+	payResult, err := router.VerifyOrder(order)
+	sugar.Infof("Payment result: %+v", payResult)
+
+	if err != nil {
+		sugar.Error(err)
+		_ = render.New(w).InternalServerError(err.Error())
+		return
+	}
 
 	_ = render.New(w).OK(payResult)
-
 }
