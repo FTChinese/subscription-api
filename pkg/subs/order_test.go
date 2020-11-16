bde7fb28a59d3ecfd750e31629958aaa08d89044
weiguo.ni <neefrankie@gmail.com>
2020-11-16 14:38:16 +0800

    fix: test

--- pkg/subs/order_test.go
+++ pkg/subs/order_test.go
@@ -3,6 +3,8 @@ package subs
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/ali"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/guregu/null"
 	"github.com/stretchr/testify/assert"
@@ -35,22 +37,30 @@ func TestOrder_Confirm(t *testing.T) {
 			order: order,
 			args: args{
 				pr: PaymentResult{
+					PaymentState:  ali.TradeStatusSuccess,
 					Amount:        null.IntFrom(12800),
 					TransactionID: "1234",
 					OrderID:       order.ID,
 					PaidAt:        chrono.TimeNow(),
+					ConfirmedUTC:  chrono.TimeNow(),
+					PayMethod:     enum.PayMethodAli,
 				},
 				m: reader.Membership{},
 			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
 			got, err := tt.order.Confirm(tt.args.pr, tt.args.m)
-			assert.NoError(t, err)
-			assert.NotZero(t, got.Membership)
-			assert.Zero(t, got.Snapshot)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Confirm() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
 		})
 	}
 }
