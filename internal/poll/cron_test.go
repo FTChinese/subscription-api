75d933be47108dcdd5da502b23ed4c21cdc07d22
weiguo.ni <neefrankie@gmail.com>
2020-11-23 16:15:03 +0800

    test

--- internal/poll/cron_test.go
+++ internal/poll/cron_test.go
@@ -1,1 +1,22 @@
 package poll
+
+import (
+	"github.com/robfig/cron/v3"
+	"testing"
+	"time"
+)
+
+func TestCron(t *testing.T) {
+	c := cron.New()
+
+	_, err := c.AddFunc("* * * * *", func() {
+		t.Logf("Hello %s", time.Now().Format(time.RFC3339))
+	})
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	c.Run()
+}
