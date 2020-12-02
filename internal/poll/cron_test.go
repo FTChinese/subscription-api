02eca6a26fdc5a3d36b91363b3fb01db61512d2c
neefrankie <neefrankie@gmail.com>
2020-12-03 05:05:41 +0800

    test cron

--- internal/poll/cron_test.go
+++ internal/poll/cron_test.go
@@ -1,20 +1,17 @@
 package poll
 
 import (
-	"fmt"
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/go-co-op/gocron"
 	"testing"
 )
 
-func task() {
-	fmt.Print("I'm running")
-}
-
 func TestCron(t *testing.T) {
 	s := gocron.NewScheduler(chrono.TZShanghai)
 
-	_, err := s.Every(1).Second().Do(task)
+	_, err := s.Every(1).Seconds().Do(func() {
+		t.Log("I am a running task")
+	})
 
 	if err != nil {
 		t.Log(err)
