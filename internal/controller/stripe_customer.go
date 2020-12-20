c8ce003528d19a44c21cfbc7470e22d62e89040e
neefrankie <neefrankie@gmail.com>
2020-12-20 21:11:27 +0800

    feat: change default payment method

--- internal/controller/stripe_customer.go
+++ internal/controller/stripe_customer.go
@@ -83,8 +83,54 @@ func (router StripeRouter) GetCustomer(w http.ResponseWriter, req *http.Request)
 	_ = render.New(w).OK(stripe.NewCustomer(account, cus))
 }
 
+func (router StripeRouter) ChangeDefaultPaymentMethod(w http.ResponseWriter, req *http.Request) {
+	ftcID := req.Header.Get(userIDKey)
+	cusID, err := getURLParam(req, "id").ToString()
+	if err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	var pm stripe.PaymentInput
+	if err := gorest.ParseJSON(req.Body, &pm); err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+	pm.CustomerID = cusID
+
+	if ve := pm.Validate(); ve != nil {
+		_ = render.New(w).Unprocessable(ve)
+		return
+	}
+
+	account, err := router.readerRepo.AccountByFtcID(ftcID)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+	if account.StripeID.IsZero() {
+		_ = render.New(w).NotFound("Not a stripe customer")
+		return
+	}
+	if account.StripeID.String != cusID {
+		_ = render.New(w).NotFound("")
+	}
+
+	cus, err := router.client.SetDefaultPaymentMethod(pm)
+	if err != nil {
+		err = handleErrResp(w, err)
+		if err != nil {
+			_ = render.New(w).BadRequest(err.Error())
+		}
+		return
+	}
+
+	_ = render.New(w).OK(stripe.NewCustomer(account, cus))
+}
+
 // GetDefaultPaymentMethod retrieves a user's invoice default payment method.
 // GET /stripe/customers/{id}/default_payment_method
+// Deprecated
 func (router StripeRouter) GetDefaultPaymentMethod(w http.ResponseWriter, req *http.Request) {
 	// Get stripe customer id from url.
 	id, err := getURLParam(req, "id").ToString()
@@ -114,6 +160,7 @@ func (router StripeRouter) GetDefaultPaymentMethod(w http.ResponseWriter, req *h
 // POST /stripe/customers/{id}/default_payment_method
 //
 // Input: {defaultPaymentMethod: string}. The id of the default payment method.
+// Deprecated
 func (router StripeRouter) SetDefaultPaymentMethod(w http.ResponseWriter, req *http.Request) {
 	// Get stripe customer id from url.
 	cusID, err := getURLParam(req, "id").ToString()
