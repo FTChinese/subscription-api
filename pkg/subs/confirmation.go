449a0b57ec3385ec6094b9236dfb50ff536abb66
weiguo.ni <neefrankie@gmail.com>
2021-01-11 17:50:29 +0800

    refactor: order confirmation

--- pkg/subs/confirmation.go
+++ pkg/subs/confirmation.go
@@ -1,6 +1,11 @@
 package subs
 
-import "github.com/FTChinese/subscription-api/pkg/reader"
+import (
+	"errors"
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/pkg/reader"
+	"github.com/guregu/null"
+)
 
 const StmtSaveConfirmResult = `
 INSERT INTO premium.confirmation_result
@@ -18,33 +23,108 @@ func (c ConfirmError) Error() string {
 	return c.Message
 }
 
+type ConfirmationParams struct {
+	Payment PaymentResult
+	Order   Order
+	Member  reader.Membership
+}
+
+type PaymentConfirmed struct {
+	Order    Order                 `json:"order"` // The confirmed order.
+	AddOn    AddOn                 `json:"addOn"`
+	Snapshot reader.MemberSnapshot `json:"-"` // Snapshot of previous membership
+}
+
+func NewPaymentConfirmed(p ConfirmationParams) (PaymentConfirmed, error) {
+	snapshot := p.Member.Snapshot(reader.FtcArchiver(p.Order.Kind))
+	if !p.Member.IsZero() {
+		snapshot = snapshot.WithOrder(p.Order.ID)
+	}
+
+	switch p.Order.Kind {
+	case enum.OrderKindCreate, enum.OrderKindRenew:
+		return PaymentConfirmed{
+			Order:    p.Order.newOrRenewalConfirm(p.Payment.ConfirmedUTC, p.Member.ExpireDate),
+			AddOn:    AddOn{},
+			Snapshot: snapshot,
+		}, nil
+
+	case enum.OrderKindUpgrade:
+		if p.Member.Tier == enum.TierPremium {
+			p.Order.Kind = enum.OrderKindRenew
+			return PaymentConfirmed{
+				Order:    p.Order.newOrRenewalConfirm(p.Payment.ConfirmedUTC, p.Member.ExpireDate),
+				AddOn:    AddOn{},
+				Snapshot: snapshot,
+			}, nil
+		}
+
+		return PaymentConfirmed{
+			Order:    p.Order.upgradeConfirm(p.Payment.ConfirmedUTC),
+			AddOn:    NewUpgradeAddOn(p.Order, p.Member),
+			Snapshot: snapshot,
+		}, nil
+
+	case enum.OrderKindAddOn:
+		p.Order.ConfirmedAt = p.Payment.ConfirmedUTC
+		return PaymentConfirmed{
+			Order:    p.Order,
+			AddOn:    NewAddOn(p.Order),
+			Snapshot: snapshot,
+		}, nil
+
+	default:
+		return PaymentConfirmed{}, errors.New("unknown order kind")
+	}
+}
+
+func NewMembership(p PaymentConfirmed) reader.Membership {
+	// If an order is created as an add-on, only add the reserved days
+	// to current membership.
+	if p.Order.Kind == enum.OrderKindAddOn {
+		m := p.Snapshot.Membership
+		return m.WithReservedDays(p.AddOn.ToReservedDays())
+	}
+
+	return reader.Membership{
+		MemberID:      p.Order.MemberID,
+		Edition:       p.Order.Edition,
+		LegacyTier:    null.Int{},
+		LegacyExpire:  null.Int{},
+		ExpireDate:    p.Order.EndDate,
+		PaymentMethod: p.Order.PaymentMethod,
+		FtcPlanID:     null.StringFrom(p.Order.PlanID),
+		StripeSubsID:  null.String{},
+		StripePlanID:  null.String{},
+		AutoRenewal:   false,
+		Status:        enum.SubsStatusNull,
+		AppleSubsID:   null.String{},
+		B2BLicenceID:  null.String{},
+		ReservedDays:  p.Snapshot.Membership.ReservedDays.Plus(p.AddOn.ToReservedDays()),
+	}.Sync()
+}
+
 // ConfirmationResult contains all the data in the process of confirming an order.
 // This is also used as the http response for manual confirmation.
 type ConfirmationResult struct {
-	orderModified bool
-	Order         Order `json:"order"` // The confirmed order.
-	memberUpdated bool
-	Membership    reader.Membership     `json:"membership"` // The updated membership. Empty if order is already confirmed.
-	Payment       PaymentResult         `json:"payment"`    // Empty if order is already confirmed.
-	Snapshot      reader.MemberSnapshot `json:"-"`          // Snapshot of previous membership
+	Payment PaymentResult `json:"payment"` // Empty if order is already confirmed.
+	PaymentConfirmed
+	Membership reader.Membership `json:"membership"` // The updated membership. Empty if order is already confirmed.
+	Notify     bool              `json:"-"`
 }
 
-// Modified checks if Order and Membership are actually modified.
-// Usually if an order is confirmed, membership will be modified.
-// There might be case while an order is confirmed, the membership is not changed. We treat it as out of sync.
-// For verification, an order should already confirmed and membership updated. In such case, nothing modified and data should be sent to client as is.
-// As long as it returns true, membership must be modified..
-func (r *ConfirmationResult) Modified() bool {
-	// As long as order is modified, all data are changed.
-	if r.orderModified {
-		return true
+func NewConfirmationResult(p ConfirmationParams) (ConfirmationResult, error) {
+	pc, err := NewPaymentConfirmed(p)
+	if err != nil {
+		return ConfirmationResult{}, err
 	}
 
-	// If order is not modified, membership is modified if it is out of sync; otherwise it is not modified.
-	return r.memberUpdated
-}
+	m := NewMembership(pc)
 
-// ShouldUpdateOrder checks if the order is modified.
-func (r *ConfirmationResult) ShouldUpdateOrder() bool {
-	return r.orderModified
+	return ConfirmationResult{
+		Payment:          p.Payment,
+		PaymentConfirmed: pc,
+		Membership:       m,
+		Notify:           true,
+	}, nil
 }
