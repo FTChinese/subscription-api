48c7fb42fccb1d0df4bdd6b8ba049e39673cd613
weiguo.ni <neefrankie@gmail.com>
2021-01-11 17:47:52 +0800

    test

--- pkg/reader/membership_test.go
+++ pkg/reader/membership_test.go
@@ -629,3 +629,48 @@ func TestMembership_RemainingDays(t *testing.T) {
 
 	t.Logf("%d", m2.RemainingDays())
 }
+
+func TestReservedDays_Plus(t *testing.T) {
+	type fields struct {
+		Standard int64
+		Premium  int64
+	}
+	type args struct {
+		other ReservedDays
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   ReservedDays
+	}{
+		{
+			name: "Plus",
+			fields: fields{
+				Standard: 31,
+				Premium:  101,
+			},
+			args: args{
+				other: ReservedDays{
+					Standard: 15,
+					Premium:  19,
+				},
+			},
+			want: ReservedDays{
+				Standard: 31 + 15,
+				Premium:  101 + 19,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := ReservedDays{
+				Standard: tt.fields.Standard,
+				Premium:  tt.fields.Premium,
+			}
+			if got := d.Plus(tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Plus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
