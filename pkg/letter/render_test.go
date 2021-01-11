656475696707b9379fbcec253866cfa3d5b83b53
weiguo.ni <neefrankie@gmail.com>
2021-01-11 17:46:34 +0800

    refactor: subscription letters

--- pkg/letter/render_test.go
+++ pkg/letter/render_test.go
@@ -3,114 +3,153 @@ package letter
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/db"
+	"github.com/FTChinese/subscription-api/pkg/dt"
 	"github.com/FTChinese/subscription-api/pkg/product"
 	"github.com/FTChinese/subscription-api/pkg/subs"
 	"github.com/brianvoe/gofakeit/v5"
+	"github.com/google/uuid"
 	"github.com/guregu/null"
 	"testing"
 	"time"
 )
 
-func TestRenderNewSubs(t *testing.T) {
-	faker.SeedGoFake()
-
-	s, err := RenderNewSubs(CtxSubs{
-		UserName: gofakeit.Username(),
-		Order: subs.Order{
-			ID: subs.MustGenerateOrderID(),
-			Edition: product.Edition{
-				Tier:  enum.TierStandard,
-				Cycle: enum.CycleYear,
-			},
-			Charge: product.Charge{
-				Amount:   128,
-				Currency: "",
-			},
-			CreatedAt: chrono.TimeNow(),
-			StartDate: chrono.DateNow(),
-			EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-		},
-	})
-
-	if err != nil {
-		t.Error(err)
+func TestCtxSubs_Render(t *testing.T) {
+	type fields struct {
+		UserName string
+		Order    subs.Order
+		AddOn    subs.AddOn
 	}
-
-	t.Logf("%s", s)
-}
-
-func TestRenderRenewalSubs(t *testing.T) {
-	faker.SeedGoFake()
-
-	s, err := RenderRenewalSubs(CtxSubs{
-		UserName: gofakeit.Username(),
-		Order: subs.Order{
-			ID: subs.MustGenerateOrderID(),
-			Edition: product.Edition{
-				Tier:  enum.TierStandard,
-				Cycle: enum.CycleYear,
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "New subscription",
+			fields: fields{
+				UserName: gofakeit.Username(),
+				Order: subs.Order{
+					ID: db.MustOrderID(),
+					Edition: product.Edition{
+						Tier:  enum.TierStandard,
+						Cycle: enum.CycleYear,
+					},
+					Charge: product.Charge{
+						Amount:   128,
+						Currency: "",
+					},
+					CreatedAt: chrono.TimeNow(),
+					DateRange: dt.DateRange{
+						StartDate: chrono.DateNow(),
+						EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+					},
+				},
+				AddOn: subs.AddOn{},
 			},
-			Charge: product.Charge{
-				Amount:   128,
-				Currency: "",
-			},
-			CreatedAt: chrono.TimeNow(),
-			StartDate: chrono.DateNow(),
-			EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+			wantErr: false,
 		},
-	})
-
-	if err != nil {
-		t.Error(err)
+		{
+			name: "Renew subscription",
+			fields: fields{
+				UserName: "",
+				Order: subs.Order{
+					ID: db.MustOrderID(),
+					Edition: product.Edition{
+						Tier:  enum.TierStandard,
+						Cycle: enum.CycleYear,
+					},
+					Charge: product.Charge{
+						Amount:   128,
+						Currency: "",
+					},
+					CreatedAt: chrono.TimeNow(),
+					DateRange: dt.DateRange{
+						StartDate: chrono.DateNow(),
+						EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+					},
+				},
+				AddOn: subs.AddOn{},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Upgrade subscription",
+			fields: fields{
+				UserName: "",
+				Order: subs.Order{
+					ID: db.MustOrderID(),
+					Edition: product.Edition{
+						Tier:  enum.TierStandard,
+						Cycle: enum.CycleYear,
+					},
+					Charge: product.Charge{
+						Amount:   128,
+						Currency: "",
+					},
+					CreatedAt: chrono.TimeNow(),
+					DateRange: dt.DateRange{
+						StartDate: chrono.DateNow(),
+						EndDate:   chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
+					},
+				},
+				AddOn: subs.AddOn{
+					ID:           db.AddOnID(),
+					Edition:      product.NewStdYearEdition(),
+					CycleCount:   0,
+					DaysRemained: 100,
+					OrderID:      null.StringFrom(db.MustOrderID()),
+					CompoundID:   uuid.New().String(),
+					CreatedUTC:   chrono.TimeNow(),
+					ConsumedUTC:  chrono.Time{},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Add on",
+			fields: fields{
+				UserName: "",
+				Order: subs.Order{
+					ID: db.MustOrderID(),
+					Edition: product.Edition{
+						Tier:  enum.TierStandard,
+						Cycle: enum.CycleYear,
+					},
+					Charge: product.Charge{
+						Amount:   128,
+						Currency: "",
+					},
+					CreatedAt: chrono.TimeNow(),
+				},
+				AddOn: subs.AddOn{
+					ID:           db.AddOnID(),
+					Edition:      product.NewStdYearEdition(),
+					CycleCount:   1,
+					DaysRemained: 1,
+					OrderID:      null.StringFrom(db.MustOrderID()),
+					CompoundID:   uuid.New().String(),
+					CreatedUTC:   chrono.TimeNow(),
+					ConsumedUTC:  chrono.Time{},
+				},
+			},
+			wantErr: false,
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxSubs{
+				UserName: tt.fields.UserName,
+				Order:    tt.fields.Order,
+				AddOn:    tt.fields.AddOn,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-	t.Logf("%s", s)
-}
-
-func TestRenderUpgrade(t *testing.T) {
-	faker.SeedGoFake()
-
-	upID := subs.MustGenerateOrderID()
-
-	s, err := RenderUpgrade(CtxUpgrade{
-		UserName: gofakeit.Username(),
-		Order: subs.Order{
-			ID: subs.MustGenerateOrderID(),
-			Edition: product.Edition{
-				Tier:  enum.TierStandard,
-				Cycle: enum.CycleYear,
-			},
-			Charge: product.Charge{
-				Amount:   1000,
-				Currency: "",
-			},
-			TotalBalance: null.FloatFrom(998),
-			CreatedAt:    chrono.TimeNow(),
-			StartDate:    chrono.DateNow(),
-			EndDate:      chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-		},
-		Prorated: []subs.ProratedOrder{
-			{
-				OrderID:        subs.MustGenerateOrderID(),
-				Balance:        50,
-				CreatedUTC:     chrono.TimeNow(),
-				ConsumedUTC:    chrono.TimeNow(),
-				UpgradeOrderID: upID,
-			},
-			{
-				OrderID:        subs.MustGenerateOrderID(),
-				Balance:        21,
-				CreatedUTC:     chrono.TimeNow(),
-				ConsumedUTC:    chrono.TimeNow(),
-				UpgradeOrderID: upID,
-			},
-		},
-	})
-
-	if err != nil {
-		t.Error(err)
+			t.Logf("%s", got)
+		})
 	}
-
-	t.Logf("%s", s)
 }
