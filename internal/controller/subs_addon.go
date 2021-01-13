a7aa3d60378113f2fb16d46e0cdc6ffa2e6926ae
weiguo.ni <neefrankie@gmail.com>
2021-01-13 12:47:18 +0800

    feat: use add-on

---
+++ internal/controller/subs_addon.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"net/http"
+)
+
+func (router SubsRouter) RedeemAddOn(w http.ResponseWriter, req *http.Request) {
+	readerIDs := readerIDSet(req.Header)
+
+	result, err := router.SubsRepo.TransferAddOn(readerIDs)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	go func() {
+		_ = router.ReaderRepo.ArchiveMember(result.Snapshot)
+	}()
+
+	_ = render.New(w).OK(result.Membership)
+}
