b0f351b41208a9b13a7dca7fa913c507d24b9f8f
weiguo.ni <neefrankie@gmail.com>
2021-01-13 22:05:33 +0800

    feat: collection pkg

---
+++ pkg/collection/string_set.go
@@ -0,0 +1,16 @@
+package collection
+
+type StringSet map[string]interface{}
+
+func NewStringSet() StringSet {
+	return make(StringSet)
+}
+
+func (s StringSet) ToArray() []string {
+	var a []string
+	for k := range s {
+		a = append(a, k)
+	}
+
+	return a
+}
