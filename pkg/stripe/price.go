21ddd8e55beff67bf5080de2fdef3fdd0beb83a8
weiguo.ni <neefrankie@gmail.com>
2021-01-26 16:41:13 +0800

    refactor: restore Plan

--- pkg/stripe/price.go
+++ pkg/stripe/price.go
@@ -2,55 +2,11 @@ package stripe
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/FTChinese/subscription-api/pkg/product"
 	"github.com/guregu/null"
 	"github.com/stripe/stripe-go/v72"
 	"sync"
 )
-
-// PriceEdition defines the product edition used in a Stripe environment.
-type PriceEdition struct {
-	product.Edition
-	Live bool
-}
-
-// The map between Stripe id to edition.
-var presetPrices = map[string]PriceEdition{
-	"plan_FXZYLOEbcvj5Tx": {
-		Edition: product.StdMonthEdition,
-		Live:    true,
-	},
-	"plan_FXZZUEDpToPlZK": {
-		Edition: product.StdYearEdition,
-		Live:    true,
-	},
-	"plan_FXZbv1cDTsUKOg": {
-		Edition: product.PremiumEdition,
-		Live:    true,
-	},
-	"plan_FOdgPTznDwHU4i": {
-		Edition: product.StdMonthEdition,
-		Live:    false,
-	},
-	"plan_FOdfeaqzczp6Ag": {
-		Edition: product.StdYearEdition,
-		Live:    false,
-	},
-	"plan_FOde0uAr0V4WmT": {
-		Edition: product.PremiumEdition,
-		Live:    false,
-	},
-}
-
-func FindPriceEdition(priceID string) (product.Edition, error) {
-	pe, ok := presetPrices[priceID]
-	if !ok {
-		return product.Edition{}, fmt.Errorf("stripe price with id %s is not found", priceID)
-	}
-
-	return pe.Edition, nil
-}
 
 // Price is a condensed version of Stripe's price object.
 type Price struct {
@@ -66,10 +22,10 @@ type Price struct {
 }
 
 // NewPrice extract the essential data from a stripe price.
-func NewPrice(preset PriceEdition, price *stripe.Price) Price {
+func NewPrice(p Plan, price *stripe.Price) Price {
 	return Price{
 		ID:         price.ID,
-		Edition:    preset.Edition,
+		Edition:    p.Edition,
 		Active:     price.Active,
 		Currency:   string(price.Currency),
 		LiveMode:   price.Livemode,
@@ -80,28 +36,28 @@ func NewPrice(preset PriceEdition, price *stripe.Price) Price {
 	}
 }
 
-// priceStore acts as a cache after retrieving Stripe prices.
-type priceStore struct {
+// priceCache acts as a cache after retrieving Stripe prices.
+type priceCache struct {
 	len     int
 	prices  []Price        // Use an array to store the prices.
 	idIndex map[string]int // price id to its position in the array.
 	mux     sync.Mutex     // The data is global. Lock it for currency.
 }
 
-func newPriceStore() *priceStore {
-	return &priceStore{
+func newPriceStore() *priceCache {
+	return &priceCache{
 		len:     0,
 		prices:  make([]Price, 0),
 		idIndex: map[string]int{},
 	}
 }
 
-func (store *priceStore) Len() int {
+func (store *priceCache) Len() int {
 	return store.len
 }
 
 // AddAll caches an array of stripe prices.
-func (store *priceStore) AddAll(sps []*stripe.Price) {
+func (store *priceCache) AddAll(sps []*stripe.Price) {
 	store.mux.Lock()
 	for _, sp := range sps {
 		_ = store.upsert(sp)
@@ -110,24 +66,24 @@ func (store *priceStore) AddAll(sps []*stripe.Price) {
 }
 
 // Upsert inserts or update a price.
-func (store *priceStore) Upsert(sp *stripe.Price) error {
+func (store *priceCache) Upsert(sp *stripe.Price) error {
 	store.mux.Lock()
 	defer store.mux.Unlock()
 
 	return store.upsert(sp)
 }
 
-func (store *priceStore) upsert(sp *stripe.Price) error {
+func (store *priceCache) upsert(sp *stripe.Price) error {
 	// Check if a stripe price id is allowed to present.
-	preset, ok := presetPrices[sp.ID]
-	if !ok {
-		return fmt.Errorf("unknown stripe price %s", sp.ID)
+	plan, err := PlanStore.FindByID(sp.ID)
+	if err != nil {
+		return err
 	}
 
 	// If this price already cached, update it.
 	index, ok := store.idIndex[sp.ID]
 	if ok {
-		store.prices[index] = NewPrice(preset, sp)
+		store.prices[index] = NewPrice(plan, sp)
 		return nil
 	}
 
@@ -137,13 +93,13 @@ func (store *priceStore) upsert(sp *stripe.Price) error {
 	}
 
 	// The price is not cached, append to the end.
-	store.prices = append(store.prices, NewPrice(preset, sp))
+	store.prices = append(store.prices, NewPrice(plan, sp))
 	store.len++
 	return nil
 }
 
 // Find searches a price by id in the specified environment.
-func (store *priceStore) Find(id string, live bool) (Price, error) {
+func (store *priceCache) Find(id string, live bool) (Price, error) {
 	index, ok := store.idIndex[id]
 	if ok {
 		return Price{}, sql.ErrNoRows
@@ -158,7 +114,7 @@ func (store *priceStore) Find(id string, live bool) (Price, error) {
 }
 
 // List returned a list all prices in the specified environment.
-func (store *priceStore) List(live bool) []Price {
+func (store *priceCache) List(live bool) []Price {
 	var prices = make([]Price, 0)
 
 	for _, v := range store.prices {
@@ -172,4 +128,4 @@ func (store *priceStore) List(live bool) []Price {
 	return prices
 }
 
-var PriceStore = newPriceStore()
+var PriceCache = newPriceStore()
