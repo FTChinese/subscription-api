eb73fbf090e9d9831dfbaa39dfdcb946b5d8a38b
weiguo.ni <neefrankie@gmail.com>
2021-02-01 16:52:08 +0800

    feat: data structure for stripe endpoints

---
+++ pkg/stripe/input.go
@@ -0,0 +1,86 @@
+package stripe
+
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/product"
+	"github.com/FTChinese/subscription-api/pkg/reader"
+	"github.com/FTChinese/subscription-api/pkg/validator"
+	"github.com/guregu/null"
+	"strings"
+)
+
+type SharedParams struct {
+	CouponID             null.String `json:"coupon"`
+	DefaultPaymentMethod null.String `json:"defaultPaymentMethod"`
+	IdempotencyKey       string      `json:"idempotency"` // TODO: add when and why this is needed.
+}
+
+// SubsInput is the request body to create a new subscription.
+type SubsInput struct {
+	product.Edition // Use this to find strip's price id.
+	SharedParams
+}
+
+// Validate checks if customer and idempotency fields are set.
+func (i SubsInput) Validate(isUpgrade bool) *render.ValidationError {
+	if ve := i.Edition.Validate(); ve != nil {
+		return ve
+	}
+
+	if isUpgrade && i.Tier != enum.TierPremium {
+		return &render.ValidationError{
+			Message: "Not the correct edition to upgrade to",
+			Field:   "tier",
+			Code:    render.CodeInvalid,
+		}
+	}
+
+	return validator.New("idempotency").Required().Validate(i.IdempotencyKey)
+}
+
+// SubsParams contains the data used to create/upgrade a subscription.
+type SubsParams struct {
+	Account reader.FtcAccount
+	Plan    Plan
+	Params  SharedParams
+}
+
+type CancelParams struct {
+	FtcID  string
+	SubID  string
+	Cancel bool // True for cancel, false for reactivation.
+}
+
+type CheckoutInput struct {
+	product.Edition
+	SuccessURL string `json:"successUrl"`
+	CancelURL  string `json:"cancelUrl"`
+}
+
+func (i *CheckoutInput) Validate() *render.ValidationError {
+	if ve := i.Edition.Validate(); ve != nil {
+		return ve
+	}
+
+	i.SuccessURL = strings.TrimSpace(i.SuccessURL)
+	i.CancelURL = strings.TrimSpace(i.CancelURL)
+
+	ve := validator.New("successUrl").Required().Validate(i.SuccessURL)
+	if ve != nil {
+		return ve
+	}
+
+	ve = validator.New("cancelUrl").Required().Validate(i.CancelURL)
+	if ve != nil {
+		return ve
+	}
+
+	return nil
+}
+
+type CheckoutParams struct {
+	Account reader.FtcAccount
+	Plan    Plan
+	Input   CheckoutInput
+}
