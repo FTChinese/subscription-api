784345125d1db215d5482c0ac5ff23154754517a
weiguo.ni <neefrankie@gmail.com>
2021-02-01 16:51:42 +0800

    feat: data structure for stripe checkout session

--- pkg/stripe/checkout.go
+++ pkg/stripe/checkout.go
@@ -1,56 +1,32 @@
 package stripe
 
 import (
-	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/go-rest/render"
 	"github.com/FTChinese/subscription-api/pkg/product"
-	"github.com/FTChinese/subscription-api/pkg/reader"
-	"github.com/FTChinese/subscription-api/pkg/validator"
-	"github.com/guregu/null"
+	"github.com/stripe/stripe-go/v72"
 )
 
-type SubsParams struct {
-	CouponID             null.String `json:"coupon"`
-	DefaultPaymentMethod null.String `json:"defaultPaymentMethod"`
-	IdempotencyKey       string      `json:"idempotency"` // TODO: add when and why this is needed.
+type CheckoutSession struct {
+	CancelURL     string                              `json:"cancelUrl"`
+	AmountTotal   int64                               `json:"amountTotal"`
+	Currency      product.Currency                    `json:"currency"`
+	CustomerID    string                              `json:"customerId"`
+	ID            string                              `json:"id"`
+	LiveMode      bool                                `json:"liveMode"`
+	Mode          stripe.CheckoutSessionMode          `json:"mode"`
+	PaymentStatus stripe.CheckoutSessionPaymentStatus `json:"paymentStatus"`
+	SuccessURL    string                              `json:"successUrl"`
 }
 
-// SubsInput is the request body to create a new subscription.
-type SubsInput struct {
-	product.Edition // Use this to find strip's price id.
-	SubsParams
-
-	PlanID     string `json:"-"`        // Deprecated
-	FtcID      string `json:"-"`        // Deprecated.
-	CustomerID string `json:"customer"` // Deprecated.
-}
-
-// Validate checks if customer and idempotency fields are set.
-func (i SubsInput) Validate(isUpgrade bool) *render.ValidationError {
-	if ve := i.Edition.Validate(); ve != nil {
-		return ve
+func NewCheckoutSession(sc *stripe.CheckoutSession) CheckoutSession {
+	return CheckoutSession{
+		CancelURL:     sc.CancelURL,
+		AmountTotal:   sc.AmountTotal,
+		Currency:      product.Currency(sc.Currency),
+		CustomerID:    sc.Customer.ID,
+		ID:            sc.ID,
+		LiveMode:      false,
+		Mode:          sc.Mode,
+		PaymentStatus: sc.PaymentStatus,
+		SuccessURL:    sc.SuccessURL,
 	}
-
-	if isUpgrade && i.Tier != enum.TierPremium {
-		return &render.ValidationError{
-			Message: "Not the correct edition to upgrade to",
-			Field:   "tier",
-			Code:    render.CodeInvalid,
-		}
-	}
-
-	return validator.New("idempotency").Required().Validate(i.IdempotencyKey)
-}
-
-// PaymentConfig contains the data used to create/upgrade a subscription.
-type PaymentConfig struct {
-	Account reader.FtcAccount
-	Plan    Plan
-	Params  SubsParams
-}
-
-type CancelParams struct {
-	FtcID  string
-	SubID  string
-	Cancel bool // True for cancel, false for reactivation.
 }
