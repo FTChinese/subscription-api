a036d9e80d721acec3c253ab45de889016fb1f6c
neefrankie <neefrankie@gmail.com>
2021-02-04 04:43:56 +0800

    refactor: retrieve plan by edition

--- internal/controller/subs_wxpay.go
+++ internal/controller/subs_wxpay.go
@@ -63,7 +63,7 @@ func (router SubsRouter) PlaceWxOrder(tradeType wechat.TradeType) http.HandlerFu
 		}
 
 		// Retrieve the plan from DB by edition.
-		plan, err := router.prodRepo.PlanByEdition(input.Edition)
+		plan, err := router.prodRepo.FindActivePlan(input.Edition)
 		if err != nil {
 			sugar.Error(err)
 			_ = render.New(w).DBError(err)
