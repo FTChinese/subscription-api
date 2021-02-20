53c622ac4c46cc4a5d564e54b7676dae0809a5b6
neefrankie <neefrankie@gmail.com>
2021-02-20 14:21:48 +0800

    refactor: rename ProductPrice to FtcPrice

--- pkg/pw/price_schema.go
+++ pkg/pw/price_schema.go
@@ -2,6 +2,7 @@ package pw
 
 import (
 	"github.com/FTChinese/subscription-api/pkg/price"
+	"github.com/guregu/null"
 )
 
 // PriceSchema is used to retrieve a price's price together with
@@ -13,4 +14,22 @@ type PriceSchema struct {
 	price.Edition
 	PlanDesc string `db:"description"`
 	price.Discount
+}
+
+// NewFtcPrice turns the raw data from DB into FtcPrice.
+func NewFtcPrice(s PriceSchema) FtcPrice {
+	return FtcPrice{
+		Original: price.Price{
+			ID:         s.PlanID,
+			Edition:    s.Edition,
+			Active:     true,
+			Currency:   price.CurrencyCNY,
+			LiveMode:   true,
+			Nickname:   null.String{},
+			ProductID:  s.ProductID,
+			Source:     price.SourceFTC,
+			UnitAmount: s.PlanPrice,
+		},
+		PromotionOffer: s.Discount,
+	}
 }
