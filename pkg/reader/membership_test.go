82013088763fc60002c5fd5caef9fe92c28b4db6
neefrankie <neefrankie@gmail.com>
2021-02-21 13:31:10 +0800

    refactor: removed AliWxSubsKind method

--- pkg/reader/membership_test.go
+++ pkg/reader/membership_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/addon"
 	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/google/uuid"
 	"github.com/guregu/null"
@@ -356,158 +357,6 @@ func TestMembership_canRenewViaAliWx(t *testing.T) {
 	}
 }
 
-func TestMembership_AliWxSubsKind(t *testing.T) {
-
-	type args struct {
-		e price.Edition
-	}
-	tests := []struct {
-		name   string
-		fields Membership
-		args   args
-		want   enum.OrderKind
-		want1  *render.ValidationError
-	}{
-		{
-			name:   "Zero member",
-			fields: Membership{},
-			args: args{
-				e: faker.PriceStdYear.Original.Edition,
-			},
-			want:  enum.OrderKindCreate,
-			want1: nil,
-		},
-		{
-			name: "Expired member",
-			fields: Membership{
-				MemberID: MemberID{
-					CompoundID: "",
-					FtcID:      null.StringFrom(uuid.New().String()),
-					UnionID:    null.String{},
-				}.MustNormalize(),
-				Edition: price.Edition{
-					Tier:  enum.TierStandard,
-					Cycle: enum.CycleYear,
-				},
-				ExpireDate:    chrono.DateFrom(time.Now().AddDate(-1, 0, 0)),
-				PaymentMethod: enum.PayMethodAli,
-			},
-			args: args{
-				e: faker.PriceStdYear.Original.Edition,
-			},
-			want:  enum.OrderKindCreate,
-			want1: nil,
-		},
-		{
-			name: "Stripe not expired",
-			fields: Membership{
-				MemberID: MemberID{
-					CompoundID: "",
-					FtcID:      null.StringFrom(uuid.New().String()),
-					UnionID:    null.String{},
-				}.MustNormalize(),
-				Edition: price.Edition{
-					Tier:  enum.TierStandard,
-					Cycle: enum.CycleYear,
-				},
-				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-				PaymentMethod: enum.PayMethodStripe,
-				StripeSubsID:  null.StringFrom(faker.GenStripeSubID()),
-				StripePlanID:  null.StringFrom(faker.GenStripePlanID()),
-				Status:        enum.SubsStatusActive,
-			},
-			args: args{
-				e: faker.PriceStdYear.Original.Edition,
-			},
-			want: enum.OrderKindNull,
-			want1: &render.ValidationError{
-				Message: fmt.Sprintf("Already subscribed via %s", enum.PayMethodStripe),
-				Field:   "paymentMethod",
-				Code:    render.CodeInvalid,
-			},
-		},
-		{
-			name: "Renewal",
-			fields: Membership{
-				MemberID: MemberID{
-					CompoundID: "",
-					FtcID:      null.StringFrom(uuid.New().String()),
-					UnionID:    null.String{},
-				}.MustNormalize(),
-				Edition: price.Edition{
-					Tier:  enum.TierStandard,
-					Cycle: enum.CycleYear,
-				},
-				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-				PaymentMethod: enum.PayMethodAli,
-			},
-			args: args{
-				e: faker.PriceStdYear.Original.Edition,
-			},
-			want:  enum.OrderKindRenew,
-			want1: nil,
-		},
-		{
-			name: "Upgrade",
-			fields: Membership{
-				MemberID: MemberID{
-					CompoundID: "",
-					FtcID:      null.StringFrom(uuid.New().String()),
-					UnionID:    null.String{},
-				}.MustNormalize(),
-				Edition: price.Edition{
-					Tier:  enum.TierStandard,
-					Cycle: enum.CycleYear,
-				},
-				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-				PaymentMethod: enum.PayMethodAli,
-			},
-			args: args{
-				e: faker.PricePrm.Original.Edition,
-			},
-			want:  enum.OrderKindUpgrade,
-			want1: nil,
-		},
-		{
-			name: "Downgrade",
-			fields: Membership{
-				MemberID: MemberID{
-					CompoundID: "",
-					FtcID:      null.StringFrom(uuid.New().String()),
-					UnionID:    null.String{},
-				}.MustNormalize(),
-				Edition: price.Edition{
-					Tier:  enum.TierPremium,
-					Cycle: enum.CycleYear,
-				},
-				ExpireDate:    chrono.DateFrom(time.Now().AddDate(1, 0, 0)),
-				PaymentMethod: enum.PayMethodAli,
-			},
-			args: args{
-				e: faker.PriceStdYear.Original.Edition,
-			},
-			want: enum.OrderKindNull,
-			want1: &render.ValidationError{
-				Message: "Downgrading is forbidden.",
-				Field:   "downgrade",
-				Code:    render.CodeInvalid,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := tt.fields
-			got, got1 := m.AliWxSubsKind(tt.args.e)
-			if got != tt.want {
-				t.Errorf("AliWxSubsKind() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("AliWxSubsKind() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
 func TestMembership_StripeSubsKind(t *testing.T) {
 
 	type args struct {
@@ -636,13 +485,13 @@ func TestReservedDays_Plus(t *testing.T) {
 		Premium  int64
 	}
 	type args struct {
-		other ReservedDays
+		other addon.ReservedDays
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
-		want   ReservedDays
+		want   addon.ReservedDays
 	}{
 		{
 			name: "Plus",
@@ -651,12 +500,12 @@ func TestReservedDays_Plus(t *testing.T) {
 				Premium:  101,
 			},
 			args: args{
-				other: ReservedDays{
+				other: addon.ReservedDays{
 					Standard: 15,
 					Premium:  19,
 				},
 			},
-			want: ReservedDays{
+			want: addon.ReservedDays{
 				Standard: 31 + 15,
 				Premium:  101 + 19,
 			},
@@ -664,7 +513,7 @@ func TestReservedDays_Plus(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			d := ReservedDays{
+			d := addon.ReservedDays{
 				Standard: tt.fields.Standard,
 				Premium:  tt.fields.Premium,
 			}
