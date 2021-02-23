b9a715d4d1acb7fb2f2268bfc9fcb57d448d97db
weiguo.ni <neefrankie@gmail.com>
2021-02-23 15:59:14 +0800

    refactor: file structure for order confirmation

---
+++ pkg/subs/confirmation_params.go
@@ -0,0 +1,43 @@
+package subs
+
+import (
+	"github.com/FTChinese/subscription-api/lib/dt"
+	"github.com/FTChinese/subscription-api/pkg/reader"
+)
+
+// ConfirmationParams contains data used to confirm an order.
+type ConfirmationParams struct {
+	Payment PaymentResult
+	Order   Order
+	Member  reader.Membership
+}
+
+func (params ConfirmationParams) confirmNewOrRenewalOrder() Order {
+	params.Order.ConfirmedAt = params.Payment.ConfirmedUTC
+
+	startTime := dt.PickLater(params.Payment.ConfirmedUTC.Time, params.Member.ExpireDate.Time)
+
+	params.Order.DateRange = dt.NewDateRange(startTime).
+		WithCycle(params.Order.Cycle).
+		AddDays(trialDays)
+
+	return params.Order
+}
+
+func (params ConfirmationParams) confirmUpgradeOrder() Order {
+	params.Order.ConfirmedAt = params.Payment.ConfirmedUTC
+	params.Order.DateRange = dt.NewDateRange(params.Payment.ConfirmedUTC.Time).
+		WithCycle(params.Order.Cycle).
+		AddDays(trialDays)
+
+	return params.Order
+}
+
+func (params ConfirmationParams) snapshot() reader.MemberSnapshot {
+	if params.Member.IsZero() {
+		return reader.MemberSnapshot{}
+	}
+
+	return params.Member.Snapshot(
+		reader.FtcArchiver(params.Order.Kind))
+}
