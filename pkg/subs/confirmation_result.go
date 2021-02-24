0a48a74fe07001fdd4fe6cfb51f8a6dc403ee784
weiguo.ni <neefrankie@gmail.com>
2021-02-24 17:14:18 +0800

    refactor: move newMembership as a method ConfirmedOrder

--- pkg/subs/confirmation_result.go
+++ pkg/subs/confirmation_result.go
@@ -1,9 +1,7 @@
 package subs
 
 import (
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/pkg/reader"
-	"github.com/guregu/null"
 )
 
 const StmtSaveConfirmResult = `
@@ -20,33 +18,6 @@ type ConfirmError struct {
 
 func (c ConfirmError) Error() string {
 	return c.Message
-}
-
-func newMembership(co ConfirmedOrder, currentMember reader.Membership) reader.Membership {
-	// If an order is created as an add-on, only add the reserved days
-	// to current membership.
-	if co.Order.Kind == enum.OrderKindAddOn {
-		return currentMember.
-			WithReservedDays(co.AddOn.ToReservedDays())
-	}
-
-	return reader.Membership{
-		MemberID:      co.Order.MemberID,
-		Edition:       co.Order.Edition,
-		LegacyTier:    null.Int{},
-		LegacyExpire:  null.Int{},
-		ExpireDate:    co.Order.EndDate,
-		PaymentMethod: co.Order.PaymentMethod,
-		FtcPlanID:     null.StringFrom(co.Order.PlanID),
-		StripeSubsID:  null.String{},
-		StripePlanID:  null.String{},
-		AutoRenewal:   false,
-		Status:        enum.SubsStatusNull,
-		AppleSubsID:   null.String{},
-		B2BLicenceID:  null.String{},
-		ReservedDays: currentMember.ReservedDays.
-			Plus(co.AddOn.ToReservedDays()),
-	}.Sync()
 }
 
 // ConfirmationResult contains all the data in the process of confirming an order.
@@ -68,7 +39,7 @@ func NewConfirmationResult(p ConfirmationParams) (ConfirmationResult, error) {
 	return ConfirmationResult{
 		Payment:        p.Payment,
 		ConfirmedOrder: co,
-		Membership:     newMembership(co, p.Member),
+		Membership:     co.newMembership(p.Member),
 		Snapshot:       p.snapshot(),
 		Notify:         true,
 	}, nil
--- pkg/subs/confirmed_order.go
+++ pkg/subs/confirmed_order.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/pkg/addon"
+	"github.com/FTChinese/subscription-api/pkg/reader"
+	"github.com/guregu/null"
 )
 
 // ConfirmedOrder contains the result of an order in confirmed state,
@@ -49,4 +51,33 @@ func NewConfirmedOrder(params ConfirmationParams) (ConfirmedOrder, error) {
 	default:
 		return ConfirmedOrder{}, errors.New("unknown order kind")
 	}
+}
+
+// newMembership creates a updates current membership based on
+// a one-time purchase.
+func (co ConfirmedOrder) newMembership(currentMember reader.Membership) reader.Membership {
+	// If an order is created as an add-on, only add the reserved days
+	// to current membership.
+	if co.Order.Kind == enum.OrderKindAddOn {
+		return currentMember.
+			WithReservedDays(co.AddOn.ToReservedDays())
+	}
+
+	return reader.Membership{
+		MemberID:      co.Order.MemberID,
+		Edition:       co.Order.Edition,
+		LegacyTier:    null.Int{},
+		LegacyExpire:  null.Int{},
+		ExpireDate:    co.Order.EndDate,
+		PaymentMethod: co.Order.PaymentMethod,
+		FtcPlanID:     null.StringFrom(co.Order.PlanID),
+		StripeSubsID:  null.String{},
+		StripePlanID:  null.String{},
+		AutoRenewal:   false,
+		Status:        enum.SubsStatusNull,
+		AppleSubsID:   null.String{},
+		B2BLicenceID:  null.String{},
+		ReservedDays: currentMember.ReservedDays.
+			Plus(co.AddOn.ToReservedDays()),
+	}.Sync()
 }
