b4dde25a0391a7274e522fa922d1d0bd1c265c45
weiguo.ni <neefrankie@gmail.com>
2021-02-24 14:28:59 +0800

    fix: formatMethods

--- pkg/cart/checkout_intent.go
+++ pkg/cart/checkout_intent.go
@@ -10,17 +10,32 @@ import (
 )
 
 func formatMethods(methods []enum.PayMethod) string {
-	var str []string
-	lastIndex := len(methods) - 1
-	for i, m := range methods {
-		if i != lastIndex {
-			str = append(str, ","+m.String())
-		} else {
-			str = append(str, "or "+m.String())
+	l := len(methods)
+	switch {
+	case l == 0:
+		return ""
+
+	case l == 1:
+		return methods[0].String()
+
+	case l >= 2:
+		var buf strings.Builder
+		for i, v := range methods {
+			if i == 0 {
+				buf.WriteString(v.String())
+				continue
+			}
+			if i == l-1 {
+				buf.WriteString(" or " + v.String())
+				continue
+			}
+
+			buf.WriteString(", " + v.String())
 		}
+		return buf.String()
 	}
 
-	return strings.Join(str, " ")
+	return ""
 }
 
 // CheckoutIntent decides how user want to purchase a product.
