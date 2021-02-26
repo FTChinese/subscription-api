3f1063b795c4c2794e50e6869967c1c957a9c9ac
weiguo.ni <neefrankie@gmail.com>
2021-02-26 10:16:07 +0800

    refactor: rename DateRange to DatePeriod and DateTimePeriod to DateTimeRange

--- lib/dt/time_range.go
+++ lib/dt/time_range.go
@@ -6,72 +6,89 @@ import (
 	"time"
 )
 
-type DateTimeRange struct {
-	StartUTC chrono.Time `json:"startUtc" db:"start_utc"`
-	EndUTC   chrono.Time `json:"endUtc" db:"end_utc"`
+type TimeRange struct {
+	Start time.Time
+	End   time.Time
 }
 
-// DateRange is used to build the subscription period of a one-time purchase.
-type DateRange struct {
-	// Membership start date for this order. If might be ConfirmedAt or user's existing membership's expire date.
-	StartDate chrono.Date `json:"startDate" db:"start_date"`
-	// Membership end date for this order. Depends on start date.
-	EndDate chrono.Date `json:"endDate" db:"end_date"`
-}
-
-// NewDateRange creates a new instance of DateRange from a moment, with both starting and ending time set to the same value.
-func NewDateRange(start time.Time) DateRange {
-	return DateRange{
-		StartDate: chrono.DateFrom(start),
-		EndDate:   chrono.DateFrom(start),
+func NewTimeRange(start time.Time) TimeRange {
+	return TimeRange{
+		Start: start,
+		End:   start,
 	}
 }
 
-// WithCycle adds a billing cycle to end date.
-func (d DateRange) WithCycle(cycle enum.Cycle) DateRange {
+func (r TimeRange) WithCycle(cycle enum.Cycle) TimeRange {
 	switch cycle {
 	case enum.CycleYear:
-		d.EndDate = chrono.DateFrom(d.EndDate.AddDate(1, 0, 0))
+		r.End = r.End.AddDate(1, 0, 0)
 
 	case enum.CycleMonth:
-		d.EndDate = chrono.DateFrom(d.EndDate.AddDate(0, 1, 0))
+		r.End = r.End.AddDate(0, 1, 0)
 	}
 
-	return d
+	return r
 }
 
 // WithCycleN adds n cycles to end date.
-func (d DateRange) WithCycleN(cycle enum.Cycle, n int) DateRange {
+func (r TimeRange) WithCycleN(cycle enum.Cycle, n int) TimeRange {
 	switch cycle {
 	case enum.CycleYear:
-		d.EndDate = chrono.DateFrom(d.EndDate.AddDate(n, 0, 0))
+		r.End = r.End.AddDate(n, 0, 0)
 	case enum.CycleMonth:
-		d.EndDate = chrono.DateFrom(d.EndDate.AddDate(0, n, 0))
+		r.End = r.End.AddDate(0, n, 0)
 	}
 
-	return d
+	return r
 }
 
-func (d DateRange) AddYears(years int) DateRange {
-	d.EndDate = chrono.DateFrom(d.EndDate.AddDate(years, 0, 0))
-	return d
+func (r TimeRange) AddYears(years int) TimeRange {
+	r.End = r.End.AddDate(years, 0, 0)
+	return r
 }
 
-func (d DateRange) AddMonths(months int) DateRange {
-	d.EndDate = chrono.DateFrom(d.EndDate.AddDate(0, months, 0))
-	return d
+func (r TimeRange) AddMonths(months int) TimeRange {
+	r.End = r.End.AddDate(0, months, 0)
+	return r
 }
 
-func (d DateRange) AddDays(days int) DateRange {
-	d.EndDate = chrono.DateFrom(d.EndDate.AddDate(0, 0, days))
+func (r TimeRange) AddDays(days int) TimeRange {
+	r.End = r.End.AddDate(0, 0, days)
 
-	return d
+	return r
 }
 
 // AddDate adds the specified years, months, days to end date.
 // This is a simple wrapper of Time.AddDate.
-func (d DateRange) AddDate(years, months, days int) DateRange {
-	d.EndDate = chrono.DateFrom(d.EndDate.AddDate(years, months, days))
+func (r TimeRange) AddDate(years, months, days int) TimeRange {
+	r.End = r.End.AddDate(years, months, days)
 
-	return d
+	return r
+}
+
+func (r TimeRange) ToDatePeriod() DatePeriod {
+	return DatePeriod{
+		StartDate: chrono.DateFrom(r.Start),
+		EndDate:   chrono.DateFrom(r.End),
+	}
+}
+
+func (r TimeRange) ToDateTimePeriod() DateTimePeriod {
+	return DateTimePeriod{
+		StartUTC: chrono.TimeFrom(r.Start),
+		EndUTC:   chrono.TimeFrom(r.End),
+	}
+}
+
+type DateTimePeriod struct {
+	StartUTC chrono.Time `json:"startUtc" db:"start_utc"`
+	EndUTC   chrono.Time `json:"endUtc" db:"end_utc"`
+}
+
+// DatePeriod is used to build the subscription period of a one-time purchase.
+type DatePeriod struct {
+	// Membership start date for this order. If might be ConfirmedAt or user's existing membership's expire date.
+	StartDate chrono.Date `json:"startDate" db:"start_date"`
+	// Membership end date for this order. Depends on start date.
+	EndDate chrono.Date `json:"endDate" db:"end_date"`
 }
