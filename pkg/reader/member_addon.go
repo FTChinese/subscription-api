d89d9bb2540fbbb5c2dddb304b53d62231b6b36e
neefrankie <neefrankie@gmail.com>
2021-03-07 04:46:56 +0800

    refactor: dropped addon table

--- pkg/reader/member_addon.go
+++ pkg/reader/member_addon.go
@@ -2,24 +2,11 @@ package reader
 
 import (
 	"errors"
-	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/subscription-api/lib/collection"
-	"github.com/FTChinese/subscription-api/lib/dt"
 	"github.com/FTChinese/subscription-api/pkg/addon"
-	"github.com/FTChinese/subscription-api/pkg/db"
-	"github.com/guregu/null"
-	"time"
 )
 
-type AddOnConsumed struct {
-	AddOnIDs   collection.StringSet
-	Membership Membership
-	Snapshot   MemberSnapshot
-}
-
-func (m Membership) WithReservedDays(days addon.ReservedDays) Membership {
-	m.ReservedDays = m.ReservedDays.Plus(days)
+func (m Membership) WithReservedDays(days addon.AddOn) Membership {
+	m.AddOn = m.AddOn.Plus(days)
 	return m
 }
 
@@ -41,73 +28,4 @@ func (m Membership) ShouldUseAddOn() error {
 	}
 
 	return nil
-}
-
-func (m Membership) CarryOver(source addon.Source) addon.AddOn {
-	return addon.AddOn{
-		ID:              db.AddOnID(),
-		Edition:         m.Edition,
-		CycleCount:      0,
-		DaysRemained:    m.RemainingDays(),
-		CarryOverSource: source,
-		PaymentMethod:   m.PaymentMethod,
-		CompoundID:      m.CompoundID,
-		OrderID:         null.String{},
-		PlanID:          m.FtcPlanID, // Save it so that it could be restored together with the remaining days.
-		CreatedUTC:      chrono.TimeNow(),
-		ConsumedUTC:     chrono.Time{},
-	}
-}
-
-// WithAddOnSum extends membership's expiration time by adding up all add-ons.
-func (m Membership) WithAddOnSum(sum addon.Sum) Membership {
-	startTime := dt.PickLater(time.Now(), m.ExpireDate.Time)
-
-	endTime := startTime.AddDate(sum.Years, sum.Months, sum.Days)
-
-	return Membership{
-		MemberID:      m.MemberID,
-		Edition:       sum.Latest.Edition,
-		LegacyTier:    null.Int{},
-		LegacyExpire:  null.Int{},
-		ExpireDate:    chrono.DateFrom(endTime),
-		PaymentMethod: sum.Latest.PaymentMethod,
-		FtcPlanID:     sum.Latest.PlanID,
-		StripeSubsID:  null.String{},
-		StripePlanID:  null.String{},
-		AutoRenewal:   false,
-		Status:        0,
-		AppleSubsID:   null.String{},
-		B2BLicenceID:  null.String{},
-		ReservedDays:  m.ReservedDays.Clear(sum.Latest.Tier),
-	}.Sync()
-}
-
-// ConsumeAddOns transfer addon periods to current membership.
-func (m Membership) ConsumeAddOns(addOns []addon.AddOn) AddOnConsumed {
-	addOnSums := addon.GroupAndReduce(addOns)
-
-	snapshot := m.Snapshot(FtcArchiver(enum.OrderKindAddOn))
-
-	prmSum, ok := addOnSums[enum.TierPremium]
-	if ok {
-		return AddOnConsumed{
-			AddOnIDs:   prmSum.IDs,
-			Membership: m.WithAddOnSum(prmSum),
-			Snapshot:   snapshot,
-		}
-	}
-
-	stdSum, ok := addOnSums[enum.TierStandard]
-	if ok {
-		return AddOnConsumed{
-			AddOnIDs:   stdSum.IDs,
-			Membership: m.WithAddOnSum(stdSum),
-			Snapshot:   snapshot,
-		}
-	}
-
-	return AddOnConsumed{
-		Membership: m,
-	}
 }
