52a8f60e562d1ccfcdeffb3270bfc7ad0fa00bcc
neefrankie <neefrankie@gmail.com>
2021-03-08 23:07:26 +0800

    test

---
+++ internal/repository/readerrepo/addon_test.go
@@ -0,0 +1,78 @@
+package readerrepo
+
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg"
+	"github.com/FTChinese/subscription-api/pkg/invoice"
+	"github.com/FTChinese/subscription-api/pkg/reader"
+	"github.com/FTChinese/subscription-api/test"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+	"testing"
+	"time"
+)
+
+func TestEnv_ClaimAddOn(t *testing.T) {
+	userID := uuid.New().String()
+
+	repo := test.NewRepo()
+	repo.MustSaveMembership(reader.NewMockMemberBuilder(userID).
+		WithExpiration(time.Now().AddDate(0, 0, -1)).
+		Build())
+	repo.MustSaveInvoiceN([]invoice.Invoice{
+		reader.NewMockMemberBuilder(userID).
+			Build().
+			CarryOverInvoice(),
+		invoice.NewMockInvoiceBuilder(userID).
+			WithOrderKind(enum.OrderKindAddOn).
+			Build(),
+	})
+
+	type fields struct {
+		db     *sqlx.DB
+		logger *zap.Logger
+	}
+	type args struct {
+		ids pkg.UserIDs
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    reader.AddOnClaimed
+		wantErr bool
+	}{
+		{
+			name: "Claim addon",
+			fields: fields{
+				db:     test.DB,
+				logger: zaptest.NewLogger(t),
+			},
+			args: args{
+				ids: pkg.NewFtcUserID(userID),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				logger: tt.fields.logger,
+			}
+			got, err := env.ClaimAddOn(tt.args.ids)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ClaimAddOn() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("ClaimAddOn() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
