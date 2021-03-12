1e2004159466b64ce72d0b85ddc543a2e6ca066c
weiguo.ni <neefrankie@gmail.com>
2021-03-12 14:32:54 +0800

    fix: webhook url

--- pkg/subs/checkout.go
+++ pkg/subs/checkout.go
@@ -1,52 +1,9 @@
 package subs
 
 import (
-	"fmt"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/pkg/price"
 )
-
-func WebhookURL(sandbox bool, method enum.PayMethod) string {
-	var baseURL string
-	if sandbox {
-		baseURL = "http://www.ftacademy.cn/api/sandbox"
-	} else {
-		baseURL = "http://www.ftacademy.cn/api/v1"
-	}
-
-	switch method {
-	case enum.PayMethodWx:
-		return baseURL + "/webhook/wxpay"
-	case enum.PayMethodAli:
-		return baseURL + "/webhook/alipay"
-	}
-
-	return ""
-}
-
-// PaymentTitle is used as the value of `subject` for alipay,
-// and `body` for wechat pay.
-// * 订阅FT中文网标准会员/年
-// * 订阅FT中文网高端会员/年
-// * 购买FT中文网标准会员/年
-// * 购买FT中文网高端会员/年
-func PaymentTitle(k enum.OrderKind, e price.Edition) string {
-	var prefix string
-
-	switch k {
-	case enum.OrderKindCreate:
-	case enum.OrderKindRenew:
-	case enum.OrderKindUpgrade:
-		prefix = "订阅"
-
-	case enum.OrderKindAddOn:
-		prefix = "购买"
-
-	default:
-	}
-
-	return fmt.Sprintf("%sFT中文网%s", prefix, e.StringCN())
-}
 
 // Checkout contains the calculation result of a purchase transaction.
 type Checkout struct {
