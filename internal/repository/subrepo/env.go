effc47f18f37efccebe73e7bc607f49268bc7b65
weiguo.ni <neefrankie@gmail.com>
2021-03-16 12:15:14 +0800

    refactor: split db reader and write

--- internal/repository/subrepo/env.go
+++ internal/repository/subrepo/env.go
@@ -2,27 +2,27 @@ package subrepo
 
 import (
 	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
-	"github.com/jmoiron/sqlx"
+	"github.com/FTChinese/subscription-api/pkg/db"
 	"go.uber.org/zap"
 )
 
 // Env wraps database connection
 type Env struct {
-	rwdDB  *sqlx.DB // Need delete privilege.
+	dbs    db.ReadWriteSplit
 	logger *zap.Logger
 }
 
 // NewEnv creates a new instance of Env.
 // `sandbox` is used to determine which table to write subscription data.
-func NewEnv(db *sqlx.DB, logger *zap.Logger) Env {
+func NewEnv(dbs db.ReadWriteSplit, logger *zap.Logger) Env {
 	return Env{
-		rwdDB:  db,
+		dbs:    dbs,
 		logger: logger,
 	}
 }
 
 func (env Env) BeginOrderTx() (txrepo.MemberTx, error) {
-	tx, err := env.rwdDB.Beginx()
+	tx, err := env.dbs.Delete.Beginx()
 
 	if err != nil {
 		return txrepo.MemberTx{}, err
