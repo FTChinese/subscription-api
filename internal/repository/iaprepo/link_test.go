3ecfb4591768d647a9a172bd202b5e03df7b6d53
weiguo.ni <neefrankie@gmail.com>
2021-03-16 17:48:28 +0800

    refactor: split db reader and write

--- internal/repository/iaprepo/link_test.go
+++ internal/repository/iaprepo/link_test.go
@@ -4,11 +4,8 @@ import (
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/pkg/apple"
+	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/FTChinese/subscription-api/test"
-	"github.com/go-redis/redis/v8"
-	"github.com/jmoiron/sqlx"
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 	"testing"
 )
@@ -16,28 +13,27 @@ import (
 func TestEnv_GetSubAndSetFtcID(t *testing.T) {
 	p := test.NewPersona().SetPayMethod(enum.PayMethodApple)
 
-	test.NewRepo().MustSaveIAPSubs(p.IAPSubs())
+	test.NewRepo().MustSaveIAPSubs(apple.
+		NewMockSubsBuilder(p.FtcID).
+		WithOriginalTxID(p.AppleSubID).
+		Build())
 
-	type fields struct {
-		db     *sqlx.DB
-		rdb    *redis.Client
-		logger *zap.Logger
+	env := Env{
+		dbs:    test.SplitDB,
+		rdb:    nil,
+		logger: zaptest.NewLogger(t),
 	}
+
 	type args struct {
 		input apple.LinkInput
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name: "Get subscription and optionally set ftc id",
-			fields: fields{
-				db:     test.DB,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{
 				input: apple.LinkInput{
 					FtcID:        p.FtcID,
@@ -49,11 +45,6 @@ func TestEnv_GetSubAndSetFtcID(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				db:     tt.fields.db,
-				rdb:    tt.fields.rdb,
-				logger: tt.fields.logger,
-			}
 			got, err := env.GetSubAndSetFtcID(tt.args.input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetSubAndSetFtcID() error = %v, wantErr %v", err, tt.wantErr)
@@ -69,27 +60,22 @@ func TestEnv_ArchiveLinkCheating(t *testing.T) {
 
 	p := test.NewPersona()
 
-	type fields struct {
-		db     *sqlx.DB
-		rdb    *redis.Client
-		logger *zap.Logger
+	env := Env{
+		dbs:    test.SplitDB,
+		rdb:    nil,
+		logger: zaptest.NewLogger(t),
 	}
+
 	type args struct {
 		link apple.LinkInput
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name: "Archive link cheating",
-			fields: fields{
-				db:     test.DB,
-				rdb:    test.Redis,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{link: apple.LinkInput{
 				FtcID:        p.FtcID,
 				OriginalTxID: p.AppleSubID,
@@ -98,11 +84,6 @@ func TestEnv_ArchiveLinkCheating(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				db:     tt.fields.db,
-				rdb:    tt.fields.rdb,
-				logger: tt.fields.logger,
-			}
 			if err := env.ArchiveLinkCheating(tt.args.link); (err != nil) != tt.wantErr {
 				t.Errorf("ArchiveLinkCheating() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -111,16 +92,23 @@ func TestEnv_ArchiveLinkCheating(t *testing.T) {
 }
 
 func TestEnv_Unlink(t *testing.T) {
-
 	p := test.NewPersona().SetPayMethod(enum.PayMethodApple)
 
-	sub := p.IAPSubs()
-
 	repo := test.NewRepo()
-	repo.MustSaveMembership(p.Membership())
-	repo.MustSaveIAPSubs(sub)
+	repo.MustSaveMembership(reader.
+		NewMockMemberBuilder(p.FtcID).
+		WithPayMethod(enum.PayMethodApple).
+		WithIapID(p.AppleSubID).Build())
+	repo.MustSaveIAPSubs(apple.
+		NewMockSubsBuilder(p.FtcID).
+		WithOriginalTxID(p.AppleSubID).
+		Build())
 
-	env := NewEnv(test.DB, test.Redis, zaptest.NewLogger(t))
+	env := Env{
+		dbs:    test.SplitDB,
+		rdb:    test.Redis,
+		logger: zaptest.NewLogger(t),
+	}
 
 	type args struct {
 		input apple.LinkInput
@@ -131,27 +119,26 @@ func TestEnv_Unlink(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Unlink IAP",
+			name: "Unlink",
 			args: args{
 				input: apple.LinkInput{
 					FtcID:        p.FtcID,
 					OriginalTxID: p.AppleSubID,
+					Force:        false,
 				},
 			},
-			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			got, err := env.Unlink(tt.args.input)
-
-			if tt.wantErr {
-				assert.Error(t, err)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Unlink() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-
-			assert.NoError(t, err)
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("Unlink() got = %v, want %v", got, tt.want)
+			//}
 
 			t.Logf("%s", faker.MustMarshalIndent(got))
 		})
@@ -162,7 +149,11 @@ func TestEnv_ArchiveUnlink(t *testing.T) {
 
 	p := test.NewPersona()
 
-	env := NewEnv(test.DB, test.Redis, zaptest.NewLogger(t))
+	env := Env{
+		dbs:    test.SplitDB,
+		rdb:    test.Redis,
+		logger: zaptest.NewLogger(t),
+	}
 
 	type args struct {
 		link apple.LinkInput
