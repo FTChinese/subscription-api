358aa5e5f857d1aacfa168d0199e40adc7695abc
weiguo.ni <neefrankie@gmail.com>
2021-03-16 12:15:27 +0800

    refactor: split db reader and write

--- internal/repository/subrepo/order_test.go
+++ internal/repository/subrepo/order_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/lib/dt"
 	"github.com/FTChinese/subscription-api/pkg"
+	"github.com/FTChinese/subscription-api/pkg/db"
 	"github.com/FTChinese/subscription-api/pkg/price"
+	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/FTChinese/subscription-api/pkg/subs"
 	"github.com/FTChinese/subscription-api/test"
 	"github.com/guregu/null"
-	"github.com/jmoiron/sqlx"
-	"github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
@@ -23,35 +23,31 @@ func TestEnv_CreateOrder(t *testing.T) {
 	wxID := faker.GenWxID()
 	repo := test.NewRepo()
 
-	p := test.NewPersona()
-	repo.MustSaveMembership(p.Membership())
+	newPersona := test.NewPersona()
+	renewalPerson := test.NewPersona()
+	upgradePerson := test.NewPersona()
+	addOnPerson := test.NewPersona()
 
-	p2 := test.NewPersona().SetPayMethod(enum.PayMethodApple)
-	repo.MustSaveMembership(p2.Membership())
-
-	type fields struct {
-		rwdDB  *sqlx.DB
-		logger *zap.Logger
+	env := Env{
+		dbs:    test.SplitDB,
+		logger: zaptest.NewLogger(t),
 	}
+
 	type args struct {
 		counter subs.Counter
+		p       *test.Persona
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		want    subs.Order
 		wantErr bool
 	}{
 		{
 			name: "New order",
-			fields: fields{
-				rwdDB:  test.DB,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{
 				counter: subs.Counter{
-					Account: p.FtcAccount(),
+					Account: newPersona.FtcAccount(),
 					Price:   faker.PriceStdYear,
 					Method:  enum.PayMethodAli,
 					WxAppID: null.String{},
@@ -59,7 +55,7 @@ func TestEnv_CreateOrder(t *testing.T) {
 			},
 			want: subs.Order{
 				ID:         "",
-				UserIDs:    p.AccountID(),
+				UserIDs:    newPersona.AccountID(),
 				PlanID:     faker.PriceStdYear.ID,
 				DiscountID: null.String{},
 				Price:      faker.PriceStdYear.UnitAmount,
@@ -80,21 +76,18 @@ func TestEnv_CreateOrder(t *testing.T) {
 		},
 		{
 			name: "Renewal order",
-			fields: fields{
-				rwdDB:  test.DB,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{
 				counter: subs.Counter{
-					Account: p.FtcAccount(),
+					Account: renewalPerson.FtcAccount(),
 					Price:   faker.PriceStdYear,
 					Method:  enum.PayMethodWx,
 					WxAppID: null.StringFrom(wxID),
 				},
+				p: renewalPerson,
 			},
 			want: subs.Order{
 				ID:         "",
-				UserIDs:    p.AccountID(),
+				UserIDs:    renewalPerson.AccountID(),
 				PlanID:     faker.PriceStdYear.ID,
 				DiscountID: null.String{},
 				Price:      faker.PriceStdYear.UnitAmount,
@@ -115,21 +108,18 @@ func TestEnv_CreateOrder(t *testing.T) {
 		},
 		{
 			name: "Upgrade order",
-			fields: fields{
-				rwdDB:  test.DB,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{
 				counter: subs.Counter{
-					Account: p.FtcAccount(),
+					Account: upgradePerson.FtcAccount(),
 					Price:   faker.PricePrm,
 					Method:  enum.PayMethodWx,
 					WxAppID: null.StringFrom(wxID),
 				},
+				p: upgradePerson,
 			},
 			want: subs.Order{
 				ID:         "",
-				UserIDs:    p.AccountID(),
+				UserIDs:    upgradePerson.AccountID(),
 				PlanID:     faker.PricePrm.ID,
 				DiscountID: null.String{},
 				Price:      faker.PricePrm.UnitAmount,
@@ -150,21 +140,18 @@ func TestEnv_CreateOrder(t *testing.T) {
 		},
 		{
 			name: "Add-on order",
-			fields: fields{
-				rwdDB:  test.DB,
-				logger: zaptest.NewLogger(t),
-			},
 			args: args{
 				counter: subs.Counter{
-					Account: p2.FtcAccount(),
+					Account: addOnPerson.FtcAccount(),
 					Price:   faker.PriceStdYear,
 					Method:  enum.PayMethodWx,
 					WxAppID: null.StringFrom(wxID),
 				},
+				p: addOnPerson,
 			},
 			want: subs.Order{
 				ID:         "",
-				UserIDs:    p2.AccountID(),
+				UserIDs:    addOnPerson.AccountID(),
 				PlanID:     faker.PriceStdYear.ID,
 				DiscountID: null.String{},
 				Price:      faker.PriceStdYear.UnitAmount,
@@ -186,9 +173,25 @@ func TestEnv_CreateOrder(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				rwdDB:  tt.fields.rwdDB,
-				logger: tt.fields.logger,
+			p := tt.args.p
+
+			switch tt.want.Kind {
+			case enum.OrderKindRenew:
+				repo.MustSaveMembership(
+					reader.NewMockMemberBuilder(p.FtcID).
+						Build(),
+				)
+			case enum.OrderKindUpgrade:
+				repo.MustSaveMembership(
+					reader.NewMockMemberBuilder(p.FtcID).
+						Build(),
+				)
+			case enum.OrderKindAddOn:
+				repo.MustSaveMembership(
+					reader.NewMockMemberBuilder(p.FtcID).
+						WithPayMethod(enum.PayMethodStripe).
+						Build(),
+				)
 			}
 			got, err := env.CreateOrder(tt.args.counter)
 
@@ -200,7 +203,7 @@ func TestEnv_CreateOrder(t *testing.T) {
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CreateOrder() got = %v\n, want %v", got, tt.want)
+				t.Errorf("CreateOrder() got = \n%v, want \n%v", got, tt.want)
 			}
 		})
 	}
@@ -208,8 +211,7 @@ func TestEnv_CreateOrder(t *testing.T) {
 
 func TestEnv_LogOrderMeta(t *testing.T) {
 	type fields struct {
-		db     *sqlx.DB
-		cache  *cache.Cache
+		dbs    db.ReadWriteSplit
 		logger *zap.Logger
 	}
 	type args struct {
@@ -224,7 +226,7 @@ func TestEnv_LogOrderMeta(t *testing.T) {
 		{
 			name: "Log order meta data",
 			fields: fields{
-				db: test.DB,
+				dbs: test.SplitDB,
 			},
 			args: args{
 				m: subs.OrderMeta{
@@ -238,11 +240,11 @@ func TestEnv_LogOrderMeta(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			env := Env{
-				rwdDB:  tt.fields.db,
+				dbs:    test.SplitDB,
 				logger: tt.fields.logger,
 			}
-			if err := env.LogOrderMeta(tt.args.m); (err != nil) != tt.wantErr {
-				t.Errorf("LogOrderMeta() error = %v, wantErr %v", err, tt.wantErr)
+			if err := env.SaveOrderMeta(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("SaveOrderMeta() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
@@ -255,25 +257,21 @@ func TestEnv_RetrieveOrder(t *testing.T) {
 	repo := test.NewRepo()
 	repo.MustSaveOrder(order)
 
-	type fields struct {
-		db     *sqlx.DB
-		cache  *cache.Cache
-		logger *zap.Logger
+	env := Env{
+		dbs:    test.SplitDB,
+		logger: zaptest.NewLogger(t),
 	}
+
 	type args struct {
 		orderID string
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name: "Retrieve order",
-			fields: fields{
-				db: test.DB,
-			},
 			args: args{
 				orderID: order.ID,
 			},
@@ -282,10 +280,6 @@ func TestEnv_RetrieveOrder(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				rwdDB:  tt.fields.db,
-				logger: tt.fields.logger,
-			}
 			got, err := env.RetrieveOrder(tt.args.orderID)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("RetrieveOrder() error = %v, wantErr %v", err, tt.wantErr)
@@ -306,7 +300,10 @@ func TestEnv_LoadFullOrder(t *testing.T) {
 
 	test.NewRepo().MustSaveOrder(order)
 
-	env := NewEnv(test.DB, zaptest.NewLogger(t))
+	env := Env{
+		dbs:    test.SplitDB,
+		logger: zaptest.NewLogger(t),
+	}
 
 	type args struct {
 		orderID string
