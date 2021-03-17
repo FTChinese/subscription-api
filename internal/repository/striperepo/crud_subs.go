4a6ef99fb071debf119db55b02abcb14ddeb6ffa
weiguo.ni <neefrankie@gmail.com>
2021-03-17 14:15:18 +0800

    refactor: split db reader and write

--- internal/repository/striperepo/crud_subs.go
+++ internal/repository/striperepo/crud_subs.go
@@ -4,7 +4,7 @@ import "github.com/FTChinese/subscription-api/pkg/stripe"
 
 // SaveSubsError saves any error in stripe response.
 func (env Env) SaveSubsError(e stripe.APIError) error {
-	_, err := env.db.NamedExec(stripe.StmtSaveAPIError, e)
+	_, err := env.dbs.Write.NamedExec(stripe.StmtSaveAPIError, e)
 
 	if err != nil {
 		return err
@@ -14,7 +14,7 @@ func (env Env) SaveSubsError(e stripe.APIError) error {
 }
 
 func (env Env) UpsertSubs(s stripe.Subs) error {
-	_, err := env.db.NamedExec(stripe.StmtInsertSubs, s)
+	_, err := env.dbs.Write.NamedExec(stripe.StmtInsertSubs, s)
 	if err != nil {
 		return err
 	}
@@ -24,7 +24,7 @@ func (env Env) UpsertSubs(s stripe.Subs) error {
 
 func (env Env) RetrieveSubs(id string) (stripe.Subs, error) {
 	var s stripe.Subs
-	err := env.db.Get(&s, stripe.StmtRetrieveSubs, id)
+	err := env.dbs.Read.Get(&s, stripe.StmtRetrieveSubs, id)
 	if err != nil {
 		return s, err
 	}
@@ -34,7 +34,7 @@ func (env Env) RetrieveSubs(id string) (stripe.Subs, error) {
 
 func (env Env) SubsExists(id string) (bool, error) {
 	var ok bool
-	err := env.db.Get(&ok, stripe.StmtSubsExists, id)
+	err := env.dbs.Read.Get(&ok, stripe.StmtSubsExists, id)
 	if err != nil {
 		return false, err
 	}
--- internal/repository/striperepo/invoice.go
+++ internal/repository/striperepo/invoice.go
@@ -3,7 +3,7 @@ package striperepo
 import "github.com/FTChinese/subscription-api/pkg/stripe"
 
 func (env Env) UpsertInvoice(i stripe.Invoice) error {
-	_, err := env.db.NamedExec(stripe.StmtUpsertInvoice, i)
+	_, err := env.dbs.Write.NamedExec(stripe.StmtUpsertInvoice, i)
 	if err != nil {
 		return err
 	}
