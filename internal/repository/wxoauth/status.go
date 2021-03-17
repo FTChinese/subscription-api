fa148decad79ce72aa18b9b5e05422d2827bb9a1
weiguo.ni <neefrankie@gmail.com>
2021-03-17 14:15:59 +0800

    refactor: split db reader and write

--- internal/repository/wxoauth/status.go
+++ internal/repository/wxoauth/status.go
@@ -5,7 +5,7 @@ import "github.com/FTChinese/subscription-api/pkg/wxlogin"
 // SaveWxStatus saves Wechat OAuth API error response into data so that we could know what errors on hell Wechat actually produced.
 func (env Env) SaveWxStatus(code int64, message string) error {
 
-	_, err := env.db.Exec(wxlogin.StmtInsertStatus,
+	_, err := env.dbs.Write.Exec(wxlogin.StmtInsertStatus,
 		code,
 		message,
 	)
--- internal/repository/wxoauth/userinfo_test.go
+++ internal/repository/wxoauth/userinfo_test.go
@@ -3,28 +3,24 @@ package wxoauth
 import (
 	"github.com/FTChinese/subscription-api/pkg/wxlogin"
 	"github.com/FTChinese/subscription-api/test"
-	"github.com/jmoiron/sqlx"
 	"testing"
 )
 
 func TestEnv_SaveWxUser(t *testing.T) {
-	type fields struct {
-		db *sqlx.DB
+	env := Env{
+		dbs: test.SplitDB,
 	}
+
 	type args struct {
 		u wxlogin.UserInfoSchema
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
 			name: "Save wx user",
-			fields: fields{
-				db: test.DB,
-			},
 			args: args{
 				u: test.NewPersona().WxUser(),
 			},
@@ -33,9 +29,6 @@ func TestEnv_SaveWxUser(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			env := Env{
-				db: tt.fields.db,
-			}
 			if err := env.SaveWxUser(tt.args.u); (err != nil) != tt.wantErr {
 				t.Errorf("SaveWxUser() error = %v, wantErr %v", err, tt.wantErr)
 			}
