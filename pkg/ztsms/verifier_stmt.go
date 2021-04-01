31c228163229c6e5b5100e0c0cd8884cf8984040
weiguo.ni <neefrankie@gmail.com>
2021-04-01 14:33:58 +0800

    feat: Verifier

---
+++ pkg/ztsms/verifier_stmt.go
@@ -0,0 +1,40 @@
+package ztsms
+
+const StmtSaveVerifier = `
+INSERT INTO user_db.phone_verifier
+SET mobile_phone = :mobile_phone,
+	sms_code = :sms_code,
+	expires_in = :expires_in,
+	created_utc = :created_utc`
+
+const StmtRetrieveVerifier = `
+SELECT mobile_phone,
+	sms_code,
+	expires_in,
+	created_utc,
+	used_utc
+FROM user_db.phone_verifier
+WHERE mobile_phone = ?
+	AND sms_code = ?
+	AND used_utc IS NULL
+LIMIT 1`
+
+const StmtVerifierUsed = `
+UPDATE user_db.phone_verifier
+SET used_utc = :used_utc
+WHERE mobile_phone = :mobile_phone
+	AND sms_code = :sms_code
+	AND used_utc IS NULL
+LIMIT 1`
+
+const StmtUserIDByPhone = `
+SELECT user_id
+FROM user_db.profile
+WHERE mobile_phone = ?
+LIMIT 1`
+
+const StmtSetPhone = `
+UPDATE user_db.profile
+SET mobile_phone = :mobile_phone
+WHERE user_id = :ftc_id
+LIMIT 1`
