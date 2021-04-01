0c7bafd2961a353ad40b3915ce6f5080e373c20b
weiguo.ni <neefrankie@gmail.com>
2021-04-01 14:33:58 +0800

    feat: Verifier

---
+++ pkg/ztsms/verifier.go
@@ -0,0 +1,72 @@
+package ztsms
+
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/lib/validator"
+	"github.com/FTChinese/subscription-api/pkg"
+	"github.com/guregu/null"
+	"time"
+)
+
+type VerifierParams struct {
+	Mobile      string      `json:"mobile"`
+	Code        null.String `json:"code"`
+	DeviceToken null.String `json:"deviceToken"`
+}
+
+func (p VerifierParams) ValidateMobile() *render.ValidationError {
+	ok := validator.IsMobile(p.Mobile)
+	if !ok {
+		return &render.ValidationError{
+			Message: "Invalid mobile number",
+			Field:   "code",
+			Code:    render.CodeInvalid,
+		}
+	}
+
+	return nil
+}
+
+func (p VerifierParams) Validate() *render.ValidationError {
+	ve := p.ValidateMobile()
+	if ve != nil {
+		return ve
+	}
+
+	return validator.New("code").
+		MinLen(6).
+		MaxLen(6).
+		Required().
+		Validate(p.Code.String)
+}
+
+type Verifier struct {
+	Mobile     string      `db:"mobile_phone"`
+	Code       string      `db:"sms_code"`
+	ExpiresIn  int         `db:"expires_in"`
+	CreatedUTC chrono.Time `db:"created_utc"`
+	UsedUTC    chrono.Time `db:"used_utc"`
+}
+
+// NewPhoneVerifier generates a new verification code for a mobile phone.
+func NewVerifier(mobile string) Verifier {
+	return Verifier{
+		Mobile:     mobile,
+		Code:       pkg.SMSCode(),
+		ExpiresIn:  5 * 60,
+		CreatedUTC: chrono.TimeNow(),
+		UsedUTC:    chrono.Time{},
+	}
+}
+
+// WithUsed sets when a code is used so that it shouldn't be used again.
+func (v Verifier) WithUsed() Verifier {
+	v.UsedUTC = chrono.TimeNow()
+	return v
+}
+
+func (v Verifier) Valid() bool {
+	return v.CreatedUTC.Add(time.Duration(v.ExpiresIn) * time.Second).
+		After(time.Now())
+}
