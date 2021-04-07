163acc14b900a52af79d22e7179a0684a2463775
weiguo.ni <neefrankie@gmail.com>
2021-04-07 15:56:14 +0800

    refactor: change reader.FtcAccount to account.BaseAccount

---
+++ internal/repository/txrepo/account_tx_test.go
@@ -0,0 +1,109 @@
+package txrepo
+
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/account"
+	"github.com/FTChinese/subscription-api/test"
+	"github.com/guregu/null"
+	"github.com/jmoiron/sqlx"
+	"testing"
+)
+
+func TestAccountTx_BaseAccountForStripe(t *testing.T) {
+
+	a := account.NewMockFtcAccountBuilder(enum.AccountKindFtc).Build()
+
+	test.NewRepo().MustCreateFtcAccount(a)
+
+	type fields struct {
+		Tx *sqlx.Tx
+	}
+	type args struct {
+		ftcID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    account.BaseAccount
+		wantErr bool
+	}{
+		{
+			name: "Retrieve base account",
+			fields: fields{
+				Tx: test.SplitDB.Read.MustBegin(),
+			},
+			args: args{
+				ftcID: a.FtcID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				Tx: tt.fields.Tx,
+			}
+			got, err := tx.BaseAccountForStripe(tt.args.ftcID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BaseAccountForStripe() error = %v, wantErr %v", err, tt.wantErr)
+				_ = tx.Rollback()
+				return
+			}
+
+			_ = tx.Commit()
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
+
+func TestAccountTx_SavedStripeID(t *testing.T) {
+
+	a := account.NewMockFtcAccountBuilder(enum.AccountKindFtc).Build()
+
+	t.Logf("Creating account %s", a.FtcID)
+
+	test.NewRepo().MustCreateFtcAccount(a)
+
+	a.StripeID = null.StringFrom(faker.GenCustomerID())
+
+	type fields struct {
+		Tx *sqlx.Tx
+	}
+	type args struct {
+		a account.BaseAccount
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Set stripe customer id",
+			fields: fields{
+				Tx: test.SplitDB.Write.MustBegin(),
+			},
+			args: args{
+				a: a,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				Tx: tt.fields.Tx,
+			}
+			if err := tx.SavedStripeID(tt.args.a); (err != nil) != tt.wantErr {
+				t.Errorf("SavedStripeID() error = %v, wantErr %v", err, tt.wantErr)
+				_ = tx.Rollback()
+				return
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
