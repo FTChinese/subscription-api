f59c81a029933acdeeb638f788473555d78fa5b6
weiguo.ni <neefrankie@gmail.com>
2021-04-07 15:46:28 +0800

    refactor: sql

--- pkg/ztsms/verifier_stmt.go
+++ pkg/ztsms/verifier_stmt.go
@@ -1,26 +1,28 @@
 package ztsms
 
 const StmtSaveVerifier = `
-INSERT INTO user_db.phone_verifier
+INSERT INTO user_db.mobile_verifier
 SET mobile_phone = :mobile_phone,
 	sms_code = :sms_code,
 	expires_in = :expires_in,
-	created_utc = :created_utc`
+	created_utc = :created_utc,
+	ftc_id = :ftc_id`
 
 const StmtRetrieveVerifier = `
 SELECT mobile_phone,
 	sms_code,
 	expires_in,
 	created_utc,
-	used_utc
-FROM user_db.phone_verifier
+	used_utc,
+	ftc_id
+FROM user_db.mobile_verifier
 WHERE mobile_phone = ?
 	AND sms_code = ?
 	AND used_utc IS NULL
 LIMIT 1`
 
 const StmtVerifierUsed = `
-UPDATE user_db.phone_verifier
+UPDATE user_db.mobile_verifier
 SET used_utc = :used_utc
 WHERE mobile_phone = :mobile_phone
 	AND sms_code = :sms_code
@@ -35,6 +37,7 @@ LIMIT 1`
 
 const StmtSetPhone = `
 UPDATE user_db.profile
-SET mobile_phone = :mobile_phone
+SET mobile_phone = :mobile_phone,
+	updated_utc = UTC_TIMESTAMP()
 WHERE user_id = :ftc_id
 LIMIT 1`
