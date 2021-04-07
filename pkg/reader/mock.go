b8da77ad3921617b0097aef8b0fe638c4b68bab2
weiguo.ni <neefrankie@gmail.com>
2021-04-07 16:02:28 +0800

    feat: removed MockNewFtcAccount

--- pkg/reader/mock.go
+++ pkg/reader/mock.go
@@ -9,44 +9,10 @@ import (
 	"github.com/FTChinese/subscription-api/pkg"
 	"github.com/FTChinese/subscription-api/pkg/addon"
 	"github.com/FTChinese/subscription-api/pkg/price"
-	"github.com/brianvoe/gofakeit/v5"
 	"github.com/google/uuid"
 	"github.com/guregu/null"
 	"time"
 )
-
-func MockNewFtcAccount(kind enum.AccountKind) FtcAccount {
-	switch kind {
-	case enum.AccountKindFtc:
-		return FtcAccount{
-			FtcID:    uuid.New().String(),
-			UnionID:  null.String{},
-			StripeID: null.StringFrom(faker.GenCustomerID()),
-			Email:    gofakeit.Email(),
-			UserName: null.StringFrom(gofakeit.Username()),
-		}
-
-	case enum.AccountKindWx:
-		return FtcAccount{
-			FtcID:    "",
-			UnionID:  null.StringFrom(faker.GenWxID()),
-			StripeID: null.String{},
-			Email:    "",
-			UserName: null.String{},
-		}
-
-	case enum.AccountKindLinked:
-		return FtcAccount{
-			FtcID:    uuid.New().String(),
-			UnionID:  null.StringFrom(faker.GenWxID()),
-			StripeID: null.StringFrom(faker.GenCustomerID()),
-			Email:    gofakeit.Email(),
-			UserName: null.StringFrom(gofakeit.Username()),
-		}
-	}
-
-	return FtcAccount{}
-}
 
 type MockMemberBuilder struct {
 	ids          pkg.UserIDs
