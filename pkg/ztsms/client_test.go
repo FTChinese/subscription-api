bbe54081f11fa18199e7ad9490520f822f60f010
weiguo.ni <neefrankie@gmail.com>
2021-04-07 17:19:44 +0800

    feat: test sms

--- pkg/ztsms/client_test.go
+++ pkg/ztsms/client_test.go
@@ -1,14 +1,24 @@
 package ztsms
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"github.com/FTChinese/subscription-api/pkg/config"
-	"reflect"
+	"github.com/google/uuid"
+	"github.com/guregu/null"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+	"strconv"
 	"testing"
+	"time"
 )
 
 func TestClient_SendVerifier(t *testing.T) {
+	config.MustSetupViper()
+
 	type fields struct {
 		credentials config.Credentials
+		logger      *zap.Logger
 	}
 	type args struct {
 		v Verifier
@@ -20,21 +30,48 @@ func TestClient_SendVerifier(t *testing.T) {
 		want    MessageResponse
 		wantErr bool
 	}{
-		{},
+		{
+			name: "Send verifier",
+			fields: fields{
+				credentials: config.MustSMSCredentials(),
+				logger:      zaptest.NewLogger(t),
+			},
+			args: args{
+				v: NewVerifier("15011481214", null.StringFrom(uuid.New().String())),
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := Client{
 				credentials: tt.fields.credentials,
+				logger:      tt.fields.logger,
 			}
 			got, err := c.SendVerifier(tt.args.v)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("SendVerifier() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SendVerifier() got = %v, want %v", got, tt.want)
-			}
+
+			t.Logf("%v", got)
 		})
 	}
+}
+
+func TestClient_hashPassword(t *testing.T) {
+
+	ts := strconv.FormatInt(time.Now().Unix(), 10)
+
+	config.MustSetupViper()
+
+	c := config.MustSMSCredentials()
+
+	hash := md5.Sum([]byte(c.Password))
+	s := hex.EncodeToString(hash[:])
+	t.Logf("%s", s)
+
+	hash = md5.Sum([]byte(s + ts))
+
+	t.Logf("%s", hex.EncodeToString(hash[:]))
 }
