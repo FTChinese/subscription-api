d8b30b009b87ad3714f9c3e2e94d640ba613d25e
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:45:26 +0800

    feat: embed readers.Env

--- internal/repository/striperepo/env.go
+++ internal/repository/striperepo/env.go
@@ -1,46 +1,35 @@
 package striperepo
 
 import (
+	"github.com/FTChinese/subscription-api/internal/repository/readers"
 	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
 	"github.com/FTChinese/subscription-api/pkg/db"
-	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 )
 
 // Env wraps database connection
 type Env struct {
-	db     *sqlx.DB
-	dbs    db.ReadWriteSplit
+	readers.Env
 	client Client
 	logger *zap.Logger
 }
 
-// NewSubEnv creates a new instance of SubEnv.
+// New creates a new instance of Env.
 // `sandbox` is used to determine which table to write subscription data.
-func NewEnv(dbs db.ReadWriteSplit, client Client, logger *zap.Logger) Env {
+func New(dbs db.ReadWriteSplit, client Client, logger *zap.Logger) Env {
 	return Env{
-		dbs:    dbs,
+		Env:    readers.New(dbs, logger),
 		client: client,
 		logger: logger,
 	}
 }
 
-func (env Env) beginAccountTx() (txrepo.AccountTx, error) {
-	tx, err := env.dbs.Write.Beginx()
+func (env Env) beginStripeTx() (txrepo.StripeTx, error) {
+	tx, err := env.DBs.Write.Beginx()
 
 	if err != nil {
-		return txrepo.AccountTx{}, err
+		return txrepo.StripeTx{}, err
 	}
 
-	return txrepo.NewAccountTx(tx), nil
-}
-
-func (env Env) beginSubsTx() (txrepo.MemberTx, error) {
-	tx, err := env.dbs.Write.Beginx()
-
-	if err != nil {
-		return txrepo.MemberTx{}, err
-	}
-
-	return txrepo.NewMemberTx(tx), nil
+	return txrepo.NewStripeTx(tx), nil
 }
