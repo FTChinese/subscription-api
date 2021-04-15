2ac97f397a500ed4ff8cc2d8ed601e384495d71b
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:26:01 +0800

    refactor: relocate VerifierParams

--- pkg/ztsms/verifier.go
+++ pkg/ztsms/verifier.go
@@ -2,44 +2,10 @@ package ztsms
 
 import (
 	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/render"
-	"github.com/FTChinese/subscription-api/lib/validator"
 	"github.com/FTChinese/subscription-api/pkg"
 	"github.com/guregu/null"
 	"time"
 )
-
-type VerifierParams struct {
-	Mobile      string `json:"mobile"`
-	Code        string `json:"code"`
-	DeviceToken string `json:"deviceToken"`
-}
-
-func (p VerifierParams) ValidateMobile() *render.ValidationError {
-	ok := validator.IsMobile(p.Mobile)
-	if !ok {
-		return &render.ValidationError{
-			Message: "Invalid mobile number",
-			Field:   "code",
-			Code:    render.CodeInvalid,
-		}
-	}
-
-	return nil
-}
-
-func (p VerifierParams) Validate() *render.ValidationError {
-	ve := p.ValidateMobile()
-	if ve != nil {
-		return ve
-	}
-
-	return validator.New("code").
-		MinLen(6).
-		MaxLen(6).
-		Required().
-		Validate(p.Code)
-}
 
 type Verifier struct {
 	Mobile     string      `db:"mobile_phone"`
@@ -50,7 +16,7 @@ type Verifier struct {
 	FtcID      null.String `db:"ftc_id"`
 }
 
-// NewPhoneVerifier generates a new verification code for a mobile phone.
+// NewVerifier generates a new verification code for a mobile phone.
 func NewVerifier(mobile string, ftcID null.String) Verifier {
 	return Verifier{
 		Mobile:     mobile,
