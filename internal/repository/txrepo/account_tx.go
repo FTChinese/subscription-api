d7c9289b3f7df96bbb7899888374272b845bcbd9
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:36:10 +0800

    feat: AccountTx

--- internal/repository/txrepo/account_tx.go
+++ internal/repository/txrepo/account_tx.go
@@ -6,34 +6,49 @@ import (
 )
 
 type AccountTx struct {
-	*sqlx.Tx
+	SharedTx
 }
 
 func NewAccountTx(tx *sqlx.Tx) AccountTx {
 	return AccountTx{
-		Tx: tx,
+		SharedTx: NewSharedTx(tx),
 	}
 }
 
-// BaseAccountForStripe loads an ftc account by uuid
-func (tx AccountTx) BaseAccountForStripe(ftcID string) (account.BaseAccount, error) {
-	var a account.BaseAccount
-	err := tx.Get(
-		&a,
-		account.StmtLockBaseAccount,
-		ftcID)
-	if err != nil {
-		return account.BaseAccount{}, err
-	}
+// CreateAccount create a ftc account.
+func (tx AccountTx) CreateAccount(a account.BaseAccount) error {
 
-	return a, nil
-}
-
-func (tx AccountTx) SavedStripeID(a account.BaseAccount) error {
 	_, err := tx.NamedExec(
-		account.StmtSetStripeID,
-		a,
-	)
+		account.StmtCreateFtc,
+		a)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CreateProfile creates profile when creating ftc account.
+func (tx AccountTx) CreateProfile(a account.BaseAccount) error {
+	_, err := tx.NamedExec(
+		account.StmtCreateProfile,
+		a)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AddUnionIDToFtc links two existing account by setting wechat union id to userinfo table.
+func (tx AccountTx) AddUnionIDToFtc(a account.BaseAccount) error {
+
+	_, err := tx.NamedExec(
+		account.StmtLinkAccount,
+		a)
+
 	if err != nil {
 		return err
 	}
