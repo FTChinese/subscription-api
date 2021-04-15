9a3e205ccf91a1f8a1d4ead3574c68fae4aac7fd
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:46:50 +0800

    feat: embed readers.Env

--- internal/repository/iaprepo/env.go
+++ internal/repository/iaprepo/env.go
@@ -2,6 +2,7 @@ package iaprepo
 
 import (
 	"context"
+	"github.com/FTChinese/subscription-api/internal/repository/readers"
 	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
 	"github.com/FTChinese/subscription-api/pkg/db"
 	"github.com/go-redis/redis/v8"
@@ -11,31 +12,31 @@ import (
 var ctx = context.Background()
 
 type Env struct {
-	dbs    db.ReadWriteSplit
+	readers.Env
 	rdb    *redis.Client
 	logger *zap.Logger
 }
 
 func NewEnv(dbs db.ReadWriteSplit, rdb *redis.Client, logger *zap.Logger) Env {
 	return Env{
-		dbs:    dbs,
+		Env:    readers.New(dbs, logger),
 		rdb:    rdb,
 		logger: logger,
 	}
 }
 
-// BeginTx starts a transaction.
+// beginIAPTx starts a transaction.
 // NOTE: here the sandbox is different from the environment
 // field send by apple. It only determines whether the
 // sandbox db should be used and is determined by
 // the CLI argument `-sandbox`.
 // All messages from apple is save in production DBs.
-func (env Env) BeginTx() (txrepo.MemberTx, error) {
-	tx, err := env.dbs.Delete.Beginx()
+func (env Env) beginIAPTx() (txrepo.IAPTx, error) {
+	tx, err := env.DBs.Delete.Beginx()
 
 	if err != nil {
-		return txrepo.MemberTx{}, err
+		return txrepo.IAPTx{}, err
 	}
 
-	return txrepo.NewMemberTx(tx), nil
+	return txrepo.NewIAPTx(tx), nil
 }
