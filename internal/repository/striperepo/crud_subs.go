4b033ae6bef36f62a51ef5fae9790c0af414fba5
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:46:33 +0800

    refactor: doc

--- internal/repository/striperepo/crud_subs.go
+++ internal/repository/striperepo/crud_subs.go
@@ -4,7 +4,7 @@ import "github.com/FTChinese/subscription-api/pkg/stripe"
 
 // SaveSubsError saves any error in stripe response.
 func (env Env) SaveSubsError(e stripe.APIError) error {
-	_, err := env.dbs.Write.NamedExec(stripe.StmtSaveAPIError, e)
+	_, err := env.DBs.Write.NamedExec(stripe.StmtSaveAPIError, e)
 
 	if err != nil {
 		return err
@@ -13,8 +13,9 @@ func (env Env) SaveSubsError(e stripe.APIError) error {
 	return nil
 }
 
+// UpsertSubs saves a reduced version os stripe subscription.
 func (env Env) UpsertSubs(s stripe.Subs) error {
-	_, err := env.dbs.Write.NamedExec(stripe.StmtInsertSubs, s)
+	_, err := env.DBs.Write.NamedExec(stripe.StmtInsertSubs, s)
 	if err != nil {
 		return err
 	}
@@ -22,9 +23,10 @@ func (env Env) UpsertSubs(s stripe.Subs) error {
 	return nil
 }
 
+// RetrieveSubs retrieves the stripe subscription stored in our db.
 func (env Env) RetrieveSubs(id string) (stripe.Subs, error) {
 	var s stripe.Subs
-	err := env.dbs.Read.Get(&s, stripe.StmtRetrieveSubs, id)
+	err := env.DBs.Read.Get(&s, stripe.StmtRetrieveSubs, id)
 	if err != nil {
 		return s, err
 	}
@@ -32,9 +34,10 @@ func (env Env) RetrieveSubs(id string) (stripe.Subs, error) {
 	return s, nil
 }
 
+// SubsExists checks whether we stored a stripe subscription.
 func (env Env) SubsExists(id string) (bool, error) {
 	var ok bool
-	err := env.dbs.Read.Get(&ok, stripe.StmtSubsExists, id)
+	err := env.DBs.Read.Get(&ok, stripe.StmtSubsExists, id)
 	if err != nil {
 		return false, err
 	}
