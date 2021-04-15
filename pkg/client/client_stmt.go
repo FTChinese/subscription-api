c1705c7db10e80a31e47e28e6edbdcb1ef4904f4
weiguo.ni <neefrankie@gmail.com>
2021-04-15 17:30:19 +0800

    feat: sql to save client app meta data

--- pkg/client/client_stmt.go
+++ pkg/client/client_stmt.go
@@ -1,5 +1,17 @@
 package client
 
+const StmtInsertClient = `
+INSERT INTO user_db.client_footprint
+SET ftc_id 			= :ftc_id,
+    platform 		= :platform,
+    client_version 	= :client_version,
+    user_ip 		= INET6_ATON(:user_ip),
+    user_agent 		= :user_agent,
+    created_utc 	= UTC_TIMESTAMP(),
+    source 			= :source,
+	auth_method 	= :auth_method,
+	device_token 	= :device_token`
+
 const StmtInsertOrderClient = `
 INSERT INTO premium.client
 SET order_id = :order_id,
