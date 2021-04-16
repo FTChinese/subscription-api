ba145a1cd83d28803b9da4ce6ee8d7a56afdb9e8
weiguo.ni <neefrankie@gmail.com>
2021-04-16 14:23:43 +0800

    test

--- internal/repository/accounts/base_account_test.go
+++ internal/repository/accounts/base_account_test.go
@@ -2,6 +2,8 @@ package accounts
 
 import (
 	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/internal/repository/readers"
 	"github.com/FTChinese/subscription-api/pkg/account"
 	"github.com/FTChinese/subscription-api/test"
 	"go.uber.org/zap/zaptest"
@@ -36,6 +38,100 @@ func TestEnv_CreateAccount(t *testing.T) {
 			if err := env.CreateAccount(tt.args.a); (err != nil) != tt.wantErr {
 				t.Errorf("CreateAccount() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestEnv_BaseAccountByEmail(t *testing.T) {
+	a := account.NewMockFtcAccountBuilder(enum.AccountKindFtc).Build()
+
+	test.NewRepo().MustCreateFtcAccount(a)
+
+	type fields struct {
+		Env readers.Env
+	}
+	type args struct {
+		email string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    account.BaseAccount
+		wantErr bool
+	}{
+		{
+			name: "Retrieve base account by email",
+			fields: fields{
+				Env: readers.New(test.SplitDB, zaptest.NewLogger(t)),
+			},
+			args: args{
+				email: a.Email,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				Env: tt.fields.Env,
+			}
+			got, err := env.BaseAccountByEmail(tt.args.email)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BaseAccountByEmail() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("BaseAccountByEmail() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
+
+func TestEnv_BaseAccountByMobile(t *testing.T) {
+	a := account.NewMockFtcAccountBuilder(enum.AccountKindFtc).Build()
+
+	test.NewRepo().MustCreateFtcAccount(a)
+
+	type fields struct {
+		Env readers.Env
+	}
+	type args struct {
+		mobile string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    account.BaseAccount
+		wantErr bool
+	}{
+		{
+			name:   "Retrieve account by mobile",
+			fields: fields{Env: readers.New(test.SplitDB, zaptest.NewLogger(t))},
+			args: args{
+				mobile: a.Mobile.String,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				Env: tt.fields.Env,
+			}
+			got, err := env.BaseAccountByMobile(tt.args.mobile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BaseAccountByMobile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("BaseAccountByMobile() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
 		})
 	}
 }
