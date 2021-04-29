409f7e8306c7bc314c659ecaff7deb399980ec23
weiguo.ni <neefrankie@gmail.com>
2021-04-29 15:09:59 +0800

    fix: .Link field

--- pkg/letter/render_test.go
+++ pkg/letter/render_test.go
@@ -11,6 +11,242 @@ import (
 	"time"
 )
 
+func TestCtxVerification_Render(t *testing.T) {
+	type fields struct {
+		UserName string
+		Email    string
+		Link     string
+		IsSignUp bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Email verification letter",
+			fields: fields{
+				UserName: gofakeit.Username(),
+				Email:    gofakeit.Email(),
+				Link:     gofakeit.URL(),
+				IsSignUp: true,
+			},
+		},
+		{
+			name: "Email verification letter",
+			fields: fields{
+				UserName: gofakeit.Username(),
+				Email:    gofakeit.Email(),
+				Link:     gofakeit.URL(),
+				IsSignUp: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxVerification{
+				UserName: tt.fields.UserName,
+				Email:    tt.fields.Email,
+				Link:     tt.fields.Link,
+				IsSignUp: tt.fields.IsSignUp,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if got != tt.want {
+			//	t.Errorf("Render() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", got)
+		})
+	}
+}
+
+func TestCtxPwReset_Render(t *testing.T) {
+	type fields struct {
+		UserName string
+		URL      string
+		AppCode  string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Password reset in app",
+			fields: fields{
+				UserName: gofakeit.Username(),
+				URL:      "",
+				AppCode:  "12345678",
+			},
+		},
+		{
+			name: "Password reset in browser",
+			fields: fields{
+				UserName: gofakeit.Username(),
+				URL:      gofakeit.URL(),
+				AppCode:  "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxPwReset{
+				UserName: tt.fields.UserName,
+				URL:      tt.fields.URL,
+				AppCode:  tt.fields.AppCode,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if got != tt.want {
+			//	t.Errorf("Render() got = %v, want %v", got, tt.want)
+			//}
+			t.Logf("%v", got)
+		})
+	}
+}
+
+func TestCtxWxSignUp_Render(t *testing.T) {
+	type fields struct {
+		CtxLinkBase CtxLinkBase
+		URL         string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Wechat Signup",
+			fields: fields{
+				CtxLinkBase: CtxLinkBase{
+					UserName:   gofakeit.Username(),
+					Email:      gofakeit.Email(),
+					WxNickname: gofakeit.Username(),
+				},
+				URL: gofakeit.URL(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxWxSignUp{
+				CtxLinkBase: tt.fields.CtxLinkBase,
+				URL:         tt.fields.URL,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if got != tt.want {
+			//	t.Errorf("Render() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", got)
+		})
+	}
+}
+
+func TestCtxAccountLink_Render(t *testing.T) {
+	type fields struct {
+		CtxLinkBase CtxLinkBase
+		Membership  reader.Membership
+		FtcMember   reader.Membership
+		WxMember    reader.Membership
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Link email to wechat",
+			fields: fields{
+				CtxLinkBase: CtxLinkBase{
+					UserName:   gofakeit.Username(),
+					Email:      gofakeit.Email(),
+					WxNickname: gofakeit.Username(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxAccountLink{
+				CtxLinkBase: tt.fields.CtxLinkBase,
+				Membership:  tt.fields.Membership,
+				FtcMember:   tt.fields.FtcMember,
+				WxMember:    tt.fields.WxMember,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if got != tt.want {
+			//	t.Errorf("Render() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", got)
+		})
+	}
+}
+
+func TestCtxAccountUnlink_Render(t *testing.T) {
+	type fields struct {
+		CtxLinkBase CtxLinkBase
+		Membership  reader.Membership
+		Anchor      string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Unlink account",
+			fields: fields{
+				CtxLinkBase: CtxLinkBase{
+					UserName:   gofakeit.Username(),
+					Email:      gofakeit.Email(),
+					WxNickname: gofakeit.Username(),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := CtxAccountUnlink{
+				CtxLinkBase: tt.fields.CtxLinkBase,
+				Membership:  tt.fields.Membership,
+				Anchor:      tt.fields.Anchor,
+			}
+			got, err := ctx.Render()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if got != tt.want {
+			//	t.Errorf("Render() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", got)
+		})
+	}
+}
+
 func TestCtxSubs_Render(t *testing.T) {
 	faker.SeedGoFake()
 
