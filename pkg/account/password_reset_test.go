5b8648dc32197eb5354aef6f433c11822490e206
weiguo.ni <neefrankie@gmail.com>
2021-05-10 17:43:46 +0800

    refactor: extends password reset letter validation length to 30 minutes for mobile platform

--- pkg/account/password_reset_test.go
+++ pkg/account/password_reset_test.go
@@ -3,6 +3,7 @@ package account
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/subscription-api/pkg"
+	"github.com/brianvoe/gofakeit/v5"
 	"github.com/guregu/null"
 	"reflect"
 	"testing"
@@ -120,6 +121,63 @@ func TestPwResetSession_IsExpired(t *testing.T) {
 			}
 			if got := s.IsExpired(); got != tt.want {
 				t.Errorf("IsExpired() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPwResetSession_DurHours(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		fields PwResetSession
+		want   int64
+	}{
+		{
+			name: "Web session",
+			fields: MustNewPwResetSession(pkg.ForgotPasswordParams{
+				Email:     gofakeit.Email(),
+				UseCode:   false,
+				SourceURL: null.StringFrom(gofakeit.URL()),
+			}),
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields
+
+			t.Logf("%v", tt.fields)
+
+			if got := s.DurHours(); got != tt.want {
+				t.Errorf("DurHours() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPwResetSession_DurMinutes(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		fields PwResetSession
+		want   int64
+	}{
+		{
+			name: "App session",
+			fields: MustNewPwResetSession(pkg.ForgotPasswordParams{
+				Email:     gofakeit.Email(),
+				UseCode:   true,
+				SourceURL: null.String{},
+			}),
+			want: 30,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields
+			if got := s.DurMinutes(); got != tt.want {
+				t.Errorf("DurMinutes() = %v, want %v", got, tt.want)
 			}
 		})
 	}
