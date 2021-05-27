e452f13ce71089d8e7b0a519a86db9019e154636
weiguo.ni <neefrankie@gmail.com>
2021-05-27 08:44:11 +0800

    refactor: doc

--- internal/repository/readers/env.go
+++ internal/repository/readers/env.go
@@ -6,6 +6,10 @@ import (
 )
 
 // Env contains shared functionalities of a reader.
+// It contains methods to retrieve user's
+// basic account data using various id fields.
+// It also contains methods to retrieve membership
+// using various ids.
 type Env struct {
 	DBs    db.ReadWriteSplit
 	Logger *zap.Logger
