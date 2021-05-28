52d75ee55732a7bf22b1f129e5336bb144f7c3a6
weiguo.ni <neefrankie@gmail.com>
2021-05-28 20:30:39 +0800

    feat: add Add method to YearMonthDay

--- lib/dt/year_month_day.go
+++ lib/dt/year_month_day.go
@@ -7,12 +7,14 @@ const (
 	daysOfMonth = 31
 )
 
+// YearMonthDay is the unit of a enum.Cycle.
 type YearMonthDay struct {
 	Years  int64 `json:"years" db:"years"`
 	Months int64 `json:"months" db:"months"`
 	Days   int64 `json:"days" db:"days"`
 }
 
+// NewYearMonthDay creates a new instance for a enum.Cycle.
 func NewYearMonthDay(cycle enum.Cycle) YearMonthDay {
 	switch cycle {
 	case enum.CycleYear:
@@ -34,6 +36,7 @@ func NewYearMonthDay(cycle enum.Cycle) YearMonthDay {
 	}
 }
 
+// NewYearMonthDayN creates a new instance for n enum.Cycle.
 func NewYearMonthDayN(cycle enum.Cycle, n int) YearMonthDay {
 	switch cycle {
 	case enum.CycleYear:
@@ -55,6 +58,16 @@ func NewYearMonthDayN(cycle enum.Cycle, n int) YearMonthDay {
 	}
 }
 
+// TotalDays calculates the number of days of by adding the days of the year, month and days.
 func (y YearMonthDay) TotalDays() int64 {
 	return y.Years*daysOfYear + y.Months*daysOfMonth + y.Days
+}
+
+// Add adds two instances.
+func (y YearMonthDay) Add(other YearMonthDay) YearMonthDay {
+	y.Years = y.Years + other.Years
+	y.Months = y.Months + other.Months
+	y.Days = y.Days + other.Days
+
+	return y
 }
