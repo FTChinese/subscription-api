f30ffa324faea299ccb130fbc89cb0d42b6c72d1
weiguo.ni <neefrankie@gmail.com>
2021-06-01 16:40:26 +0800

    refactor: move NewOrderInvoice and unexport

--- pkg/subs/invoices.go
+++ pkg/subs/invoices.go
@@ -1,46 +1,10 @@
 package subs
 
 import (
-	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/pkg"
-	"github.com/FTChinese/subscription-api/pkg/addon"
 	"github.com/FTChinese/subscription-api/pkg/invoice"
 	"github.com/FTChinese/subscription-api/pkg/reader"
-	"github.com/guregu/null"
 )
-
-// NewOrderInvoice creates a new invoice from a unconfirmed order.
-func NewOrderInvoice(timeParams PurchasedTimeParams, o Order) (invoice.Invoice, error) {
-	timeRange, err := timeParams.Build()
-	if err != nil {
-		return invoice.Invoice{}, err
-	}
-
-	var addOnSource addon.Source
-	confirmedAt := timeParams.ConfirmedAt
-	if o.Kind == enum.OrderKindAddOn {
-		addOnSource = addon.SourceUserPurchase
-		confirmedAt = chrono.Time{}
-	}
-
-	return invoice.Invoice{
-		ID:             pkg.InvoiceID(),
-		CompoundID:     o.CompoundID,
-		Edition:        o.Edition,
-		YearMonthDay:   timeParams.Date,
-		AddOnSource:    addOnSource,
-		OrderID:        null.StringFrom(o.ID),
-		OrderKind:      timeParams.OrderKind, // Note: use the calibrated order kind.
-		PaidAmount:     o.Amount,
-		PaymentMethod:  o.PaymentMethod,
-		PriceID:        null.StringFrom(o.PlanID),
-		CreatedUTC:     chrono.TimeNow(),
-		ConsumedUTC:    confirmedAt,
-		DateTimePeriod: timeRange.ToDateTimePeriod(),
-		CarriedOverUtc: chrono.Time{},
-	}, nil
-}
 
 type Invoices struct {
 	Purchased   invoice.Invoice
