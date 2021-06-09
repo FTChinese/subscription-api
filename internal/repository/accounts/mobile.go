7b31d4e659180d2512f9e13c4b9e117f26147d79
weiguo.ni <neefrankie@gmail.com>
2021-06-09 16:33:08 +0800

    refactor: set mobile phone in a transaction

--- internal/repository/accounts/mobile.go
+++ internal/repository/accounts/mobile.go
@@ -1,7 +1,8 @@
 package accounts
 
 import (
-	"github.com/FTChinese/subscription-api/pkg/account"
+	"database/sql"
+	"errors"
 	"github.com/FTChinese/subscription-api/pkg/ztsms"
 )
 
@@ -34,9 +35,55 @@ func (env Env) SMSVerifierUsed(v ztsms.Verifier) error {
 	return nil
 }
 
-func (env Env) SetPhone(a account.BaseAccount) error {
-	_, err := env.DBs.Write.NamedExec(ztsms.StmtSetPhone, a)
+// SetMobile inserts a new row in profile table or set
+// mobile phone filed if empty.
+// Possibilities when you are trying to set the phone number:
+// * The row with this user id does not exists at all.
+//   In such case you should insert a row directly;
+// * The row with this user id exists:
+//   * If this row does not have mobile phone set, set it;
+//   * If this row does have mobile phone set:
+//     * If existing mobile matches the required one, stop;
+//     * If existing mobile does not match the required one, it's conflict error.
+func (env Env) SetMobile(params ztsms.MobileUpdater) error {
+	defer env.Logger.Sync()
+	sugar := env.Logger.Sugar()
+
+	tx, err := env.beginAccountTx()
 	if err != nil {
+		sugar.Error(err)
+		return err
+	}
+
+	current, err := tx.RetrieveMobile(params.FtcID)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			sugar.Error(err)
+			_ = tx.Rollback()
+			return err
+		}
+		// Fallthrough for error no rows.
+	}
+
+	// If mobile already exists.
+	if current.Mobile.Valid {
+		_ = tx.Rollback()
+		if current.Mobile.String == params.Mobile.String {
+			return errors.New("mobile already set")
+		} else {
+			return errors.New("already taken by another mobile")
+		}
+	}
+
+	err = tx.SetMobile(params)
+	if err != nil {
+		sugar.Error(err)
+		_ = tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		sugar.Error(err)
 		return err
 	}
 
