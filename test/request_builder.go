3115bd2646db814d8779ce72eaaffcf70bf814a7
weiguo.ni <neefrankie@gmail.com>
2021-06-09 16:30:54 +0800

    feat: request for mobile link

--- test/request_builder.go
+++ test/request_builder.go
@@ -6,14 +6,20 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/go-rest/rand"
 	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg"
+	"github.com/FTChinese/subscription-api/pkg/account"
+	"github.com/FTChinese/subscription-api/pkg/ztsms"
+	"github.com/guregu/null"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 )
 
-const baseUrl = "http://localhost:8200"
+const baseUrl = "http://localhost:8202"
 
 func BuildReqBody(v interface{}) (io.Reader, error) {
 	b, err := json.Marshal(v)
@@ -24,6 +30,16 @@ func BuildReqBody(v interface{}) (io.Reader, error) {
 	return bytes.NewReader(b), nil
 }
 
+func GetRespBody(body io.Reader) []byte {
+	b, err := ioutil.ReadAll(body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
+
 func MustBuildReqBody(v interface{}) io.Reader {
 	r, err := BuildReqBody(v)
 	if err != nil {
@@ -31,6 +47,65 @@ func MustBuildReqBody(v interface{}) io.Reader {
 	}
 
 	return r
+}
+
+type MobileLinkAccountKind int
+
+const (
+	MobileLinkNoProfile MobileLinkAccountKind = iota
+	MobileLinkHasProfileNoPhone
+	MobileLinkHasProfilePhoneSet
+	MobileLinkHasProfilePhoneTaken
+)
+
+func (r Repo) GenerateMobileLinkParams(kind MobileLinkAccountKind) (string, pkg.MobileLinkParams) {
+	defer r.logger.Sync()
+	sugar := r.logger.Sugar()
+
+	builder := account.NewMockFtcAccountBuilder(enum.AccountKindFtc)
+	if kind == MobileLinkHasProfileNoPhone {
+		builder = builder.WithMobile("")
+	}
+	baseAccount := builder.Build()
+
+	sugar.Infof("Account to link mobile: %+v", baseAccount)
+
+	r.MustCreateUserInfo(baseAccount)
+	if kind != MobileLinkNoProfile {
+		r.MustCreateProfile(baseAccount)
+	}
+
+	var phone string
+	if kind == MobileLinkHasProfilePhoneSet {
+		phone = baseAccount.Mobile.String
+	} else {
+		phone = faker.GenPhone()
+	}
+
+	v := ztsms.NewVerifier(phone, null.String{})
+	sugar.Infof("Mobile verifier: %+v", v)
+	r.MustSaveMobileVerifier(v)
+
+	param := pkg.MobileLinkParams{
+		EmailLoginParams: pkg.EmailLoginParams{
+			Email:       baseAccount.Email,
+			Password:    baseAccount.Password,
+			DeviceToken: null.StringFrom(rand.String(36)),
+		},
+		Mobile: v.Mobile,
+	}
+	sugar.Infof("%s", faker.MustMarshalIndent(param))
+
+	return baseAccount.FtcID, param
+}
+
+func (r Repo) BuildMobileLinkReq(kind MobileLinkAccountKind) *http.Request {
+	_, params := r.GenerateMobileLinkParams(kind)
+
+	return httptest.NewRequest(
+		"POST",
+		baseUrl+"/auth/mobile/link",
+		MustBuildReqBody(params))
 }
 
 func AppleLinkReq() *http.Request {
