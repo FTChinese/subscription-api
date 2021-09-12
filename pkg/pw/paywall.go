a62b8a1363f2a86e68869e7c8496781006e08e42
weiguo.ni <neefrankie@gmail.com>
2021-09-12 17:53:02 +0800

    refactor: find proper price of current paywall

--- pkg/pw/paywall.go
+++ pkg/pw/paywall.go
@@ -1,7 +1,9 @@
 package pw
 
 import (
+	"errors"
 	"github.com/FTChinese/subscription-api/lib/dt"
+	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/guregu/null"
 )
 
@@ -33,12 +35,62 @@ type Paywall struct {
 	Banner   Banner    `json:"banner"`
 	Promo    Promo     `json:"promo"`
 	Products []Product `json:"products"`
+	LiveMode bool      `json:"liveMode"`
 }
 
-func NewPaywall(b BannerSchema, p []Product) Paywall {
+func NewPaywall(b BannerSchema, p []Product, live bool) Paywall {
 	return Paywall{
 		Banner:   b.Banner,
 		Promo:    b.Promo,
 		Products: p,
+		LiveMode: live,
 	}
+}
+
+func (w Paywall) CollectPrices() []price.FtcPrice {
+	var list = make([]price.FtcPrice, 0)
+	for _, product := range w.Products {
+		for _, p := range product.Prices {
+			list = append(list, p)
+		}
+	}
+	return list
+}
+
+func (w Paywall) FindPrice(p price.Price) (price.FtcPrice, error) {
+	for _, prod := range w.Products {
+		for _, v := range prod.Prices {
+			if v.ID == p.ID {
+				return v, nil
+			}
+		}
+	}
+
+	return price.FtcPrice{}, errors.New("the requested price is not found")
+}
+
+func (w Paywall) findFtcPrice(id string) (price.FtcPrice, error) {
+	for _, prod := range w.Products {
+		for _, v := range prod.Prices {
+			if v.ID == id {
+				return v, nil
+			}
+		}
+	}
+
+	return price.FtcPrice{}, errors.New("the requested price is not found")
+}
+
+func (w Paywall) FindCheckoutItem(priceID, offerID string) (price.CheckoutItem, error) {
+	ftcPrice, err := w.findFtcPrice(priceID)
+	if err != nil {
+		return price.CheckoutItem{}, err
+	}
+
+	offer, _ := ftcPrice.Offers.FindValid(offerID)
+
+	return price.CheckoutItem{
+		Price: ftcPrice.Price,
+		Offer: offer,
+	}, nil
 }
