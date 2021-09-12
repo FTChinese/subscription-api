8dbdcfaae4fb10c3d0acb3a2e68457ce9ddc3103
weiguo.ni <neefrankie@gmail.com>
2021-09-12 17:44:48 +0800

    feat: embed viper config file

--- pkg/config/setup.go
+++ pkg/config/setup.go
@@ -1,7 +1,15 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"bytes"
+	"github.com/spf13/viper"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
+// SetupViper config viper
+// Deprecated.
 func SetupViper() error {
 	viper.SetConfigName("api")
 	viper.AddConfigPath("$HOME/config")
@@ -14,8 +22,47 @@ func SetupViper() error {
 	return nil
 }
 
+// MustSetupViper config viper or panic
+// Deprecated
 func MustSetupViper() {
 	if err := SetupViper(); err != nil {
 		panic(err)
 	}
+}
+
+func SetupViperV2(b []byte) error {
+	viper.SetConfigType("toml")
+	//viper.SetConfigName("api")
+	//viper.AddConfigPath("$HOME/config")
+
+	err := viper.ReadConfig(bytes.NewBuffer(b))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func MustSetupViperV2(b []byte) {
+	if err := SetupViperV2(b); err != nil {
+		panic(err)
+	}
+}
+
+func ReadConfigFile() ([]byte, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(filepath.Join(home, "config", "api.toml"))
+}
+
+func MustReadConfigFile() []byte {
+	b, err := ReadConfigFile()
+	if err != nil {
+		panic(err)
+	}
+
+	return b
 }
