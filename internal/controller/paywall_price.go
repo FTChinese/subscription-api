353592604c826c9a6fd2fb2f25143b3b69e44710
weiguo.ni <neefrankie@gmail.com>
2021-09-12 17:59:29 +0800

    feat: create/update price

---
+++ internal/controller/paywall_price.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	gorest "github.com/FTChinese/go-rest"
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/price"
+	"net/http"
+)
+
+// ListPrices retrieves all prices under a product.
+func (router PaywallRouter) ListPrices(w http.ResponseWriter, req *http.Request) {
+	productID, err := gorest.GetQueryParam(req, "product_id").ToString()
+	if err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	prices, err := router.repo.ListPrices(productID)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	_ = render.New(w).OK(prices)
+}
+
+func (router PaywallRouter) CreatePrice(w http.ResponseWriter, req *http.Request) {
+	var params price.FtcPriceParams
+	if err := gorest.ParseJSON(req.Body, &params); err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	if ve := params.Validate(); ve != nil {
+		_ = render.New(w).Unprocessable(ve)
+		return
+	}
+
+	p := price.NewFtcPrice(params)
+	err := router.repo.CreatePrice(p)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	_ = render.New(w).OK(p)
+}
+
+func (router PaywallRouter) RefreshPrice(w http.ResponseWriter, req *http.Request) {
+	priceID, err := getURLParam(req, "id").ToString()
+	if err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	// Find the price for this discoun first.
+	ftcPrice, err := router.repo.RetrieveFtcPrice(priceID)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	ftcPrice, err = router.repo.RefreshFtcPriceOffers(ftcPrice)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	_ = render.New(w).OK(ftcPrice)
+}
