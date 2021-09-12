8dd859dbb5b7e1d709e2990dfacb87c8ecb5078c
weiguo.ni <neefrankie@gmail.com>
2021-09-12 17:46:42 +0800

    feat: implement Scan and Value

--- pkg/price/currency.go
+++ pkg/price/currency.go
@@ -1,5 +1,10 @@
 package price
 
+import (
+	"database/sql/driver"
+	"errors"
+)
+
 type Currency string
 
 const (
@@ -20,6 +25,30 @@ var currencySymbolMap = map[Currency]string{
 	CurrencyUSD: "US$",
 }
 
-func (c Currency) Symbol() string {
-	return currencySymbolMap[c]
+func (x Currency) Symbol() string {
+	return currencySymbolMap[x]
+}
+
+func (x *Currency) Scan(src interface{}) error {
+	if src == nil {
+		*x = ""
+		return nil
+	}
+
+	switch s := src.(type) {
+	case []byte:
+		*x = Currency(s)
+		return nil
+
+	default:
+		return errors.New("incompatible type to scan to OfferKind")
+	}
+}
+
+func (x Currency) Value() (driver.Value, error) {
+	if x == "" {
+		return nil, nil
+	}
+
+	return string(x), nil
 }
