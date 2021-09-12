c8161f9bebd3b14a0f4a563959ae46ff85bfd682
weiguo.ni <neefrankie@gmail.com>
2021-09-13 00:32:45 +0800

    feat: add Verify method

--- pkg/price/ftc_price.go
+++ pkg/price/ftc_price.go
@@ -2,6 +2,7 @@ package price
 
 import (
 	"errors"
+	"fmt"
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/go-rest/render"
@@ -105,7 +106,24 @@ func (f FtcPrice) VerifyOffer(o Discount) error {
 	return errors.New("the requested offer is not found")
 }
 
+// CheckoutItem contains a price user is trying to purchase and optional discount of this price
+// applicable to this user.
 type CheckoutItem struct {
 	Price Price    `json:"price"`
 	Offer Discount `json:"offer"` // Optional
+}
+
+// Verify checks if the price and offer match after retrieved from
+// db separately.
+func (i CheckoutItem) Verify(live bool) error {
+	// If the discount does not
+	if i.Price.ID != i.Offer.PriceID {
+		return errors.New("the price and discount does not match")
+	}
+
+	if i.Price.LiveMode != live {
+		return fmt.Errorf("price defined in %s environment cannot be used in %s environment", ids.GetBoolKey(i.Price.LiveMode), ids.GetBoolKey(live))
+	}
+
+	return nil
 }
