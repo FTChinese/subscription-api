9b61b6e16e88df5cb703e55fba02c172969c8f48
weiguo.ni <neefrankie@gmail.com>
2021-09-12 17:51:23 +0800

    refactor: price creation

--- pkg/price/price.go
+++ pkg/price/price.go
@@ -1,29 +1,26 @@
 package price
 
 import (
+	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/guregu/null"
-)
-
-type Source string
-
-const (
-	SourceFTC    = "ftc"
-	SourceStripe = "stripe"
 )
 
 // Price presents the price of a price. It unified prices coming
 // from various source, e.g., FTC in-house or Stripe API.
 type Price struct {
-	ID string `json:"id"`
+	ID string `json:"id" db:"price_id"`
 	Edition
-	Active     bool        `json:"active"`
-	Currency   Currency    `json:"currency"`
-	LiveMode   bool        `json:"liveMode"`
-	Nickname   null.String `json:"nickname"`
-	ProductID  string      `json:"productId"`
-	Source     Source      `json:"source"`
-	UnitAmount float64     `json:"unitAmount"`
+	Active      bool        `json:"active" db:"is_active"`
+	Currency    Currency    `json:"currency" db:"currency"`
+	Description null.String `json:"description" db:"description"`
+	LiveMode    bool        `json:"liveMode" db:"live_mode"`
+	Nickname    null.String `json:"nickname" db:"nickname"`
+	ProductID   string      `json:"productId" db:"product_id"`
+	Source      Source      `json:"source" db:"source"`
+	UnitAmount  float64     `json:"unitAmount" db:"unit_amount"`
+	CreatedUTC  chrono.Time `json:"createdUtc" db:"created_utc"`
+	CreatedBy   string      `json:"createdBy" db:"created_by"` // Use-facing client should ignore this field.
 }
 
 // DailyCost calculates the daily average price depending on the cycles.
