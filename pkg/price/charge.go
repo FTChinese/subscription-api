4ff9a8723728b4a3fc643c8f833d18cbfbb0f526
weiguo.ni <neefrankie@gmail.com>
2021-09-22 17:48:00 +0800

    fix: floating precision problem

--- pkg/price/charge.go
+++ pkg/price/charge.go
@@ -11,9 +11,11 @@ type Charge struct {
 	Currency string  `json:"currency" db:"currency"`     // in which currency.
 }
 
+// NewCharge calculates the final price user should pay.
 func NewCharge(price Price, offer Discount) Charge {
+	// This is a temporary solution to avoid float precision problem.
 	return Charge{
-		Amount:   price.UnitAmount - offer.PriceOff.Float64,
+		Amount:   (price.UnitAmount*100 - offer.PriceOff.Float64*100) / 100,
 		Currency: string(price.Currency),
 	}
 }
