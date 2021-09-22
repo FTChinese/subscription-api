ff0946fd3169cac4c18b81f4b976cc6335c62d15
weiguo.ni <neefrankie@gmail.com>
2021-09-22 17:48:06 +0800

    test

--- pkg/price/charge_test.go
+++ pkg/price/charge_test.go
@@ -1,1 +1,49 @@
 package price
+
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/subscription-api/lib/dt"
+	"github.com/guregu/null"
+	"testing"
+)
+
+func TestFloat(t *testing.T) {
+	diff := 35.00 - 34.99
+
+	t.Logf("%f", diff)
+}
+
+func TestNewCharge(t *testing.T) {
+	charge := NewCharge(Price{
+		ID:          "",
+		Edition:     Edition{},
+		Active:      false,
+		Archived:    false,
+		Currency:    "cny",
+		Description: null.String{},
+		LiveMode:    false,
+		Nickname:    null.String{},
+		ProductID:   "",
+		Source:      "",
+		UnitAmount:  35,
+		CreatedUTC:  chrono.Time{},
+		CreatedBy:   "",
+	}, Discount{
+		ID: "",
+		DiscountParams: DiscountParams{
+			CreatedBy:      "",
+			Description:    null.String{},
+			Kind:           "",
+			Percent:        null.Int{},
+			DateTimePeriod: dt.DateTimePeriod{},
+			PriceOff:       null.FloatFrom(34.99),
+			PriceID:        "",
+			Recurring:      false,
+		},
+		LiveMode:   false,
+		Status:     "",
+		CreatedUTC: chrono.Time{},
+	})
+
+	t.Logf("%+v", charge)
+}
