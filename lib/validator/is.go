e89cc1394bae69ecd4acd567551417b99d39f453
weiguo.ni <neefrankie@gmail.com>
2021-09-23 14:19:06 +0800

    fix: mobile validation

--- lib/validator/is.go
+++ lib/validator/is.go
@@ -3,7 +3,7 @@ package validator
 import "regexp"
 
 func IsMobile(m string) bool {
-	matched, _ := regexp.MatchString(`^(1[3|4|5|8][0-9]\d{4,8})$`, m)
+	matched, _ := regexp.MatchString(`^1[3-9]\d{9}$`, m)
 
 	return matched
 }
