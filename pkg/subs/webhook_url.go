ff0850511e337d07b707006c172af0051c145e9d
weiguo.ni <neefrankie@gmail.com>
2021-09-29 15:03:55 +0800

    fix: set ali/wx webhook to v3

--- pkg/subs/webhook_url.go
+++ pkg/subs/webhook_url.go
@@ -5,9 +5,9 @@ import "github.com/FTChinese/go-rest/enum"
 func WebhookURL(sandbox bool, method enum.PayMethod) string {
 	var baseURL string
 	if sandbox {
-		baseURL = "http://www.ftacademy.cn/api/sandbox"
+		baseURL = "https://www.ftacademy.cn/api/sandbox"
 	} else {
-		baseURL = "http://www.ftacademy.cn/api/v2"
+		baseURL = "https://www.ftacademy.cn/api/v3"
 	}
 
 	switch method {
