9a9717244161f48d713ef11df41a2706c55cd777
weiguo.ni <neefrankie@gmail.com>
2021-10-11 18:21:09 +0800

    refactor: add StmtSearchByEmail and StmtSearchByMobile

--- pkg/account/search_stmt.go
+++ pkg/account/search_stmt.go
@@ -17,3 +17,17 @@ SELECT EXISTS (
 	SELECT *
 	FROM cmstmp01.userinfo
 	WHERE user_name = ?)`
+
+const StmtSearchByEmail = `
+SELECT user_id AS id
+FROM cmstmp01.userinfo
+WHERE email = ?
+LIMIT 1`
+
+// StmtSearchByMobile tries to find a row from the profile
+// db by mobile_phone column.
+const StmtSearchByMobile = `
+SELECT profile.user_id AS id
+FROM user_db.profile
+WHERE mobile_phone = ?
+LIMIT 1`
