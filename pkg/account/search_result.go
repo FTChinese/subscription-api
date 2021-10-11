5c2f99678673e9907f72ec4653e9c88542d30933
weiguo.ni <neefrankie@gmail.com>
2021-10-11 18:20:41 +0800

    refactor: add db tag

--- pkg/account/search_result.go
+++ pkg/account/search_result.go
@@ -2,8 +2,10 @@ package account
 
 import "github.com/guregu/null"
 
+// SearchResult contains a user's uuid to indicates whether
+// the user is found.
 type SearchResult struct {
-	ID null.String `json:"id"`
+	ID null.String `json:"id" db:"id"`
 }
 
 func NewSearchResult(id string) SearchResult {
