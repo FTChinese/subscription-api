1ea82e1b12e42aae3b3b33546340d1ff63ba69e3
weiguo.ni <neefrankie@gmail.com>
2021-10-11 18:16:12 +0800

    refactor: rename SetMobile to UpsertMobile

--- internal/repository/accounts/mobile.go
+++ internal/repository/accounts/mobile.go
@@ -35,17 +35,17 @@ func (env Env) SMSVerifierUsed(v ztsms.Verifier) error {
 	return nil
 }
 
-// SetMobile inserts a new row in profile table or set
-// mobile phone filed if empty.
+// UpsertMobile inserts a new row in profile table or set
+// mobile phone field if empty.
 // Possibilities when you are trying to set the phone number:
-// * The row with this user id does not exists at all.
+// * The row with this user id does not exist at all.
 //   In such case you should insert a row directly;
 // * The row with this user id exists:
 //   * If this row does not have mobile phone set, set it;
 //   * If this row does have mobile phone set:
 //     * If existing mobile matches the required one, stop;
 //     * If existing mobile does not match the required one, it's conflict error.
-func (env Env) SetMobile(params ztsms.MobileUpdater) error {
+func (env Env) UpsertMobile(params ztsms.MobileUpdater) error {
 	defer env.Logger.Sync()
 	sugar := env.Logger.Sugar()
 
@@ -55,6 +55,8 @@ func (env Env) SetMobile(params ztsms.MobileUpdater) error {
 		return err
 	}
 
+	// Retrieve a row matching the specified ftc id from profile table.
+	// If the row does not exist, get a zero value.
 	current, err := tx.RetrieveMobile(params.FtcID)
 	if err != nil {
 		if err != sql.ErrNoRows {
@@ -65,9 +67,11 @@ func (env Env) SetMobile(params ztsms.MobileUpdater) error {
 		// Fallthrough for error no rows.
 	}
 
+	// If current.Mobile exists, the row must exist.
 	// If mobile already exists.
 	if current.Mobile.Valid {
 		_ = tx.Rollback()
+		// This ftc id already has this mobile set.
 		if current.Mobile.String == params.Mobile.String {
 			return errors.New("mobile already set")
 		} else {
@@ -75,6 +79,10 @@ func (env Env) SetMobile(params ztsms.MobileUpdater) error {
 		}
 	}
 
+	// Now the Mobile field is zero.
+	// There are two cases here:
+	// 1. The row does not exist, insert a new row into profile;
+	// 2. The row exists but mobile is not set, update it.
 	err = tx.SetMobile(params)
 	if err != nil {
 		sugar.Error(err)
