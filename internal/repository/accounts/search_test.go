a643e418ad4ca7dfd60b109d43342ba84c31f9fd
weiguo.ni <neefrankie@gmail.com>
2021-10-11 18:15:47 +0800

    feat: add SearchByMobile method

--- internal/repository/accounts/search_test.go
+++ internal/repository/accounts/search_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/internal/repository/readers"
 	"github.com/FTChinese/subscription-api/pkg/account"
+	"github.com/FTChinese/subscription-api/pkg/db"
 	"github.com/FTChinese/subscription-api/test"
 	"go.uber.org/zap/zaptest"
 	"testing"
@@ -142,6 +143,93 @@ func TestEnv_NameExists(t *testing.T) {
 			if got != tt.want {
 				t.Errorf("NameExists() got = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestEnv_SearchByEmail(t *testing.T) {
+	a := test.NewPersona().MobileBaseAccount()
+	repo := test.NewRepo()
+	repo.MustCreateFtcAccount(a)
+
+	env := New(db.MockMySQL(), zaptest.NewLogger(t))
+
+	type args struct {
+		email string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    account.SearchResult
+		found   bool
+		wantErr bool
+	}{
+		{
+			name: "Search by fake mobile email",
+			args: args{
+				email: a.Email,
+			},
+			want:    account.SearchResult{},
+			found:   true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := env.SearchByEmail(tt.args.email)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SearchByEmail() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("SearchByEmail() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("Search result %v", got)
+
+			if got.ID.Valid != tt.found {
+				t.Errorf("SearchByEmail() found = %t, want found %t", got.ID.Valid, tt.found)
+			}
+		})
+	}
+}
+
+func TestEnv_SearchByMobile(t *testing.T) {
+	a := test.NewPersona().MobileBaseAccount()
+	test.NewRepo().MustCreateFtcAccount(a)
+
+	env := New(db.MockMySQL(), zaptest.NewLogger(t))
+
+	type args struct {
+		mobile string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    account.SearchResult
+		wantErr bool
+	}{
+		{
+			name: "Search by mobile",
+			args: args{
+				mobile: a.Mobile.String,
+			},
+			want:    account.SearchResult{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := env.SearchByMobile(tt.args.mobile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SearchByMobile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("SearchByMobile() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("Found user %v", got)
 		})
 	}
 }
