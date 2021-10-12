28f730056f2cb55ae7aa5bcf1b68a27039c3d0b8
weiguo.ni <neefrankie@gmail.com>
2021-10-12 18:46:57 +0800

    test

--- internal/controller/auth_mobile_test.go
+++ internal/controller/auth_mobile_test.go
@@ -1,21 +1,30 @@
 package controller
 
 import (
+	"github.com/FTChinese/subscription-api/faker"
+	"github.com/FTChinese/subscription-api/pkg/db"
+	"github.com/FTChinese/subscription-api/pkg/ztsms"
 	"github.com/FTChinese/subscription-api/test"
+	"github.com/guregu/null"
 	"go.uber.org/zap/zaptest"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestAuthRouter_LinkMobile(t *testing.T) {
+func TestAuthRouter_VerifySMSCode(t *testing.T) {
 
-	repo := test.NewRepoV2(zaptest.NewLogger(t))
+	repo := test.NewRepo()
 
-	router := NewAuthRouter(test.SplitDB, test.Postman, zaptest.NewLogger(t))
+	router := NewAuthRouter(
+		db.MockMySQL(),
+		test.Postman,
+		zaptest.NewLogger(t),
+	)
 
 	type args struct {
-		w    *httptest.ResponseRecorder
-		kind test.MobileLinkAccountKind
+		w   *httptest.ResponseRecorder
+		req *http.Request
 	}
 	tests := []struct {
 		name       string
@@ -23,43 +32,102 @@ func TestAuthRouter_LinkMobile(t *testing.T) {
 		wantStatus int
 	}{
 		{
-			name: "Mobile row not exist",
+			name: "Existing mobile",
 			args: args{
-				w:    httptest.NewRecorder(),
-				kind: test.MobileLinkNoProfile,
+				w: httptest.NewRecorder(),
+				req: repo.ReqVerifySMSCode(test.
+					NewPersona().
+					MobileVerifier()),
+			},
+			wantStatus: 200,
+		},
+		{
+			name: "Email derived from mobile",
+			args: args{
+				w: httptest.NewRecorder(),
+				req: repo.ReqVerifySMSForMobileEmail(
+					ztsms.NewVerifier(faker.GenPhone(), null.String{}),
+				),
+			},
+			wantStatus: 200,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			router.VerifySMSCode(tt.args.w, tt.args.req)
+
+			resp := tt.args.w.Result()
+
+			if resp.StatusCode != tt.wantStatus {
+				t.Errorf("VerifySMS() want status %d, got %d", tt.wantStatus, resp.StatusCode)
+				t.Logf("%s", faker.MustReadBody(resp.Body))
+				return
+			}
+
+			t.Logf("%s", faker.MustReadBody(resp.Body))
+		})
+	}
+}
+
+func TestAuthRouter_LinkMobile(t *testing.T) {
+
+	repo := test.NewRepo()
+
+	router := NewAuthRouter(test.SplitDB, test.Postman, zaptest.NewLogger(t))
+
+	type args struct {
+		w   *httptest.ResponseRecorder
+		req *http.Request
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+	}{
+		{
+			name: "Profile row does not exist",
+			args: args{
+				w:   httptest.NewRecorder(),
+				req: repo.ReqMobileLinkNoProfile(test.NewPersona().EmailOnlyAccount()),
 			},
 			wantStatus: 200,
 		},
 		{
 			name: "Mobile row exist with empty phone",
 			args: args{
-				w:    httptest.NewRecorder(),
-				kind: test.MobileLinkHasProfileNoPhone,
+				w: httptest.NewRecorder(),
+				req: repo.ReqMobileLinkWithProfile(
+					test.NewPersona().
+						EmailOnlyAccount()),
 			},
 			wantStatus: 200,
 		},
 		{
 			name: "Mobile row exist with phone set",
 			args: args{
-				w:    httptest.NewRecorder(),
-				kind: test.MobileLinkHasProfilePhoneSet,
+				w: httptest.NewRecorder(),
+				req: repo.ReqMobileLinkWithProfile(
+					test.NewPersona().
+						EmailMobileAccount()),
 			},
 			wantStatus: 200,
 		},
 		{
 			name: "Mobile row exist with phone taken",
 			args: args{
-				w:    httptest.NewRecorder(),
-				kind: test.MobileLinkHasProfilePhoneTaken,
+				w: httptest.NewRecorder(),
+				req: repo.ReqMobileLinkPhoneTaken(
+					test.NewPersona().
+						EmailMobileAccount()),
 			},
 			wantStatus: 422,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req := repo.BuildMobileLinkReq(tt.args.kind)
 
-			router.LinkMobile(tt.args.w, req)
+			router.LinkMobile(tt.args.w, tt.args.req)
 
 			resp := tt.args.w.Result()
 
@@ -68,7 +136,7 @@ func TestAuthRouter_LinkMobile(t *testing.T) {
 				return
 			}
 
-			t.Logf("%s", test.GetRespBody(resp.Body))
+			t.Logf("%s", faker.MustReadBody(resp.Body))
 		})
 	}
 }
