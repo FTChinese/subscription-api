fa0c43cc75a9f6a4273539254ab391c1183814cb
weiguo.ni <neefrankie@gmail.com>
2021-10-14 12:02:03 +0800

    refactor

--- pkg/account/errors.go
+++ pkg/account/errors.go
@@ -3,7 +3,5 @@ package account
 import "errors"
 
 var (
-	ErrMobileTaken         = errors.New("the mobile phone is already taken by another account")
-	ErrMobileSet           = errors.New("the mobile already set on this account")
-	ErrAccountHasMobileSet = errors.New("this account already linked to another mobile")
+	ErrMobileTakenByOther = errors.New("the mobile phone is already taken by another account")
 )
