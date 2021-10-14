17e5d1e536d4e52112aa03fffd172a2beb796897
weiguo.ni <neefrankie@gmail.com>
2021-10-14 12:01:13 +0800

    test

--- test/postman_test.go
+++ test/postman_test.go
@@ -12,36 +12,78 @@ import (
 	"testing"
 )
 
-func TestMobileDerivedEmailAccount(t *testing.T) {
-	a := NewPersona().MobileOnlyAccount()
-
-	repo := NewRepo()
-
-	repo.MustCreateUserInfo(a)
+// Create a row in userinfo for mobile-only email.
+func TestEmailSignUp_WithMobile(t *testing.T) {
+	a := NewPersona().EmailMobileAccount()
 
 	t.Logf("%s", faker.MustMarshalIndent(a))
 }
 
-// Generate sms verification parameters for a mobile-only user.
-func TestVerifySMSForMobileEmail(t *testing.T) {
-	p := NewPersona()
-
+func TestVerifySMSCode_NewMobile(t *testing.T) {
 	repo := NewRepo()
 
-	repo.MustCreateUserInfo(p.MobileOnlyAccount())
-
-	v := ztsms.NewVerifier(p.Mobile, null.String{})
+	v := ztsms.NewVerifier(faker.GenPhone(), null.String{})
 	repo.MustSaveMobileVerifier(v)
 
-	t.Logf("%s", faker.MustMarshalIndent(ztsms.VerifierParams{
-		Mobile:      v.Mobile,
-		Code:        v.Code,
-		DeviceToken: null.String{},
-	}))
+	t.Logf("%s", faker.MustMarshalIndent(v))
 }
 
-func TestLinkMobile(t *testing.T) {
-	a := account.NewMockFtcAccountBuilder(enum.AccountKindFtc).WithMobile("").Build()
+// Generate sms verification parameters for a mobile-only user.
+// * Create a mobile-derived account in userinfo.
+// * Create a verifier for this phone.
+func TestVerifySMSCode_MobileOnlyAccount(t *testing.T) {
+	repo := NewRepo()
+
+	a := NewPersona().MobileOnlyAccount()
+	repo.MustCreateUserInfo(a)
+
+	v := ztsms.NewVerifier(a.Mobile.String, null.String{})
+	repo.MustSaveMobileVerifier(v)
+
+	t.Logf("%s", faker.MustMarshalIndent(a))
+	t.Logf("%s", faker.MustMarshalIndent(v))
+}
+
+func TestMobileSignUp_RealPhone(t *testing.T) {
+	v := ztsms.NewVerifier("15011481214", null.String{})
+
+	NewRepo().MustSaveMobileVerifier(v)
+
+	t.Logf("%s", faker.MustMarshalIndent(v))
+}
+
+// Mobile login case 1: mobile used for the 1st time,
+// and user wants to create a new account with this mobile.
+// Simply generate a new phone works.
+func TestMobileSignUp_FakePhoneEmail(t *testing.T) {
+	v := ztsms.NewVerifier(faker.GenPhone(), null.String{})
+
+	NewRepo().MustSaveMobileVerifier(v)
+
+	t.Logf("%s", faker.MustMarshalIndent(v))
+}
+
+// Mobile login case 2: mobile used for the 1st time,
+// and user wants to create a new email account with
+// mobile set.
+// Generates a new phone, and generates a new email account
+// but does not save it.
+func TestMobileSignUp_NewEmailAccount(t *testing.T) {
+	a := NewPersona().EmailOnlyAccount()
+
+	v := ztsms.NewVerifier(faker.GenPhone(), null.String{})
+
+	NewRepo().MustSaveMobileVerifier(v)
+
+	t.Logf("%s", faker.MustMarshalIndent(a))
+	t.Logf("%s", faker.MustMarshalIndent(v))
+}
+
+// Mobile login case 3: mobile used for the 1st time,
+// and user wants to link to an existing email account.
+// Generate a new phone, and create an email-only account.
+func TestMobileLinkExistingEmail(t *testing.T) {
+	a := NewPersona().EmailOnlyAccount()
 	v := ztsms.NewVerifier(faker.GenPhone(), null.String{})
 
 	repo := NewRepo()
@@ -50,6 +92,15 @@ func TestLinkMobile(t *testing.T) {
 
 	t.Logf("%s", faker.MustMarshalIndent(a))
 	t.Logf("%s", faker.MustMarshalIndent(v))
+}
+
+func TestLoadAccountByFtcID_SyncMobile(t *testing.T) {
+	repo := NewRepo()
+
+	a := NewPersona().MobileOnlyAccount()
+	repo.CreateUserInfo(a)
+
+	t.Logf("%s", faker.MustMarshalIndent(a))
 }
 
 func TestUnsetMobile(t *testing.T) {
