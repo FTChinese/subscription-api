4be3bbd91ef78e47496a73ed47256f58ca5d201e
weiguo.ni <neefrankie@gmail.com>
2021-10-26 14:16:32 +0800

    fix: removed unused vars

--- pkg/subs/errors.go
+++
@@ -1,8 +0,0 @@
-package subs
-
-import "errors"
-
-var (
-	// Reasons to deny upgrade.
-	ErrNotUpgradeIntent = errors.New("upgrade could only be performed on an existing non-premium membership purchased via alipay or wxpay")
-)
