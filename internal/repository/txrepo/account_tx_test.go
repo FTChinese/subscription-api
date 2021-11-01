748aeef411a13baa532baa34ed4851918a252b2b
weiguo.ni <neefrankie@gmail.com>
2021-11-01 11:52:36 +0800

    test

--- internal/repository/txrepo/account_tx_test.go
+++ internal/repository/txrepo/account_tx_test.go
@@ -1,6 +1,7 @@
 package txrepo
 
 import (
+	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/pkg/account"
@@ -255,6 +256,227 @@ func TestAccountTx_RetrieveMobiles(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("RetrieveMobiles() got = %v, want %v", got, tt.want)
 			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestAccountTx_InsertMobile(t *testing.T) {
+	a := test.NewPersona().EmailMobileAccount()
+
+	type fields struct {
+		SharedTx SharedTx
+	}
+	type args struct {
+		params account.MobileUpdater
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Insert profile with mobile",
+			fields: fields{
+				SharedTx: NewSharedTx(db.MockTx()),
+			},
+			args: args{
+				params: account.MobileUpdater{
+					FtcID:  a.FtcID,
+					Mobile: null.StringFrom(a.Mobile.String),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				SharedTx: tt.fields.SharedTx,
+			}
+			if err := tx.InsertMobile(tt.args.params); (err != nil) != tt.wantErr {
+				t.Errorf("InsertMobile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestAccountTx_UpdateMobile(t *testing.T) {
+
+	a := test.NewPersona().EmailMobileAccount()
+
+	test.NewRepo().CreateProfile(a)
+
+	type fields struct {
+		SharedTx SharedTx
+	}
+	type args struct {
+		params account.MobileUpdater
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Update mobile",
+			fields: fields{
+				SharedTx: NewSharedTx(db.MockTx()),
+			},
+			args: args{
+				params: account.MobileUpdater{
+					FtcID:  a.FtcID,
+					Mobile: null.StringFrom(a.Mobile.String),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				SharedTx: tt.fields.SharedTx,
+			}
+			if err := tx.UpdateMobile(tt.args.params); (err != nil) != tt.wantErr {
+				t.Errorf("UpdateMobile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestAccountTx_DeleteUserInfo(t *testing.T) {
+	a := test.NewPersona().EmailOnlyAccount()
+
+	test.NewRepo().CreateFtcAccount(a)
+
+	type fields struct {
+		SharedTx SharedTx
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Delete user info",
+			fields: fields{
+				SharedTx: NewSharedTx(db.MockTx()),
+			},
+			args: args{
+				id: a.FtcID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				SharedTx: tt.fields.SharedTx,
+			}
+			if err := tx.DeleteUserInfo(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteUserInfo() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestAccountTx_DeleteProfile(t *testing.T) {
+	a := test.NewPersona().EmailOnlyAccount()
+
+	test.NewRepo().CreateFtcAccount(a)
+
+	type fields struct {
+		SharedTx SharedTx
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Delete profile",
+			fields: fields{
+				SharedTx: NewSharedTx(db.MockTx()),
+			},
+			args: args{
+				id: a.FtcID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				SharedTx: tt.fields.SharedTx,
+			}
+			if err := tx.DeleteProfile(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteProfile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestAccountTx_SaveDeletedUser(t *testing.T) {
+	a := test.NewPersona().EmailOnlyAccount()
+
+	test.NewRepo().CreateFtcAccount(a)
+
+	type fields struct {
+		SharedTx SharedTx
+	}
+	type args struct {
+		d account.DeletedUser
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save deleted  user",
+			fields: fields{
+				SharedTx: NewSharedTx(db.MockTx()),
+			},
+			args: args{
+				d: account.DeletedUser{
+					ID:         a.FtcID,
+					Email:      a.Email,
+					CreatedUTC: chrono.TimeNow(),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := AccountTx{
+				SharedTx: tt.fields.SharedTx,
+			}
+			if err := tx.SaveDeletedUser(tt.args.d); (err != nil) != tt.wantErr {
+				t.Errorf("SaveDeletedUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			_ = tx.Commit()
 		})
 	}
 }
