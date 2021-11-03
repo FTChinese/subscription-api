12a60b2456ca725cc725037ad72d4af3b8916eda
weiguo.ni <neefrankie@gmail.com>
2021-11-03 17:54:32 +0800

    refactor: mocking

--- pkg/db/mock.go
+++ pkg/db/mock.go
@@ -1,14 +1,15 @@
+//go:build !production
 // +build !production
 
 package db
 
 import (
-	"github.com/FTChinese/subscription-api/pkg/config"
+	"github.com/FTChinese/subscription-api/faker"
 	"github.com/jmoiron/sqlx"
 )
 
 func MockMySQL() ReadWriteMyDBs {
-	config.MustSetupViperV2(config.MustReadConfigFile())
+	faker.MustSetupViper()
 	return MustNewMyDBs(false)
 }
 
