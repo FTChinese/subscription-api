e3730e68fd287387c04310da42d860282d3f2f15
weiguo.ni <neefrankie@gmail.com>
2021-11-03 17:57:40 +0800

    refactor: mocking

---
+++ faker/setup_viper.go
@@ -0,0 +1,30 @@
+package faker
+
+import (
+	"github.com/FTChinese/subscription-api/pkg/config"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func ReadConfigFile() ([]byte, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(filepath.Join(home, "config", "api.toml"))
+}
+
+func MustReadConfigFile() []byte {
+	b, err := ReadConfigFile()
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
+
+func MustSetupViper() {
+	config.MustSetupViperV2(MustReadConfigFile())
+}
