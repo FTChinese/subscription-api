946629f07682ab05dd984d775953026fe0c29b4f
weiguo.ni <neefrankie@gmail.com>
2021-11-03 17:55:42 +0800

    refactor: removed ReadConfigFile and MustReadConfigFile

--- pkg/config/setup.go
+++ pkg/config/setup.go
@@ -3,9 +3,6 @@ package config
 import (
 	"bytes"
 	"github.com/spf13/viper"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 )
 
 // SetupViper config viper
@@ -32,8 +29,6 @@ func MustSetupViper() {
 
 func SetupViperV2(b []byte) error {
 	viper.SetConfigType("toml")
-	//viper.SetConfigName("api")
-	//viper.AddConfigPath("$HOME/config")
 
 	err := viper.ReadConfig(bytes.NewBuffer(b))
 	if err != nil {
@@ -47,22 +42,4 @@ func MustSetupViperV2(b []byte) {
 	if err := SetupViperV2(b); err != nil {
 		panic(err)
 	}
-}
-
-func ReadConfigFile() ([]byte, error) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-
-	return ioutil.ReadFile(filepath.Join(home, "config", "api.toml"))
-}
-
-func MustReadConfigFile() []byte {
-	b, err := ReadConfigFile()
-	if err != nil {
-		panic(err)
-	}
-
-	return b
 }
