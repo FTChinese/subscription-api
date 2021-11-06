df25a3182bdafa423e8abac678dc723414af2a61
weiguo.ni <neefrankie@gmail.com>
2021-11-06 19:11:44 +0800

    feat: handle wx oauth web callback

--- internal/controller/wxauth_web_code.go
+++ internal/controller/wxauth_web_code.go
@@ -1,48 +1,29 @@
 package controller
 
 import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/wxlogin"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
-// WebCallback is used to help web app to get OAuth 2.0 code.
-// The code and state is transferred back to next-user web app since Wechat only recognize the ftacacemy.cn URL.
-func (router WxAuthRouter) WebCallback(w http.ResponseWriter, req *http.Request) {
+// WxCallbackHandler is used to help web app to get OAuth 2.0 code.
+// The code and state is transferred back to the web app initialized the
+// OAuth workflow since Wechat only recognize the www.ftacacemy.cn URL.
+// * If granted, query parameter will be:
+// `?code=<string>&state=<string>`
+func WxCallbackHandler(app wxlogin.CallbackApp) http.HandlerFunc {
 
-	err := req.ParseForm()
-	if err != nil {
-		query := url.Values{}
-		query.Set("error", "invalid_request")
+	return func(w http.ResponseWriter, req *http.Request) {
+		redirectTo, err := wxlogin.GetCallbackURL(app, req.Form)
+		if err != nil {
+			_ = render.New(w).BadRequest(err.Error())
+			return
+		}
 
 		http.Redirect(
 			w,
 			req,
-			wxOAuthCallback+query.Encode(),
+			redirectTo,
 			http.StatusFound)
-		return
 	}
-	// The code returned by wechat
-	code := req.FormValue("code")
-	// The nonce code we send to wechat.
-	state := req.FormValue("state")
-
-	code = strings.TrimSpace(code)
-
-	if code == "" && state != "" {
-		query := url.Values{}
-		query.Set("error", "access_denied")
-		http.Redirect(
-			w,
-			req,
-			wxOAuthCallback+query.Encode(),
-			http.StatusFound)
-		return
-	}
-
-	http.Redirect(
-		w,
-		req,
-		wxOAuthCallback+req.Form.Encode(),
-		http.StatusFound)
 }
