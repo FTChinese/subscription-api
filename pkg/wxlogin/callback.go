cd3c6c6e9d943e14fb551fba4bfaff6abde6b2e7
weiguo.ni <neefrankie@gmail.com>
2021-11-06 19:11:29 +0800

    feat: handle wx oauth web callback

---
+++ pkg/wxlogin/callback.go
@@ -0,0 +1,54 @@
+package wxlogin
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const (
+	CallbackAppNextUser CallbackApp = iota
+	CallbackAppFtaReader
+)
+
+type CallbackApp int
+
+var cbAppNames = []string{
+	"next-user",
+	"fta-reader",
+}
+
+func (a CallbackApp) String() string {
+	if a < CallbackAppNextUser || a > CallbackAppFtaReader {
+		return ""
+	}
+
+	return cbAppNames[a]
+}
+
+var callbackURLs = map[CallbackApp]url.URL{
+	// the callback url of next-user
+	// to which this API should forward OAuth's code and state.
+	CallbackAppNextUser: {
+		Scheme:   "https",
+		Host:     "users.ftchinese.com",
+		Path:     "/login/wechat/callback",
+		RawQuery: "",
+	},
+	CallbackAppFtaReader: {
+		Scheme:   "https",
+		Opaque:   "www.ftacademy.cn",
+		Path:     "/reader/oauth/callback",
+		RawQuery: "",
+	},
+}
+
+// GetCallbackURL finds the callback url for an app, and appends query parameter to it.
+func GetCallbackURL(app CallbackApp, query url.Values) (string, error) {
+	u, ok := callbackURLs[app]
+	if !ok {
+		return "", fmt.Errorf("url to redirect to %s not found", app)
+	}
+
+	u.RawQuery = query.Encode()
+	return u.String(), nil
+}
