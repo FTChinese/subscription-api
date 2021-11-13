4a4a28265f5e93b948d057704f41d308090c031b
weiguo.ni <neefrankie@gmail.com>
2021-11-14 06:07:22 +0800

    refactor: return type uses StripePrice

--- internal/repository/striperepo/prices.go
+++ internal/repository/striperepo/prices.go
@@ -4,7 +4,7 @@ import (
 	"github.com/FTChinese/subscription-api/pkg/price"
 )
 
-func (env Env) ListPrices(force bool) ([]price.Price, error) {
+func (env Env) ListPrices(force bool) ([]price.StripePrice, error) {
 	if !force && price.StripePriceCache.Len() != 0 {
 		return price.StripePriceCache.
 			List(env.client.live), nil
@@ -19,23 +19,4 @@ func (env Env) ListPrices(force bool) ([]price.Price, error) {
 
 	return price.StripePriceCache.
 		List(env.client.live), nil
-}
-
-func (env Env) GetPrice(id string) (price.Price, error) {
-	cachedPrice, err := price.StripePriceCache.Find(id, env.client.live)
-	if err == nil {
-		return cachedPrice, nil
-	}
-
-	sp, err := env.client.GetPrice(id)
-	if err != nil {
-		return price.Price{}, err
-	}
-
-	err = price.StripePriceCache.Upsert(sp)
-	if err != nil {
-		return price.Price{}, err
-	}
-
-	return price.StripePriceCache.Find(id, env.client.live)
 }
