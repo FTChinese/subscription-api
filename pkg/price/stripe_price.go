857639a8cdf976d70e68684a85e1d27292e6f444
weiguo.ni <neefrankie@gmail.com>
2021-11-14 06:06:20 +0800

    refactor: a new data type to hold stripe price

--- pkg/price/stripe_price.go
+++ pkg/price/stripe_price.go
@@ -1,117 +1,76 @@
 package price
 
 import (
-	"database/sql"
-	"github.com/guregu/null"
+	"github.com/FTChinese/go-rest/enum"
 	"github.com/stripe/stripe-go/v72"
-	"sync"
+	"strconv"
 )
 
-// NewStripe extract the essential data from a stripe price.
-func NewStripe(p StripeEdition, sp *stripe.Price) Price {
-	return Price{
-		ID:         sp.ID,
-		Edition:    p.Edition,
-		Active:     sp.Active,
-		Currency:   Currency(sp.Currency),
-		LiveMode:   sp.Livemode,
-		Nickname:   null.NewString(sp.Nickname, sp.Nickname != ""),
-		ProductID:  sp.Product.ID,
-		Source:     SourceStripe,
-		UnitAmount: float64(sp.UnitAmount) / 100,
+type StripePriceMeta struct {
+	Tier         enum.Tier `json:"tier"`
+	PeriodDays   int       `json:"periodDays"`
+	Introductory bool      `json:"introductory"`
+}
+
+func NewStripePriceMeta(m map[string]string) StripePriceMeta {
+	t, _ := enum.ParseTier(m["tier"])
+	d, _ := strconv.Atoi(m["period_days"])
+	ok, _ := strconv.ParseBool(m["introductory"])
+	return StripePriceMeta{
+		Tier:         t,
+		PeriodDays:   d,
+		Introductory: ok,
 	}
 }
 
-// stripePriceCache acts as a cache after retrieving Stripe prices.
-type stripePriceCache struct {
-	len     int
-	prices  []Price        // Use an array to store the prices.
-	idIndex map[string]int // price id to its position in the array.
-	mux     sync.Mutex     // The data is global. Lock it for concurrency.
+type StripePriceRecurring struct {
+	Interval      stripe.PriceRecurringInterval  `json:"interval"`
+	IntervalCount int64                          `json:"intervalCount"`
+	UsageType     stripe.PriceRecurringUsageType `json:"usageType"`
 }
 
-func newStripePriceStore() *stripePriceCache {
-	return &stripePriceCache{
-		len:     0,
-		prices:  make([]Price, 0),
-		idIndex: map[string]int{},
+func NewStripePriceRecurring(r *stripe.PriceRecurring) StripePriceRecurring {
+	if r == nil {
+		return StripePriceRecurring{}
+	}
+
+	return StripePriceRecurring{
+		Interval:      r.Interval,
+		IntervalCount: r.IntervalCount,
+		UsageType:     r.UsageType,
 	}
 }
 
-func (store *stripePriceCache) Len() int {
-	return store.len
+func (r StripePriceRecurring) IsZero() bool {
+	return r.Interval == "" && r.IntervalCount == 0 && r.UsageType == ""
 }
 
-// AddAll caches an array of stripe prices.
-func (store *stripePriceCache) AddAll(sps []*stripe.Price) {
-	store.mux.Lock()
-	for _, sp := range sps {
-		_ = store.upsert(sp)
-	}
-	store.mux.Unlock()
+type StripePrice struct {
+	Active     bool                 `json:"active"`
+	Created    int64                `json:"created"`
+	Currency   stripe.Currency      `json:"currency"`
+	ID         string               `json:"id"`
+	LiveMode   bool                 `json:"liveMode"`
+	Metadata   StripePriceMeta      `json:"metadata"`
+	Nickname   string               `json:"nickname"`
+	Product    string               `json:"product"`
+	Recurring  StripePriceRecurring `json:"recurring"`
+	Type       stripe.PriceType     `json:"type"`
+	UnitAmount int64                `json:"unitAmount"`
 }
 
-// Upsert inserts or update a price.
-func (store *stripePriceCache) Upsert(sp *stripe.Price) error {
-	store.mux.Lock()
-	defer store.mux.Unlock()
-
-	return store.upsert(sp)
+func NewStripePrice(p *stripe.Price) StripePrice {
+	return StripePrice{
+		Active:     p.Active,
+		Created:    p.Created,
+		Currency:   p.Currency,
+		ID:         p.ID,
+		LiveMode:   p.Livemode,
+		Metadata:   NewStripePriceMeta(p.Metadata),
+		Nickname:   p.Nickname,
+		Product:    p.Product.ID,
+		Recurring:  NewStripePriceRecurring(p.Recurring),
+		Type:       p.Type,
+		UnitAmount: p.UnitAmount,
+	}
 }
-
-func (store *stripePriceCache) upsert(sp *stripe.Price) error {
-	// Check if a stripe price id is allowed to present.
-	plan, err := StripeEditions.FindByID(sp.ID)
-	if err != nil {
-		return err
-	}
-
-	// If this price already cached, update it.
-	index, ok := store.idIndex[sp.ID]
-	if ok {
-		store.prices[index] = NewStripe(plan, sp)
-		return nil
-	}
-
-	// If stripe plan is not active and not added to the store, ignore it.
-	if !sp.Active {
-		return nil
-	}
-
-	// The price is not cached, append to the end.
-	store.prices = append(store.prices, NewStripe(plan, sp))
-	store.len++
-	return nil
-}
-
-// Find searches a price by id in the specified environment.
-func (store *stripePriceCache) Find(id string, live bool) (Price, error) {
-	index, ok := store.idIndex[id]
-	if ok {
-		return Price{}, sql.ErrNoRows
-	}
-
-	cachedPrice := store.prices[index]
-	if cachedPrice.LiveMode != live {
-		return Price{}, sql.ErrNoRows
-	}
-
-	return cachedPrice, nil
-}
-
-// List returned a list all prices in the specified environment.
-func (store *stripePriceCache) List(live bool) []Price {
-	var prices = make([]Price, 0)
-
-	for _, v := range store.prices {
-		if v.LiveMode != live {
-			continue
-		}
-
-		prices = append(prices, v)
-	}
-
-	return prices
-}
-
-var StripePriceCache = newStripePriceStore()
