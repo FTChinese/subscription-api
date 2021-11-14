2fd9f7c5e6acd045f38a076eb88d664e5693be46
weiguo.ni <neefrankie@gmail.com>
2021-11-14 12:28:11 +0800

    feat: GetAndCachePrice

--- internal/repository/striperepo/client_price.go
+++ internal/repository/striperepo/client_price.go
@@ -1,6 +1,9 @@
 package striperepo
 
-import "github.com/stripe/stripe-go/v72"
+import (
+	"github.com/FTChinese/subscription-api/pkg/price"
+	"github.com/stripe/stripe-go/v72"
+)
 
 func (c Client) ListPrices() ([]*stripe.Price, error) {
 	iter := c.sc.Prices.List(&stripe.PriceListParams{
@@ -22,4 +25,16 @@ func (c Client) GetPrice(id string) (*stripe.Price, error) {
 	return c.sc.Prices.Get(id, &stripe.PriceParams{
 		Active: stripe.Bool(true),
 	})
+}
+
+func (c Client) GetAndCachePrice(id string) (price.StripePrice, error) {
+	sp, err := c.GetPrice(id)
+	if err != nil {
+		return price.StripePrice{}, err
+	}
+
+	p := price.NewStripePrice(sp)
+	price.StripePriceCache.Upsert(p)
+
+	return p, nil
 }
