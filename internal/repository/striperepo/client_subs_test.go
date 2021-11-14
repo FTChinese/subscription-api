c36f38829016c390143198b00c4515aaece7d4b9
weiguo.ni <neefrankie@gmail.com>
2021-11-14 22:12:05 +0800

    test introductory offer

---
+++ internal/repository/striperepo/client_subs_test.go
@@ -0,0 +1,61 @@
+package striperepo
+
+import (
+	"github.com/FTChinese/subscription-api/faker"
+	ftcStripe "github.com/FTChinese/subscription-api/pkg/stripe"
+	"github.com/stripe/stripe-go/v72"
+	"go.uber.org/zap/zaptest"
+	"testing"
+)
+
+func TestClient_NewSubs(t *testing.T) {
+	faker.MustSetupViper()
+
+	client := NewClient(false, zaptest.NewLogger(t))
+
+	type args struct {
+		params *stripe.SubscriptionParams
+	}
+	tests := []struct {
+		name    string
+		fields  Client
+		args    args
+		want    *stripe.Subscription
+		wantErr bool
+	}{
+		{
+			name:   "Introductory offer",
+			fields: client,
+			args: args{
+				params: ftcStripe.CheckoutItem{
+					Price:        ftcStripe.MockPriceStdYear,
+					Introductory: ftcStripe.MockPriceStdIntro,
+				}.
+					NewSubParams(
+						"cus_KXMeDzH46HnpMB",
+						ftcStripe.SubSharedParams{}),
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Client{
+				live:   tt.fields.live,
+				sc:     tt.fields.sc,
+				logger: tt.fields.logger,
+			}
+			got, err := c.NewSubs(tt.args.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewSubs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("NewSubs() got = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
