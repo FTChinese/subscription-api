f3ace3e7f4ca5c51111b13f01f8d22fcf40fefc5
weiguo.ni <neefrankie@gmail.com>
2021-11-14 13:57:09 +0800

    refactor: import name

--- internal/repository/striperepo/client_price.go
+++ internal/repository/striperepo/client_price.go
@@ -1,15 +1,15 @@
 package striperepo
 
 import (
-	"github.com/FTChinese/subscription-api/pkg/price"
-	"github.com/stripe/stripe-go/v72"
+	"github.com/FTChinese/subscription-api/pkg/stripe"
+	stripeSdk "github.com/stripe/stripe-go/v72"
 )
 
-func (c Client) ListPrices() ([]*stripe.Price, error) {
-	iter := c.sc.Prices.List(&stripe.PriceListParams{
-		Active: stripe.Bool(true),
-		ListParams: stripe.ListParams{
-			Limit: stripe.Int64(100),
+func (c Client) ListPrices() ([]*stripeSdk.Price, error) {
+	iter := c.sc.Prices.List(&stripeSdk.PriceListParams{
+		Active: stripeSdk.Bool(true),
+		ListParams: stripeSdk.ListParams{
+			Limit: stripeSdk.Int64(100),
 		},
 	})
 
@@ -21,20 +21,20 @@ func (c Client) ListPrices() ([]*stripe.Price, error) {
 	return list.Data, nil
 }
 
-func (c Client) GetPrice(id string) (*stripe.Price, error) {
-	return c.sc.Prices.Get(id, &stripe.PriceParams{
-		Active: stripe.Bool(true),
+func (c Client) GetPrice(id string) (*stripeSdk.Price, error) {
+	return c.sc.Prices.Get(id, &stripeSdk.PriceParams{
+		Active: stripeSdk.Bool(true),
 	})
 }
 
-func (c Client) GetAndCachePrice(id string) (price.StripePrice, error) {
+func (c Client) GetAndCachePrice(id string) (stripe.Price, error) {
 	sp, err := c.GetPrice(id)
 	if err != nil {
-		return price.StripePrice{}, err
+		return stripe.Price{}, err
 	}
 
-	p := price.NewStripePrice(sp)
-	price.StripePriceCache.Upsert(p)
+	p := stripe.NewPrice(sp)
+	StripePriceCache.Upsert(p)
 
 	return p, nil
 }
