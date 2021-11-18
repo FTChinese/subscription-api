4183abde70d267ba5833ef2fbcb99379308369d2
weiguo.ni <neefrankie@gmail.com>
2021-11-18 16:57:06 +0800

    refactor: eliminate getCheckoutItem

--- internal/controller/stripe_plan.go
+++ internal/controller/stripe_plan.go
@@ -7,11 +7,6 @@ import (
 
 // ListPrices retrieves all prices defined in Stripe.
 func (router StripeRouter) ListPrices(w http.ResponseWriter, req *http.Request) {
-	if err := req.ParseForm(); err != nil {
-		_ = render.New(w).BadRequest(err.Error())
-		return
-	}
-
 	refresh := req.FormValue("refresh") == "true"
 
 	prices, err := router.stripeRepo.ListPrices(refresh)
@@ -32,4 +27,27 @@ func (router StripeRouter) ListPrices(w http.ResponseWriter, req *http.Request) 
 	}
 
 	_ = render.New(w).OK(prices)
+}
+
+func (router StripeRouter) LoadPrice(w http.ResponseWriter, req *http.Request) {
+	refresh := req.FormValue("refresh") == "true"
+	id, err := getURLParam(req, "id").ToString()
+	if err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	price, err := router.stripeRepo.LoadPrice(id, refresh)
+
+	if err != nil {
+		err := handleErrResp(w, err)
+		if err == nil {
+			return
+		}
+
+		_ = render.New(w).InternalServerError(err.Error())
+		return
+	}
+
+	_ = render.New(w).OK(price)
 }
--- internal/repository/striperepo/prices.go
+++ internal/repository/striperepo/prices.go
@@ -20,3 +20,43 @@ func (env Env) ListPrices(force bool) ([]stripe.Price, error) {
 	return PriceCache.
 		List(env.client.live), nil
 }
+
+func (env Env) LoadPrice(id string, force bool) (stripe.Price, error) {
+	if !force {
+		p, ok := PriceCache.Find(id)
+		if ok {
+			return p, nil
+		}
+	}
+
+	sp, err := env.client.RetrievePrice(id)
+	if err != nil {
+		return stripe.Price{}, err
+	}
+
+	return stripe.NewPrice(sp), nil
+}
+
+func (env Env) LoadCheckoutItem(params stripe.SubsParams) (stripe.CheckoutItem, error) {
+	p, err := env.LoadPrice(params.PriceID, false)
+	if err != nil {
+		return stripe.CheckoutItem{}, err
+	}
+
+	if params.IntroductoryPriceID.IsZero() {
+		return stripe.CheckoutItem{
+			Price:        p,
+			Introductory: stripe.Price{},
+		}, nil
+	}
+
+	introPrice, err := env.LoadPrice(params.IntroductoryPriceID.String, false)
+	if err != nil {
+		return stripe.CheckoutItem{}, err
+	}
+
+	return stripe.CheckoutItem{
+		Price:        p,
+		Introductory: introPrice,
+	}, nil
+}
