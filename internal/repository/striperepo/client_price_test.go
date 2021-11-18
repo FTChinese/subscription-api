5d91000019ea632b7e02ffa5397bc8ccb61d7c07
weiguo.ni <neefrankie@gmail.com>
2021-11-18 15:49:36 +0800

    feat: RetrievePrice

--- internal/repository/striperepo/client_price_test.go
+++ internal/repository/striperepo/client_price_test.go
@@ -24,3 +24,16 @@ func TestClient_ListPrices(t *testing.T) {
 		t.Logf("%s", faker.MustMarshalIndent(v))
 	}
 }
+
+func TestClient_RetrievePrice(t *testing.T) {
+	faker.MustSetupViper()
+
+	client := NewClient(false, zaptest.NewLogger(t))
+	p, err := client.RetrievePrice("price_1Juuu2BzTK0hABgJTXiK4NTt")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("%s", faker.MustMarshalIndent(p))
+}
