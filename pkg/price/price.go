e6b8940a27428b14f1650efdfb4a188acf027b81
weiguo.ni <neefrankie@gmail.com>
2021-11-22 15:57:05 +0800

    refactor: removed Source field

--- pkg/price/price.go
+++ pkg/price/price.go
@@ -12,13 +12,12 @@ type Price struct {
 	ID string `json:"id" db:"price_id"`
 	Edition
 	Active      bool        `json:"active" db:"is_active"`
-	Archived    bool        `json:"archived" db:"archived"` // Deprecated
+	Archived    bool        `json:"archived" db:"archived"`
 	Currency    Currency    `json:"currency" db:"currency"`
 	Description null.String `json:"description" db:"description"`
 	LiveMode    bool        `json:"liveMode" db:"live_mode"`
 	Nickname    null.String `json:"nickname" db:"nickname"`
 	ProductID   string      `json:"productId" db:"product_id"`
-	Source      Source      `json:"source" db:"source"`
 	UnitAmount  float64     `json:"unitAmount" db:"unit_amount"`
 	CreatedUTC  chrono.Time `json:"createdUtc" db:"created_utc"`
 	CreatedBy   string      `json:"createdBy" db:"created_by"` // Use-facing client should ignore this field.
