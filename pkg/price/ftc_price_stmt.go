eb48618c08e326763129ae0911baef45ad72b860
weiguo.ni <neefrankie@gmail.com>
2021-11-23 16:20:51 +0800

    refactor: add live_mode in where clause when list prices

--- pkg/price/ftc_price_stmt.go
+++ pkg/price/ftc_price_stmt.go
@@ -42,7 +42,9 @@ ORDER BY p.cycle DESC
 `
 
 const StmtListPricesOfProduct = colFtcPrice + `
-WHERE p.product_id = ? AND p.archived = FALSE
+WHERE p.product_id = ? 
+	AND live_mode = ?
+	AND p.archived = FALSE
 ORDER BY p.is_active DESC, p.cycle DESC, p.created_utc DESC
 `
 
