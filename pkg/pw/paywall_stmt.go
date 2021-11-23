612c2fd2e5c3fc1cc4e4f5df9c61506ac265c148
weiguo.ni <neefrankie@gmail.com>
2021-11-23 11:42:02 +0800

    refactor: save/retrieve paywall banner as json

--- pkg/pw/paywall_stmt.go
+++ pkg/pw/paywall_stmt.go
@@ -1,21 +1,21 @@
 package pw
 
-const StmtBanner = `
-SELECT b.id AS banner_id,
-	b.heading AS heading,
-	b.cover_url AS cover_url,
-	b.sub_heading AS sub_heading,
-	b.content AS content,
-	p.id AS promo_id,
-	p.heading AS promo_heading,
-	p.cover_url AS promo_cover_url,
-	p.sub_heading AS promo_sub_heading,
-	p.content AS promo_content,
-	p.terms_conditions AS terms_conditions,
-	p.start_utc AS start_utc,
-	p.end_utc AS end_utc
-FROM subs_product.paywall_banner AS b
-	LEFT JOIN subs_product.paywall_promo AS p
-	ON b.promo_id = p.id
-WHERE b.id = 1
+const StmtInsertPaywallDoc = `
+INSERT INTO subs_product.paywall_doc
+SET daily_banner = :daily_banner,
+	promo_banner = :promo_banner,
+	live_mode = :live_mode,
+	created_utc = :created_utc,
+	created_by = :created_by
+`
+
+const StmtRetrievePaywallDoc = `
+SELECT id,
+	daily_banner,
+	promo_banner,
+	live_mode,
+	created_utc
+FROM subs_product.paywall_doc
+WHERE live_mode = ?
+ORDER BY id
 LIMIT 1`
