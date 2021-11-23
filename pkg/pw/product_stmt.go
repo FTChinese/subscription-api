5273ea69903b179b767e72f97180b45f05904fc9
weiguo.ni <neefrankie@gmail.com>
2021-11-23 16:18:49 +0800

    feat: create, update product

--- pkg/pw/product_stmt.go
+++ pkg/pw/product_stmt.go
@@ -3,11 +3,71 @@ package pw
 const StmtCreateProduct = `
 INSERT INTO subs_product.product
 SET id = :product_id,
-    tier = :tier,
-    heading = :heading,
-    description = :description,
+	description = :description,
+	heading = :heading,
+	is_active = :is_active,
+	live_mode = :live_mode,
     small_print = :small_print,
+	tier = :tier,
     created_by = :created_by,
     created_utc = :created_utc,
     updated_utc = :updated_utc
 `
+
+const colProduct = `
+SELECT product_id,
+	description,
+	heading,
+	is_active,
+	live_mode,
+	small_print,
+	tier,
+	created_by,
+	created_utc,
+	updated_utc
+FROM subs_product.product
+`
+
+// StmtRetrieveProduct refer to paywall_product table.
+const StmtRetrieveProduct = colProduct + `
+WHERE id = ?
+	AND live_mode = ?
+LIMIT 1
+`
+
+const StmtListProducts = colProduct + `
+WHERE live_mode = ?
+ORDER BY is_active DESC, tier ASC, created_utc DESC
+`
+
+const StmtUpdateProduct = `
+UPDATE subs_product.product
+SET heading = :heading,
+    description = :description,
+    small_print = :small_print,
+    updated_utc = :updated_utc
+WHERE id = :product_id
+LIMIT 1
+`
+
+const StmtDeactivateSiblingProducts = `
+UPDATE subs_product.product
+SET is_active = FALSE
+WHERE tier = :tier
+	AND live_mode = :live_mode
+	AND id != :product_id
+`
+
+const StmtActivateProduct = `
+UPDATE subs_product.product
+SET is_active = TRUE
+WHERE id = :product_id
+LIMIT 1
+`
+
+const StmtPutProductOnPaywall = `
+INSERT INTO subs_product.paywall_product
+SET product_id = :product_id,
+	tier = :tier
+ON DUPLICATE KEY UPDATE
+	product_id = :product_id`
