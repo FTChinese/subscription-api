0b843e21bbd97c0b86b724529b9e3e4be1d034a1
weiguo.ni <neefrankie@gmail.com>
2021-11-23 12:07:04 +0800

    refactor: move StmtPaywallProduct to separate file

--- pkg/pw/product_stmt.go
+++ pkg/pw/product_stmt.go
@@ -1,24 +1,5 @@
 package pw
 
-// StmtPaywallProducts retrieves products put on paywall.
-// Current active products under paywall is specified by
-// paywall_product table. By joining it with product table
-// you will retrieve only those put on paywall.
-const StmtPaywallProducts = `
-SELECT prod.id AS product_id,
-	prod.tier,
-	prod.heading,
-	prod.description,
-	prod.small_print,
-	prod.created_by AS created_by,
-    prod.created_utc AS created_utc,
-    prod.updated_utc AS updated_utc
-FROM subs_product.paywall_product AS pp
-	LEFT JOIN subs_product.product AS prod
-	ON pp.product_id = prod.id
-WHERE prod.id IS NOT NULL
-ORDER BY prod.tier ASC`
-
 const StmtCreateProduct = `
 INSERT INTO subs_product.product
 SET id = :product_id,
