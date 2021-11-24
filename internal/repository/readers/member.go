eabb6b678c12d74614fbc3f42e37c6b789792134
weiguo.ni <neefrankie@gmail.com>
2021-11-24 15:06:02 +0800

    refactor: removed ListSnapshot

--- internal/repository/readers/member.go
+++ internal/repository/readers/member.go
@@ -2,8 +2,6 @@ package readers
 
 import (
 	"database/sql"
-	gorest "github.com/FTChinese/go-rest"
-	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 )
 
@@ -66,79 +64,4 @@ func (env Env) VersionMembership(v reader.MembershipVersioned) error {
 	}
 
 	return nil
-}
-
-func (env Env) countMemberSnapshot(ids ids.UserIDs) (int64, error) {
-	var count int64
-	err := env.DBs.Read.Get(
-		&count,
-		reader.StmtCountSnapshot,
-		ids.BuildFindInSet(),
-	)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return count, nil
-}
-
-func (env Env) listMemberSnapshot(ids ids.UserIDs, p gorest.Pagination) ([]reader.MemberSnapshot, error) {
-	var s = make([]reader.MemberSnapshot, 0)
-	err := env.DBs.Read.Select(
-		&s,
-		reader.StmtListSnapshots,
-		ids.BuildFindInSet(),
-		p.Limit,
-		p.Offset(),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return s, nil
-}
-
-func (env Env) ListSnapshot(ids ids.UserIDs, p gorest.Pagination) (reader.SnapshotList, error) {
-	defer env.Logger.Sync()
-	sugar := env.Logger.Sugar()
-
-	countCh := make(chan int64)
-	listCh := make(chan reader.SnapshotList)
-
-	go func() {
-		defer close(countCh)
-		n, err := env.countMemberSnapshot(ids)
-		if err != nil {
-			sugar.Error(err)
-		}
-
-		countCh <- n
-	}()
-
-	go func() {
-		defer close(listCh)
-		s, err := env.listMemberSnapshot(ids, p)
-		if err != nil {
-			sugar.Error(err)
-		}
-		listCh <- reader.SnapshotList{
-			Total:      0,
-			Pagination: gorest.Pagination{},
-			Data:       s,
-			Err:        err,
-		}
-	}()
-
-	count, listResult := <-countCh, <-listCh
-
-	if listResult.Err != nil {
-		return reader.SnapshotList{}, listResult.Err
-	}
-
-	return reader.SnapshotList{
-		Total:      count,
-		Pagination: p,
-		Data:       listResult.Data,
-	}, nil
 }
