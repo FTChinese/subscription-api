a17730548c86fae986191963d003dbaf5b097d1a
weiguo.ni <neefrankie@gmail.com>
2021-12-01 14:54:04 +0800

    feat: YearMonthDayJSON

--- lib/dt/year_month_day.go
+++ lib/dt/year_month_day.go
@@ -1,6 +1,11 @@
 package dt
 
-import "github.com/FTChinese/go-rest/enum"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"github.com/FTChinese/go-rest/enum"
+)
 
 const (
 	daysOfYear  = 366
@@ -70,4 +75,49 @@ func (y YearMonthDay) Add(other YearMonthDay) YearMonthDay {
 	y.Days = y.Days + other.Days
 
 	return y
+}
+
+func (y YearMonthDay) IsZero() bool {
+	return y.Years == 0 && y.Months == 0 && y.Days == 0
+}
+
+type YearMonthDayJSON struct {
+	YearMonthDay
+}
+
+// Value implements Valuer interface by serializing an Invitation into
+// JSON data.
+func (j YearMonthDayJSON) Value() (driver.Value, error) {
+	if j.IsZero() {
+		return nil, nil
+	}
+
+	b, err := json.Marshal(j)
+	if err != nil {
+		return nil, err
+	}
+
+	return string(b), nil
+}
+
+// Scan implements Valuer interface by deserializing an invitation field.
+func (j *YearMonthDayJSON) Scan(src interface{}) error {
+	if src == nil {
+		*j = YearMonthDayJSON{}
+		return nil
+	}
+
+	switch s := src.(type) {
+	case []byte:
+		var tmp YearMonthDayJSON
+		err := json.Unmarshal(s, &tmp)
+		if err != nil {
+			return err
+		}
+		*j = tmp
+		return nil
+
+	default:
+		return errors.New("incompatible type to scan to YearMonthDayJSON")
+	}
 }
