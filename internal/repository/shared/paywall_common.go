2617f5cabbe0c79e6ede105d6e60c1039c671ed5
neefrankie <neefrankie@gmail.com>
2021-12-02 10:55:56 +0800

    feat: PaywallCommon

--- internal/repository/shared/paywall_common.go
+++ internal/repository/shared/paywall_common.go
@@ -1,15 +1,23 @@
-package products
+package shared
 
 import (
+	"database/sql"
+	"github.com/FTChinese/subscription-api/pkg/db"
+	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/FTChinese/subscription-api/pkg/pw"
 	"github.com/patrickmn/go-cache"
 )
 
+type PaywallCommon struct {
+	DBs   db.ReadWriteMyDBs
+	Cache *cache.Cache
+}
+
 // LoadPaywall tries to load paywall from cache.
 // Fallback to db if not found in cache.
-func (env Env) LoadPaywall(live bool) (pw.Paywall, error) {
-	x, found := env.cache.Get(getPaywallCacheKey(live))
+func (env PaywallCommon) LoadPaywall(live bool) (pw.Paywall, error) {
+	x, found := env.Cache.Get(ids.PaywallCacheKey(live))
 
 	// If found in cache, and it can be cast to Paywall, return it;
 	// otherwise retrieve from DB.
@@ -32,7 +40,7 @@ func (env Env) LoadPaywall(live bool) (pw.Paywall, error) {
 
 // retrievePaywall retrieves all elements of paywall concurrently
 // and then build them into a single Paywall instance.
-func (env Env) retrievePaywall(live bool) (pw.Paywall, error) {
+func (env PaywallCommon) retrievePaywall(live bool) (pw.Paywall, error) {
 	pwDocCh, productsCh, plansCh := env.asyncPwDoc(live), env.asyncRetrieveActiveProducts(live), env.asyncListActivePrices(live)
 
 	// Retrieve banner and its promo, products, and each price's plans
@@ -59,11 +67,34 @@ func (env Env) retrievePaywall(live bool) (pw.Paywall, error) {
 }
 
 // cachePaywall caches paywall data after retrieved from db.
-func (env Env) cachePaywall(p pw.Paywall) {
-	env.cache.Set(
-		getPaywallCacheKey(p.LiveMode),
+func (env PaywallCommon) cachePaywall(p pw.Paywall) {
+	env.Cache.Set(
+		ids.PaywallCacheKey(p.LiveMode),
 		p,
 		cache.NoExpiration)
+}
+
+// RetrievePaywallDoc loads the latest row of paywall document.
+func (env PaywallCommon) RetrievePaywallDoc(live bool) (pw.PaywallDoc, error) {
+	var pwb pw.PaywallDoc
+
+	err := env.DBs.Read.Get(
+		&pwb,
+		pw.StmtRetrievePaywallDoc,
+		live)
+
+	if err != nil {
+		if err != sql.ErrNoRows {
+			return pw.PaywallDoc{}, err
+		}
+
+		// No paywall doc exists yet. Returns an empty version.
+		return pw.PaywallDoc{
+			LiveMode: live,
+		}, nil
+	}
+
+	return pwb, nil
 }
 
 type pwDocResult struct {
@@ -71,7 +102,8 @@ type pwDocResult struct {
 	error error
 }
 
-func (env Env) asyncPwDoc(live bool) <-chan pwDocResult {
+// asyncPwDoc loads paywall document in background.
+func (env PaywallCommon) asyncPwDoc(live bool) <-chan pwDocResult {
 	c := make(chan pwDocResult)
 
 	go func() {
@@ -94,10 +126,10 @@ type productsResult struct {
 }
 
 // retrieveActiveProducts retrieve all products present on paywall.
-func (env Env) retrieveActiveProducts(live bool) ([]pw.Product, error) {
+func (env PaywallCommon) retrieveActiveProducts(live bool) ([]pw.Product, error) {
 	var products = make([]pw.Product, 0)
 
-	err := env.dbs.Read.Select(
+	err := env.DBs.Read.Select(
 		&products,
 		pw.StmtPaywallProducts,
 		live)
@@ -110,7 +142,7 @@ func (env Env) retrieveActiveProducts(live bool) ([]pw.Product, error) {
 }
 
 // asyncRetrieveActiveProducts retrieves products in a goroutine.
-func (env Env) asyncRetrieveActiveProducts(live bool) <-chan productsResult {
+func (env PaywallCommon) asyncRetrieveActiveProducts(live bool) <-chan productsResult {
 	ch := make(chan productsResult)
 
 	go func() {
@@ -132,10 +164,10 @@ type activePricesResult struct {
 }
 
 // ListActivePrices lists active prices of products on paywall, directly from DB.
-func (env Env) ListActivePrices(live bool) ([]price.FtcPrice, error) {
+func (env PaywallCommon) ListActivePrices(live bool) ([]price.FtcPrice, error) {
 	var prices = make([]price.FtcPrice, 0)
 
-	err := env.dbs.Read.Select(
+	err := env.DBs.Read.Select(
 		&prices,
 		price.StmtListPaywallPrice,
 		live)
@@ -148,7 +180,7 @@ func (env Env) ListActivePrices(live bool) ([]price.FtcPrice, error) {
 
 // asyncListActivePrices retrieves a list of plans in a goroutine.
 // This is used to construct the paywall data.
-func (env Env) asyncListActivePrices(live bool) <-chan activePricesResult {
+func (env PaywallCommon) asyncListActivePrices(live bool) <-chan activePricesResult {
 	ch := make(chan activePricesResult)
 
 	go func() {
@@ -163,8 +195,4 @@ func (env Env) asyncListActivePrices(live bool) <-chan activePricesResult {
 	}()
 
 	return ch
-}
-
-func (env Env) ClearCache() {
-	env.cache.Flush()
 }
