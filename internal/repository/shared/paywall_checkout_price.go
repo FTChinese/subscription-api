704a25aa56bda71ccea7c724d1f471e4a3959ae6
neefrankie <neefrankie@gmail.com>
2021-12-02 10:55:56 +0800

    feat: PaywallCommon

--- internal/repository/shared/paywall_checkout_price.go
+++ internal/repository/shared/paywall_checkout_price.go
@@ -1,16 +1,32 @@
-package products
+package shared
 
 import (
 	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/guregu/null"
 )
 
+// RetrieveFtcPrice retrieves a single row by plan id.
+func (env PaywallCommon) RetrieveFtcPrice(id string, live bool) (price.FtcPrice, error) {
+	var p price.FtcPrice
+	err := env.DBs.Read.Get(
+		&p,
+		price.StmtFtcPrice,
+		id,
+		live)
+
+	if err != nil {
+		return price.FtcPrice{}, err
+	}
+
+	return p, nil
+}
+
 type asyncPriceResult struct {
 	value price.FtcPrice
 	error error
 }
 
-func (env Env) asyncLoadPrice(id string, live bool) <-chan asyncPriceResult {
+func (env PaywallCommon) asyncLoadPrice(id string, live bool) <-chan asyncPriceResult {
 	c := make(chan asyncPriceResult)
 
 	go func() {
@@ -27,13 +43,27 @@ func (env Env) asyncLoadPrice(id string, live bool) <-chan asyncPriceResult {
 	return c
 }
 
+// LoadDiscount retrieve a single row of discount
+func (env PaywallCommon) LoadDiscount(id string) (price.Discount, error) {
+	var d price.Discount
+	err := env.DBs.Read.Get(
+		&d,
+		price.StmtSelectDiscount,
+		id)
+	if err != nil {
+		return price.Discount{}, err
+	}
+
+	return d, nil
+}
+
 type asyncDiscountResult struct {
 	value price.Discount
 	error error
 }
 
 // load discount asynchronously. If id is empty, returns immediately.
-func (env Env) asyncLoadDiscount(id string) <-chan asyncDiscountResult {
+func (env PaywallCommon) asyncLoadDiscount(id string) <-chan asyncDiscountResult {
 	c := make(chan asyncDiscountResult)
 
 	if id == "" {
@@ -58,7 +88,7 @@ func (env Env) asyncLoadDiscount(id string) <-chan asyncDiscountResult {
 }
 
 // LoadCheckoutItem loads a price and a discount from db.
-func (env Env) LoadCheckoutItem(priceID string, discountID null.String, live bool) (price.CheckoutItem, error) {
+func (env PaywallCommon) LoadCheckoutItem(priceID string, discountID null.String, live bool) (price.CheckoutItem, error) {
 	priceCh, discCh := env.asyncLoadPrice(priceID, live), env.asyncLoadDiscount(discountID.String)
 
 	priceResult, discResult := <-priceCh, <-discCh
