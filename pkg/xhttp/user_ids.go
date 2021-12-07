b479f7737d7d9b0c42531ed2d584d26404611982
weiguo.ni <neefrankie@gmail.com>
2021-12-07 15:36:29 +0800

    feat: UserIDsFromQuery

--- pkg/xhttp/user_ids.go
+++ pkg/xhttp/user_ids.go
@@ -4,15 +4,27 @@ import (
 	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/guregu/null"
 	"net/http"
+	"net/url"
 )
 
-func GetUserIDs(h http.Header) ids.UserIDs {
+func UserIDsFromHeader(h http.Header) ids.UserIDs {
 	ftcID := h.Get(XUserID)
 	unionID := h.Get(XUnionID)
 
 	return ids.UserIDs{
 		CompoundID: "",
 		FtcID:      null.NewString(ftcID, ftcID != ""),
+		UnionID:    null.NewString(unionID, unionID != ""),
+	}.MustNormalize()
+}
+
+func UserIDsFromQuery(v url.Values) ids.UserIDs {
+	ftcId := v.Get("ftc_id")
+	unionID := v.Get("union_id")
+
+	return ids.UserIDs{
+		CompoundID: "",
+		FtcID:      null.NewString(ftcId, ftcId != ""),
 		UnionID:    null.NewString(unionID, unionID != ""),
 	}.MustNormalize()
 }
