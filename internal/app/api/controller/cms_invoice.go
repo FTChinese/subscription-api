719d2adf5f85c10af2cca938b841f3c9d3771fc2
weiguo.ni <neefrankie@gmail.com>
2021-12-07 11:51:49 +0800

    feat: add CreateAddOn method

---
+++ internal/app/api/controller/cms_invoice.go
@@ -0,0 +1,43 @@
+package controller
+
+import (
+	gorest "github.com/FTChinese/go-rest"
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/invoice"
+	"net/http"
+)
+
+// CreateAddOn manually add an addon to a user.
+// This is usually used to perform compensation.
+func (router CMSRouter) CreateAddOn(w http.ResponseWriter, req *http.Request) {
+	defer router.Logger.Sync()
+	sugar := router.Logger.Sugar()
+
+	var params invoice.AddOnParams
+	if err := gorest.ParseJSON(req.Body, &params); err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
+	}
+
+	if ve := params.Validate(); ve != nil {
+		_ = render.New(w).Unprocessable(ve)
+		return
+	}
+
+	inv := invoice.NewAddonInvoice(params)
+
+	result, err := router.Repo.CreateAddOn(inv)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	go func() {
+		err := router.ReaderRepo.ArchiveMember(result.Snapshot)
+		if err != nil {
+			sugar.Error(err)
+		}
+	}()
+
+	_ = render.New(w).OK(result)
+}
