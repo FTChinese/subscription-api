70420f29e9117a31d263ca7338c6eb46163bbda8
weiguo.ni <neefrankie@gmail.com>
2021-12-09 17:55:12 +0800

    refactor: move Archive, Activate, Update to parent

--- pkg/price/ftc_price.go
+++ pkg/price/ftc_price.go
@@ -3,110 +3,15 @@ package price
 import (
 	"errors"
 	"fmt"
-	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/go-rest/render"
-	"github.com/FTChinese/subscription-api/lib/validator"
 	"github.com/FTChinese/subscription-api/pkg/ids"
-	"github.com/guregu/null"
-	"strings"
 )
-
-type FtcPriceUpdateParams struct {
-	Description   null.String `json:"description"`
-	Nickname      null.String `json:"nickname"`
-	StripePriceID string      `json:"stripePriceId"`
-}
-
-func (p FtcPriceUpdateParams) Validate() *render.ValidationError {
-	return validator.New("stripePriceId").Required().Validate(p.StripePriceID)
-}
-
-// FtcPriceParams is the form data submitted to create a price.
-// A new plan is always created under a certain product.
-// Therefore, the input data does not have tier field.
-type FtcPriceParams struct {
-	CreatedBy string `json:"createdBy"`
-	Edition
-	FtcPriceUpdateParams
-	ProductID  string  `json:"productId"`
-	UnitAmount float64 `json:"unitAmount"`
-}
-
-// Validate checks whether the input data to create a new plan is valid.
-// `productTier` is used to specify for which edition of product this plan is created.
-// Premium product is not allowed to have a monthly pricing plan.
-func (p *FtcPriceParams) Validate() *render.ValidationError {
-
-	p.Description.String = strings.TrimSpace(p.Description.String)
-
-	if p.Cycle == enum.CycleNull {
-		return &render.ValidationError{
-			Message: "Invalid cycle",
-			Field:   "cycle",
-			Code:    render.CodeInvalid,
-		}
-	}
-
-	if p.UnitAmount <= 0 {
-		return &render.ValidationError{
-			Message: "Price could not be below 0",
-			Field:   "price",
-			Code:    render.CodeInvalid,
-		}
-	}
-
-	ve := validator.New("productId").Required().Validate(p.ProductID)
-	if ve != nil {
-		return ve
-	}
-
-	return p.FtcPriceUpdateParams.Validate()
-}
 
 // FtcPrice contains a price's original price and promotion.
 // The actual price user paid should be the original price minus
 // promotion offer if promotion period is valid.
 type FtcPrice struct {
 	Price
-	StripePriceID string           `json:"stripePriceId" db:"stripe_price_id"`
-	Offers        DiscountListJSON `json:"offers" db:"discount_list"`
-}
-
-// NewFtcPrice creates a price for ftc product
-func NewFtcPrice(p FtcPriceParams, live bool) FtcPrice {
-
-	return FtcPrice{
-		Price: Price{
-			ID:          ids.PriceID(),
-			Edition:     p.Edition,
-			Active:      false,
-			Archived:    false,
-			Currency:    "cny",
-			Description: p.Description,
-			LiveMode:    live,
-			Nickname:    p.Nickname,
-			ProductID:   p.ProductID,
-			UnitAmount:  p.UnitAmount,
-			CreatedUTC:  chrono.TimeNow(),
-			CreatedBy:   p.CreatedBy,
-		},
-		StripePriceID: p.StripePriceID,
-		Offers:        make([]Discount, 0),
-	}
-}
-
-func (f FtcPrice) Update(p FtcPriceUpdateParams) FtcPrice {
-	f.Description = p.Description
-	f.Nickname = p.Nickname
-	f.StripePriceID = p.StripePriceID
-
-	return f
-}
-
-func (f FtcPrice) Activate() FtcPrice {
-	f.Active = true
-	return f
+	Offers DiscountListJSON `json:"offers" db:"discount_list"`
 }
 
 func (f FtcPrice) SetOffers(o []Discount) FtcPrice {
@@ -122,13 +27,6 @@ func (f FtcPrice) VerifyOffer(o Discount) error {
 	}
 
 	return errors.New("the requested offer is not found")
-}
-
-// Archive put a price into archive and no longer usable.
-// No idea why I created this.
-func (f FtcPrice) Archive() FtcPrice {
-	f.Archived = true
-	return f
 }
 
 // CheckoutItem contains a price user is trying to purchase and optional discount of this price
