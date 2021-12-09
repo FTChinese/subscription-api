42e274c9aa5128208915aae57487d823040af50a
weiguo.ni <neefrankie@gmail.com>
2021-12-09 17:56:25 +0800

    refactor: move Archive, Activate, Update to parent

--- internal/repository/txrepo/price_tx.go
+++ internal/repository/txrepo/price_tx.go
@@ -21,7 +21,7 @@ func NewPriceTx(tx *sqlx.Tx) PriceTx {
 // state simultaneously.
 // The passed-in FtcPrice is the one that should be excluded
 // from deactivating.s
-func (tx PriceTx) DeactivateSiblingPrice(p price.FtcPrice) error {
+func (tx PriceTx) DeactivateSiblingPrice(p price.Price) error {
 	_, err := tx.NamedExec(
 		price.StmtDeactivatePricesOfSameEdition,
 		p)
@@ -36,7 +36,7 @@ func (tx PriceTx) DeactivateSiblingPrice(p price.FtcPrice) error {
 // ActivatePrice turns the is_active state to true for the
 // specified price.
 // It should be called immediately following the above one.
-func (tx PriceTx) ActivatePrice(p price.FtcPrice) error {
+func (tx PriceTx) ActivatePrice(p price.Price) error {
 	_, err := tx.NamedExec(price.StmtActivatePrice, p)
 	if err != nil {
 		return err
@@ -45,7 +45,7 @@ func (tx PriceTx) ActivatePrice(p price.FtcPrice) error {
 	return nil
 }
 
-func (tx PriceTx) ActivatePriceLegacy(p price.FtcPrice) error {
+func (tx PriceTx) ActivatePriceLegacy(p price.Price) error {
 	_, err := tx.NamedExec(price.StmtActivatePriceUnderProduct, p)
 	if err != nil {
 		return err
