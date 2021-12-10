95e2c98bf0d290e870c0e26d957e56fe8c119791
weiguo.ni <neefrankie@gmail.com>
2021-12-10 18:43:29 +0800

    refactor: move DiscountListJSON

--- pkg/price/discount.go
+++ pkg/price/discount.go
@@ -1,16 +1,12 @@
 package price
 
 import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/FTChinese/subscription-api/lib/dt"
 	"github.com/FTChinese/subscription-api/lib/validator"
 	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/guregu/null"
-	"sort"
 	"time"
 )
 
@@ -105,83 +101,4 @@ func (d Discount) IsValid() bool {
 	}
 
 	return true
-}
-
-type DiscountListJSON []Discount
-
-func (l DiscountListJSON) Value() (driver.Value, error) {
-	if len(l) == 0 {
-		return nil, nil
-	}
-	b, err := json.Marshal(l)
-	if err != nil {
-		return nil, err
-	}
-
-	return string(b), nil
-}
-
-func (l *DiscountListJSON) Scan(src interface{}) error {
-	if src == nil {
-		*l = DiscountListJSON{}
-		return nil
-	}
-	switch s := src.(type) {
-	case []byte:
-		var tmp []Discount
-		err := json.Unmarshal(s, &tmp)
-		if err != nil {
-			return err
-		}
-		*l = tmp
-		return nil
-
-	default:
-		return errors.New("incompatible type to scan to DiscountListJSON")
-	}
-}
-
-// FindApplicable select an offer from all Offers that a user eligible for.
-// Filter criteria:
-// * The offer validity period is in effect
-// * User is qualified to enjoy
-// * Lowest price/Highest discount rate.
-// filters - all possible offers a membership currently enjoys, used to narrow down
-// offers provided.
-// If more than one offer is applicable, use the one with the largest price off.
-func (l DiscountListJSON) FindApplicable(filters []OfferKind) Discount {
-	// Filter all valid discount offers.
-	var filtered = make([]Discount, 0)
-	for _, v := range l {
-		if v.IsValid() && v.Kind.ContainedBy(filters) {
-			filtered = append(filtered, v)
-		}
-	}
-
-	switch len(filtered) {
-	case 0:
-		return Discount{}
-	case 1:
-		return filtered[0]
-	default:
-		sort.SliceStable(filtered, func(i, j int) bool {
-			return filtered[i].PriceOff.Float64 > filtered[j].PriceOff.Float64
-		})
-
-		return filtered[0]
-	}
-}
-
-func (l DiscountListJSON) FindValid(id string) (Discount, error) {
-	for _, v := range l {
-		if v.ID == id {
-			if v.IsValid() {
-				return v, nil
-			} else {
-				return Discount{}, errors.New("invalid offer selected")
-			}
-		}
-	}
-
-	return Discount{}, errors.New("the requested offer is not found")
 }
