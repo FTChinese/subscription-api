6a809a6d6fc4145b105b0e4a495155db233ca8b7
weiguo.ni <neefrankie@gmail.com>
2021-12-10 18:44:18 +0800

    feat: ColPaywallPrice

--- pkg/price/price_stmt.go
+++ pkg/price/price_stmt.go
@@ -69,32 +69,3 @@ SET archived = :archived,
 WHERE id = :price_id
 LIMIT 1
 `
-
-const ColPaywallPrice = `
-SELECT p.id AS price_id,
-	p.tier,
-	p.cycle,
-	p.is_active AS is_active,
-	p.archived AS archived,
-	p.currency AS currency,
-	p.kind AS kind,
-	p.live_mode AS live_mode,
-	p.nickname AS nickname,
-	p.period_count AS period_count,
-	p.product_id AS product_id,
-	IFNULL(stripe_price_id, '') AS stripe_price_id,
-	p.title AS title,
-	p.unit_amount AS unit_amount,
-	p.discount_list AS discount_list,
-	p.start_utc,
-	p.end_utc,
-	p.created_utc
-FROM subs_product.price AS p
-`
-
-const StmtListPricesOfProduct = ColPaywallPrice + `
-WHERE p.product_id = ?
-	AND p.live_mode = ?
-	AND p.archived = FALSE
-ORDER BY p.is_active DESC, p.cycle DESC, p.created_utc DESC
-`
