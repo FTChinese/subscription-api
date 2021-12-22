b56aa9e34dc62c55b72e72ddcacacbb28f33a3f6
weiguo.ni <neefrankie@gmail.com>
2021-12-22 14:52:35 +0800

    doc

--- internal/repository/stripeclient/client.go
+++ internal/repository/stripeclient/client.go
@@ -23,6 +23,31 @@ func New(live bool, logger *zap.Logger) Client {
 }
 
 // CreateEphemeralKey generate a key so that client could restrict customer API directly.
+// Response:
+// {
+//  "id": "ephkey_1K9LFkBzTK0hABgJGBr4xtqx",
+//  "object": "ephemeral_key",
+//  "associated_objects": [
+//    {
+//      "type": "customer",
+//      "id": "cus_KoyzEFNfmfdEpK"
+//    }
+//  ],
+//  "created": 1640142752,
+//  "expires": 1640146352,
+//  "livemode": false,
+//  "secret": "ek_test_YWNjdF8xRXBXM0VCelRLMGhBQmdKLHFBYmlRRGUyd1RUN0NMMU0yaHVkSG9PZlEyYjBvREY_00RG22HwXI"
+// }
+// Error response:
+// {
+//    "code": "resource_missing",
+//    "doc_url": "https://stripe.com/docs/error-codes/resource-missing",
+//    "status": 400,
+//    "message": "No such customer: 'cus_IXp31Fk2jYJmU3'",
+//    "param": "customer",
+//    "request_id": "req_bXfWHybYVXFvWj",
+//    "type": "invalid_request_error"
+// }
 func (c Client) CreateEphemeralKey(cusID, version string) ([]byte, error) {
 	params := &stripe.EphemeralKeyParams{
 		Customer:      stripe.String(cusID),
