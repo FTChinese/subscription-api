d29c1e156aef8b5d83f2751fa2b9081c613daf68
weiguo.ni <neefrankie@gmail.com>
2021-12-22 14:52:11 +0800

    change v5 to v6

--- pkg/config/version.go
+++ pkg/config/version.go
@@ -1,6 +1,6 @@
 package config
 
-const Version = "v5"
-const Port = "8205"
+const Version = "v6"
+const Port = "8206"
 const aliWxWebhookBaseURL = "https://www.ftacademy.cn/api/" + Version
 const stripeWebhookKey = "api_keys.stripe_webhook_" + Version
