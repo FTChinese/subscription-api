d0e1a80a25effc30d8c23a1ad8f04080602ce1d2
weiguo.ni <neefrankie@gmail.com>
2021-12-25 23:13:55 +0800

    refactor: redesign Order fields

--- internal/pkg/subs/order_stmt.go
+++ internal/pkg/subs/order_stmt.go
@@ -1,42 +1,73 @@
 package subs
 
-// StmtInsertOrder a new order. The newly created order does not have
+// StmtCreateOrder a new order. The newly created order does not have
 // ConfirmedAt, StartDate and EndDate set.
-const StmtInsertOrder = `
+const StmtCreateOrder = `
 INSERT INTO premium.ftc_trade
 SET trade_no = :order_id,
-	trade_price = :price,
-	trade_amount = :charged_amount,
 	user_id = :compound_id,
 	ftc_user_id = :ftc_id,
 	wx_union_id = :union_id,
-	plan_id = :plan_id,
-	discount_id = :discount_id,
 	tier_to_buy = :tier,
 	billing_cycle = :cycle,
 	category = :kind,
+	trade_price = :original_price,
+	trade_amount = :payable_amount,
 	payment_method = :payment_method,
+	years_count = :years_count,
+	months_count = :months_count,
+	days_count = :days_count,
 	wx_app_id = :wx_app_id,
-	created_utc = UTC_TIMESTAMP()`
+	created_utc = :created_utc
+`
+
+// Split row retrieval into multiple parts to circumvent
+// problematic server setting of not being able to
+// retrieve too much data onece.
+const colsOrderHeader = `
+o.trade_no AS order_id,
+o.user_id AS compound_id,
+o.ftc_user_id AS ftc_id,
+o.wx_union_id AS union_id,
+o.tier_to_buy AS tier,
+o.billing_cycle AS cycle,
+o.category AS kind,
+o.trade_price AS original_price,
+o.trade_amount AS payable_amount
+`
+
+const colsOrderTail = `
+o.payment_method,
+o.years_count,
+o.months_count,
+o.days_count,
+o.wx_app_id,
+o.created_utc,
+o.confirmed_utc,
+o.start_date,
+o.end_date
+`
 
 const StmtOrderCols = `
-SELECT o.trade_no AS order_id,
-	o.trade_price AS price,
-	o.trade_amount AS charged_amount,
-	o.user_id AS compound_id,
-	o.ftc_user_id AS ftc_id,
-	o.wx_union_id AS union_id,
-	IFNULL(o.plan_id, '') AS plan_id,
-	o.discount_id,
-	o.tier_to_buy AS tier,
-	o.billing_cycle AS cycle,
-	o.category AS kind,
-	o.payment_method,
-	o.wx_app_id,
-	o.created_utc,
-	o.confirmed_utc,
-	o.start_date,
-	o.end_date
+SELECT 
+` + colsOrderHeader + `,
+` + colsOrderTail
+
+// StmtOrderHeader retrieves the first half of an order
+// to circumvent the problem of oversize packet transfer between
+// db and server.
+const StmtOrderHeader = `
+SELECT ` + colsOrderHeader + `
+FROM premium.ftc_trade AS o
+WHERE trade_no = ?
+LIMIT 1
+`
+
+const StmtOrderTail = `
+SELECT ` + colsOrderTail + `
+FROM premium.ftc_trade AS o
+WHERE trade_no = ?
+LIMIT 1
 `
 
 const StmtSelectOrder = StmtOrderCols + `
@@ -44,17 +75,6 @@ FROM premium.ftc_trade AS o
 WHERE trade_no = ?
 LIMIT 1
 `
-
-const StmtListOrders = StmtOrderCols + `
-FROM premium.ftc_trade AS o
-WHERE FIND_IN_SET(o.user_id, ?) > 0
-ORDER BY o.created_utc DESC
-LIMIT ? OFFSET ?`
-
-const StmtCountOrders = `
-SELECT COUNT(*) AS row_count
-FROM premium.ftc_trade
-WHERE FIND_IN_SET(user_id, ?) > 0`
 
 // StmtLockOrder locks a row to retrieve onl the minimal fields
 // used to determine whether the order is confirmed.
@@ -70,36 +90,6 @@ WHERE trade_no = ?
 LIMIT 1
 FOR UPDATE`
 
-// StmtOrderHeader retrieves the first half of an order
-// to circumvent the problem of oversize packet transfer between
-// db and server.
-const StmtOrderHeader = `
-SELECT trade_no AS order_id,
-	trade_price AS price,
-	trade_amount AS charged_amount,
-	user_id AS compound_id,
-	ftc_user_id AS ftc_id,
-	wx_union_id AS union_id,
-	IFNULL(plan_id, '') AS plan_id,
-	discount_id,
-	tier_to_buy AS tier,
-	billing_cycle AS cycle
-FROM premium.ftc_trade
-WHERE trade_no = ?
-LIMIT 1`
-
-const StmtOrderTail = `
-SELECT category AS kind,
-	payment_method,
-	wx_app_id,
-	created_utc,
-	confirmed_utc,
-	start_date,
-	end_date
-FROM premium.ftc_trade
-WHERE trade_no = ?
-LIMIT 1`
-
 // StmtConfirmOrder build SQL to set an order confirmed.
 const StmtConfirmOrder = `
 UPDATE premium.ftc_trade
@@ -108,3 +98,14 @@ SET confirmed_utc = :confirmed_utc,
 	end_date = :end_date
 WHERE trade_no = :order_id
 LIMIT 1`
+
+const StmtListOrders = StmtOrderCols + `
+FROM premium.ftc_trade AS o
+WHERE FIND_IN_SET(o.user_id, ?) > 0
+ORDER BY o.created_utc DESC
+LIMIT ? OFFSET ?`
+
+const StmtCountOrders = `
+SELECT COUNT(*) AS row_count
+FROM premium.ftc_trade
+WHERE FIND_IN_SET(user_id, ?) > 0`
