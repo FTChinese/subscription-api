d7d6e3edbfbcb1eab3cacdc323e8002724bc5f8f
weiguo.ni <neefrankie@gmail.com>
2021-12-25 22:54:23 +0800

    refactor: removed AliPride and AmountInCent

--- pkg/price/charge.go
+++ pkg/price/charge.go
@@ -1,9 +1,5 @@
 package price
 
-import (
-	"github.com/FTChinese/subscription-api/pkg/conv"
-)
-
 // Charge represents how much we should ask user to pay in what currency with which discount.
 type Charge struct {
 	// The actual amount payable.
@@ -18,16 +14,4 @@ func NewCharge(price Price, offer Discount) Charge {
 		Amount:   (price.UnitAmount*100 - offer.PriceOff.Float64*100) / 100,
 		Currency: string(price.Currency),
 	}
-}
-
-// AliPrice converts Charged price to ailpay format
-// Deprecated.
-func (c Charge) AliPrice() string {
-	return conv.FormatMoney(c.Amount)
-}
-
-// AmountInCent converts Charged price to int64 in cent for comparison with wx notification.
-// Deprecated
-func (c Charge) AmountInCent() int64 {
-	return int64(c.Amount * 100)
 }
