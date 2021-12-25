381c08a8b527e893316b79144e4bed0d103f0e1d
weiguo.ni <neefrankie@gmail.com>
2021-12-25 23:08:01 +0800

    feat: order query

--- pkg/wechat/order_query.go
+++ pkg/wechat/order_query.go
@@ -1,8 +1,6 @@
 package wechat
 
 import (
-	"github.com/FTChinese/go-rest/render"
-	"github.com/guregu/null"
 	"github.com/objcoding/wxpay"
 )
 
@@ -16,12 +14,40 @@ const (
 	TradeStatePayError = "PAYERROR"
 )
 
+// OrderQueryParams contains parameters required to search an order.
+type OrderQueryParams struct {
+	AppID      string `map:"appid"`          // 微信支付分配的公众账号ID
+	MchID      string `map:"mch_id"`         // 微信支付分配的商户号
+	TxnID      string `map:"transaction_id"` // 微信的订单号，建议优先使用
+	OutTradeNo string `map:"out_trade_no"`   // 商户系统内部订单号
+	Nonce      string `map:"nonce_str"`      // 随机字符串，不长于32位
+	Sign       string `map:"sign"`           // 通过签名算法计算得出的签名值
+	SignType   string `map:"sign_type"`      // 签名类型，目前支持HMAC-SHA256和MD5，默认为MD5
+}
+
+func NewOrderQueryParams(orderID string) OrderQueryParams {
+	return OrderQueryParams{
+		AppID:      "",
+		MchID:      "",
+		TxnID:      "",
+		OutTradeNo: orderID,
+		Nonce:      "",
+		Sign:       "",
+		SignType:   "",
+	}
+}
+
+func (q OrderQueryParams) Marshal() wxpay.Params {
+	return make(wxpay.Params).
+		SetString(keyOrderID, q.OutTradeNo)
+}
+
 // OrderQueryResp for https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_2&index=4
 type OrderQueryResp struct {
-	BaseResp
-	// 以下字段在return_code 和result_code都为SUCCESS的时候有返回
-	OpenID    null.String `db:"open_id"`
-	TradeType null.String `db:"trade_type"` // APP
+	FTCOrderID    string `map:"out_trade_no"`
+	TimeEnd       string `map:"time_end"` // 订单支付时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010
+	TotalFee      int64  `map:"total_fee"`
+	TransactionID string `map:"transaction_id"`
 	// SUCCESS—支付成功
 	// REFUND—转入退款
 	// NOTPAY—未支付
@@ -29,56 +55,17 @@ type OrderQueryResp struct {
 	// REVOKED—已撤销（刷卡支付）
 	// USERPAYING--用户支付中
 	// PAYERROR--支付失败(其他原因，如银行返回失败)
-	// This is `trade_state` field in wechat response.
-	TradeState null.String `db:"trade_state"`
-	// This is the `trade_state_desc` field.
-	TradeStateDesc null.String `db:"trade_state_desc"`
-	BankType       null.String `db:"bank_type"`
-	TotalFee       null.Int    `db:"total_fee"`
-	Currency       null.String `db:"currency"`
-	TransactionID  null.String `db:"transaction_id"`
-	FTCOrderID     null.String `db:"ftc_order_id"`
-	TimeEnd        null.String `db:"time_end"` // 20141030133525
-	Invalid        *render.ValidationError
+	TradeState     string `map:"trade_state"`
+	TradeStateDesc string `map:"trade_state_desc"` //对当前查询订单状态的描述和下一步操作的指引
 }
 
 func NewOrderQueryResp(p wxpay.Params) OrderQueryResp {
-	r := OrderQueryResp{
-		BaseResp: NewBaseResp(p),
+	return OrderQueryResp{
+		FTCOrderID:     GetOrderID(p),
+		TimeEnd:        p.GetString(keyEndTime),
+		TotalFee:       p.GetInt64(keyTotalAmount),
+		TransactionID:  p.GetString(keyTxnID),
+		TradeState:     p.GetString("trade_state"),
+		TradeStateDesc: p.GetString("trade_state_desc"),
 	}
-
-	//r.Populate(p)
-	v, ok := p["openid"]
-	r.OpenID = null.NewString(v, ok)
-
-	v, ok = p["trade_type"]
-	r.TradeType = null.NewString(v, ok)
-
-	v, ok = p["trade_state"]
-	r.TradeState = null.NewString(v, ok)
-
-	v, ok = p["bank_type"]
-	r.BankType = null.NewString(v, ok)
-
-	price := p.GetInt64("total_fee")
-	r.TotalFee = null.NewInt(price, price != 0)
-
-	v, ok = p["fee_type"]
-	r.Currency = null.NewString(v, ok)
-
-	v, ok = p["transaction_id"]
-	r.TransactionID = null.NewString(v, ok)
-
-	v, ok = p["out_trade_no"]
-	r.FTCOrderID = null.NewString(v, ok)
-
-	// 订单支付时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010
-	v, ok = p["time_end"]
-	r.TimeEnd = null.NewString(v, ok)
-
-	// 对当前查询订单状态的描述和下一步操作的指引
-	v, ok = p["trade_state_desc"]
-	r.TradeStateDesc = null.NewString(v, ok)
-
-	return r
 }
