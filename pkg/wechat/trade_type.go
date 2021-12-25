5f43e22276e78a5138b887ee42a3b473d685ab0f
weiguo.ni <neefrankie@gmail.com>
2021-12-25 23:06:21 +0800

    feat: turn trade type to string alias

--- pkg/wechat/trade_type.go
+++ pkg/wechat/trade_type.go
@@ -2,27 +2,11 @@ package wechat
 
 // TradeType is an enum for wechat's various nuance payment
 // channel.
-type TradeType int
+type TradeType string
 
 const (
-	TradeTypeDesktop TradeType = iota
-	TradeTypeMobile
-	TradeTypeJSAPI
-	TradeTypeApp
+	TradeTypeDesktop TradeType = "NATIVE"
+	TradeTypeMobile  TradeType = "MWEB"
+	TradeTypeJSAPI   TradeType = "JSAPI"
+	TradeTypeApp     TradeType = "APP"
 )
-
-// String produces values that can be used in request to wechat.
-func (x TradeType) String() string {
-	names := [...]string{
-		"NATIVE",
-		"MWEB",
-		"JSAPI",
-		"APP",
-	}
-
-	if x < TradeTypeDesktop || x > TradeTypeApp {
-		return ""
-	}
-
-	return names[x]
-}
