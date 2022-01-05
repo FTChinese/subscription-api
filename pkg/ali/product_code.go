0a16d23c8fb585052fdcb3eb9497951483381aa8
weiguo.ni <neefrankie@gmail.com>
2022-01-05 16:00:09 +0800

    refactor: change ProductCode to string

--- pkg/ali/product_code.go
+++ pkg/ali/product_code.go
@@ -1,21 +1,8 @@
 package ali
 
-type ProductCode int
+type ProductCode string
 
 const (
-	ProductCodeWeb ProductCode = iota
-	ProductCodeApp
+	ProductCodeWeb ProductCode = "FAST_INSTANT_TRADE_PAY"
+	ProductCodeApp ProductCode = "QUICK_MSECURITY_PAY"
 )
-
-func (x ProductCode) String() string {
-	names := [...]string{
-		"FAST_INSTANT_TRADE_PAY",
-		"QUICK_MSECURITY_PAY",
-	}
-
-	if x < ProductCodeWeb || x > ProductCodeApp {
-		return ""
-	}
-
-	return names[x]
-}
