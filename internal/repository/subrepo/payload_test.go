8a4de2e8280a10354a41819c64416ccc665f364e
weiguo.ni <neefrankie@gmail.com>
2022-01-06 12:56:59 +0800

    test

--- internal/repository/subrepo/payload_test.go
+++ internal/repository/subrepo/payload_test.go
@@ -1,9 +1,11 @@
 package subrepo
 
 import (
+	"github.com/FTChinese/subscription-api/pkg/ali"
 	"github.com/FTChinese/subscription-api/pkg/db"
 	"github.com/FTChinese/subscription-api/pkg/wechat"
 	"github.com/FTChinese/subscription-api/test"
+	"github.com/smartwalle/alipay"
 	"go.uber.org/zap/zaptest"
 	"testing"
 )
@@ -38,6 +40,37 @@ func TestEnv_SaveWxPayload(t *testing.T) {
 			}
 
 			t.Logf("Tests run")
+		})
+	}
+}
+
+func TestEnv_SaveAliWebhookPayload(t *testing.T) {
+
+	env := New(db.MockMySQL(), zaptest.NewLogger(t))
+
+	type args struct {
+		p ali.WebhookPayload
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save webhook payload",
+			args: args{
+				p: ali.NewWebhookPayload(
+					&alipay.TradeNotification{}),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if err := env.SaveAliWebhookPayload(tt.args.p); (err != nil) != tt.wantErr {
+				t.Errorf("SaveAliWebhookPayload() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
