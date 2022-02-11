70e979838d1506ab4df1a0cdfee1fb6c7ebbe75c
weiguo.ni <neefrankie@gmail.com>
2022-02-11 16:06:24 +0800

    feat: save payment intent

---
+++ internal/pkg/stripe/payment_intent_stmt.go
@@ -0,0 +1,25 @@
+package stripe
+
+const colUpsertPI = `
+amount = :amount,
+amount_received = :amount_received,
+canceled_at = :canceled_at,
+cancellation_reason = :cancellation_reason,
+client_secret = :client_secret,
+created = :created,
+currency = :currency,
+customer_id = :customer_id,
+invoice_id = :invoice_id,
+live_mode = :live_mode,
+next_action = :next_action,
+payment_method_id = :payment_method_id,
+payment_method_types = :payment_method_types,
+receipt_email = :receipt_email,
+setup_future_usage = :setup_future_usage
+`
+const StmtInsertPaymentIntent = `
+INSERT INTO premium.stripe_payment_intent
+SET id = :id,
+` + colUpsertPI + `
+ON DUPLICATE KEY UPDATE
+` + colUpsertPI
