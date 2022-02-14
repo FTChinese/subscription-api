60e53436d9c8f9759cdd1e804db5b10412740ae8
weiguo.ni <neefrankie@gmail.com>
2022-02-14 10:56:30 +0800

    feat: paged list

---
+++ internal/pkg/paged_list.go
@@ -0,0 +1,10 @@
+package pkg
+
+import "github.com/FTChinese/go-rest"
+
+// PagedList is used as the bases to show a list of items with pagination support.
+type PagedList struct {
+	Total int64 `json:"total" db:"row_count"`
+	gorest.Pagination
+	Err error `json:"-"`
+}
