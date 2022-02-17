20643c0380293ac2e7bd70acb6ecc26a8b49e3fb
weiguo.ni <neefrankie@gmail.com>
2022-02-17 12:18:24 +0800

    refactor: removed ListPaymentMethods

--- internal/repository/stripe_payment_method.go
+++ internal/repository/stripe_payment_method.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	gorest "github.com/FTChinese/go-rest"
-	"github.com/FTChinese/subscription-api/internal/pkg"
 	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
 )
 
@@ -33,80 +31,4 @@ func (repo StripeRepo) RetrievePaymentMethod(id string) (stripe.PaymentMethod, e
 	}
 
 	return pm, nil
-}
-
-func (repo StripeRepo) listPaymentMethods(cusID string, page gorest.Pagination) ([]stripe.PaymentMethod, error) {
-	var paymentMethods = make([]stripe.PaymentMethod, 0)
-
-	err := repo.dbs.Read.Select(
-		&paymentMethods,
-		stripe.StmtListPaymentMethods,
-		cusID,
-		page.Limit,
-		page.Offset())
-
-	if err != nil {
-		return nil, err
-	}
-
-	return paymentMethods, nil
-}
-
-func (repo StripeRepo) countPaymentMethods(cusID string) (int64, error) {
-	var total int64
-	err := repo.dbs.Read.Get(
-		&total,
-		cusID)
-
-	if err != nil {
-		return -1, err
-	}
-
-	return total, nil
-}
-
-func (repo StripeRepo) ListPaymentMethods(cusID string, page gorest.Pagination) (stripe.PagedPaymentMethods, error) {
-	defer repo.Logger.Sync()
-	sugar := repo.Logger.Sugar()
-
-	countCh := make(chan int64)
-	listCh := make(chan stripe.PagedPaymentMethods)
-
-	go func() {
-		defer close(countCh)
-		n, err := repo.countPaymentMethods(cusID)
-		if err != nil {
-			sugar.Error(err)
-		}
-
-		countCh <- n
-	}()
-
-	go func() {
-		defer close(listCh)
-		methods, err := repo.listPaymentMethods(cusID, page)
-
-		listCh <- stripe.PagedPaymentMethods{
-			PagedList: pkg.PagedList{
-				Total:      0,
-				Pagination: gorest.Pagination{},
-				Err:        err,
-			},
-			Data: methods,
-		}
-	}()
-
-	count, listRes := <-countCh, <-listCh
-
-	if listRes.Err != nil {
-		return stripe.PagedPaymentMethods{}, listRes.Err
-	}
-
-	listRes.PagedList = pkg.PagedList{
-		Total:      count,
-		Pagination: page,
-		Err:        nil,
-	}
-
-	return listRes, nil
 }
