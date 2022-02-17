fb42d9e6c101169b6aadb6ba0d40574d9f76cf8c
weiguo.ni <neefrankie@gmail.com>
2022-02-17 12:17:12 +0800

    feat: add LoadOrFetchPaymentMethod

--- internal/repository/stripeenv/payment_method.go
+++ internal/repository/stripeenv/payment_method.go
@@ -19,3 +19,23 @@ func (env Env) FetchAndSavePaymentMethod(pmID string) (stripe.PaymentMethod, err
 
 	return pm, nil
 }
+
+// LoadOrFetchPaymentMethod retrieve a payment method from
+// db, and if not found, fallback to stripe API.
+func (env Env) LoadOrFetchPaymentMethod(id string) (stripe.PaymentMethod, error) {
+	defer env.Logger.Sync()
+	sugar := env.Logger.Sugar()
+
+	pi, err := env.RetrievePaymentMethod(id)
+	if err == nil {
+		return pi, nil
+	}
+	sugar.Error(err)
+
+	rawPM, err := env.Client.FetchPaymentMethod(id)
+	if err != nil {
+		return stripe.PaymentMethod{}, err
+	}
+
+	return stripe.NewPaymentMethod(rawPM), nil
+}
