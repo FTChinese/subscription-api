06b2363fe6b98ed7132753b09057637fa5b79a27
weiguo.ni <neefrankie@gmail.com>
2022-02-17 12:19:38 +0800

    refactor: removed getPaymentMethod

--- internal/app/api/stripe_payment_method.go
+++ internal/app/api/stripe_payment_method.go
@@ -3,27 +3,42 @@ package api
 import (
 	gorest "github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/internal/pkg/input"
 	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
 	"github.com/FTChinese/subscription-api/pkg/xhttp"
 	"net/http"
 )
 
-func (router StripeRouter) getPaymentMethod(id string) (stripe.PaymentMethod, error) {
-	defer router.Logger.Sync()
-	sugar := router.Logger.Sugar()
-
-	pi, err := router.Env.RetrievePaymentMethod(id)
-	if err == nil {
-		return pi, nil
+// UpsertPaymentMethod insert or update a new payment method
+// to our db as requested by client.
+// Used when a new payment method is created on the client
+// but the server on our side has no idea it exists.
+func (router StripeRouter) UpsertPaymentMethod(w http.ResponseWriter, req *http.Request) {
+	var pmIDParam input.IDParam
+	if err := gorest.ParseJSON(req.Body, &pmIDParam); err != nil {
+		_ = render.New(w).BadRequest(err.Error())
+		return
 	}
-	sugar.Error(err)
 
-	rawPM, err := router.Env.Client.FetchPaymentMethod(id)
+	if ve := pmIDParam.Validate(); ve != nil {
+		_ = render.New(w).Unprocessable(ve)
+		return
+	}
+
+	rawPM, err := router.Env.Client.FetchPaymentMethod(pmIDParam.ID)
 	if err != nil {
-		return stripe.PaymentMethod{}, err
+		_ = xhttp.HandleStripeErr(w, err)
+		return
 	}
 
-	return stripe.NewPaymentMethod(rawPM), nil
+	pm := stripe.NewPaymentMethod(rawPM)
+	err = router.Env.UpsertPaymentMethod(pm)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	_ = render.New(w).OK(pm)
 }
 
 func (router StripeRouter) LoadPaymentMethod(w http.ResponseWriter, req *http.Request) {
@@ -38,7 +53,7 @@ func (router StripeRouter) LoadPaymentMethod(w http.ResponseWriter, req *http.Re
 		return
 	}
 
-	pm, err := router.getPaymentMethod(pmID)
+	pm, err := router.Env.LoadOrFetchPaymentMethod(pmID)
 	if err != nil {
 		sugar.Error(err)
 		_ = xhttp.HandleStripeErr(w, err)
@@ -55,31 +70,4 @@ func (router StripeRouter) LoadPaymentMethod(w http.ResponseWriter, req *http.Re
 	}
 
 	_ = render.New(w).OK(pm)
-}
-
-func (router StripeRouter) ListPaymentMethods(w http.ResponseWriter, req *http.Request) {
-	defer router.Logger.Sync()
-	sugar := router.Logger.Sugar()
-
-	ftcID := xhttp.GetFtcID(req.Header)
-	p := gorest.GetPagination(req)
-
-	baseAccount, err := router.ReaderRepo.BaseAccountByUUID(ftcID)
-	if err != nil {
-		sugar.Error(err)
-		_ = render.New(w).DBError(err)
-		return
-	}
-	if baseAccount.StripeID.IsZero() {
-		_ = render.New(w).NotFound("Stripe customer not found")
-		return
-	}
-
-	list, err := router.Env.ListPaymentMethods(baseAccount.StripeID.String, p)
-	if err != nil {
-		_ = render.New(w).DBError(err)
-		return
-	}
-
-	_ = render.New(w).OK(list)
 }
