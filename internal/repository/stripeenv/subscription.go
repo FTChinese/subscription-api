45a316ce549ef0902e57349bb68b1d3897337f36
weiguo.ni <neefrankie@gmail.com>
2022-02-24 16:41:45 +0800

    feat: LoadOrFetchSubs

--- internal/repository/stripeenv/subscription.go
+++ internal/repository/stripeenv/subscription.go
@@ -1,1 +1,19 @@
 package stripeenv
+
+import "github.com/FTChinese/subscription-api/internal/pkg/stripe"
+
+func (env Env) LoadOrFetchSubs(id string, refresh bool) (stripe.Subs, error) {
+	if !refresh {
+		subs, err := env.RetrieveSubs(id)
+		if err == nil {
+			return subs, nil
+		}
+	}
+
+	rawSubs, err := env.Client.FetchSubs(id, false)
+	if err != nil {
+		return stripe.Subs{}, err
+	}
+
+	return stripe.NewSubs("", rawSubs), nil
+}
