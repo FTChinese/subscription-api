735cb718a07dc816b39245da72eace9ca3da5fb9
weiguo.ni <neefrankie@gmail.com>
2022-04-08 17:40:29 +0800

    feat: PaymentShared features

---
+++ internal/app/api/payment_shared.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"github.com/FTChinese/subscription-api/internal/repository"
+	"github.com/FTChinese/subscription-api/internal/repository/stripeenv"
+	"github.com/FTChinese/subscription-api/internal/stripeclient"
+	"github.com/FTChinese/subscription-api/pkg/db"
+	"github.com/FTChinese/subscription-api/pkg/pw"
+	"github.com/patrickmn/go-cache"
+	"go.uber.org/zap"
+)
+
+type PaymentShared struct {
+	stripeRepo  stripeenv.Env
+	paywallRepo repository.PaywallRepo
+	cacheRepo   repository.CacheRepo
+	logger      *zap.Logger
+	live        bool
+}
+
+func NewPaymentShared(
+	dbs db.ReadWriteMyDBs,
+	c *cache.Cache,
+	logger *zap.Logger,
+	live bool,
+) PaymentShared {
+	return PaymentShared{
+		stripeRepo: stripeenv.New(
+			stripeclient.New(live, logger),
+			repository.NewStripeRepo(dbs, logger),
+		),
+		paywallRepo: repository.NewPaywallRepo(dbs),
+		cacheRepo:   repository.NewCacheRepo(c),
+		logger:      logger,
+		live:        live,
+	}
+}
+
+// LoadPaywall directly from database or Stripe API.
+func (ps PaymentShared) LoadPaywall() (pw.Paywall, error) {
+	defer ps.logger.Sync()
+	sugar := ps.logger.Sugar()
+
+	paywall, err := ps.paywallRepo.RetrievePaywall(ps.live)
+	if err != nil {
+		sugar.Error(err)
+		return pw.Paywall{}, err
+	}
+
+	stripeIDs := paywall.StripePriceIDs()
+	stripePrices, err := ps.stripeRepo.ListPrices(stripeIDs)
+	if err != nil {
+		return pw.Paywall{}, err
+	}
+
+	paywall.StripePrices = stripePrices
+
+	return paywall, nil
+}
+
+func (ps PaymentShared) LoadCachedPaywall(bust bool) (pw.Paywall, error) {
+	if !bust {
+		paywall, err := ps.cacheRepo.LoadPaywall(ps.live)
+		if err == nil {
+			return paywall, nil
+		}
+	}
+
+	paywall, err := ps.LoadPaywall()
+	if err != nil {
+		return pw.Paywall{}, err
+	}
+
+	ps.cacheRepo.CachePaywall(paywall, ps.live)
+
+	return paywall, nil
+}
