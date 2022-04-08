a61c1508bd0564037d7feaa9f66d965610b4283c
weiguo.ni <neefrankie@gmail.com>
2022-04-08 17:35:46 +0800

    feat: upsert stripe prices

---
+++ internal/repository/stripe_price.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
+	"github.com/FTChinese/subscription-api/pkg/price"
+)
+
+func (repo StripeRepo) UpsertPrice(p price.StripePrice) error {
+	_, err := repo.dbs.Write.NamedExec(
+		stripe.StmtUpsertPrice,
+		p)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repo StripeRepo) RetrievePrice(id string) (price.StripePrice, error) {
+	var row stripe.PriceRow
+
+	err := repo.dbs.Read.Get(
+		&row,
+		stripe.StmtRetrievePrice,
+		id)
+
+	if err != nil {
+		return price.StripePrice{}, err
+	}
+
+	return row.Data.StripePrice, nil
+}
