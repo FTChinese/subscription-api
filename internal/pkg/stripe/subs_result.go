c4adcb82dd358a4526cfec020e52b384b6a74346
weiguo.ni <neefrankie@gmail.com>
2022-04-08 17:35:08 +0800

    refactor: subs result building

--- internal/pkg/stripe/subs_result.go
+++ internal/pkg/stripe/subs_result.go
@@ -4,22 +4,30 @@ import (
 	"github.com/FTChinese/subscription-api/pkg/addon"
 	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/FTChinese/subscription-api/pkg/invoice"
+	"github.com/FTChinese/subscription-api/pkg/pw"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/stripe/stripe-go/v72"
 )
 
+type SubsError struct {
+	Membership reader.Membership
+	Raw        error
+}
+
+func (s SubsError) Error() string {
+	return s.Raw.Error()
+}
+
 // SubsResultParams uses the data of a user's subscription to build the data to be saved to db.
 type SubsResultParams struct {
-	UserIDs ids.UserIDs // UserIDs might comes from user account, or from current membership for refreshing.
-	Kind    reader.SubsKind
-	// To build membership, the above three fields are enough.
-
+	UserIDs       ids.UserIDs // UserIDs might comes from user account, or from current membership for refreshing.
+	Kind          reader.SubsKind
 	CurrentMember reader.Membership    // Used for backup.
 	Action        reader.ArchiveAction // Who performed the backup.
 }
 
-// SubsResult contains the data to save to db.
-type SubsResult struct {
+// SubsSuccess contains the data to save to db.
+type SubsSuccess struct {
 	Modified         bool                       `json:"-"` // Indicate whether membership actually modified.
 	Subs             Subs                       `json:"subs"`
 	Member           reader.Membership          `json:"membership"` // New membership.
@@ -27,18 +35,31 @@ type SubsResult struct {
 	CarryOverInvoice invoice.Invoice            `json:"-"` // Only exists when user shifting from one-time purchase to Stripe. It does not exists upon refreshing/upgrading/cancellation.
 }
 
-func NewSubsResult(ss *stripe.Subscription, params SubsResultParams) SubsResult {
+func NewSubsResult(ss *stripe.Subscription, params SubsResultParams) SubsSuccess {
 	return newSubsResult(
 		NewSubs(params.UserIDs.FtcID.String, ss),
 		params,
 	)
 }
 
+func NewSubsCreated(cart pw.ShoppingCart, ss *stripe.Subscription) SubsSuccess {
+	subs := NewSubs(cart.Account.FtcID, ss)
+
+	return newSubsResult(
+		subs,
+		SubsResultParams{
+			UserIDs:       cart.Account.CompoundIDs(),
+			Kind:          cart.Intent.Kind,
+			CurrentMember: cart.CurrentMember,
+			Action:        reader.ArchiveActionCreate,
+		})
+}
+
 // newSubsResult exists for testing convenience.
-func newSubsResult(subs Subs, params SubsResultParams) SubsResult {
+func newSubsResult(subs Subs, params SubsResultParams) SubsSuccess {
 
 	var inv invoice.Invoice
-	if params.Kind == reader.SubsKindOneTimeToSub {
+	if params.Kind == reader.SubsKindOneTimeToAutoRenew {
 		inv = params.
 			CurrentMember.
 			CarryOverInvoice().
@@ -67,7 +88,7 @@ func newSubsResult(subs Subs, params SubsResultParams) SubsResult {
 		versioned = versioned.WithPriorVersion(params.CurrentMember)
 	}
 
-	return SubsResult{
+	return SubsSuccess{
 		Modified:         isModified,
 		Subs:             subs,
 		Member:           newMmb,
