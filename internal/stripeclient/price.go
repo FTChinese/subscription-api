599ec048865b130a30756c07e827a8748f9cbc9d
weiguo.ni <neefrankie@gmail.com>
2022-04-13 17:17:54 +0800

    feat: FetchPricesOf

--- internal/stripeclient/price.go
+++ internal/stripeclient/price.go
@@ -1,7 +1,16 @@
 package stripeclient
 
 import (
+	"context"
+	"github.com/FTChinese/subscription-api/pkg/price"
 	stripeSdk "github.com/stripe/stripe-go/v72"
+	"golang.org/x/sync/semaphore"
+	"runtime"
+)
+
+var (
+	maxWorkers = runtime.GOMAXPROCS(0)
+	sem        = semaphore.NewWeighted(int64(maxWorkers))
 )
 
 func (c Client) ListPrices() ([]*stripeSdk.Price, error) {
@@ -26,6 +35,44 @@ func (c Client) ListPrices() ([]*stripeSdk.Price, error) {
 // says no parameter is required.
 func (c Client) FetchPrice(id string) (*stripeSdk.Price, error) {
 	return c.sc.Prices.Get(id, nil)
+}
+
+func (c Client) FetchPricesOf(ids []string) (map[string]price.StripePrice, error) {
+	defer c.logger.Sync()
+	sugar := c.logger.Sugar()
+	ctx := context.Background()
+
+	var prices = make(map[string]price.StripePrice)
+	var anyErr error
+
+	for _, id := range ids {
+		if err := sem.Acquire(ctx, 1); err != nil {
+			sugar.Errorf("Failed to acquire semaphore: %v", err)
+			break
+		}
+
+		go func(id string) {
+			p, err := c.FetchPrice(id)
+			if err != nil {
+				sugar.Error(err)
+				anyErr = err
+			} else {
+				prices[id] = price.NewStripePrice(p)
+			}
+			sem.Release(1)
+		}(id)
+	}
+
+	if anyErr != nil {
+		return nil, anyErr
+	}
+
+	if err := sem.Acquire(ctx, int64(maxWorkers)); err != nil {
+		sugar.Error(err)
+		return nil, err
+	}
+
+	return prices, nil
 }
 
 func (c Client) SetPriceMeta(id string, meta map[string]string) (*stripeSdk.Price, error) {
