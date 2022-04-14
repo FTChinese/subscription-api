033769415cc0b68333ca7db3d3b89a63edaafd3f
weiguo.ni <neefrankie@gmail.com>
2022-04-14 17:01:23 +0800

    feat: InvoiceHasCoupon

---
+++ internal/app/api/stripe_invoice.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/subscription-api/pkg/xhttp"
+	"net/http"
+)
+
+// InvoiceHasCoupon checks if an invoice has coupon applied.
+// An invoice could only use one coupon.
+func (router StripeRouter) InvoiceHasCoupon(w http.ResponseWriter, req *http.Request) {
+	invID, _ := xhttp.GetURLParam(req, "id").ToString()
+
+	ok, err := router.stripeRepo.InvoiceHasCouponApplied(invID)
+	if err != nil {
+		_ = render.New(w).DBError(err)
+		return
+	}
+
+	_ = render.New(w).OK(map[string]bool{
+		"exists": ok,
+	})
+}
