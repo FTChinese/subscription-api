9356f3c7b02339dcb79573a5eab1cf54116d7348
weiguo.ni <neefrankie@gmail.com>
2022-04-14 16:59:08 +0800

    feat: coupon sql

--- internal/pkg/stripe/coupon_redeemed.go
+++ internal/pkg/stripe/coupon_redeemed.go
@@ -1,12 +1,40 @@
 package stripe
 
-import "github.com/FTChinese/go-rest/chrono"
+import (
+	"github.com/FTChinese/go-rest/chrono"
+)
 
+// CouponRedeemed records which invoice has redeemed
+// a coupon.
+// One an invoice has any coupon applied to it, it can
+// no longer use any others.
 type CouponRedeemed struct {
-	FtcID       string      `db:"ftc_id"`
-	SubsID      string      `db:"subs_id"`
 	InvoiceID   string      `db:"invoice_id"`
+	FtcID       string      `db:"ftc_user_id"`
+	SubsID      string      `db:"subs_id"`
 	CouponID    string      `db:"coupon_id"`
 	CreatedUTC  chrono.Time `db:"created_utc"`
-	RedeemedUTC chrono.Time `db:"redeemed_tc"`
+	RedeemedUTC chrono.Time `db:"redeemed_utc"`
 }
+
+func (c CouponRedeemed) IsZero() bool {
+	return c.InvoiceID == ""
+}
+
+const StmtInsertCouponRedeemed = `
+INSERT INTO premium.stripe_coupon_redeemed
+SET invoice_id = :invoice_id,
+	ftc_user_id = :ftc_user_id,
+	subs_id = :subs_id,
+	coupon_id = :coupon_id,
+	created_utc = :created_utc,
+	redeemed_utc = :redeemed_utc
+`
+
+const StmtInvoiceHasCoupon = `
+SELECT EXISTS (
+	SELECT invoice_id
+	FROM premium.stripe_coupon_redeemed
+	WHERE invoice_id = ?
+) AS alreadyExists
+`
