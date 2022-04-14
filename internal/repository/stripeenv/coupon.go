a1d560addb3cfbedeea282faf2ab929424353f64
weiguo.ni <neefrankie@gmail.com>
2022-04-14 13:33:02 +0800

    feat: load and modify coupon

---
+++ internal/repository/stripeenv/coupon.go
@@ -0,0 +1,43 @@
+package stripeenv
+
+import "github.com/FTChinese/subscription-api/pkg/price"
+
+func (env Env) LoadOrFetchCoupon(id string, refresh bool) (price.StripeCoupon, error) {
+	defer env.Logger.Sync()
+	sugar := env.Logger.Sugar()
+
+	if !refresh {
+		c, err := env.RetrieveCoupon(id)
+		if err == nil {
+			return c, nil
+		}
+
+		sugar.Error(err)
+	}
+
+	rawCoupon, err := env.Client.FetchCoupon(id)
+	if err != nil {
+		return price.StripeCoupon{}, err
+	}
+
+	return price.NewStripeCoupon(rawCoupon), nil
+}
+
+func (env Env) ModifyCoupon(id string, params price.StripeCouponMeta) (price.StripeCoupon, error) {
+	defer env.Logger.Sync()
+	sugar := env.Logger.Sugar()
+
+	rawCoupon, err := env.Client.UpdateCoupon(id, params.ToMap())
+	if err != nil {
+		return price.StripeCoupon{}, err
+	}
+
+	coupon := price.NewStripeCoupon(rawCoupon)
+
+	err = env.UpsertCoupon(coupon)
+	if err != nil {
+		sugar.Error(err)
+	}
+
+	return coupon, nil
+}
