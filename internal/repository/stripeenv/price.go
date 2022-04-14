abec5857cf89f987c85c3e81c9b482aa9e5f32b3
weiguo.ni <neefrankie@gmail.com>
2022-04-14 13:33:57 +0800

    refactor

--- internal/repository/stripeenv/price.go
+++ internal/repository/stripeenv/price.go
@@ -1,17 +1,7 @@
 package stripeenv
 
 import (
-	"context"
-	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
 	"github.com/FTChinese/subscription-api/pkg/price"
-	"github.com/FTChinese/subscription-api/pkg/reader"
-	"golang.org/x/sync/semaphore"
-	"runtime"
-)
-
-var (
-	maxWorkers = runtime.GOMAXPROCS(0)
-	sem        = semaphore.NewWeighted(int64(maxWorkers))
 )
 
 // LoadOrFetchPrice tris to retrieve a price from
@@ -34,79 +24,7 @@ func (env Env) LoadOrFetchPrice(id string, refresh bool) (price.StripePrice, err
 		return price.StripePrice{}, err
 	}
 
-	return price.NewPrice(rawPrice), nil
-}
-
-func (env Env) LoadCachedPrice(id string, refresh bool) (price.StripePrice, error) {
-	defer env.Logger.Sugar()
-	sugar := env.Logger.Sugar()
-
-	if !refresh {
-		sugar.Infof("Loading stripe price %s from cache", id)
-		p, ok := env.Cache.Find(id)
-		if ok {
-			sugar.Infof("Cached stripe price %s found", id)
-			return p, nil
-		}
-	}
-
-	p, err := env.LoadOrFetchPrice(id, refresh)
-	if err != nil {
-		sugar.Error(err)
-		return price.StripePrice{}, err
-	}
-
-	env.Cache.Upsert(p)
-
-	if p.IsFromStripe {
-		go func() {
-			err := env.UpsertPrice(p)
-			if err != nil {
-				sugar.Error(err)
-			}
-		}()
-	}
-
-	return p, nil
-}
-
-// ListPrices loads all stripe prices for a list of ids.
-func (env Env) ListPrices(ids []string, refresh bool) (map[string]price.StripePrice, error) {
-	defer env.Logger.Sync()
-	sugar := env.Logger.Sugar()
-	ctx := context.Background()
-
-	var prices = make(map[string]price.StripePrice)
-	var anyErr error
-
-	for _, id := range ids {
-		if err := sem.Acquire(ctx, 1); err != nil {
-			sugar.Errorf("Failed to acquire semaphore: %v", err)
-			break
-		}
-
-		go func(id string) {
-			p, err := env.LoadOrFetchPrice(id, refresh)
-			if err != nil {
-				sugar.Error(err)
-				anyErr = err
-			} else {
-				prices[id] = p
-			}
-			sem.Release(1)
-		}(id)
-	}
-
-	if anyErr != nil {
-		return nil, anyErr
-	}
-
-	if err := sem.Acquire(ctx, int64(maxWorkers)); err != nil {
-		sugar.Error(err)
-		return nil, err
-	}
-
-	return prices, nil
+	return price.NewStripePrice(rawPrice), nil
 }
 
 // ListPricesCompat fetches stripe prices from cache or
@@ -127,25 +45,4 @@ func (env Env) ListPricesCompat(live bool, refresh bool) ([]price.StripePrice, e
 
 	return env.Cache.
 		List(live), nil
-}
-
-// LoadCheckoutItem from database, or from Stripe API if not found in database.
-func (env Env) LoadCheckoutItem(params stripe.SubsParams) (reader.CartItemStripe, error) {
-	recurring, err := env.LoadOrFetchPrice(params.PriceID, false)
-	if err != nil {
-		return reader.CartItemStripe{}, err
-	}
-
-	var introPrice price.StripePrice
-	if params.IntroductoryPriceID.Valid {
-		introPrice, err = env.LoadOrFetchPrice(params.IntroductoryPriceID.String, false)
-		if err != nil {
-			return reader.CartItemStripe{}, err
-		}
-	}
-
-	return reader.CartItemStripe{
-		Recurring:    recurring,
-		Introductory: introPrice,
-	}, nil
 }
