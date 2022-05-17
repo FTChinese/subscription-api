b1ee86d4c2a5b7499061fca3b75435eeb1dca161
weiguo.ni <neefrankie@gmail.com>
2022-05-17 17:24:17 +0800

    feat: HexStringSum

---
+++ pkg/codec/hex.go
@@ -0,0 +1,15 @@
+package codec
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// HexStringSum returns a hexdecimal string of a string's md5 checksum.
+// You are recommended to use VARBINARY(16) to save it in SQL.
+// Two hex chars requires 8-bit (1 byte).
+// 128-bits md5 checksum requires 128/8 = 16 bytes.
+func HexStringSum(s string) string {
+	h := md5.Sum([]byte(s))
+	return hex.EncodeToString(h[:])
+}
