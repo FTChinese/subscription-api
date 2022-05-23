8bed28e3babcfaf9a5aebd4611887b341bccf7e8
weiguo.ni <neefrankie@gmail.com>
2022-05-23 15:15:34 +0800

    refactor: plan table

--- pkg/price/plan_stmt.go
+++ pkg/price/plan_stmt.go
@@ -1,18 +1,20 @@
 package price
 
+// StmtCreatePlan is used sync price table with legacy plan
+// table. Do not remove this unless API v5 is removed.
 const StmtCreatePlan = `
 INSERT INTO subs_product.plan
 SET id = :id,
 	tier = :tier,
 	cycle = :cycle,
-	is_active = :active,
+	is_active = :is_active,
 	archived = :archived,
 	currency = :currency,
-	description = :description,
 	live_mode = :live_mode,
 	nickname = :nickname,
 	product_id = :product_id,
 	stripe_price_id = :stripe_price_id,
+	description = :title,
 	price = :unit_amount,
 	created_by = "",
 	created_utc = :created_utc
