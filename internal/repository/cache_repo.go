a3e1de18479d7d0c5e5b8f888a377d2e4ef68207
weiguo.ni <neefrankie@gmail.com>
2022-06-20 18:04:01 +0800

    feat: cache android latest release

--- internal/repository/cache_repo.go
+++ internal/repository/cache_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"github.com/FTChinese/subscription-api/internal/pkg/android"
 	"github.com/FTChinese/subscription-api/pkg/ids"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 	"github.com/patrickmn/go-cache"
@@ -10,6 +11,8 @@ import (
 func paywallCacheKey(live bool) string {
 	return "paywall_" + ids.GetBoolKey(live)
 }
+
+const cacheKeyAndroidLatest = "android_latest_release"
 
 type CacheRepo struct {
 	cache *cache.Cache
@@ -21,6 +24,8 @@ func NewCacheRepo(c *cache.Cache) CacheRepo {
 	}
 }
 
+// CachePaywall saves paywall data in cache.
+// It never expires.
 func (repo CacheRepo) CachePaywall(p reader.Paywall, live bool) {
 	repo.cache.Set(
 		paywallCacheKey(live),
@@ -37,6 +42,24 @@ func (repo CacheRepo) LoadPaywall(live bool) (reader.Paywall, error) {
 	}
 
 	return reader.Paywall{}, errors.New("not found")
+}
+
+func (repo CacheRepo) AndroidLatest(release android.Release) {
+	repo.cache.Set(
+		cacheKeyAndroidLatest,
+		release,
+		cache.DefaultExpiration)
+}
+
+func (repo CacheRepo) LoadAndroidLatest() (android.Release, error) {
+	x, found := repo.cache.Get(cacheKeyAndroidLatest)
+	if found {
+		if release, ok := x.(android.Release); ok {
+			return release, nil
+		}
+	}
+
+	return android.Release{}, errors.New("not found")
 }
 
 func (repo CacheRepo) Clear() {
