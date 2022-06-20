8d6f5412c36f5696c57f84929cdc47c8d9a6ba1d
weiguo.ni <neefrankie@gmail.com>
2022-06-20 18:04:58 +0800

    feat: android release creation and update

--- internal/repository/apprepo/releases.go
+++ internal/repository/apprepo/releases.go
@@ -3,12 +3,40 @@ package apprepo
 import (
 	gorest "github.com/FTChinese/go-rest"
 	"github.com/FTChinese/subscription-api/internal/pkg/android"
+	"github.com/FTChinese/subscription-api/pkg"
+	"log"
 )
 
-func (env Env) Latest() (android.Release, error) {
+// CreateRelease insert a new row of android release.
+func (env Env) CreateRelease(r android.Release) (android.Release, error) {
+	_, err := env.dbs.Write.NamedExec(
+		android.StmtInsertRelease,
+		r)
+
+	if err != nil {
+		return android.Release{}, err
+	}
+
+	return r, nil
+}
+
+// UpdateRelease updates a release.
+func (env Env) UpdateRelease(release android.Release) error {
+	_, err := env.dbs.Write.NamedExec(
+		android.StmtUpdateRelease,
+		release)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (env Env) RetrieveLatest() (android.Release, error) {
 	var r android.Release
 
-	err := env.dbs.Read.Get(&r, android.StmtLatest)
+	err := env.dbs.Read.Get(&r, android.StmtLatestRelease)
 
 	if err != nil {
 		return r, err
@@ -17,32 +45,110 @@ func (env Env) Latest() (android.Release, error) {
 	return r, nil
 }
 
-func (env Env) Releases(p gorest.Pagination) ([]android.Release, error) {
+func (env Env) RetrieveRelease(versionName string) (android.Release, error) {
+	var r android.Release
 
+	err := env.dbs.Read.Get(
+		&r,
+		android.StmtSelectRelease,
+		versionName)
+
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (env Env) countRelease() (int64, error) {
+	var count int64
+	err := env.dbs.Read.Get(&count, android.StmtCountRelease)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (env Env) listReleases(p gorest.Pagination) ([]android.Release, error) {
 	releases := make([]android.Release, 0)
 
 	err := env.dbs.Read.Select(
 		&releases,
-		android.StmtList,
+		android.StmtListRelease,
 		p.Limit,
 		p.Offset())
 
 	if err != nil {
-
 		return nil, err
 	}
 
 	return releases, nil
 }
 
-func (env Env) SingleRelease(versionName string) (android.Release, error) {
-	var r android.Release
+// ListReleases lists all releases.
+func (env Env) ListReleases(p gorest.Pagination) (android.ReleaseList, error) {
+	countCh := make(chan int64)
+	listCh := make(chan android.ReleaseList)
 
-	err := env.dbs.Read.Get(&r, android.StmtRelease, versionName)
+	go func() {
+		defer close(countCh)
+		n, err := env.countRelease()
+		if err != nil {
+			log.Print(err)
+		}
 
-	if err != nil {
-		return r, err
+		countCh <- n
+	}()
+
+	go func() {
+		defer close(listCh)
+		list, err := env.listReleases(p)
+		listCh <- android.ReleaseList{
+			Data: list,
+			PagedList: pkg.PagedList{
+				Total:      0,
+				Pagination: gorest.Pagination{},
+				Err:        err,
+			},
+		}
+	}()
+
+	count, listResult := <-countCh, <-listCh
+
+	if listResult.Err != nil {
+		return android.ReleaseList{}, listResult.Err
 	}
 
-	return r, nil
+	return android.ReleaseList{
+		PagedList: pkg.PagedList{
+			Total:      count,
+			Pagination: p,
+			Err:        nil,
+		},
+		Data: listResult.Data,
+	}, nil
+}
+
+// Exists checks whether a release already exists.
+func (env Env) Exists(tag string) (bool, error) {
+	var ok bool
+	err := env.dbs.Read.Get(&ok, android.StmtReleaseExists, tag)
+
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+// DeleteRelease a release removes a release.
+func (env Env) DeleteRelease(versionName string) error {
+	_, err := env.dbs.Write.Exec(android.StmtDeleteRelease, versionName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
