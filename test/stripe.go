c4c0283538997ae230d5ab94dbf42174b00ae128
weiguo.ni <neefrankie@gmail.com>
2022-07-12 15:19:35 +0800

    refactor: save discount if exists

--- test/stripe.go
+++ test/stripe.go
@@ -6,14 +6,10 @@ import (
 	"time"
 
 	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/FTChinese/go-rest/rand"
 	"github.com/FTChinese/subscription-api/faker"
 	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
-	"github.com/FTChinese/subscription-api/lib/dt"
-	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/guregu/null"
-	sdk "github.com/stripe/stripe-go/v72"
 )
 
 func (p Persona) StripeCustomer() stripe.Customer {
@@ -38,7 +34,8 @@ func StripeInvoice() stripe.Invoice {
 		CollectionMethod:     stripe.InvoiceCollectionMethod{},
 		Currency:             "gbp",
 		CustomerID:           faker.StripeCustomerID(),
-		DefaultPaymentMethod: null.String{},
+		DefaultPaymentMethod: null.StringFrom("payment-method-id"),
+		Discounts:            []string{"discount-1", "discount-b"},
 		HostedInvoiceURL:     null.String{},
 		LiveMode:             false,
 		Paid:                 true,
@@ -88,93 +85,5 @@ func StripePaymentIntent() stripe.PaymentIntent {
 		ReceiptEmail:       "",
 		SetupFutureUsage:   stripe.SetupFutureUsage{},
 		Status:             "",
-	}
-}
-
-func StripePaymentMethod() stripe.PaymentMethod {
-	return stripe.PaymentMethod{
-		ID:         faker.StripePaymentMethodID(),
-		CustomerID: faker.StripeCustomerID(),
-		Kind:       sdk.PaymentMethodTypeCard,
-		Card: stripe.PaymentMethodCard{
-			Brand:             "visa",
-			Country:           "us",
-			ExpMonth:          9,
-			ExpYear:           23,
-			Fingerprint:       "",
-			Funding:           "",
-			Last4:             "1234",
-			Network:           sdk.PaymentMethodCardNetworks{},
-			ThreeDSecureUsage: sdk.PaymentMethodCardThreeDSecureUsage{},
-		},
-		Created:  time.Now().Unix(),
-		LiveMode: false,
-	}
-}
-
-func (p Persona) StripeSubsBuilder() StripeSubsBuilder {
-	return StripeSubsBuilder{
-		ftcID:  p.FtcID,
-		price:  price.MockStripeStdYearPrice,
-		status: enum.SubsStatusActive,
-	}
-}
-
-type StripeSubsBuilder struct {
-	ftcID  string
-	price  price.StripePrice
-	status enum.SubsStatus
-}
-
-func (b StripeSubsBuilder) WithPrice(p price.StripePrice) StripeSubsBuilder {
-	b.price = p
-	return b
-}
-
-func (b StripeSubsBuilder) WithStatus(s enum.SubsStatus) StripeSubsBuilder {
-	b.status = s
-	return b
-}
-
-func (b StripeSubsBuilder) WithCanceled() StripeSubsBuilder {
-	return b.WithStatus(enum.SubsStatusCanceled)
-}
-
-func (b StripeSubsBuilder) Build() stripe.Subs {
-	start := time.Now()
-	end := dt.NewSlotBuilder(start).WithPeriod(b.price.PeriodCount.YearMonthDay).End
-	canceled := time.Time{}
-
-	subsID := faker.StripeSubsID()
-
-	return stripe.Subs{
-		ID:                     subsID,
-		Edition:                b.price.Edition(),
-		WillCancelAtUtc:        chrono.Time{},
-		CancelAtPeriodEnd:      false,
-		CanceledUTC:            chrono.TimeFrom(canceled), // Set it for automatic cancel.
-		CurrentPeriodEnd:       chrono.TimeFrom(end),
-		CurrentPeriodStart:     chrono.TimeFrom(start),
-		CustomerID:             faker.StripeCustomerID(),
-		DefaultPaymentMethodID: null.String{},
-		EndedUTC:               chrono.Time{},
-		FtcUserID:              null.StringFrom(b.ftcID),
-		Items: []stripe.SubsItem{
-			{
-				ID:             faker.StripeSubsItemID(),
-				Price:          b.price,
-				Created:        time.Now().Unix(),
-				Quantity:       1,
-				SubscriptionID: subsID,
-			},
-		},
-		LatestInvoiceID: faker.StripeInvoiceID(),
-		LatestInvoice:   stripe.Invoice{},
-		LiveMode:        false,
-		PaymentIntentID: null.StringFrom(faker.StripePaymentIntentID()),
-		PaymentIntent:   stripe.PaymentIntent{},
-		StartDateUTC:    chrono.TimeNow(),
-		Status:          b.status,
-		Created:         time.Now().Unix(),
 	}
 }
