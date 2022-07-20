683e3c27b727b3c7887c2851bb693460e7c16c5c
neefrankie <neefrankie@gmail.com>
2022-07-20 21:04:51 +0800

    feat: mimic makefile with powershell script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,20 @@
-#git tag -l --sort=-v:refname
-git log --max-count=1 --pretty=format:%aI_%h
+param (
+    $stage = 'build'
+)
+
+$version = git tag -l --sort=-v:refname | Select-Object -First 1
+$build_at = Get-Date -Format "yyyy-MM-ddTHH:mm:ssK"
+$commit = git log --max-count=1 --pretty=format:%aI_%h
+
+$ldflags = "-w -s -X main.version=$version -X main.build=$build_at -X main.commit=$commit"
+
+$app_name = 'subs-api-v6.exe'
+$out_dir = './build'
+
+$exec = "$out_dir/$app_name"
+
+switch ($stage)
+{
+    'build' { go build -o $exec -ldflags $ldflags -tags production -v . }
+    'run' { "$exec -production=false -livemode=false" | Invoke-Expression }
+}
