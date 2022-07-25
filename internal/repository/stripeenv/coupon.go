b346f4b67f35fd3dc0f8f10e4fef8a87d8c02f40
weiguo.ni <neefrankie@gmail.com>
2022-07-25 13:45:42 +0800

    refactor: doc

--- internal/repository/stripeenv/coupon.go
+++ internal/repository/stripeenv/coupon.go
@@ -23,6 +23,8 @@ func (env Env) LoadOrFetchCoupon(id string, refresh bool) (price.StripeCoupon, e
 	return price.NewStripeCoupon(rawCoupon), nil
 }
 
+// ModifyCoupon modifies stripe coupon's metadata and then insert/update
+// to FTC's database.
 func (env Env) ModifyCoupon(id string, params price.StripeCouponMeta) (price.StripeCoupon, error) {
 	defer env.Logger.Sync()
 	sugar := env.Logger.Sugar()
