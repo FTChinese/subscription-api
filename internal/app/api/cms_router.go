9990cce2192ac3656f6252414a6ccf0e04c776dc
weiguo.ni <neefrankie@gmail.com>
2022-08-01 15:54:17 +0800

    refactor: removed PaymentShared; add paywallRepo, logger, and live fields

--- internal/app/api/cms_router.go
+++ internal/app/api/cms_router.go
@@ -2,25 +2,29 @@ package api
 
 import (
 	"github.com/FTChinese/subscription-api/internal/pkg/letter"
+	"github.com/FTChinese/subscription-api/internal/repository"
 	"github.com/FTChinese/subscription-api/internal/repository/cmsrepo"
 	"github.com/FTChinese/subscription-api/internal/repository/shared"
 	"github.com/FTChinese/subscription-api/pkg/db"
-	"github.com/patrickmn/go-cache"
 	"go.uber.org/zap"
 )
 
 type CMSRouter struct {
 	repo         cmsrepo.Env
 	readerRepo   shared.ReaderCommon
+	paywallRepo  repository.PaywallRepo
 	emailService letter.Service
-	PaymentShared
+	logger       *zap.Logger
+	live         bool
 }
 
-func NewCMSRouter(dbs db.ReadWriteMyDBs, c *cache.Cache, logger *zap.Logger, live bool) CMSRouter {
+func NewCMSRouter(dbs db.ReadWriteMyDBs, live bool, logger *zap.Logger) CMSRouter {
 	return CMSRouter{
-		repo:          cmsrepo.New(dbs, logger),
-		readerRepo:    shared.NewReaderCommon(dbs),
-		emailService:  letter.NewService(logger),
-		PaymentShared: NewPaymentShared(dbs, c, logger, live),
+		repo:         cmsrepo.New(dbs, logger),
+		readerRepo:   shared.NewReaderCommon(dbs),
+		paywallRepo:  repository.NewPaywallRepo(dbs),
+		emailService: letter.NewService(logger),
+		live:         live,
+		logger:       logger,
 	}
 }
