46ae8904f94c61260bbb9a5d94fc9d19cb5d28f3
weiguo.ni <neefrankie@gmail.com>
2022-08-15 14:02:48 +0800

    refactor: webhook url builder

--- pkg/config/webhook_url.go
+++ pkg/config/webhook_url.go
@@ -2,34 +2,38 @@ package config
 
 import "github.com/FTChinese/go-rest/enum"
 
-const hostFTA = "https://www.ftacademy.cn"
+const readerAppBase = "https://next.ftacademy.cn"
 
 const (
 	// EmailVerificationURL is the base url to construct url in email verification letter.
 	// Previously we used https://users.chineseft.com/verify/email created by the next-user app, which is no longer maintained.
-	EmailVerificationURL = hostFTA + "/reader/verification"
+	EmailVerificationURL = readerAppBase + "/reader/verification"
 	// PasswordResetURL is the base url to construct url to reset password.
 	// Previously we used https://users.ftchinese.com/password-reset created by the next-user app.
-	PasswordResetURL = hostFTA + "/reader/password-reset"
+	PasswordResetURL = readerAppBase + "/reader/password-reset"
 )
 
 // AliWxWebhookURL builds the url for one-time purchase.
 // If isProd is true, use online production server;
 // otherwise always use sandbox server.
+// The webhook url for one-off purchase:
+// https://www.ftacademy.cn/api/<v6|sandbox>/webhook/<wxpay|alipay>
 func AliWxWebhookURL(isProd bool, method enum.PayMethod) string {
-	var baseURL string
+	var baseURL = "https://www.ftacademy.cn"
+	var v string
+	var m string
 	if isProd {
-		baseURL = hostFTA + "/api/" + Version
+		v = Version
 	} else {
-		baseURL = hostFTA + "/api/sandbox"
+		v = "sandbox"
 	}
 
 	switch method {
 	case enum.PayMethodWx:
-		return baseURL + "/webhook/wxpay"
+		m = "wxpay"
 	case enum.PayMethodAli:
-		return baseURL + "/webhook/alipay"
+		m = "alipay"
 	}
 
-	return ""
+	return baseURL + "/api/" + v + "/webhook/" + m
 }
