ee00463f485c2ffba8b80825ff6270f3f9c32a07
weiguo.ni <neefrankie@gmail.com>
2023-05-18 14:45:38 +0800

    string representation of cycle

--- pkg/price/kind.go
+++ pkg/price/kind.go
@@ -1,5 +1,7 @@
 package price
 
+import "github.com/FTChinese/go-rest/enum"
+
 // Kind represents the type of price.
 type Kind string
 
@@ -7,3 +9,13 @@ const (
 	KindRecurring Kind = "recurring"
 	KindOneTime   Kind = "one_time"
 )
+
+// cycleStrOfKind produces a string of a price's recurring cycle.
+// If price if of one_time, then the cycle is string null.
+func cycleStrOfKind(k Kind, c enum.Cycle) string {
+	if k == KindOneTime {
+		return "null"
+	}
+
+	return c.String()
+}
