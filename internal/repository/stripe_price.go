107ec9ae64afa5b733958e25da05bb04a58c30ad
weiguo.ni <neefrankie@gmail.com>
2023-05-19 12:13:00 +0800

    add ActivatePrice and DeactivatePrice

--- internal/repository/stripe_price.go
+++ internal/repository/stripe_price.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
 	"github.com/FTChinese/subscription-api/pkg/price"
 )
 
@@ -31,4 +32,44 @@ func (repo StripeRepo) RetrievePrice(id string, live bool) (price.StripePrice, e
 	}
 
 	return p, nil
+}
+
+// ActivatePrice adds an entry into product_active_price table.
+func (repo StripeRepo) ActivatePrice(p price.StripePrice) error {
+	tx, err := txrepo.BeginPriceTx(repo.dbs.Write)
+	if err != nil {
+		return err
+	}
+
+	err = tx.UpsertActivePrice(p.ActiveEntry())
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repo StripeRepo) DeactivePrice(p price.StripePrice) error {
+	tx, err := txrepo.BeginPriceTx(repo.dbs.Delete)
+	if err != nil {
+		return err
+	}
+
+	err = tx.RemoveActivePrice(p.ActiveEntry())
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+
 }
