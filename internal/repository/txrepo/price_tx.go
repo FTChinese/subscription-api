16728e5133e2c00a8dd689ece0f546c262fe1fe6
weiguo.ni <neefrankie@gmail.com>
2023-05-19 12:11:40 +0800

    add BeginPriceTx

--- internal/repository/txrepo/price_tx.go
+++ internal/repository/txrepo/price_tx.go
@@ -9,10 +9,15 @@ type PriceTx struct {
 	*sqlx.Tx
 }
 
-func NewPriceTx(tx *sqlx.Tx) PriceTx {
+func BeginPriceTx(dbx *sqlx.DB) (PriceTx, error) {
+	tx, err := dbx.Beginx()
+	if err != nil {
+		return PriceTx{}, err
+	}
+
 	return PriceTx{
 		Tx: tx,
-	}
+	}, nil
 }
 
 // DeactivateFtcSiblingPrice turns to false the is_active flag of
@@ -45,6 +50,19 @@ func (tx PriceTx) ActivateFtcPrice(p price.FtcPrice) error {
 	return nil
 }
 
+// DeactivateFtcPrice changes a price's is_active or archived fields
+func (tx PriceTx) DeactivateFtcPrice(p price.FtcPrice) error {
+	_, err := tx.NamedExec(
+		price.StmtFtcPriceState,
+		p)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // UpsertActivePrice inserts a new entry into subs_product.product_active_plans,
 // or update it in case of id conflict.
 func (tx PriceTx) UpsertActivePrice(p price.ActivePrice) error {
@@ -59,28 +77,16 @@ func (tx PriceTx) UpsertActivePrice(p price.ActivePrice) error {
 	return nil
 }
 
-func (tx PriceTx) DeactivateFtcPrice(p price.FtcPrice) error {
-	_, err := tx.NamedExec(
-		price.StmtFtcPriceState,
-		p)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// RemoveFtcActivePrice from product_active_price if
+// RemoveActivePrice from product_active_price if
 // the table has this entry.
 // DO NOT use the id field to perform this operation
 // since you might be accidentally remove a valid
 // price in case the the price to be removed
 // is attached to an inactive product.
-func (tx PriceTx) RemoveFtcActivePrice(p price.FtcPrice) error {
-	_, err := tx.Exec(
-		price.StmtRemoveFtcActivePrice,
-		p.ID)
+func (tx PriceTx) RemoveActivePrice(p price.ActivePrice) error {
+	_, err := tx.NamedExec(
+		price.StmtRemoveActivePrice,
+		p)
 	if err != nil {
 		return err
 	}
