617d30d1c03958579633281280789860f996cfff
weiguo.ni <neefrankie@gmail.com>
2023-05-23 11:45:54 +0800

    removed WebhookSubsResult

--- internal/pkg/stripe/webhook_result.go
+++ internal/pkg/stripe/webhook_result.go
@@ -1,12 +1,7 @@
 package stripe
 
 import (
-	"errors"
-
 	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/subscription-api/pkg/addon"
-	"github.com/FTChinese/subscription-api/pkg/ids"
-	"github.com/FTChinese/subscription-api/pkg/invoice"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 )
 
@@ -33,112 +28,4 @@ type WebhookError struct {
 
 func (e WebhookError) Error() string {
 	return e.Message
-}
-
-type WebhookSubsResult struct {
-	Member           reader.Membership          // New membership.
-	Versioned        reader.MembershipVersioned // If AnteChange is zero, update Member; otherwise insert Member.
-	CarryOverInvoice invoice.Invoice
-}
-
-type WebhookResultBuilder struct {
-	Subs         Subs
-	UserIDs      ids.UserIDs
-	StripeMember reader.Membership
-	FtcMember    reader.Membership
-}
-
-// Build generates WebhookSubsResult.
-// Possibilities:
-// * No membership corresponds to stripe subscription id. In such case what you can do depends on the ftc side:
-//   - Ftc side has no membership. You are safe to create a stripe membership directly;
-//   - Ftc side has membership but expired. You can override it.
-//   - Ftc side is a one-time purchase, possibilities are:
-//   - Stripe subscription expired, you are not allowed to touch ftc membership
-//   - Stripe subscription is not expired, override ftc side with carry over addon.
-//
-// * Stripe side has membership:
-//   - If its user id does not match the account retrieve using customer id, it indicates data inconsistency, stop;
-//   - Otherwise the stripe membership already exists, simply update it.
-func (b WebhookResultBuilder) Build() (WebhookSubsResult, error) {
-	if b.StripeMember.IsZero() {
-		// If ftc side does not have membership.
-		if b.FtcMember.IsZero() {
-			newMmb := b.Subs.BuildMembership(
-				b.UserIDs,
-				addon.AddOn{})
-			return WebhookSubsResult{
-				Member: newMmb,
-				Versioned: reader.NewMembershipVersioned(newMmb).
-					ArchivedBy(reader.NewArchiver().ByStripe().ActionWebhook()),
-				CarryOverInvoice: invoice.Invoice{},
-			}, nil
-		}
-
-		// Ftc side has expired membership.
-		// It's safe to override it.
-		if b.FtcMember.IsExpired() {
-			newMmb := b.Subs.BuildMembership(
-				b.UserIDs,
-				addon.AddOn{})
-			return WebhookSubsResult{
-				Member: newMmb,
-				Versioned: reader.NewMembershipVersioned(newMmb).
-					WithPriorVersion(b.FtcMember).
-					ArchivedBy(reader.NewArchiver().ByStripe().ActionWebhook()),
-				CarryOverInvoice: invoice.Invoice{},
-			}, nil
-		}
-
-		// Ftc side is not expired.
-		// If stripe subs is valid, permit stripe to only
-		// override a one-time payment.
-		if b.FtcMember.IsOneTime() {
-			if b.Subs.IsExpired() {
-				return WebhookSubsResult{}, errors.New("expired stripe subscription cannot override a valid ftc membership")
-			}
-
-			// Turn current ftc side to carry over.
-			inv := b.FtcMember.CarryOverInvoice()
-			newMmb := b.Subs.BuildMembership(
-				b.UserIDs,
-				b.FtcMember.NextRoundAddOn(inv))
-
-			return WebhookSubsResult{
-				Member: newMmb,
-				Versioned: reader.NewMembershipVersioned(newMmb).
-					WithPriorVersion(b.FtcMember).
-					ArchivedBy(reader.NewArchiver().ByStripe().ActionWebhook()),
-				CarryOverInvoice: inv,
-			}, nil
-		}
-
-		// The ftc side is still valid and might come from
-		// another stripe subscription or apple.
-		// Overriding a non-onetime valid membership should be
-		// forbidden. We should record such mismatch.
-		return WebhookSubsResult{}, errors.New("cannot permit stripe to override an existing membership")
-	}
-
-	// Since stripe membership exists, it must match user id;
-	// otherwise it indicates we are retrieving different users
-	// by subscription id and customer id, which shouldn't
-	// happen.
-	if b.StripeMember.CompoundID != b.UserIDs.CompoundID {
-		return WebhookSubsResult{}, errors.New("stripe subscription is not targeting this user")
-	}
-
-	// Current membership must be created from the subs,
-	// simply update it.
-	newMmb := b.Subs.BuildMembership(
-		b.UserIDs,
-		b.StripeMember.AddOn)
-
-	return WebhookSubsResult{
-		Member: newMmb,
-		Versioned: reader.NewMembershipVersioned(newMmb).
-			WithPriorVersion(b.FtcMember).
-			ArchivedBy(reader.NewArchiver().ByStripe().ActionWebhook()),
-		CarryOverInvoice: invoice.Invoice{},
-	}, nil
 }
