aa8631272b13dd75f9efcd61aa0e142520654743
weiguo.ni <neefrankie@gmail.com>
2023-05-26 14:53:36 +0800

    add PagedData

--- pkg/paged_list.go
+++ pkg/paged_list.go
@@ -1,10 +1,10 @@
 package pkg
 
-import "github.com/FTChinese/go-rest"
+import gorest "github.com/FTChinese/go-rest"
 
 // PagedList is used as the bases to show a list of items with pagination support.
-type PagedList struct {
+type PagedData[T interface{}] struct {
 	Total int64 `json:"total" db:"row_count"`
 	gorest.Pagination
-	Err error `json:"-"` // Deprecated
+	Data []T `json:"data"`
 }
