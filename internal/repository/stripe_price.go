9e23c0714f47bcdf93cb5ae3a615cc2f528e1bb7
weiguo.ni <neefrankie@gmail.com>
2023-05-26 14:55:56 +0800

    add ListPrices

--- internal/repository/stripe_price.go
+++ internal/repository/stripe_price.go
@@ -1,9 +1,79 @@
 package repository
 
 import (
+	"log"
+
+	gorest "github.com/FTChinese/go-rest"
 	"github.com/FTChinese/subscription-api/internal/repository/txrepo"
+	"github.com/FTChinese/subscription-api/pkg"
 	"github.com/FTChinese/subscription-api/pkg/price"
 )
+
+// countPrices gets the total entry of stripe prices
+// in db.
+func (repo StripeRepo) countPrices() (int64, error) {
+	var count int64
+	err := repo.dbs.Read.Get(&count, price.StmtCountStripePrice)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (repo StripeRepo) listPrices(p gorest.Pagination) ([]price.StripePrice, error) {
+	list := make([]price.StripePrice, 0)
+
+	err := repo.dbs.Read.Select(
+		&list,
+		price.StmtStripePrices,
+		p.Limit,
+		p.Offset(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return list, nil
+}
+
+func (repo StripeRepo) ListPrices(p gorest.Pagination) (pkg.PagedData[price.StripePrice], error) {
+
+	countCh := make(chan int64)
+	listCh := make(chan pkg.AsyncResult[[]price.StripePrice])
+
+	go func() {
+		defer close(countCh)
+		n, err := repo.countPrices()
+		if err != nil {
+			log.Print(err)
+		}
+
+		countCh <- n
+	}()
+
+	go func() {
+		defer close(listCh)
+		list, err := repo.listPrices(p)
+		listCh <- pkg.AsyncResult[[]price.StripePrice]{
+			Err:   err,
+			Value: list,
+		}
+	}()
+
+	count, listResult := <-countCh, <-listCh
+
+	if listResult.Err != nil {
+		return pkg.PagedData[price.StripePrice]{}, listResult.Err
+	}
+
+	return pkg.PagedData[price.StripePrice]{
+		Total:      count,
+		Pagination: p,
+		Data:       listResult.Value,
+	}, nil
+}
 
 func (repo StripeRepo) UpsertPrice(p price.StripePrice) error {
 
