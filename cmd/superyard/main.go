b26f67c3ecc5df8aa78433d5d8014a82cc035600
neefrankie <neefrankie@gmail.com>
2023-05-26 22:39:54 +0800

    try to merge superyard

---
+++ cmd/superyard/main.go
@@ -0,0 +1,33 @@
+package main
+
+//go:embed build/api.toml
+var tomlConfig string
+
+//go:embed client_version_next
+var clientVersionNext string
+
+var clientVersionNg string
+
+var (
+	isProduction bool
+	version      string
+	build        string
+)
+
+func init() {
+	flag.BoolVar(&isProduction, "production", false, "Indicate productions environment if present")
+	var v = flag.Bool("v", false, "print current version")
+
+	flag.Parse()
+
+	if *v {
+		fmt.Printf("%s\nBuild at %s\n", version, build)
+		os.Exit(0)
+	}
+
+	config.MustSetupViper([]byte(tomlConfig))
+}
+
+func main() {
+	
+}
