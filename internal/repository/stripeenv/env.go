d183b404c1bcfccbccd58f33623e59a2b074c77b
weiguo.ni <neefrankie@gmail.com>
2023-05-29 16:14:33 +0800

    removed Cache field

--- internal/repository/stripeenv/env.go
+++ internal/repository/stripeenv/env.go
@@ -1,21 +1,18 @@
 package stripeenv
 
 import (
-	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
 	"github.com/FTChinese/subscription-api/internal/repository"
 	"github.com/FTChinese/subscription-api/internal/stripeclient"
 )
 
 type Env struct {
 	Client stripeclient.Client
-	Cache  *stripe.PriceCache // Deprecated
 	repository.StripeRepo
 }
 
 func New(client stripeclient.Client, repo repository.StripeRepo) Env {
 	return Env{
 		Client:     client,
-		Cache:      stripe.NewPriceCache(),
 		StripeRepo: repo,
 	}
 }
