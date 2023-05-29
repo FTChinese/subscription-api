24301ccbb237da69a2ad7b484552c7211ccc1e69
weiguo.ni <neefrankie@gmail.com>
2023-05-29 16:09:52 +0800

    refactor test

--- internal/stripeclient/price_test.go
+++ internal/stripeclient/price_test.go
@@ -1,31 +1,11 @@
 package stripeclient
 
 import (
-	"github.com/FTChinese/subscription-api/faker"
-	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
-	"go.uber.org/zap/zaptest"
 	"testing"
+
+	"github.com/FTChinese/subscription-api/faker"
+	"go.uber.org/zap/zaptest"
 )
-
-func TestClient_ListPrices(t *testing.T) {
-	faker.MustSetupViper()
-
-	client := New(false, zaptest.NewLogger(t))
-	stripePrices, err := client.ListPrices()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	cache := stripe.NewPriceCache()
-	cache.AddAll(stripePrices)
-
-	prices := cache.List(false)
-
-	for _, v := range prices {
-		t.Logf("%s", faker.MustMarshalIndent(v))
-	}
-}
 
 func TestClient_RetrievePrice(t *testing.T) {
 	faker.MustSetupViper()
