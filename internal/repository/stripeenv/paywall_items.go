ace0b0edf4d765070a4bf3f91985c150a8f0bc55
weiguo.ni <neefrankie@gmail.com>
2023-05-29 16:13:58 +0800

    refactor: LoadOrFetchPaywallPrices

--- internal/repository/stripeenv/paywall_items.go
+++ internal/repository/stripeenv/paywall_items.go
@@ -2,31 +2,29 @@ package stripeenv
 
 import (
 	"github.com/FTChinese/subscription-api/internal/pkg/stripe"
+	"github.com/FTChinese/subscription-api/pkg"
 	"github.com/FTChinese/subscription-api/pkg/price"
 	"github.com/FTChinese/subscription-api/pkg/reader"
 )
 
-// loadOrFetchPaywallPrices loads all stripe prices for a list of ids.
-func (env Env) loadOrFetchPaywallPrices(ids []string, refresh bool, live bool) ([]price.StripePrice, error) {
+// LoadOrFetchPaywallPrices loads all stripe prices for a list of ids.
+func (env Env) LoadOrFetchPaywallPrices(refresh bool, live bool) ([]price.StripePrice, error) {
 	defer env.Logger.Sync()
 	sugar := env.Logger.Sugar()
 
-	var diff []string
+	sugar.Infof("Load stripe prices from db")
+	list, err := env.ListPaywallPrices(live)
+	if err != nil {
+		return nil, err
+	}
+
 	if !refresh {
-		sugar.Infof("Load stripe prices from db")
-		list, err := env.RetrievePaywallPrices(ids, live)
-		if err != nil {
-			sugar.Error(err)
-			diff = ids
-		} else {
-			diff = isAllPriceRetrieved(ids, list)
-		}
+		return list, nil
+	}
 
-		if len(diff) == 0 {
-			return list, nil
-		}
-
-		sugar.Infof("Some of stripes not found in db: %s", ids)
+	ids := make([]string, 0)
+	for _, v := range list {
+		ids = append(ids, v.ID)
 	}
 
 	sugar.Infof("Fetch from Stripe API...")
@@ -38,35 +36,43 @@ func (env Env) loadOrFetchPaywallPrices(ids []string, refresh bool, live bool) (
 	return priceList, nil
 }
 
-func isAllPriceRetrieved(ids []string, prices []price.StripePrice) []string {
-	var result = make([]string, 0)
-	var received = make(map[string]interface{})
-	for _, v := range prices {
-		received[v.ID] = nil
-	}
+func (env Env) LoadOrFetchPaywallItems(refresh bool, live bool) ([]reader.StripePaywallItem, error) {
+	pCh := make(chan pkg.AsyncResult[[]price.StripePrice])
+	cCh := make(chan pkg.AsyncResult[[]price.StripeCoupon])
 
-	for _, id := range ids {
-		_, ok := received[id]
-		if !ok {
-			result = append(result, id)
+	go func() {
+		defer close(pCh)
+
+		prices, err := env.LoadOrFetchPaywallPrices(refresh, live)
+
+		pCh <- pkg.AsyncResult[[]price.StripePrice]{
+			Value: prices,
+			Err:   err,
 		}
+	}()
+
+	go func() {
+		defer close(cCh)
+
+		coupons, err := env.ListPaywallCoupons(live)
+
+		cCh <- pkg.AsyncResult[[]price.StripeCoupon]{
+			Value: coupons,
+			Err:   err,
+		}
+	}()
+
+	pRes, cRes := <-pCh, <-cCh
+
+	if pRes.Err != nil {
+		return nil, pRes.Err
 	}
 
-	return result
-}
-
-func (env Env) LoadOrFetchPaywallItems(priceIDs []string, refresh bool, live bool) ([]reader.StripePaywallItem, error) {
-	prices, err := env.loadOrFetchPaywallPrices(priceIDs, refresh, live)
-	if err != nil {
-		return nil, err
+	if cRes.Err != nil {
+		return nil, cRes.Err
 	}
 
-	coupons, err := env.RetrievePaywallCoupons(priceIDs, live)
-	if err != nil {
-		return nil, err
-	}
-
-	return reader.NewPaywallStripe(prices, coupons), nil
+	return reader.NewPaywallStripe(pRes.Value, cRes.Value), nil
 }
 
 // LoadCheckoutItem from database, or from Stripe API if not found in database.
