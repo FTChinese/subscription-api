c73334eaec6b32d99cf088df225f7adc88bfe2fd
weiguo.ni <neefrankie@gmail.com>
2023-05-29 16:06:11 +0800

    removed cycleOfKind

--- pkg/price/kind.go
+++ pkg/price/kind.go
@@ -1,7 +1,5 @@
 package price
 
-import "github.com/FTChinese/go-rest/enum"
-
 // Kind represents the type of price.
 type Kind string
 
@@ -9,13 +7,3 @@ const (
 	KindRecurring Kind = "recurring"
 	KindOneTime   Kind = "one_time"
 )
-
-// cycleStrOfKind produces a string of a price's recurring cycle.
-// If price if of one_time, then the cycle is string null.
-func cycleStrOfKind(k Kind, c enum.Cycle) string {
-	if k == KindOneTime {
-		return "null"
-	}
-
-	return c.String()
-}
