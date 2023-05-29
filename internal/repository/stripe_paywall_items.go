0366b3d9a1a9bbb845a45473d8550a2a2208ebcb
weiguo.ni <neefrankie@gmail.com>
2023-05-29 16:12:42 +0800

    refactor how stripe data is retrieved

--- internal/repository/stripe_paywall_items.go
+++ internal/repository/stripe_paywall_items.go
@@ -1,32 +1,32 @@
 package repository
 
 import (
-	"github.com/FTChinese/subscription-api/lib/sq"
 	"github.com/FTChinese/subscription-api/pkg/price"
 )
 
-func (repo StripeRepo) RetrievePaywallPrices(ids []string, live bool) ([]price.StripePrice, error) {
-	var list = make([]price.StripePrice, 0)
+// ListPaywallPrices retrieves all stripe prices present on paywall.
+func (repo StripeRepo) ListPaywallPrices(live bool) ([]price.StripePrice, error) {
+	var prices = make([]price.StripePrice, 0)
+
 	err := repo.dbs.Read.Select(
-		&list,
-		price.StmtListStripePrice,
-		sq.FindInSetValue(ids),
-		live)
+		&prices,
+		price.StmtStripeActivePrices,
+		live,
+	)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return list, nil
+	return prices, nil
 }
 
-// RetrievePaywallCoupons retrieves active coupons of the specified prices used on paywall.
-func (repo StripeRepo) RetrievePaywallCoupons(priceIDs []string, live bool) ([]price.StripeCoupon, error) {
+// ListPaywallCoupons retrieves active coupons of the all stripe prices present on paywall.
+func (repo StripeRepo) ListPaywallCoupons(live bool) ([]price.StripeCoupon, error) {
 	var list = make([]price.StripeCoupon, 0)
 	err := repo.dbs.Read.Select(
 		&list,
-		price.StmtActiveCouponsOfPrices,
-		sq.FindInSetValue(priceIDs),
+		price.StmtPaywallStripeCoupons,
 		live)
 
 	if err != nil {
