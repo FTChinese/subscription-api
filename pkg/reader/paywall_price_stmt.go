25e70b8f17ec62f39e8748c32d7150d7e250a2ee
neefrankie <neefrankie@gmail.com>
2023-06-03 13:16:30 +0800

    add StmtListActivePrices

--- pkg/reader/paywall_price_stmt.go
+++ pkg/reader/paywall_price_stmt.go
@@ -10,8 +10,8 @@ LIMIT 1
 
 const colPriceWithOffers = `
 SELECT p.id AS price_id,
-	p.tier,
-	p.cycle,
+	p.tier AS tier,
+	p.cycle AS cycle,
 	p.is_active AS is_active,
 	p.archived AS archived,
 	p.currency AS currency,
@@ -24,16 +24,16 @@ SELECT p.id AS price_id,
 	p.title AS title,
 	p.unit_amount AS unit_amount,
 	p.discount_list AS discount_list,
-	p.start_utc,
-	p.end_utc,
-	p.created_utc
+	p.start_utc AS start_utc,
+	p.end_utc AS end_utc,
+	p.created_utc AS created_utc
 FROM subs_product.price AS p
 `
 
 // StmtSelectPaywallPrice retrieves a row from plan table, regardless of archived or not.
 const StmtSelectPaywallPrice = colPriceWithOffers + `
 WHERE p.id = ?
-	AND live_mode = ?
+	AND p.live_mode = ?
 LIMIT 1
 `
 
@@ -52,6 +52,7 @@ ORDER BY p.is_active DESC, p.cycle DESC, p.created_utc DESC
 // table and filter out those nullable product id.
 // NOTE we don't need an extra table to record which prices
 // are put on paywall.
+// Deprecated. Use StmtListActivePrices.
 const StmtListPaywallPrice = colPriceWithOffers + `
 LEFT JOIN subs_product.paywall_product_v4 AS active_prod
 	ON p.product_id = active_prod.product_id
@@ -60,5 +61,16 @@ WHERE active_prod.product_id IS NOT NULL
 	AND p.archived = FALSE
 	AND p.kind = 'recurring'
 	AND p.live_mode = ?
+ORDER BY p.cycle DESC
+`
+
+// StmtListActivePrices finds all prices set to active
+// on paywall.
+const StmtListActivePrices = colPriceWithOffers + `
+RIGHT JOIN subs_product.product_active_price AS a
+	ON p.id = a.price_id
+WHERE p.live_mode = ?
+	AND p.archived = FALSE
+	AND a.source = 'ftc'
 ORDER BY p.cycle DESC
 `
