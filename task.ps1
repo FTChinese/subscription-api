9f160759df44c5c5754577c7485098a791dfad1a
neefrankie <neefrankie@gmail.com>
2023-06-03 08:11:22 +0800

    add version parameter

--- task.ps1
+++ task.ps1
@@ -17,4 +17,13 @@ switch ($stage)
 {
     'build' { go build -o $exec -ldflags $ldflags -tags production -v . }
     'run' { "$exec -production=false -livemode=false" | Invoke-Expression }
+    'version' {
+        New-Item -Path "$out_dir/version" -ItemType "file" -Value $version -Force
+
+        New-Item -Path "$out_dir/build_time" -ItemType "file" -Value $build_at -Force
+
+        New-Item -Path "$out_dir/commit" -ItemType "file" -Value $commit -Force
+
+        Copy-Item -Path '~/config/env.dev.toml' -Destination "$out_dir/api.toml"
+    }
 }
